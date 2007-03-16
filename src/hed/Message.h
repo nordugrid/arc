b3ffefbe0add3406b42cb75456c82a8afe809eb4
Aleksandr Konstantinov <akonstantinov@yahoo.com>
2007-03-16 20:42:38 +0000

    Documenting Message class. Changing it to contain pointer - easier to update.

--- src/hed/Message.h
+++ src/hed/Message.h
@@ -5,34 +5,39 @@
 
 namespace Arc {
 
-// Undefined (yet) classes
+/** Class MessageAuth will contain authencity information, 
+  authorization tokens and decisions. */
 class MessageAuth;
+
+/** Class MessageAttr will contain all attributes which 
+  MCC developers find useful to expose. */
 class MessageAttr;
 
-// Base class for content of message paased through chain. It's not 
-// intended to be used directly. Instead functional classes must be 
-// derived from it.
+/** Base class for content of message passed through chain. It's not 
+  intended to be used directly. Instead functional classes must be 
+  derived from it. */
 class MessagePayload {
  public:
   MessagePayload(void) { };
   virtual ~MessagePayload(void) { };
 };
 
-// Message. It is going to contain main content (payload), 
-// authentication/authorization information, attributes, etc.
+/** Message is passed through chain of MCCs. 
+  It is going to contain main content (payload), authentication/authorization 
+  information, attributes, etc. */
 class Message {
  private:
-  MessagePayload& payload_;
-  MessageAuth& auth_;
-  MessageAttr& attr_;
+  MessagePayload* payload_; /** Main content of message */
+  MessageAuth& auth_; /** Auth. related information */
+  MessageAttr& attr_; /** Various useful attributes */
  public:
-  Message(MessagePayload& payload):payload_(payload),auth_(*(MessageAuth*)NULL),attr_(*(MessageAttr*)NULL) { };
+  Message(void):payload_(NULL),auth_(NULL),attr_(NULL) { };
   ~Message(void);
-  // Get current payload
-  MessagePayload& Payload(void) { return payload_; };
-  // Update payload
-  MessagePayload& Payload(MessagePayload& new_payload) {
-    MessagePayload& p = payload_;
+  /** Returns pointer to current payload or NULL if no payload assigned. */
+  MessagePayload* Payload(void) { return payload_; };
+  /** Replace payload with new one */
+  MessagePayload* Payload(MessagePayload* new_payload) {
+    MessagePayload* p = payload_;
     payload_=new_payload;
     return p;
   };
