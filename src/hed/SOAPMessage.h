96ad6342ee5085b105b615b0175109aefffc8038
Aleksandr Konstantinov <akonstantinov@yahoo.com>
2007-03-16 19:40:02 +0000

    Documenting SOAPMessage class.

--- src/hed/SOAPMessage.h
+++ src/hed/SOAPMessage.h
@@ -7,65 +7,84 @@
 
 namespace Arc {
 
-// Wrapper for XML document representing SOAP message.
+/** SOAPMessage extends XMLNode class to support structures of SOAP message.
+  All XMLNode methods are exposed with top node translated to Envelope part
+  of SOAP. */
 class SOAPMessage: public XMLNode {
  public:
-  // SOAP Fault - provides access to Fault elements 
-  // Fault elements of previous versioa are mapped to those of version 1.2
+  /** SOAPFault provides an interface to conveinet access to elements of SOAP faults.
+    It also tries to expose single interface for both version 1.0 and 1.2 faults.
+    This class is not intended to 'own' any information stored. It's purpose 
+    is to manipulate information which under control of XMLNode or SOAPMessage
+    classes. If instance does not refer to valid SOAP Fault structure all 
+    manipulation methods will have no effect. */
   class SOAPFault {
    friend class SOAPMessage;
    private:
-    bool ver12;        // If SOAP version is 1.2
-    XMLNode fault;     // Fault element of SOAP
-    XMLNode code;      // Code element of SOAP Fault
-    XMLNode reason;    // Reason element of SOAP Fault
-    XMLNode node;      // Node element of SOAP Fault
-    XMLNode role;      // Role element of SOAP Fault
-    XMLNode detail;    // Detail element of SOAP Fault
+    bool ver12;        /** true if SOAP version is 1.2 */
+    XMLNode fault;     /** Fault element of SOAP */
+    XMLNode code;      /** Code element of SOAP Fault */
+    XMLNode reason;    /** Reason element of SOAP Fault */
+    XMLNode node;      /** Node element of SOAP Fault */
+    XMLNode role;      /** Role element of SOAP Fault */
+    XMLNode detail;    /** Detail element of SOAP Fault */
    public:
-     // Fault codes of SOAP specs
+    /** Fault codes of SOAP specs */
     typedef enum {
       undefined,
       unknown,
       VersionMismatch,
       MustUnderstand,
-      Sender,   /* Client */
-      Receiver, /* Server */
+      Sender,   /* Client in SOAP 1.0 */
+      Receiver, /* Server in SOAP 1.0 */
       DataEncodingUnknown
     } SOAPFaultCode;
-    // Parse Fault elements of SOAP Body
+    /** Parse Fault elements of SOAP Body or any other XML tree with Fault element */
     SOAPFault(XMLNode& body);
+    /** Returns true if instance refers to SOAP Fault */
     operator bool(void) { return (bool)fault; };
-    // Get/set Fault Code
+    /** Returns Fault Code element */
     SOAPFaultCode Code(void);
+    /** Set Fault Code element */
     void Code(SOAPFaultCode code);
-    // Get/set Fault Subcode at various levels (0 is for Code)
+    /** Returns Fault Subcode element at various levels (0 is for Code) */
     std::string Subcode(int level);
+    /** Set Fault Subcode element at various levels (0 is for Code) to 'subcode' */
     void Subcode(int level,const char* subcode);
-    // Get/set Fault reason at various levels
+    /** Returns content of Fault Reason element at various levels */
     std::string Reason(int num = 0);
+    /** Set Fault Reason content at various levels to 'reason' */
     void Reason(int num,const char* reason);
+    /** Set Fault Reason element at top level */
     void Reason(const char* reason) { Reason(0,reason); };
-    // Get/set Fault Node
+    /** Returns content of Fault Node element */
     std::string Node(void);
-    void Node(const char* reason);
-    // Get/set Fault Role
+    /** Set content of Fault Node element to 'node' */
+    void Node(const char* node);
+    /** Returns content of Fault Role element */
     std::string Role(void);
-    void Role(const char* reason);
-    // Get/set Fault Detail
+    /** Set content of Fault Role element to 'role' */
+    void Role(const char* role);
+    /** Access Fault Detail element. If create is set to true this element is creted if not present. */
     XMLNode Detail(bool create = false);
   };
-  // Parse SOAP message in XML document. Sets default namespaces 
-  // to prefixes as specified below.
+  /** Create new SOAP message from textual representation of XML document.
+    Created XML structure is owned by this instance.
+    This constructor also sets default namespaces to default prefixes 
+    as specified below. */
   SOAPMessage(const std::string& xml);
-  // Create SOAP message with specified namespaces
+  /** Create new SOAP message with specified namespaces.
+    Created XML structure is owned by this instance.
+    If argument fault is set to true created message is fault.  */
   SOAPMessage(const NS& ns,bool fault = false);
   ~SOAPMessage(void);
-  // Modify namespaces. Default namespaces are
-  // soap-enc http://schemas.xmlsoap.org/soap/encoding/
-  // soap-env http://schemas.xmlsoap.org/soap/envelope/
-  // xsi      http://www.w3.org/2001/XMLSchema-instance
-  // xsd      http://www.w3.org/2001/XMLSchema
+  /** Modify assigned namespaces. 
+    Default namespaces and prefixes are
+     soap-enc http://schemas.xmlsoap.org/soap/encoding/
+     soap-env http://schemas.xmlsoap.org/soap/envelope/
+     xsi      http://www.w3.org/2001/XMLSchema-instance
+     xsd      http://www.w3.org/2001/XMLSchema
+  */
   void Namespaces(const NS& namespaces);
   // Setialize SOAP message into XML document
   void GetXML(std::string& xml) const;
@@ -76,12 +95,14 @@ class SOAPMessage: public XMLNode {
   // Get Fault part of message. Returns NULL if message is not Fault.
   SOAPFault* Fault(void) { return fault; };
  private:
-  XMLNode doc;      // Top XML document
-  XMLNode envelope; // Envelope element of SOAP
-  XMLNode header;   // Header element of SOAP
-  XMLNode body;     // Body element of SOAP
-  SOAPFault* fault; // Fault element of SOAP
-  bool ver12;       // If SOAP version 1.2 is used
+  XMLNode doc;      /** Top XML node - XML document */
+  XMLNode envelope; /** Envelope element of SOAP */
+  XMLNode header;   /** Header element of SOAP */
+  XMLNode body;     /** Body element of SOAP */
+  SOAPFault* fault; //**Fault element of SOAP, NULL is message is not fault. */
+  bool ver12;       /** Is true if SOAP version 1.2 is used */
+  /** Fill instance variables parent XMLNode class. 
+    This method is called from constructors. */
   void set(void);
 };
 
