fc67b69e132a29ff071705a23bae90b9435ee07d
Aleksandr Konstantinov <akonstantinov@yahoo.com>
2007-04-04 08:19:19 +0000

    More inline documentation.

--- src/hed/libs/loader/MCC.h
+++ src/hed/libs/loader/MCC.h
@@ -9,15 +9,15 @@ namespace Arc {
 /** Message Chain Component - base class for every MCC plugin.
   This is virtual class which defines interface (in a future also 
  common functionality) for every MCC plugin. Interface is made of 
- maethod process() which is called by previous MCC in chain.
+ method process() which is called by previous MCC in chain.
  */
 class MCC
 {
     public:
-		/** Example contructor - MCC takes at least it's configuration subtree */
+        /** Example contructor - MCC takes at least it's configuration subtree */
         MCC(Arc::Config *cfg) { };
         virtual ~MCC(void) { };
-		/** Method for processing of information.
+        /** Method for processing of information.
            This method is called by previos MCC in chain.
            This method must call similar method of next MCC in chain unless 
           any failure happens. Result returned by call to next MCC should
@@ -30,7 +30,7 @@ class MCC
 
 } // namespace Arc
 
-/** This structure describes MCC stored in shard library.
+/** This structure describes MCC stored in shared library.
    Each MCC is detected by presence of element named 'descriptor' 
   of this type. It contains version number and pointer to function which 
   creates an instance of object inherited from MCC class. */
--- src/hed/libs/loader/Service.h
+++ src/hed/libs/loader/Service.h
@@ -5,16 +5,30 @@
 
 namespace Arc {
 
+/** Service - last plugin in a Message Chain.
+  This is virtual class which defines interface (in a future also 
+ common functionality) for every Service plugin. Interface is made of 
+ method process() which is called by Plexer class.
+ */
 class Service
 {
     public:
+        /** Example contructor - Server takes at least it's configuration subtree */
         Service(Arc::Config *cfg) { };
         virtual ~Service(void) { };
+        /** Method for processing of information.
+           This method is called by Plexer class.
+           This method must process passed information and return valid 
+          response. Respons may contain failure but still must tbe valid. */
         virtual void process(void) { };
 };
 
 } // namespace Arc
 
+/** This structure describes Service stored in shared library.
+   Each Service is detected by presence of element named 'descriptor' 
+  of this type. It contains version number and pointer to function which 
+  creates an instance of object inherited from Service class. */
 typedef struct {
     int version;
     Arc::Service *(*get_instance)(Arc::Config *cfg);
