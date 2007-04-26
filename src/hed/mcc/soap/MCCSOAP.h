76c29bbb0f0f569adc2319d16e90fa4fdbbe6022
Aleksandr Konstantinov <akonstantinov@yahoo.com>
2007-04-26 21:10:33 +0000

    Documeting SOAP MCC

--- src/hed/mcc/soap/MCCSOAP.h
+++ src/hed/mcc/soap/MCCSOAP.h
@@ -5,17 +5,30 @@
 
 namespace Arc {
 
+/** This MCC processes parsed SOAP message from input payload.
+  On input payload with PayloadRawInterface is expected. It's
+ converted into PayloadSOAP and passed next MCC. Returned 
+ PayloadSOAP is converted into PayloadRaw and returned to calling
+ MCC. */
 class MCC_SOAP_Service: public MCC
 {
     public:
+        /* Constructor takes configuration of MCC. 
+         Currently there are no configuration parameters for this MCC */
         MCC_SOAP_Service(Arc::Config *cfg);
         virtual ~MCC_SOAP_Service(void);
         virtual MCC_Status process(Message&,Message&);
 };
 
+/* This is client side of SOAP processing MCC.
+  It accepts and produces PayloadSOAP kind of payloads in it's
+ process() method. Comminication to next MCC is done over payloads
+ implementing PayloadRawInterface. */
 class MCC_SOAP_Client: public MCC
 {
     public:
+        /* Constructor takes configuration of MCC. 
+         Currently there are no configuration parameters for this MCC */
         MCC_SOAP_Client(Arc::Config *cfg);
         virtual ~MCC_SOAP_Client(void);
         virtual MCC_Status process(Message&,Message&);
