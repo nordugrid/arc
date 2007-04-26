abac1b718da8f00aa6ae4290a66e9f8590b2d7fb
Aleksandr Konstantinov <akonstantinov@yahoo.com>
2007-04-26 21:25:45 +0000

    Documenting HTTP MCC.

--- src/hed/mcc/http/MCCHTTP.h
+++ src/hed/mcc/http/MCCHTTP.h
@@ -5,6 +5,13 @@
 
 namespace Arc {
 
+/** This class implements MCC to processes HTTP request.
+  On input payload with PayloadStreamInterface is expected. 
+  HTTP message is read from stream ans it's body is converted 
+ into PayloadRaw and passed next MCC. Returned payload of 
+ PayloadRawInterface type is treated as body part of returning
+ PayloadHTTP. Generated HTTP response is sent though stream
+ passed in input payload. */
 class MCC_HTTP_Service: public MCC
 {
     public:
@@ -13,6 +20,12 @@ class MCC_HTTP_Service: public MCC
         virtual MCC_Status process(Message&,Message&);
 };
 
+/** This class is a client part of HTTP MCC.
+  It accepts PayloadRawInterface payload and uses it as body to
+ generate HTTP request. Request is passed to next MCC as 
+ PayloadRawInterface type of payload. Returned PayloadStreamInterface
+ payload is parsed into HTTP respinse and it's body is passed back to
+ calling MCC. */ 
 class MCC_HTTP_Client: public MCC
 {
     protected:
