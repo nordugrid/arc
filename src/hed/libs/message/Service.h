240d90abff6481004ef92c8f9b609fda9044f227
Aleksandr Konstantinov <akonstantinov@yahoo.com>
2007-04-27 11:11:15 +0000

    Extending documentation of Service class. Small guide on how to write services.

--- src/hed/libs/message/Service.h
+++ src/hed/libs/message/Service.h
@@ -9,7 +9,22 @@ namespace Arc {
 /** Service - last plugin in a Message Chain.
   This is virtual class which defines interface (in a future also 
  common functionality) for every Service plugin. Interface is made of 
- method process() which is called by Plexer class.
+ method process() which is called by Plexer or MCC class.
+  There is one Service object created for every service description 
+ processed by Loader class objects. Classes derived from Service class 
+ must implement process() method of MCCInterface. 
+  It is up to developer how internal state of service is stored and 
+ communicated to other services and external utilites.
+  Service is free to expect any type of payload passed to it and generate
+ any payload as well. Useful types depend on MCCs in chain which leads to 
+ that service. For example if service is expected to by linked to SOAP
+ MCC it must accespt and generate messages with PayloadSOAP payload.
+  Method process() of class derived from Service class may be called 
+ concurently in multiple threads. Developers must take that into account 
+ and write thread-safe implementation.
+  Simple example of service is provided in /src/tests/echo/echo.cpp .
+  The way to write client couterpart of corresponding service is 
+ undefined. For example see /src/tests/echo/test.cpp .
  */
 class Service: public MCCInterface
 {
