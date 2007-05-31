72da659c52a8ae3af01de723358d78ce77da6c3f
Markus Norden <markus.norden@tsl.uu.se>
2007-05-31 14:36:44 +0000

    Counter library added.

---
+++ src/hed/libs/counter/Counter.cpp
@@ -0,0 +1,109 @@
+// Counter.cpp
+
+#include "Counter.h"
+
+namespace Arc {
+
+  const Glib::TimeVal ETERNAL(G_MAXLONG,0);
+  const Glib::TimeVal HISTORIC(G_MINLONG,0);
+
+  Counter::Counter(){
+    // Nothing needs to be done.
+  }
+
+  Counter::Counter(const Counter& unique){
+    // Executing this code should be impossible!
+    exit(EXIT_FAILURE);
+  }
+
+  Counter::~Counter(){
+    // Nothing needs to be done.
+  }
+
+  void Counter::operator=(const Counter& unique){
+    // Executing this code should be impossible!
+    exit(EXIT_FAILURE);
+  }
+
+  Glib::TimeVal Counter::getCurrentTime(){
+    Glib::TimeVal currentTime;
+    currentTime.assign_current_time();
+    return currentTime;
+  }
+  
+  Glib::TimeVal Counter::getExpiryTime(Glib::TimeVal duration){
+    if (duration<ETERNAL)
+      return getCurrentTime()+duration;
+    else
+      return ETERNAL;
+  }
+
+  CounterTicket Counter::getCounterTicket(Counter::IDType reservationID,
+				 Glib::TimeVal expiryTime,
+				 Counter* counter)
+  {
+    return CounterTicket(reservationID, expiryTime, counter);
+  }
+
+  ExpirationReminder Counter::getExpirationReminder(Glib::TimeVal expTime,
+					   Counter::IDType resID)
+  {
+    return ExpirationReminder(expTime, resID);
+  }
+
+  CounterTicket::CounterTicket() :
+    reservationID(0),
+    expiryTime(HISTORIC),
+    counter(0)
+  {
+    // Nothing else needs to be done.
+  }
+
+  CounterTicket::CounterTicket(Counter::IDType reservationID,
+			       Glib::TimeVal expiryTime,
+			       Counter* counter) :
+    reservationID(reservationID),
+    expiryTime(expiryTime),
+    counter(counter)
+  {
+    // Nothing else needs to be done.
+  }
+
+  bool CounterTicket::isValid(){
+    return expiryTime>counter->getCurrentTime();
+  }
+
+  void CounterTicket::extend(Glib::TimeVal duration){
+    counter->extend(reservationID, expiryTime, duration);
+  }
+
+  void CounterTicket::cancel(){
+    counter->cancel(reservationID);
+    reservationID=0;
+    expiryTime=HISTORIC;
+    counter=0;
+  }
+
+  ExpirationReminder::ExpirationReminder(Glib::TimeVal expiryTime,
+					 Counter::IDType reservationID) :
+    expiryTime(expiryTime), reservationID(reservationID)
+  {
+    // Nothing else needs to be done.
+  }
+
+  bool ExpirationReminder::operator<
+    (const ExpirationReminder& other) const
+  {
+    // Smaller time has higher priority!
+    return expiryTime>other.expiryTime;
+  }
+
+  Glib::TimeVal ExpirationReminder::getExpiryTime() const {
+    return expiryTime;
+  }
+
+  Counter::IDType ExpirationReminder::getReservationID() const {
+    return reservationID;
+  }
+
+}
---
+++ src/hed/libs/counter/IntraProcessCounter.cpp
@@ -0,0 +1,175 @@
+// Counter.cpp
+
+#include "IntraProcessCounter.h"
+
+namespace Arc {
+
+  IntraProcessCounter::IntraProcessCounter(int limit, int excess) :
+    limit(limit),
+    excess(excess),
+    value(limit),
+    nextReservationID(1)
+  {
+    // Nothing else needs to be done.
+  }
+  
+  IntraProcessCounter::IntraProcessCounter(const IntraProcessCounter& unique){
+    // Executing this code should be impossible!
+    exit(EXIT_FAILURE);
+  }
+
+  IntraProcessCounter::~IntraProcessCounter(){
+    // Nothing needs to be done.
+  }
+
+  void IntraProcessCounter::operator=(const IntraProcessCounter& unique){
+    // Executing this code should be impossible!
+    exit(EXIT_FAILURE);
+  }
+
+  int IntraProcessCounter::getLimit(){
+    return limit;
+  }
+
+  int IntraProcessCounter::setLimit(int newLimit){
+    synchMutex.lock();
+    value+=newLimit-limit;
+    limit=newLimit;
+    synchMutex.unlock();
+    synchCond.signal();
+    return newLimit;
+  }
+
+  int IntraProcessCounter::changeLimit(int amount){
+    int newLimit;
+    synchMutex.lock();
+    newLimit=limit+amount;
+    value+=amount;
+    limit=newLimit;
+    synchMutex.unlock();
+    synchCond.signal();
+    return newLimit;
+  }
+
+  int IntraProcessCounter::getExcess(){
+    return excess;
+  }
+
+  int IntraProcessCounter::setExcess(int newExcess){
+    synchMutex.lock();
+    excess=newExcess;
+    synchMutex.unlock();
+    synchCond.signal();
+    return newExcess;
+  }
+
+  int IntraProcessCounter::changeExcess(int amount){
+    int newExcess;
+    synchMutex.lock();
+    newExcess=excess+amount;
+    excess+=amount;
+    synchMutex.unlock();
+    synchCond.signal();
+    return newExcess;
+  }
+
+  int IntraProcessCounter::getValue(){
+    int result;
+    synchMutex.lock();
+    result=unsafeGetValue();
+    synchMutex.unlock();
+    return result;
+  }
+
+  CounterTicket IntraProcessCounter::reserve
+  (int amount,
+   Glib::TimeVal duration,
+   bool prioritized,
+   Glib::TimeVal timeOut)
+  {
+    Glib::TimeVal deadline=getExpiryTime(timeOut);
+    Glib::TimeVal expiryTime;
+    IDType reservationID;
+    synchMutex.lock();
+    while (amount>unsafeGetValue()+(prioritized?excess:0) and
+	   getCurrentTime()<deadline)
+      synchCond.timed_wait(synchMutex,
+			   std::min(deadline,unsafeGetNextExpiration()));
+    if (amount<=unsafeGetValue()+(prioritized?excess:0)){
+      expiryTime=getExpiryTime(duration);
+      reservationID=unsafeReserve(amount, expiryTime);
+    }
+    else {
+      expiryTime=HISTORIC;
+    }
+    synchMutex.unlock();
+    return getCounterTicket(reservationID, expiryTime, this);
+  }
+
+  void IntraProcessCounter::cancel(unsigned long long int reservationID){
+    synchMutex.lock();
+    unsafeCancel(reservationID);
+    synchMutex.unlock();    
+    synchCond.signal();
+  }
+
+  void IntraProcessCounter::extend
+  (IDType& reservationID,
+   Glib::TimeVal& expiryTime,
+   Glib::TimeVal duration)
+  {
+    int amount;
+    synchMutex.lock();
+    amount=unsafeCancel(reservationID);
+    if (amount>0){
+      expiryTime=getExpiryTime(duration);
+      reservationID=unsafeReserve(amount, expiryTime);
+    }
+    else{
+      expiryTime=HISTORIC;
+      reservationID=0;
+    }
+    synchMutex.unlock();
+    synchCond.signal();
+  }
+
+  int IntraProcessCounter::unsafeGetValue(){
+    while(unsafeGetNextExpiration()<getCurrentTime()){
+      unsafeCancel(selfExpiringReservations.top().getReservationID());
+      selfExpiringReservations.pop();
+    }
+    return value;
+  }
+  
+  int IntraProcessCounter::unsafeCancel(IDType reservationID){
+    std::map<IDType,int>::iterator resIter=reservations.find(reservationID);
+    int amount=0;
+    if (resIter!=reservations.end()){
+      amount=resIter->second;
+      value+=amount;
+      reservations.erase(resIter);
+    }
+    return amount;
+  }
+  
+  Counter::IDType IntraProcessCounter::unsafeReserve(int amount,
+						     Glib::TimeVal expiryTime)
+  {
+    IDType reservationID=nextReservationID++;
+    value-=amount;
+    reservations[reservationID]=amount;
+    if (expiryTime<ETERNAL){
+      selfExpiringReservations.push
+	(getExpirationReminder(expiryTime,reservationID));
+    }
+    return reservationID;
+  }
+
+  Glib::TimeVal IntraProcessCounter::unsafeGetNextExpiration(){
+    if (selfExpiringReservations.empty())
+      return ETERNAL;
+    else
+      return selfExpiringReservations.top().getExpiryTime();
+  }
+
+}
