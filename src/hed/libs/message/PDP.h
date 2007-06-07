8ebd38bb1977fb41e718cc76e7f862bb221dee34
Weizhong Qiang <Qiang@mycompany.com>
2007-06-07 18:54:30 +0000

    Add PDP.h

---
+++ src/hed/libs/message/PDP.h
@@ -0,0 +1,22 @@
+#ifndef __ARC_PDP_H__
+#define __ARC_PDP_H__
+
+#include <stdlib.h>
+
+#include "common/ArcConfig.h"
+#include "MCC.h"
+#include "Message.h"
+
+namespace Arc {
+
+class PDP {
+ public:
+  PDP(Arc::Config* cfg) { };
+  virtual ~PDP(void) { };
+  virtual MCC_Status process(Message& msg) { return -1; };
+};
+
+} // namespace Arc
+
+#endif /* __ARC_PDP_H__ */
+
