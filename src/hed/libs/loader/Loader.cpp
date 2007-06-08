a49e1b21dcfd1d1ce9f41cd1a5db8dc5b50d7efd
Szalai Ferenc <szferi@niif.hu>
2007-06-08 07:34:34 +0000

    delete memory at deconsturctor

--- src/hed/libs/loader/Loader.cpp
+++ src/hed/libs/loader/Loader.cpp
@@ -62,6 +62,9 @@ Loader::~Loader(void)
   l.msg(Arc::DEBUG, "after loops");
   if(context_) delete context_;
   l.msg(Arc::DEBUG, "after delete context");
+  if(service_factory) delete service_factory;
+  if(mcc_factory) delete mcc_factory;
+  //if(plexer_factory) delete plexer_factory;
 }
 
 
