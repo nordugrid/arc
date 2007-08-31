a3814a5ce1cec9f3802c981417800fcec10ba62e
Weizhong Qiang <Qiang@mycompany.com>
2007-08-31 13:00:25 +0000

    Move functionality of regular expression to common library

---
+++ src/libs/common/Regex.cpp
@@ -0,0 +1,69 @@
+#include "Regex.h"
+
+namespace Arc {
+  
+  RegularExpression::RegularExpression(std::string pattern) :
+    pattern(pattern)
+  {
+    status=regcomp(&preg, pattern.c_str(), REG_EXTENDED);
+  }
+  
+  RegularExpression::RegularExpression(const RegularExpression& regex) :
+    pattern(regex.pattern)
+  {
+    status=regcomp(&preg, pattern.c_str(), 0);
+  }
+  
+  RegularExpression::~RegularExpression(){
+    regfree(&preg);
+  }
+  
+  const RegularExpression&
+  RegularExpression::operator=(const RegularExpression& regex){
+    regfree(&preg);
+    pattern=regex.pattern;
+    status=regcomp(&preg, pattern.c_str(), 0);
+    return *this;
+  }
+  
+  bool RegularExpression::isOk(){
+    return status==0;
+  }
+  
+  bool RegularExpression::hasPattern(std::string str){
+    return pattern==str;
+  }
+  
+  bool RegularExpression::match(const std::string& str) const {
+    std::list<std::string> unmatched;
+    return match(str,unmatched) && (unmatched.size() == 0);
+  }
+
+  bool RegularExpression::match(const std::string& str,std::list<std::string>& unmatched) const {
+    if (status==0){
+      int st;
+      regmatch_t rm[256];
+      unmatched.clear();
+      st = regexec(&preg, str.c_str(), 256, rm, 0);
+      if(st != 0) return false;
+      regoff_t p = 0;
+      for(int n = 0; n<256; ++n) {
+        if(rm[n].rm_so == -1) break;
+        if(rm[n].rm_so > p) {
+          unmatched.push_back(str.substr(p,rm[n].rm_so-p));
+        };
+        p=rm[n].rm_eo;
+      };
+      if(p < str.length()) unmatched.push_back(str.substr(p));
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+
+  std::string RegularExpression::getPattern() {
+    return pattern;
+  }
+
+}
--- src/libs/common/testxpath.cpp
+++ src/libs/common/testxpath.cpp
@@ -20,6 +20,7 @@ int main(void)
     }
     f.close();
 
+    //Get the "chile2" node with namespace "uri:bbb" inside the root node
     Arc::NS nsList;
     nsList.insert(std::pair<std::string, std::string>("test","uri:bbb"));
     Arc::XMLNode doc(xml_str);
@@ -31,9 +32,29 @@ int main(void)
     
     std::cout << "************************" << std::endl;
     
+    //Get the "child2" node with namespace "uri:ccc" inside the root node
     nsList.erase("test");
     nsList.insert(std::pair<std::string, std::string>("test1","uri:ccc"));
     list = doc.XPathLookup("//test1:child2", nsList);
+    for ( it=list.begin() ; it != list.end(); it++ ){
+      std::cout << (*it).Name() << std::endl;
+    }
+
+   
+    //Get the "parent"node with namespace "uri:bbb" inside the root node
+    nsList.erase("test1");
+    nsList.insert(std::pair<std::string, std::string>("test2","uri:bbb"));
+    list = doc.XPathLookup("//test2:parent", nsList);
+    Arc::XMLNode tmp;
+    for ( it=list.begin() ; it != list.end(); it++ ){
+      tmp=*it;
+      std::cout << (*it).Name() << std::endl;
+    }   
+
+    //Find the "child2" node with namespace "uri:ccc" and inside the "parent" node, does not work.
+    nsList.erase("test2");
+    nsList.insert(std::pair<std::string, std::string>("test3","uri:bbb"));
+    list = tmp.XPathLookup("//test3:child1", nsList);      //To illustrate only "root" can call XPathLookup method
     for ( it=list.begin() ; it != list.end(); it++ ){
       std::cout << (*it).Name() << std::endl;
     }
