418a90336c92ebc35a36d4334005b7db61d9c718
Weizhong Qiang <Qiang@mycompany.com>
2007-08-31 12:59:17 +0000

    Move functionality of regular expression to common library

--- src/hed/libs/loader/Plexer.h
+++ src/hed/libs/loader/Plexer.h
@@ -5,54 +5,12 @@
 
 #include <list>
 #include <string>
-#include <regex.h>
-#include "../message/MCC.h"
-#include "../message/Message.h"
-#include "../../../libs/common/ArcConfig.h"
+#include "common/Regex.h"
+#include "message/MCC.h"
+#include "message/Message.h"
+#include "common/ArcConfig.h"
 
 namespace Arc {
-
-  //! A regular expression class.
-  /*! This class is a wrapper around the functions provided in
-    regex.h.
-  */
-  class RegularExpression {
-  public:
-
-    //! Creates a reges from a pattern string.
-    RegularExpression(std::string pattern);
-    
-    //! Copy constructor.
-    RegularExpression(const RegularExpression& regex);
-
-    //! Destructor
-    ~RegularExpression();
-
-    //! Assignment operator.
-    const RegularExpression& operator=(const RegularExpression& regex);
-
-    //! Returns true if the pattern of this regex is ok.
-    bool isOk();
-
-    //! Returns true if this regex has the pattern provided.
-    bool hasPattern(std::string str);
-
-    //! Returns true if this regex matches whole string provided.
-    bool match(const std::string& str) const;
-    
-    //! Returns true if this regex matches the string provided.
-    //! Unmatched parts of the string are stored in 'unmatched'.
-    bool match(const std::string& str,std::list<std::string>& unmatched) const;
-    
-    //! Returns patter 
-    std::string getPattern();
-
-  private:
-    std::string pattern;
-    regex_t preg;
-    int status;
-  };
-  
 
   //! A pair of label (regex) and pointer to service.
   /*! A helper class that stores a label (regex) and a pointer to a
