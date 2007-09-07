372eb7d733338983b154328fc0c43d8a9f564358
Szalai Ferenc <szferi@niif.hu>
2007-09-07 21:10:14 +0000

    add missing file. It is dump but required because of language wrappers and binding

---
+++ src/hed/libs/message/MessageAuth.cpp
@@ -0,0 +1,24 @@
+#ifdef HAVE_CONFIG
+#include <config.h>
+#endif
+
+#include "MessageAuth.h"
+
+namespace Arc {
+
+void MessageAuth::set(const std::string& key, const AuthObject& value) 
+{
+    // NOP
+}
+
+AuthObject MessageAuth::get(const std::string& key, int index)
+{
+    return NULL;
+}
+
+void MessageAuth::remove(const std::string& key)
+{
+    // NOP
+}
+
+}; // namespace Arc
