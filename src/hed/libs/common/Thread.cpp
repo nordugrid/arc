ae920fe411e3b0b2a5f7af75547821d0595eab85
Aleksandr Konstantinov <akonstantinov@yahoo.com>
2007-09-22 23:46:58 +0000

    Adding todo comments.

--- src/hed/libs/common/Thread.cpp
+++ src/hed/libs/common/Thread.cpp
@@ -31,6 +31,7 @@ static ThreadInitializer thread_initializer;
 bool CreateThreadFunction(void (*func)(void*), void* arg) {
     ThreadArgument* argument = new ThreadArgument(func,arg);
     try {
+        // TODO. Who is going to destroy created object? Check for memory leaks.
         Glib::Thread::create(sigc::mem_fun(*argument,&ThreadArgument::thread),
                              false);
     } catch(std::exception& e) { 
