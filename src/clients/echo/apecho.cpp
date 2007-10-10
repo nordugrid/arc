1053284beed5dd70e93c2408eed7c6026901d647
Markus Norden <markus.norden@tsl.uu.se>
2007-10-10 09:27:48 +0000

    Added a simple command line client tool for the echo service (apecho). It will be used in the README file for the first technology preview release of new ARC in order to have something without dependencies on components from old ARC. This time I also remembered svn add... :-)

---
+++ src/clients/echo/apecho.cpp
@@ -0,0 +1,40 @@
+// apecho.cpp
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <stdexcept>
+#include "echo_client.h"
+
+//! A prototype client for the echo service.
+/*! A prototype command line tool for the echo service. In the name,
+  "ap" means "Arc Prototype".
+  
+  Usage:
+  apecho <message>
+
+  Arguments:
+  <message> A message that the echo service shall return back again.
+
+  Configuration:
+  Which echo service to use is specified in a configuration file. The
+  configuration file also specifies how to set up the communication
+  chain for the client. The location of the configuration file is
+  specified by the environment variable ARC_ECHO_CONFIG. If there is
+  no such environment variable, the configuration file is assumed to
+  be "echo_client.xml" in the current working directory.
+*/
+int main(int argc, char* argv[]){
+  try{
+    if (argc!=2)
+      throw std::invalid_argument("Wrong number of arguments!");
+    Arc::EchoClient ec;
+    std::string message = argv[1];
+    std::cout << ec.echo(message) << std::endl;
+    return EXIT_SUCCESS;
+  }
+  catch (std::exception& err){
+    std::cerr << "ERROR: " << err.what() << std::endl;
+    return EXIT_FAILURE;
+  }
+}
---
+++ src/clients/echo/echo_client.cpp
@@ -0,0 +1,97 @@
+// echo_client.cpp
+
+#include "echo_client.h"
+
+namespace Arc {
+  
+  EchoClientError::EchoClientError(const std::string& what) :
+    std::runtime_error(what)
+  {
+  }
+
+  Arc::Logger EchoClient::logger(Arc::Logger::rootLogger, "A-REX-Client");
+
+  EchoClient::EchoClient(std::string configFile)
+    throw(EchoClientError)
+  {
+    logger.msg(Arc::INFO, "Creating an echo client.");
+
+    if (configFile=="" && getenv("ARC_ECHO_CONFIG"))
+      configFile = getenv("ARC_ECHO_CONFIG");
+    if (configFile=="")
+      configFile = "./echo_client.xml";
+
+    client_config = new Arc::Config(configFile.c_str());
+    if(!*client_config) {
+      logger.msg(Arc::ERROR, "Failed to load client configuration.");
+      throw EchoClientError("Failed to load client configuration.");
+    }
+
+    client_loader = new Arc::Loader(client_config);
+    logger.msg(Arc::INFO, "Client side MCCs are loaded.");
+    client_entry = (*client_loader)["soap"];
+    if(!client_entry) {
+      logger.msg(Arc::ERROR, "Client chain does not have entry point.");
+      throw EchoClientError("Client chain does not have entry point.");
+    }
+
+    echo_ns["echo"]="urn:echo";
+  }
+  
+  EchoClient::~EchoClient()
+  {
+    delete client_loader;
+    delete client_config;
+  }
+  
+  std::string EchoClient::echo(const std::string& text)
+    throw(EchoClientError)
+  {
+    std::string answer;
+    logger.msg(Arc::INFO, "Creating and sending request.");
+
+    // Create job request
+    Arc::PayloadSOAP req(echo_ns);
+    req.NewChild("echo").NewChild("say")=text;
+
+    // Send job request
+    Arc::Message reqmsg;
+    Arc::Message repmsg;
+    Arc::MessageAttributes attributes;
+    Arc::MessageContext context;
+    reqmsg.Payload(&req);
+    reqmsg.Attributes(&attributes);
+    reqmsg.Context(&context);
+    Arc::MCC_Status status = client_entry->process(reqmsg,repmsg);
+    if(!status) {
+      logger.msg(Arc::ERROR, "Echo request failed.");
+      throw EchoClientError("Echo request failed.");
+    }
+    logger.msg(Arc::INFO, "Echo request succeed.");
+    if(repmsg.Payload() == NULL) {
+      logger.msg
+	(Arc::ERROR, "There were no response to an echo request.");
+      throw EchoClientError
+	("There were no response to the echo request.");
+    }
+    Arc::PayloadSOAP* resp = NULL;
+    try {
+      resp = dynamic_cast<Arc::PayloadSOAP*>(repmsg.Payload());
+    } catch(std::exception&) { };
+    if(resp == NULL) {
+      logger.msg(Arc::ERROR,
+		 "A response to an echo request was not a SOAP message.");
+      delete repmsg.Payload();
+      throw EchoClientError
+	("The response to the echo request was not a SOAP message.");
+    };
+    //Arc::NS ns;
+    //Arc::XMLNode id(ns);
+    //id.NewChild((*resp)["EchoResponse"]);
+    //id.GetXML(answer);
+    answer = (std::string)((*resp)["echoResponse"]["hear"]);
+    delete repmsg.Payload();
+    return answer;
+  }  
+  
+}
