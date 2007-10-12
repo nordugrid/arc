b078b7cb066dab5b026b00be13012359c6132f27
Aleksandr Konstantinov <akonstantinov@yahoo.com>
2007-10-12 22:27:44 +0000

    Use information providers of arc0 to prepare info document for WSRF interface.

--- src/services/a-rex/information_collector.cpp
+++ src/services/a-rex/information_collector.cpp
@@ -1,118 +1,107 @@
 #include <sstream>
+#include <fstream>
+
+#include <arc/Run.h>
 
 #include "ldif/LDIFtoXML.h"
+#include "config/ngconfig.h"
+#include "grid-manager/conf/environment.h"
 #include "arex.h"
 
 namespace ARex {
 
-
-static const char* ldif_str = 
-"dn: nordugrid-cluster-name=grid.uio.no,Mds-Vo-name=local,o=grid\n"
-"objectClass: Mds\n"
-"objectClass: nordugrid-cluster\n"
-"nordugrid-cluster-name: grid.uio.no\n"
-"nordugrid-cluster-aliasname: EPF (UiO/FI)\n"
-"nordugrid-cluster-owner: University of Oslo\n"
-"Mds-validfrom: 20070925162548Z\n"
-"Mds-validto: 20070925162618Z\n"
-"\n"
-"dn: nordugrid-queue-name=bigmem,nordugrid-cluster-name=grid.uio.no,Mds-Vo-name=local,o=grid\n"
-"objectClass: Mds\n"
-"objectClass: nordugrid-queue\n"
-"nordugrid-queue-name: bigmem\n"
-"nordugrid-queue-status: active\n"
-"nordugrid-queue-homogeneity: TRUE\n"
-"nordugrid-queue-nodecpu: Intel(R) Pentium(R) D CPU @ 3000 MHz\n"
-"Mds-validfrom: 20070925162548Z\n"
-"Mds-validto: 20070925162618Z\n"
-"\n"
-"dn: nordugrid-info-group-name=jobs, nordugrid-queue-name=bigmem,nordugrid-cluster-name=grid.uio.no,Mds-Vo-name=local,o=grid\n"
-"objectClass: Mds\n"
-"objectClass: nordugrid-info-group\n"
-"nordugrid-info-group-name: jobs\n"
-"Mds-validfrom: 20070925162548Z\n"
-"Mds-validto: 20070925162618Z\n"
-"\n"
-"dn: nordugrid-job-globalid=gsiftp://grid.uio.no:2811/jobs/2288011907374131619643632, nordugrid-info-group-name=jobs, nordugrid-queue-name=bigmem,nordugrid-cluster-name=grid.uio.no,Mds-Vo-name=local,o=grid\n"
-"objectClass: Mds\n"
-"objectClass: nordugrid-job\n"
-"nordugrid-job-globalid: gsiftp://grid.uio.no:2811/jobs/2288011907374131619643632\n"
-"nordugrid-job-globalowner: /C=CH/O=Universite de Geneve/CN=Xin Zhou/emailAddress=xin.zhou@medecine.unige.ch\n"
-"nordugrid-job-jobname: geneva-gift0\n"
-"nordugrid-job-execcluster: grid.uio.no\n"
-"nordugrid-job-execqueue: bigmem\n"
-"nordugrid-job-status: FINISHED\n"
-"Mds-validfrom: 20070925162548Z\n"
-"Mds-validto: 20070925162618Z\n"
-"\n"
-"dn: nordugrid-job-globalid=gsiftp://grid.uio.no:2811/jobs/2288011907374131619643633, nordugrid-info-group-name=jobs, nordugrid-queue-name=bigmem,nordugrid-cluster-name=grid.uio.no,Mds-Vo-name=local,o=grid\n"
-"objectClass: Mds\n"
-"objectClass: nordugrid-job\n"
-"nordugrid-job-globalid: gsiftp://grid.uio.no:2811/jobs/2288011907374131619643633\n"
-"nordugrid-job-globalowner: /C=CH/O=Universite de Geneve/CN=Xin Zhou/emailAddress=xin.zhou@medecine.unige.ch\n"
-"nordugrid-job-jobname: geneva-gift0\n"
-"nordugrid-job-execcluster: grid.uio.no\n"
-"nordugrid-job-execqueue: bigmem\n"
-"nordugrid-job-status: FINISHED\n"
-"Mds-validfrom: 20070925162548Z\n"
-"Mds-validto: 20070925162618Z\n"
-"\n"
-"dn: nordugrid-queue-name=smallmem,nordugrid-cluster-name=grid.uio.no,Mds-Vo-name=local,o=grid\n"
-"objectClass: Mds\n"
-"objectClass: nordugrid-queue\n"
-"nordugrid-queue-name: smallmem\n"
-"nordugrid-queue-status: active\n"
-"nordugrid-queue-homogeneity: TRUE\n"
-"nordugrid-queue-nodecpu: Intel(R) Pentium(R) D CPU @ 3000 MHz\n"
-"Mds-validfrom: 20070925162548Z\n"
-"Mds-validto: 20070925162618Z\n"
-"\n"
-"dn: nordugrid-info-group-name=users, nordugrid-queue-name=smallmem,nordugrid-cluster-name=grid.uio.no,Mds-Vo-name=local,o=grid\n"
-"objectClass: Mds\n"
-"objectClass: nordugrid-info-group\n"
-"nordugrid-info-group-name: jobs\n"
-"Mds-validfrom: 20070925162548Z\n"
-"Mds-validto: 20070925162618Z\n"
-"\n"
-"dn: nordugrid-authuser-name=Marko Niinimaki, nordugrid-info-group-name=users, nordugrid-queue-name=smallmem,nordugrid-cluster-name=grid.uio.no,Mds-Vo-name=local,o=grid\n"
-"objectClass: Mds\n"
-"objectClass: nordugrid-authuser\n"
-"nordugrid-authuser-name: Marko Niinimaki...15\n"
-"nordugrid-authuser-sn: /O=Grid/O=NorduGrid/OU=hip.fi/CN=Marko Niinimaki\n"
-"nordugrid-authuser-freecpus: 4\n"
-"nordugrid-authuser-diskspace: 22211\n"
-"nordugrid-authuser-queuelength: 0\n"
-"Mds-validfrom: 20070925162548Z\n"
-"Mds-validto: 20070925162618Z\n"
-"\n"
-"dn: nordugrid-authuser-name=Nobody, nordugrid-info-group-name=users, nordugrid-queue-name=smallmem,nordugrid-cluster-name=grid.uio.no,Mds-Vo-name=local,o=grid\n"
-"objectClass: Mds\n"
-"objectClass: nordugrid-authuser\n"
-"nordugrid-authuser-name: Nobody\n"
-"nordugrid-authuser-sn: /O=Grid/O=NorduGrid/OU=nowhere/CN=Nobody\n"
-"nordugrid-authuser-freecpus: 4\n"
-"nordugrid-authuser-diskspace: 22211\n"
-"nordugrid-authuser-queuelength: 0\n"
-"Mds-validfrom: 20070925162548Z\n"
-"Mds-validto: 20070925162618Z\n";
-
 void ARexService::InformationCollector(void) {
+  nordugrid_config_loc=gmconfig_;
+std::cerr<<"GM configuration file: "<<nordugrid_config_loc<<std::endl;
+  read_env_vars();
   for(;;) {
-    // Run information providers
-    // For testing purposes use hardcoded information
-    std::string s(ldif_str);
-    std::istringstream ldif(s);
+    std::string lrms;
+    std::list<std::string> queues;
+
+    // Parse configuration to construct command lines for information providers
+    std::ifstream f(nordugrid_config_loc.c_str());
+    if(!f) {
+    };
+    try {
+      Config cfg = NGConfig().Read(f);
+      const std::list<ConfGrp>& groups = cfg.GetConfigs();
+      for(std::list<ConfGrp>::const_iterator g = groups.begin();g!=groups.end();++g) {
+        std::string section = g->GetSection();
+        std::string subsection;
+        std::string::size_type p = section.find('/');
+        if(p != std::string::npos) {
+          subsection=section.substr(p+1);
+          section.resize(p);
+        };
+        logger_.msg(Arc::DEBUG,"Configuration: Section: %s, Subsection: %s",section.c_str(),subsection.c_str());
+        if((section == "common") || (section == "grid-manager")) {
+          std::list<std::string> values = g->FindOptionValue("lrms");
+          if(values.size() > 0) lrms=*(values.begin());
+          std::string::size_type p = lrms.find(' ');
+          if(p != std::string::npos) lrms.resize(p);
+          logger_.msg(Arc::DEBUG,"Configuration: LRMS: %s",lrms.c_str());
+        } else if(section == "queue") {
+          std::string queue = g->GetID();
+          if(queue.empty()) queue=subsection;
+          logger_.msg(Arc::DEBUG,"Configuration: Queue: %s",queue.c_str());
+          if(!queue.empty()) queues.push_back(queue);
+        };
+      };
+    } catch(ConfigError& e) {
+    };
+    // Run information provider
+    std::string ldif_str;
+    {
+      std::string cmd;
+      cmd=nordugrid_libexec_loc+"/cluster-"+lrms+".pl -valid-to 120 -config "+nordugrid_config_loc+" -dn o=grid -l 2";
+      std::cerr<<"cmd: "<<cmd<<std::endl;
+      Arc::Run run(cmd);
+      std::string stdin_str;
+      std::string stderr_str;
+      run.AssignStdin(stdin_str);
+      run.AssignStdout(ldif_str);
+      run.AssignStderr(stderr_str);
+      if(!run.Start()) {
+      };
+      if(!run.Wait(60)) {
+      };
+      int r = run.Result();
+      logger_.msg(Arc::DEBUG,"Cluster information provider result: %i",r);
+      logger_.msg(Arc::DEBUG,"Cluster information provider error: %s",stderr_str.c_str());
+    };
+    for(std::list<std::string>::iterator q = queues.begin();q!=queues.end();++q) {
+      std::string cmd;
+      cmd=nordugrid_libexec_loc+"/queue+jobs+users-"+lrms+".pl -valid-to 120 -config "+nordugrid_config_loc+" -dn nordugrid-queue-name="+(*q)+",o=grid -queue "+(*q)+" -l 2";
+      std::cerr<<"cmd: "<<cmd<<std::endl;
+      Arc::Run run(cmd);
+      std::string stdin_str;
+      std::string stderr_str;
+      run.AssignStdin(stdin_str);
+      run.AssignStdout(ldif_str);
+      run.AssignStderr(stderr_str);
+      if(!run.Start()) {
+      };
+      if(!run.Wait(60)) {
+      };
+      int r = run.Result();
+      logger_.msg(Arc::DEBUG,"Queue information provider result: %i",r);
+      logger_.msg(Arc::DEBUG,"Queue information provider error: %s",stderr_str.c_str());
+    };
+    logger_.msg(Arc::DEBUG,"Obtained LDIF: %s",ldif_str.c_str());
     // Convert result to XML
+    std::istringstream ldif(ldif_str);
     Arc::NS ns;
     ns["n"]="urn:nordugrid";
     Arc::XMLNode xml(ns);
     Arc::XMLNode root = xml.NewChild("n:nordugrid");
-    if(LDIFtoXML(ldif,"Mds-Vo-name=local,o=Grid",root)) {
+    if(LDIFtoXML(ldif,"o=grid",root)) {
       // Put result into container
       infodoc_.Assign(root);
-std::cerr<<"Assigned new informational document"<<std::endl;
+      logger_.msg(Arc::INFO,"Assigned new informational document");
+      root.GetXML(ldif_str);
+      logger_.msg(Arc::DEBUG,"Obtained XML: %s",ldif_str.c_str());
     } else {
-std::cerr<<"Failed to create informational document"<<std::endl;
+      logger_.msg(Arc::ERROR,"Failed to create informational document");
     };
     sleep(60);
   };
