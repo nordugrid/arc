088fd778e0b4f5ea7432b1c016920e5a1fa2261c
Aleksandr Konstantinov <akonstantinov@yahoo.com>
2007-10-15 23:42:10 +0000

    Adapting code for changes in XMLNode.

--- src/services/a-rex/ldif/test.cpp
+++ src/services/a-rex/ldif/test.cpp
@@ -7,10 +7,10 @@ int main(int /*args*/,char* argv[]) {
   std::ifstream f(argv[1]);
   std::string ldif_base = "Mds-Vo-name=local,O=Grid";
   Arc::NS ns;
-  Arc::XMLNode xml(ns);
-  ARex::LDIFtoXML(f,ldif_base,xml.NewChild("LDIF"));
+  Arc::XMLNode xml(ns,"LDIF");
+  ARex::LDIFtoXML(f,ldif_base,xml);
   std::string s;
-  xml.GetXML(s);
+  xml.GetDoc(s);
   std::cout<<s<<std::endl;
   return 0;
 }
