3ff94471d9a991eee2d8ad35730b7024291df5d1
Aleksandr Konstantinov <akonstantinov@yahoo.com>
2007-10-25 20:12:22 +0000

    Comments update.

--- src/hed/mcc/tls/MCCTLS.h
+++ src/hed/mcc/tls/MCCTLS.h
@@ -30,7 +30,7 @@ namespace Arc {
     //static void* ssl_idptr_cb(void);
   };
 
-/** This two classed are MCCs implementing TLS functionality. Upon creation this 
+/** This MCC implements TLS server side functionality. Upon creation this 
   object creats SSL_CTX object and configures SSL_CTX object with some environment
   information about credential. 
   Because we cannot know the "socket" when the creation of MCC_TLS_Service/MCC_TLS_Client 
@@ -41,17 +41,12 @@ namespace Arc {
   object. The "ssl" object is embeded in a payload called PayloadTLSSocket.
 
   The process() method of MCC_TLS_Service is passed payload implementing 
-  PayloadStreamInterface (actually PayloadTCPSocket), and the method return empty 
-  payload in "outmsg", just as MCC_HTTP_Service does. The ssl object is created and 
-  binded to socket object when constructing the PayloadTLSSocket in the process() method. 
+  PayloadStreamInterface and the method returns empty PayloadRaw payload in "outmsg". 
+  The ssl object is created and bound to Stream payload when constructing the PayloadTLSSocket 
+  in the process() method. 
 
-  The process() method of MCC_TLS_Client is also passed payload impementing 
-  PayloadStreamInterface and return empty payload. 
-
-  So the MCC_TLS_Service and MCC_TLS_Client will only keep the information about SSL_CTX, 
-  nothing else.  It is the PayloadTLSSocket some keeps some information about ssl session. 
-  And the PayloadTLSSocket which implements the PayloadStreamInterface will be used by 
-  PayloadHTTP.
+  During processing of message this MCC generates attribute TLS:PEERDN which contains
+  Distinguished Name of remoote peer.
 */
 
 class MCC_TLS_Service: public MCC_TLS
@@ -67,15 +62,6 @@ class MCC_TLS_Service: public MCC_TLS
 class PayloadTLSMCC;
 
 /** This class is MCC implementing TLS client.
-  Unfortunately, the MCC_TLS_Client would be put behind MCC_TCP_Client, which looks 
-  different with server side(MCC_TLS_Server is put between MCC_HTTP_Server and MCC_TCP_Server).
-  the MCC_TLS_Client should get the socket fd and attache (I use "attache" :)) ssl to the fd, 
-  and the socket fd is created in MCC_TCP_client and also used as s_.Put() in MCC_TCP_client 
-  to flush tcp request, the ssl attachement should be done before s_.Put() call. So I just put 
-  MCC_TLS_Client behind MCC_TCP_Client.
-  Also there PayloadTLSStream that implement PayloadStreamInterface, which is specified for 
-  TLS method, such like "SSL_read()" "SSL_write".
-  As Alexsandr's advice, we could replace TCP with TLS, it will be considered and done later.
 */
 class MCC_TLS_Client: public MCC_TLS
 {
