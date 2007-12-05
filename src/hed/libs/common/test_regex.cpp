be650184485e37cdda1ad4ce147de0c2f3509464
Szalai Ferenc <szferi@niif.hu>
2007-12-05 14:42:54 +0000

    add test app for regexp

---
+++ src/hed/libs/common/test_regex.cpp
@@ -0,0 +1,22 @@
+#include <ArcRegex.h>
+#include <string>
+#include <list>
+#include <iostream>
+
+int main(void)
+{
+    std::string str="[id=ahssgf0afhha0-adja-dkkda]";
+    std::string r = "([a-zA-Z0-9_\\\\-]*)=([a-zA-Z0-9_\\\\-]*)";
+    Arc::RegularExpression rx(r);
+    if (rx.isOk()) {
+        std::list<std::string> ump, mp;
+        rx.match(str, ump, mp);
+        std::list<std::string>::iterator it;
+        for (it = mp.begin(); it != mp.end(); it++) {
+            std::cout << (*it) << std::endl;
+        }     
+    } else {
+        std::cout << "Invalid regexp" << std::endl;
+    }
+    return 0;
+}
