1d4e302813102566fea5e161aec9a4af89cece16
Aleksandr Konstantinov <akonstantinov@yahoo.com>
2007-12-12 23:21:32 +0000

    Adding uploader. Fixing worst bugs in downloader.

--- src/services/a-rex/grid-manager/loaders/uploader.cpp
+++ src/services/a-rex/grid-manager/loaders/uploader.cpp
@@ -1,7 +1,7 @@
 /*
-  Download files specified in job.ID.input and check if user uploaded files.
+  Upload files specified in job.ID.output.
   result: 0 - ok, 1 - unrecoverable error, 2 - potentially recoverable.
-  arguments: job_id control_directory session_directory [cache_directory]
+  arguments: job_id control_directory session_directory
   optional arguments: -t threads , -u user
 */
 //@ #include "std.h"
@@ -10,6 +10,7 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <pwd.h>
+#include <dirent.h>
 
 #include <arc/data/DMC.h>
 #include <arc/data/CheckSum.h>
@@ -45,8 +46,6 @@
 #define odlog(LEVEL) std::cerr
 //@
 
-/* check for user uploaded files every 60 seconds */
-#define CHECK_PERIOD 60
 /* maximum number of retries (for every source/destination) */
 #define MAX_RETRIES 5
 /* maximum number simultaneous downloads */
@@ -84,116 +83,10 @@ static int pairs_initiated = 0;
 uid_t file_owner = 0;
 uid_t file_group = 0;
 
-bool is_checksum(std::string str) {
-  /* check if have : */
-  std::string::size_type n;
-  const char *s = str.c_str();
-  if((n=str.find('.')) == std::string::npos) return false;
-  if(str.find('.',n+1) != std::string::npos) return false;
-  for(int i=0;i<n;i++) { if(!isdigit(s[i])) return false; };
-  for(int i=n+1;s[i];i++) { if(!isdigit(s[i])) return false; };
-  return true;
-}
-
 int clean_files(std::list<FileData> &job_files,char* session_dir) {
   std::string session(session_dir);
-  /* delete only downloadable files, let user manage his/hers files */
-  std::list<FileData> tmp;
-  if(delete_all_files(session,job_files,false,true,false) != 2) return 0;
+  if(delete_all_files(session,job_files,true) != 2) return 0;
   return 1;
-}
-
-/*
-   Check for existence of user uploadable file
-   returns 0 if file exists 
-           1 - it is not proper file or other error
-           2 - not here yet 
-*/
-int user_file_exists(FileData &dt,char* session_dir,std::string* error = NULL) {
-  struct stat st;
-  const char *str = dt.lfn.c_str();
-  if(strcmp(str,"*.*") == 0) return 0; /* do not wait for this file */
-  std::string fname=std::string(session_dir) + '/' + dt.pfn;
-  /* check if file does exist at all */
-  if(lstat(fname.c_str(),&st) != 0) return 2;
-  /* check for misconfiguration */
-  /* parse files information */
-  char *str_;
-  unsigned long long int fsize;
-  unsigned long long int fsum;
-  bool have_size = false;
-  bool have_checksum = false;
-  fsize = strtouq(str,&str_,10);
-  if((*str_) == '.') {
-    if(str_ != str) have_size=true;
-    str=str_+1;
-    fsum = strtouq(str,&str_,10);
-    if((*str_) != 0) {
-      olog << "Invalid checksum in " << dt.lfn << " for " << dt.pfn << std::endl;
-      if(error) (*error)="Bad information about file: checksum can't be parsed.";
-      return 1;
-    };
-    have_checksum=true;
-  }
-  else {
-    if(str_ != str) have_size=true;
-    if((*str_) != 0) {
-      olog << "Invalid file size in " << dt.lfn << " for " << dt.pfn << std::endl;
-      if(error) (*error)="Bad information about file: size can't be parsed.";
-      return 1;
-    };
-  };
-  if(S_ISDIR(st.st_mode)) {
-    if(have_size || have_checksum) {
-      if(error) (*error)="Expected file. Directory found.";
-      return 1;
-    };
-    return 0;
-  };
-  if(!S_ISREG(st.st_mode)) {
-    if(error) (*error)="Expected ordinary file. Special object found.";
-    return 1;
-  };
-  /* now check if proper size */
-  if(have_size) {
-    if(st.st_size < fsize) return 2;
-    if(st.st_size > fsize) {
-      olog << "Invalid file " << dt.pfn << " is too big." << std::endl;
-      if(error) (*error)="Delivered file is bigger than specified.";
-      return 1; /* too big file */
-    };
-  };
-  if(have_checksum) {
-    int h=open(fname.c_str(),O_RDONLY);
-    if(h==-1) { /* if we can't read that file job won't too */
-      olog << "Error accessing file " << dt.pfn << std::endl;
-      if(error) (*error)="Delivered file is unreadable.";
-      return 1;
-    };
-    Arc::CRC32Sum crc;
-    char buffer[1024];
-    ssize_t l;
-    size_t ll = 0;
-    for(;;) {
-      if((l=read(h,buffer,1024)) == -1) {
-        olog << "Error reading file " << dt.pfn << std::endl; return 1;
-        if(error) (*error)="Could not read file to compute checksum.";
-      };
-      if(l==0) break; ll+=l;
-      crc.add(buffer,l);
-    };
-    close(h);
-    crc.end();
-    if(fsum != crc.crc()) {
-      if(have_size) { /* size was checked - it is an error to have wrong crc */ 
-        olog << "File " << dt.pfn << " has wrong CRC." << std::endl;
-        if(error) (*error)="Delivered file has wrong checksum.";
-        return 1;
-      };
-      return 2; /* just not uploaded yet */
-    };
-  };
-  return 0; /* all checks passed - file is ok */
 }
 
 class PointPair {
@@ -220,7 +113,6 @@ class PointPair {
 //
 //
     } else {
-      olog<<"Downloaded file "<<*(it.source)<<std::endl;
       processed_files.push_back(*it.file);
     };
     job_files.erase(it.file);
@@ -231,6 +123,43 @@ class PointPair {
   };
 };
 
+int expand_files(std::list<FileData> &job_files,char* session_dir) {
+  for(FileData::iterator i = job_files.begin();i!=job_files.end();) {
+    std::string url = i->lfn;
+    // Only ftp and gsiftp can be expanded to directories so far
+    if(strncasecmp(url.c_str(),"ftp://",6) && 
+       strncasecmp(url.c_str(),"gsiftp://",9)) { ++i; continue; };
+    // user must ask explicitly
+    if(url[url.length()-1] != '/') { ++i; continue; };
+    std::string path(session_dir); path+="/"; path+=i->pfn;
+    int l = strlen(session_dir) + 1;
+    DIR *dir = opendir(path.c_str());
+    if(dir) {
+      struct dirent file_;
+      struct dirent *file;
+      for(;;) {
+        readdir_r(dir,&file_,&file);
+        if(file == NULL) break;
+        if(!strcmp(file->d_name,".")) continue;
+        if(!strcmp(file->d_name,"..")) continue;
+        std::string path_ = path; path_+="/"; path+=file->d_name;
+        struct stat st;
+        if(lstat(path_.c_str(),&st) != 0) continue; // do not follow symlinks
+        if(S_ISREG(st.st_mode)) {
+          std::string lfn = url+file->d_name;
+          job_files.push_back(FileData(path_.c_str()+l,lfn.c_str()));
+        } else if(S_ISDIR(st.st_mode)) {
+          std::string lfn = url+file->d_name+"/"; // cause recursive search
+          job_files.push_back(FileData(path_.c_str()+l,lfn.c_str()));
+        };
+      };
+      i=job_files.erase(i);
+    } else {
+      ++i;
+    }; 
+  };
+}
+
 int main(int argc,char** argv) {
   Arc::LogStream logcerr(std::cerr, "AREXClient");
   Arc::Logger::getRootLogger().addDestination(logcerr);
@@ -238,8 +167,6 @@ int main(int argc,char** argv) {
   bool new_download;
   bool not_uploaded;
   int retries=0;
-  time_t start_time=time(NULL);
-  time_t upload_timeout = 0;
   int n_threads = 1;
   int n_files = MAX_DOWNLOADS;
 //@  LogTime::Active(true);
@@ -258,11 +185,11 @@ int main(int argc,char** argv) {
   // process optional arguments
   for(;;) {
     opterr=0;
-    int optc=getopt(argc,argv,"+hclpZn:t:n:u:U:s:S:a:i:d:");
+    int optc=getopt(argc,argv,"+hclpZn:t:u:U:s:S:a:i:d:");
     if(optc == -1) break;
     switch(optc) {
       case 'h': {
-        olog<<"downloader [-t threads] [-n files] [-c] job_id control_directory session_directory [cache_directory [cache_data_directory [cache_link_directory]]]"<<std::endl; 
+        olog<<"uploader [-t threads]  [-n files] [-c] [-u username] [-U userid] job_id control_directory session_directory [cache_directory [cache_data_directory [cache_link_directory]]]"<<std::endl; 
         exit(1);
       }; break;
       case 'c': {
@@ -397,11 +324,9 @@ int main(int argc,char** argv) {
     olog<<"Won't use more than 10 threads"<<std::endl;
     n_threads=10;
   };
-/*
- !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!  Add this to DataMove !!!!!!!!!!!!
-*/
+
   UrlMapConfig url_map;
-  olog<<"Downloader started"<<std::endl;
+  olog<<"Uploader started"<<std::endl;
 
   // Load plugins
   Arc::NS ns;
@@ -432,12 +357,6 @@ int main(int argc,char** argv) {
       user.SetCacheDir(cache_dir,cache_data_dir,cache_link_dir);
     };
   };
-  // get the list of input files
-  // odlog(DEBUG)<<"Cache: dir: "<<cache_dir<<std::endl;
-  // odlog(DEBUG)<<"Cache: data dir: "<<cache_data_dir<<std::endl;
-  // odlog(DEBUG)<<"Cache: link dir: "<<cache_link_dir<<std::endl;
-  // odlog(DEBUG)<<"Cache: uid: "<<cache_uid<<std::endl;
-  // odlog(DEBUG)<<"Cache: gid: "<<cache_gid<<std::endl;
   Arc::DataCache cache(cache_dir,cache_data_dir,cache_link_dir,id,cache_uid,cache_gid);
   Arc::DataMover mover;
   mover.secure(secure);
@@ -452,26 +371,23 @@ int main(int argc,char** argv) {
   bool transfered = true;
   bool credentials_expired = false;
 
-  if(!job_input_read_file(desc.get_id(),user,job_files)) {
-    failure_reason+="Internal error in downloader\n";
-    olog << "Can't read list of input files" << std::endl; res=1; goto exit;
+  // get the list of output files
+  if(!job_output_read_file(desc.get_id(),user,job_files)) {
+    failure_reason+="Internal error in uploader\n";
+    olog << "Can't read list of output files" << std::endl; res=1; goto exit;
+    //olog << "WARNING: Can't read list of output files - whole output will be removed" << std::endl;
   };
+
   // remove bad files
-  if(clean_files(job_files,session_dir) != 0) { 
-    failure_reason+="Internal error in downloader\n";
+  if(clean_files(job_files,session_dir) != 0) {
+    failure_reason+="Internal error in uploader\n";
     olog << "Can't remove junk files" << std::endl; res=1; goto exit;
   };
-  // initialize structures to handle download
+  expand_files(job_files,session_dir);
+  // initialize structures to handle upload
   /* TODO: add threads=# to all urls if n_threads!=1 */
-  // Compute wait time for user files
-  for(FileData::iterator i=job_files.begin();i!=job_files.end();++i) {
-    if(i->lfn.find(":") == std::string::npos) { /* is it lfn ? */
-      upload_timeout+=MAX_USER_TIME;
-    } else {
-      userfiles_only=false;
-    };
-  };
-  // Main download cycle
+  desc.GetLocalDescription(user);
+  // Main upload cycle
   if(!userfiles_only) for(;;) {
     // Initiate transfers
     int n = 0;
@@ -481,13 +397,24 @@ int main(int argc,char** argv) {
         ++n;
         if(n <= pairs_initiated) continue; // skip files being processed
         if(n > n_files) break; // quit if not allowed to process more
-        /* have place and file to download */
+        /* have source and file to upload */
         /* define place to store */
-        std::string destination=std::string("file://") + session_dir + i->pfn;
-        std::string source = i->lfn;
-        if(strncasecmp(source.c_str(),"file://",7) == 0) {
-          failure_reason+=std::string("User requested local input file ")+source.c_str()+"\n";
-          olog<<"FATAL ERROR: local source for download: "<<source<<std::endl; res=1; goto exit;
+        std::string stdlog;
+        std::string source;
+        JobLocalDescription* local = desc.get_local();
+        if(local) stdlog=local->stdlog;
+        if(stdlog.length() > 0) stdlog="/"+stdlog+"/";
+        if((stdlog.length() > 0) &&
+           (strncmp(stdlog.c_str(),i->pfn.c_str(),stdlog.length()) == 0)) {
+          stdlog=i->pfn.c_str()+stdlog.length();
+          source=std::string("file://")+control_dir+"/job."+id+"."+stdlog;
+        } else {
+          source=std::string("file://")+session_dir+i->pfn;
+        };
+        std::string destination = i->lfn;
+        if(strncasecmp(destination.c_str(),"file://",7) == 0) {
+          failure_reason+=std::string("User requested to store output locally ")+destination.c_str()+"\n";
+          olog<<"FATAL ERROR: local destination for uploader"<<destination<<std::endl; res=1; goto exit;
         };
         PointPair* pair = new PointPair(source,destination,i);
         if(!(pair->source)) {
@@ -513,84 +440,40 @@ int main(int argc,char** argv) {
     pair_condition.wait_nonblock();
     pair_condition.unlock();
   };
-  // Print download summary
+  // Print upload summary
   for(FileData::iterator i=processed_files.begin();i!=processed_files.end();++i) {
-    odlog(INFO)<<"Downloaded "<<i->lfn<<std::endl;
-    if(Arc::URL(i->lfn).Option("exec") == "yes") {
-      fix_file_permissions(session_dir+i->pfn,true);
-    };
+    odlog(INFO)<<"Uploaded "<<i->lfn<<std::endl;
   };
   for(FileDataEx::iterator i=failed_files.begin();i!=failed_files.end();++i) {
-    odlog(ERROR)<<"Failed to download "<<i->lfn<<std::endl;
+    odlog(ERROR)<<"Failed to upload "<<i->lfn<<std::endl;
     failure_reason+="Input file: "+i->lfn+" - "+Arc::DataMover::get_result_string(i->res)+"\n";
     if(i->res == Arc::DataMover::credentials_expired_error) credentials_expired=true;
     transfered=false;
   };
   // Check if all files have been properly downloaded
   if(!transfered) {
-    odlog(INFO)<<"Some downloads failed"<<std::endl; res=2;
+    odlog(INFO)<<"Some uploads failed"<<std::endl; res=2;
     if(credentials_expired) res=3;
     goto exit;
   };
-  if(!job_input_write_file(desc,user,job_files)) {
-    olog << "WARNING: Failed writing changed input file." << std::endl;
+  /* all files left should be kept */
+  for(FileData::iterator i=job_files.begin();i!=job_files.end();) {
+    i->lfn=""; ++i;
   };
-  // check for user uploadable files
-  // run cycle waiting for uploaded files
-  for(;;) {
-    not_uploaded=false;
-    for(FileData::iterator i=job_files.begin();i!=job_files.end();) {
-      if(i->lfn.find(":") == std::string::npos) { /* is it lfn ? */
-        /* process user uploadable file */
-        olog<<"Check user uploadable file: "<<i->pfn<<std::endl;
-        std::string error;
-        int err=user_file_exists(*i,session_dir,&error);
-        if(err == 0) { /* file is uploaded */
-          olog<<"User has uploaded file"<<std::endl;
-          i=job_files.erase(i);
-          if(!job_input_write_file(desc,user,(std::list<FileData>&)job_files)) {
-            olog << "WARNING: Failed writing changed input file." << std::endl;
-          };
-        }
-        else if(err == 1) { /* critical failure */
-          olog<<"Critical error for uploadable file"<<std::endl;
-          failure_reason+="User file: "+i->pfn+" - "+error+"\n";
-          res=1; goto exit;
-        }
-        else {
-          not_uploaded=true; ++i;
-        };
-      }
-      else {
-        ++i;
-      };
+  if(!userfiles_only) {
+    if(!job_output_write_file(desc,user,job_files)) {
+      olog << "WARNING: Failed writing changed output file." << std::endl;
     };
-    if(!not_uploaded) break;
-    // check for timeout
-    if((time(NULL)-start_time) > upload_timeout) {
-      for(FileData::iterator i=job_files.begin();i!=job_files.end();++i) {
-        if(i->lfn.find(":") == std::string::npos) { /* is it lfn ? */
-          failure_reason+="User file: "+i->pfn+" - Timeout waiting\n";
-        };
-      };
-      olog<<"Uploadable files timed out"<<std::endl; res=2; break;
-    };
-    sleep(CHECK_PERIOD);
-  };
-  if(!job_input_write_file(desc,user,job_files)) {
-    olog << "WARNING: Failed writing changed input file." << std::endl;
   };
 exit:
-  olog << "Leaving downloader ("<<res<<")"<<std::endl;
-  // clean unfinished files here 
+  if(!cache_dir.empty()) cache_release_url(cache_dir.c_str(),cache_data_dir.c_str(),cache_uid,cache_gid,id,true);
+  olog << "Leaving uploader ("<<res<<")"<<std::endl;
+  // clean uploaded files here 
   clean_files(job_files,session_dir);
-  // release cache just in case
-  if(res != 0) {
-    if(!cache_dir.empty()) cache_release_url(cache_dir.c_str(),cache_data_dir.c_str(),cache_uid,cache_gid,id,true);
-  };
   remove_proxy();
   if(res != 0) {
     job_failed_mark_add(desc,user,failure_reason);
   };
   return res;
 }
+
