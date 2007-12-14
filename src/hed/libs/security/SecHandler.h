8906823c207157f4141114fce641cca63b18bd3e
Aleksandr Konstantinov <akonstantinov@yahoo.com>
2007-12-14 11:14:27 +0000

    Documenting SecHandler class.

--- src/hed/libs/security/SecHandler.h
+++ src/hed/libs/security/SecHandler.h
@@ -7,10 +7,15 @@
 
 namespace ArcSec {
 
-//  class Arc::Config;
-//  class Arc::Logger;
-//  class Arc::Message;
-
+  /// Base class for simple security handling plugins
+  /** This virtual class defines method Handle() which processes 
+    security related information/attributes in Message and optionally
+    makes security decision. Instances of such classes are normally
+    arranged in chains abd are called on incoming and outgoing messages
+    in various MCC and Service plugins. Return value of Handle() defines
+    either processing should continie (true) or stop with error (false). 
+    Configuration of SecHandler is consumed during creation of instance
+    through XML subtree fed to constructor. */ 
   class SecHandler {
    public:
     SecHandler(Arc::Config*) {};
