3c2bd9e5f756e5145330b0f789ec95e6d462841b
Szalai Ferenc <szferi@niif.hu>
2008-01-16 10:34:31 +0000

    skeleton of ISIS Register

---
+++ src/hed/libs/infosys/Register.cpp
@@ -0,0 +1,63 @@
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <arc/URL.h>
+#include <arc/message/PayloadSOAP.h>
+#include "Register.h"
+
+namespace Arc
+{
+
+Register::Register(std::string &service_id, Arc::Config &cfg):logger(Arc::Logger::rootLogger, "Register")
+{
+    ns["isis"] = "http://www.nordugrid.org/schemas/isis/2007/06";
+    service_id = service_id;
+    // XXX how to get plugin paths from cfg?
+    mcc_cfg.AddPluginsPath("../../hed/mcc/tcp/.libs/");
+}
+
+void Register::AddUrl(std::string &url)
+{
+    urls.push_back(url);
+}
+
+void Register::registration(void)
+{
+    std::list<std::string>::iterator it;
+
+    for (it = urls.begin(); it != urls.end(); it++) {
+        Arc::URL u(*it);
+        const char *isis_name = (*it).c_str();
+        bool tls;
+        if (u.Protocol() == "http") {
+            tls = true;
+        } else if (u.Protocol() == "https") {
+            tls = false;
+        } else {
+            logger.msg(Arc::WARNING, "invalid protocol: %s", isis_name);
+            continue;
+        }
+        cli = new Arc::ClientSOAP(mcc_cfg, u.Host(), u.Port(), tls, u.Path());
+        logger.msg(Arc::DEBUG, "Start registartion to %s ISIS", isis_name);
+        Arc::PayloadSOAP request(ns);
+        // XXX read data from InfoCache
+        // request.NewChild()
+        Arc::PayloadSOAP *response;
+        Arc::MCC_Status status = cli->process(&request, &response);
+        if (!status) {
+            if (response) {
+                // XXX process response
+                logger.msg(Arc::DEBUG, "Succesful registartion to %s ISIS", isis_name); 
+            } else {
+                logger.msg(Arc::ERROR, "Error during registarton to %s ISIS", isis_name);
+            }
+        } else {
+            logger.msg(Arc::ERROR, "Error during registarton to %s ISIS", isis_name);
+        }
+        
+        delete cli;
+    }
+}
+
+}; // namespace
---
+++ src/hed/libs/infosys/Register.h
@@ -0,0 +1,32 @@
+#ifndef __ARC_INFOSYS_REGISTER_H__
+#define __ARC_INFOSYS_REGISTER_H__
+
+#include <list>
+#include <string>
+#include <arc/ArcConfig.h>
+#include <arc/XMLNode.h>
+#include <arc/misc/ClientInterface.h>
+#include <arc/Logger.h>
+
+namespace Arc {
+
+class Register
+{
+    private:
+        std::list<std::string> urls;
+        Arc::NS ns;
+        Arc::MCCConfig mcc_cfg;
+        Arc::ClientSOAP *cli;
+        Arc::Logger logger;
+        std::string service_id;
+
+    public:
+        Register(std::string &service_id, Arc::Config &cfg);
+        ~Register(void);
+        void AddUrl(std::string &url);
+        void registration(void);
+
+}; // class Register
+}; // namespace
+
+#endif
