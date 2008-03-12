2fe799f11ccf7b3f1b8a05a351660b3f2843da01
Bjarte Mohn <Mohn@mycompany.com>
2008-03-12 10:10:02 +0000

    Small test program for ACC plugins

---
+++ src/hed/libs/client/test.C
@@ -0,0 +1,31 @@
+#include "TargetGenerator.h"
+#include <arc/ArcConfig.h>
+#include <arc/Logger.h>
+#include <arc/misc/ClientInterface.h>
+#include <arc/XMLNode.h>
+
+
+int main(){
+
+  Arc::LogStream logcerr(std::cerr);
+  Arc::Logger::getRootLogger().addDestination(logcerr);
+  Arc::Logger::getRootLogger().setThreshold(Arc::DEBUG);
+  
+  Arc::ACCConfig acccfg;
+  Arc::NS ns;
+  Arc::Config mcfg(ns);
+  acccfg.MakeConfig(mcfg);
+  Arc::XMLNode AnotherOne = mcfg.NewChild("ArcClientComponent");
+  AnotherOne.NewAttribute("name") = "TargetRetrieverARC0";
+  AnotherOne.NewAttribute("id") = "retriever1";
+  AnotherOne.NewChild("URL") = "ldap://grid.tsl.uu.se:2135/mds-vo-name=sweden,o=grid?giisregistrationstatus?base";
+
+  mcfg.SaveToStream(std::cout);
+  std::cout << std::endl;
+
+  Arc::TargetGenerator test(mcfg);
+  test.getTargets();
+
+  return 0;
+
+}
