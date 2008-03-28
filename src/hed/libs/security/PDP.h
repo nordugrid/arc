1408060525604c42cd3f4c5dc566d76eeb090445
Weizhong Qiang <Qiang@mycompany.com>
2008-03-28 11:05:28 +0000

    Add context for pdp configuration

--- src/hed/libs/security/PDP.h
+++ src/hed/libs/security/PDP.h
@@ -7,6 +7,44 @@
 #include <arc/Logger.h>
 
 namespace ArcSec {
+  typedef struct {
+    std::list<std::string> subject;
+    std::list<std::string> resource;
+    std::list<std::string> action;
+    std::list<std::string> context;
+  } AuthzRequest;
+
+  class PDPConfigContext:public Arc::MessageContextElement {
+   private:
+    std::list<ArcSec::AuthzRequest> request;
+    std::list<std::string> policylocation;
+    
+   public:
+    PDPConfigContext(std::list<ArcSec::AuthzRequest> req, std::string& policy) {request = req; policylocation.push_back(policy); };
+    PDPConfigContext(std::list<ArcSec::AuthzRequest> req, std::list<std::string> policy) {request = req; policylocation = policy; };
+    void AddRequestItem(ArcSec::AuthzRequest requestitem) { request.push_back(requestitem); };
+    int RequestItemSize() { (int) request.size(); };
+    ArcSec::AuthzRequest GetRequestItem(int n) { 
+      std::list<ArcSec::AuthzRequest>::iterator it;
+      it = request.begin();
+      for(int i = 0; i<=n; i++) it++;
+      return (*it); 
+    };
+    void AddPolicyLocation(std::string& policy) { policylocation.push_back(policy); };
+    void SetPolicyLocation(std::list<std::string> policy) { 
+      std::list<std::string>::iterator it1 = policylocation.begin();
+      std::list<std::string>::iterator it2 = policylocation.end(); 
+      policylocation.erase(it1, it2); 
+      policylocation = policy; 
+    };
+    void SetPolicyLocation(std::string& policy) { 
+      std::list<std::string>::iterator it1 = policylocation.begin();
+      std::list<std::string>::iterator it2 = policylocation.end();  
+      policylocation.erase(it1, it2);
+      policylocation.push_back(policy); 
+    };
+    virtual ~PDPConfigContext(void) {};
+  };
 
   /// Base class for Policy Decisoion Point plugins
   /** This virtual class defines method isPermitted() which processes
