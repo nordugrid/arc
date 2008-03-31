6c59ef8bfe272018d7e230fcbebf89ef6097e346
Bjarte Mohn <Mohn@mycompany.com>
2008-03-31 14:55:02 +0000

    Simplifications for example

--- src/hed/acc/TargetRetriever/TargetRetrieverARC0.cpp
+++ src/hed/acc/TargetRetriever/TargetRetrieverARC0.cpp
@@ -19,6 +19,8 @@ namespace Arc{
   
   std::list<ACC*> TargetRetrieverARC0::getTargets(){
     
+    std::cout <<"TargetRetriverARC0 getTargets called, URL = " << m_url << std::endl;
+
     std::list<ACC*> results;
     
     NS ns;
@@ -36,25 +38,28 @@ namespace Arc{
     std::string result;
 
     while(buffer.for_write() || !buffer.eof_read()) {
-      buffer.for_write(handle, length, offset, true);
-      result.replace(offset, length, buffer[handle]);
-      buffer.is_written(handle);
+      if(buffer.for_write(handle, length, offset, true)){
+	result.append(buffer[handle], length);
+	buffer.is_written(handle);
+      }
     }
     
     handler->StopReading();
 
     XMLNode XMLresult(result);
 
-    XMLresult.SaveToStream(std::cout);
+    //XMLresult.SaveToStream(std::cout);
 
     //Next read XML result and decode into targets to be returned
 
-
-    //First do GIISes
+    //First do GIISes (if any)
     XMLNodeList GIISes = XMLresult.XPathLookup("//Mds-Vo-name", NS());
     std::cout << "#GIIS = " << GIISes.size() <<std::endl;
 
     XMLNodeList::iterator iter;
+
+    //Loop over GIISes commented out for now
+    /*
     for(iter = GIISes.begin(); iter!= GIISes.end(); iter++){
       if(!(*iter)["Mds-Service-type"]) continue; //remove first entry
       if((std::string)(*iter)["Mds-Reg-status"] == "PURGED" ) continue;      
@@ -64,8 +69,8 @@ namespace Arc{
             (std::string) (*iter)["Mds-Service-port"] + "/" +
 	    (std::string) (*iter)["Mds-Service-Ldap-suffix"] + "?giisregistrationstatus?base";
       
-      //      iter->SaveToStream(std::cout);
-      std::cout << url << std::endl;
+      //iter->SaveToStream(std::cout);
+      //std::cout << url << std::endl;
       
       NS ns;
       Arc::Config cfg(ns);
@@ -77,11 +82,76 @@ namespace Arc{
 
       results.insert(results.end(), results2.begin(), results2.end());
 
+    } //end GIISes
+    */
+
+    //Next GRISes (if any)
+    XMLNodeList GRISes = XMLresult.XPathLookup("//nordugrid-cluster-name", NS());
+    std::cout << "#GRIS = " << GRISes.size() <<std::endl;
+    for(iter = GRISes.begin(); iter!= GRISes.end(); iter++){
+      if((std::string)(*iter)["Mds-Reg-status"] == "PURGED" ) continue;      
+      
+      std::string url;
+      url = (std::string) (*iter)["Mds-Service-type"] + "://" + 
+	    (std::string) (*iter)["Mds-Service-hn"] + ":" +
+            (std::string) (*iter)["Mds-Service-port"] + "/" +
+	    (std::string) (*iter)["Mds-Service-Ldap-suffix"] + 
+	    "??" + //attributes (empty means all)
+	    "sub?" + // scope
+	    "(|(objectclass=nordugrid-cluster)(objectclass=nordugrid-queue))"; //filter
+
+      std::cout << "This is a GRIS:" << std::endl;
+      std::cout << url << std::endl;
+      
+      //Query GRIS for all relevant information
+      handler = url;
+      DataBufferPar buffer;
+      handler->StartReading(buffer);
+      std::string result;
+      
+      while(buffer.for_write() || !buffer.eof_read()){
+	if(buffer.for_write(handle, length, offset, true)){
+	  result.append(buffer[handle], length);
+	  buffer.is_written(handle);
+	}
+      }
+
+      handler->StopReading();
+
+      std::cout << "result = "<<result <<std::endl; 
+      
+      XMLNode XMLresult(result);
+      
+      //Retrieve all attributes
+      //Fill GLUE2 classes and eventually ExecutionTarget
+      std::list<std::string> attributes = getAttribute("//nordugrid-cluster-runtimeenvironment", XMLresult);
+      std::cout << "#Attributes = " << attributes.size() <<std::endl;
+
+    } //end GRISes
+    
+    return results;
+    
+  } //end getTargets()
+
+
+  
+  
+  std::list<std::string> TargetRetrieverARC0::getAttribute(std::string attr, Arc::XMLNode& node){
+    std::list<std::string> results;
+
+    XMLNodeList nodelist = node.XPathLookup(attr, NS());
+
+    XMLNodeList::iterator iter;
+    
+    for(iter = nodelist.begin(); iter!= nodelist.end(); iter++){
+
+      results.push_back((*iter));
+      
     }
 
     return results;
 
-  }
+  } //end getAttribute
 
 } //namespace
 
--- src/hed/acc/TargetRetriever/TargetRetrieverARC0.h
+++ src/hed/acc/TargetRetriever/TargetRetrieverARC0.h
@@ -3,11 +3,16 @@
 namespace Arc {
 
   class ChainContext;
+  class XMLNode;
 
   class TargetRetrieverARC0 : public Arc::TargetRetriever{
     TargetRetrieverARC0(Arc::Config *cfg);
     ~TargetRetrieverARC0();
 
+  private:
+    std::list<std::string> getAttribute(std::string attr, Arc::XMLNode& node);
+    
+    
   public:
     std::list<ACC*> getTargets(); 
     static ACC* Instance(Arc::Config *cfg, Arc::ChainContext*);
