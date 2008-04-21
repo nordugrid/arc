c60a08d3f20a83e37e5c4cb972feaec02653445f
Aleksandr Konstantinov <akonstantinov@yahoo.com>
2008-04-21 13:20:58 +0000

    Adding security attributes to message infrastructure

--- src/hed/libs/message/MessageAuth.cpp
+++ src/hed/libs/message/MessageAuth.cpp
@@ -6,4 +6,39 @@
 
 namespace Arc {
 
-} // namespace Arc
+MessageAuth::MessageAuth(void) {
+}
+
+MessageAuth::~MessageAuth(void) {
+  std::map<std::string,SecAttr*>::iterator attr = attrs_.begin();
+  for(;attr != attrs_.end();++attr) {
+    if(attr->second) delete attr->second;
+  };
+}
+
+void MessageAuth::set(const std::string& key, SecAttr* value) {
+  std::map<std::string,SecAttr*>::iterator attr = attrs_.find(key);
+  if(attr == attrs_.end()) {
+    attrs_[key]=value;
+  } else {
+    if(attr->second) delete attr->second;
+    attr->second=value;
+  };
+}
+
+void MessageAuth::remove(const std::string& key) {
+  std::map<std::string,SecAttr*>::iterator attr = attrs_.find(key);
+  if(attr != attrs_.end()) {
+    if(attr->second) delete attr->second;
+    attrs_.erase(attr);
+  };
+}
+
+SecAttr* MessageAuth::get(const std::string& key) {
+  std::map<std::string,SecAttr*>::iterator attr = attrs_.find(key);
+  if(attr == attrs_.end()) return NULL;
+  return attr->second;
+}
+
+}
+
--- src/hed/libs/message/MessageAuth.h
+++ src/hed/libs/message/MessageAuth.h
@@ -1,21 +1,27 @@
 #ifndef __ARC_MESSAGEAUTH_H__
 #define __ARC_MESSAGEAUTH_H__
 
-#include "MessageAttributes.h"
+#include <string>
+#include <map>
+
+#include "SecAttr.h"
 
 namespace Arc {
 
 /// Contains authencity information, authorization tokens and decisions.
-/** Currently this class only supports string keys and string values and is 
-  to MessageAttributes. So far it's separation from MessageAttributes is 
-  purely for convenience reasons. */
-class MessageAuth: public MessageAttributes {
+/** This class only supports string keys and SecAttr values. */
+class MessageAuth {
+  private:
+    std::map<std::string,SecAttr*> attrs_;
   public:
-    MessageAuth(void) { };
-    ~MessageAuth(void) { };
+    MessageAuth(void);
+    ~MessageAuth(void);
+    void set(const std::string& key, SecAttr* value);
+    void remove(const std::string& key);
+    SecAttr* get(const std::string& key);
 };
 
-} // namespace Arc
+}
 
-#endif /* __ARC_MESSAGEAUTH_H__ */
+#endif
 
