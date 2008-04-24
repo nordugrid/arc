8404e1b49cd7552ac1f3e3da46b1548fa2f8480c
Szalai Ferenc <szferi@niif.hu>
2008-04-24 06:46:53 +0000

    collects win32 hacks

---
+++ include/arc/win32.h
@@ -0,0 +1,1 @@
+#include "../../src/hed/libs/common/win32.h"
---
+++ src/hed/libs/common/win32.h
@@ -0,0 +1,16 @@
+#ifndef __ARC_WIN32_H__
+#define __ARC_WIN32_H_
+#define NOGDI
+
+#include <objbase.h>
+#include <io.h>
+
+#define sleep(x) Sleep((x)*1000)
+#define mkdir(x,y) mkdir((x))
+// no windows functions
+#define chown(x,y,z) (0)
+#define	lchown(x,y,z) (0) 
+#define fchown(x,y,z) (0)
+#define chmod(x,y) (0)
+#define symlink(x,y) (0)
+#endif
