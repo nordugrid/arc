4ca62940e965cd87e36babba5ed917da099472e0
Aleksandr Konstantinov <akonstantinov@yahoo.com>
2008-04-27 22:41:00 +0000

    c&p error fix - const -> static

--- src/hed/libs/common/ArcLocation.h
+++ src/hed/libs/common/ArcLocation.h
@@ -2,13 +2,25 @@
 #define __ARC_ARCLOCATION_H__
 
 #include <string>
+#include <list>
 
 namespace Arc {
 
+  /// Determines ARC installation location
   class ArcLocation {
    public:
+    /// Initializes location information
+    /** Main source is value of variable ARC_LOCATION,
+      otherwise path to executable provided in @path is used.
+      If nothing works then warning message is sent to logger 
+      and initial installation prefix is used. */
     static void Init(std::string path);
+    /// Returns ARC installation location
     static const std::string& Get();
+    /// Returns ARC plugins directory location
+    /** Main source is value of variable ARC_PLUGIN_PATH,
+      otherwise path is derived from installation location. */
+    static std::list<std::string> GetPlugins();
    private:
     static std::string location;
   };
