dc383c846c648de57ed02b0c12f04a61a73b1b38
Szalai Ferenc <szferi@niif.hu>
2008-04-28 07:42:24 +0000

    update test app to test Run class in multithread env

--- src/hed/libs/common/test_run.cpp
+++ src/hed/libs/common/test_run.cpp
@@ -1,27 +1,34 @@
 #include "Logger.h"
 #include "Run.h"
+#include "Thread.h"
 #include <string>
 #include <iostream>
+#if WIN32
+#incude <arc/win32.h>
+#endif
 
-int main(void)
+Arc::Run *executer = NULL;
+
+void start_thread(void *arg) 
 {
+    std::cout << "Start" << std::endl;
     try {
         std::string std_in;   
         std::string std_out;
         std::string std_err;
 
-        Arc::Run executer("/bin/sleep 100");
+        executer = new Arc::Run("/bin/sleep 60");
     
-        executer.AssignStdin(std_in);
-        executer.AssignStdout(std_out);
-        executer.AssignStderr(std_err);
+        executer->AssignStdin(std_in);
+        executer->AssignStdout(std_out);
+        executer->AssignStderr(std_err);
 
-        if ( !executer.Start() ) {
+        if (!executer->Start()) {
             std::cout << "Failed to start" << std::endl;
-            return -1;
+            return;
         };
-        // if ( executer.Wait(60) ) {
-        if ( executer.Wait() ) {
+        if (executer->Wait()) {
+            std::cout << "End of run" << std::endl;
             std::cout << std_out << std::endl;
         } else {
             std::cout << "Timeout" << std::endl;
@@ -31,5 +38,23 @@ int main(void)
     } catch (Glib::SpawnError &e) {
         std::cout << e.what() << std::endl;
     }
+    std::cout << "end of start_thread" << std::endl;
+}
+
+void stop_thread(void *arg)
+{
+    sleep(10);
+std::cout << "Kill" << std::endl;
+    executer->Kill(1);
+std::cout << "Killed" << std::endl;
+}
+
+int main(void)
+{
+std::cout << "main start" << std::endl;
+    Arc::CreateThreadFunction(&start_thread, NULL);
+    Arc::CreateThreadFunction(&stop_thread, NULL);
+    sleep(100000);
+std::cout << "main end" << std::endl;
     return 0;
 }
