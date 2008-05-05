38a00028664435eb93b75910331c15a08d9b520e
Szalai Ferenc <szferi@niif.hu>
2008-05-05 05:27:02 +0000

    placeholder for win32 specific common codes

---
+++ src/hed/libs/common/win32.cpp
@@ -0,0 +1,28 @@
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include "win32.h"
+
+std::string GetOsErrorMessage(void)
+{
+    std::string rv;
+    LPVOID lpMsgBuf;
+    if (FormatMessage(
+        FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
+        NULL,
+        GetLastError(),
+        MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), // Default language
+        (LPTSTR) &lpMsgBuf,
+        0,
+        NULL ))
+    {
+        rv.assign(reinterpret_cast<const char*>(lpMsgBuf));
+    }
+    else
+    {
+        rv.assign("FormatMessage API failed");
+    }
+    LocalFree(lpMsgBuf);
+    return rv;
+}
