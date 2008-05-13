74e0c32e3e09f30fabe0eea37d479f0075b0528b
Nagy Zsombor <zsombor@niif.hu>
2008-05-13 18:29:40 +0000

    fix

--- src/services/storage/cli/manager_client.py
+++ src/services/storage/cli/manager_client.py
@@ -73,6 +73,8 @@ else:
             f = file(filename,'rb')
             checksum = create_checksum(f, 'md5')
             LN = args[1]
+            if LN.endswith('/'):
+                LN = LN + filename.split('/')[-1]
             metadata = {('states', 'size') : size, ('states', 'checksum') : checksum,
                     ('states', 'checksumType') : 'md5', ('states', 'neededReplicas') : 2}
             request = {'0': (LN, metadata, ['byteio'])}
