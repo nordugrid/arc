34e25c7949d7802e45aceb44853a5cb8bd6926ed
Weizhong Qiang <Qiang@mycompany.com>
2008-05-14 20:22:41 +0000

    Minor changes for policy delegation test

--- src/hed/libs/policy-delegation/test.cpp
+++ src/hed/libs/policy-delegation/test.cpp
@@ -46,16 +46,17 @@ int main(void) {
   std::string policy;
   policy_nd.GetXML(policy);
 
+
+  /************************************/
+  //Request side
   Arc::Time t;
   std::string req_string;
-  std::string out_string;
-
-  //Request side
   ArcLib::Credential request(t, Arc::Period(24*3600));//, 2048);// "rfc", "impersonation", "");
   request.GenerateRequest(req_string);
   std::cout<<"Certificate request: "<<req_string<<std::endl;
 
   //Signing side
+  std::string out_string;
   ArcLib::Credential proxy;
   ArcLib::Credential signer(cert, key, "", cafile);
   proxy.InquireRequest(req_string);
@@ -63,9 +64,60 @@ int main(void) {
   std::string oid("1.3.6.1.5.5.7.1.21");
   std::string sn("arcpolicy");
   proxy.AddCertExtObj(sn, oid);
-  proxy.AddExtension("arcpolicy", policy);
+  proxy.AddExtension("arcpolicy", policy, true);
+  signer.SignRequest(&proxy, out_string); //The signer will send the signed certificate to request side
 
-  signer.SignRequest(&proxy, out_string);
+  //Back to request side, compose the signed proxy certificate, local private key,
+  //and signing certificate into one file.
+  std::string private_key, signing_cert, signing_certchain;
+  request.OutputPrivatekey(private_key);
+  signer.OutputCertificate(signing_cert);
+  signer.OutputCertificateChain(signing_certchain);
+  out_string.append(private_key);
+  out_string.append(signing_cert);
+  out_string.append(signing_certchain);
   std::cout<<"Signed proxy certificate: " <<out_string<<std::endl;
+
+  //Output into a file
+  std::string proxy_file("proxy.pem");
+  std::ofstream out_f(proxy_file.c_str());
+  out_f.write(out_string.c_str(), out_string.size());
+  out_f.close();
+
+  /*************************************/
+  //Request side
+  Arc::Time t1;
+  std::string req_string1;
+  ArcLib::Credential request1(t1, Arc::Period(24*3600));//, 2048);// "rfc", "impersonation", "");
+  request1.GenerateRequest(req_string1);
+  std::cout<<"Certificate request: "<<req_string1<<std::endl;
+
+  //Signing side
+  std::string out_string1;
+  ArcLib::Credential proxy1;
+  ArcLib::Credential signer1(proxy_file, "", "", cafile); //use the existing proxy as the signer
+  proxy1.InquireRequest(req_string1);
+  std::string oid1("1.3.6.1.5.5.7.1.21");
+  std::string sn1("arcpolicy");
+  proxy.AddCertExtObj(sn1, oid1);
+  signer1.SignRequest(&proxy1, out_string1); //The signer will send the signed certificate to request side
+
+  //Back to request side, compose the signed proxy certificate, local private key,
+  //and signing certificate into one file.
+  std::string private_key1, signing_cert1, signing_certchain1;
+  request1.OutputPrivatekey(private_key1);
+  signer1.OutputCertificate(signing_cert1);
+  signer1.OutputCertificateChain(signing_certchain1);
+  out_string1.append(private_key1);
+  out_string1.append(signing_cert1);
+  out_string1.append(signing_certchain1);
+  std::cout<<"Signed proxy certificate: " <<out_string1<<std::endl;
+
+  //Output into a file
+  std::string proxy_file1("proxy1.pem");
+  std::ofstream out_f1(proxy_file1.c_str());
+  out_f1.write(out_string1.c_str(), out_string1.size());
+  out_f1.close();
+
 }
 
