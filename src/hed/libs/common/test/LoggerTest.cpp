bb366ca08e5c304039089f18e247b82b55105899
Mattias Ellert <mattias.ellert@physics.uu.se>
2008-05-20 12:57:07 +0000

    Automated code style fixes

--- src/hed/libs/common/test/LoggerTest.cpp
+++ src/hed/libs/common/test/LoggerTest.cpp
@@ -1,69 +1,70 @@
+#include <sstream>
+
 #include <cppunit/extensions/HelperMacros.h>
 
 #include <arc/Logger.h>
 
-#include <sstream>
+class LoggerTest
+  : public CppUnit::TestFixture {
 
-class LoggerTest : public CppUnit::TestFixture {
+  CPPUNIT_TEST_SUITE(LoggerTest);
+  CPPUNIT_TEST(TestLoggerINFO);
+  CPPUNIT_TEST(TestLoggerDEBUG);
+  CPPUNIT_TEST_SUITE_END();
 
-	CPPUNIT_TEST_SUITE( LoggerTest );
-	CPPUNIT_TEST( TestLoggerINFO );
-	CPPUNIT_TEST( TestLoggerDEBUG );
-	CPPUNIT_TEST_SUITE_END();
+public:
+  void setUp();
+  void tearDown();
 
-	public:
-		void setUp();
-		void tearDown();
+  void TestLoggerINFO();
+  void TestLoggerDEBUG();
 
-		void TestLoggerINFO();
-		void TestLoggerDEBUG();
-
-	private:
-		std::stringstream stream;
-		Arc::LogStream *output;
-		Arc::Logger *logger;
+private:
+  std::stringstream stream;
+  Arc::LogStream *output;
+  Arc::Logger *logger;
 };
 
 
 void LoggerTest::setUp() {
-	output = new Arc::LogStream(stream);
-	Arc::Logger::getRootLogger().addDestination(*output);
-	logger = new Arc::Logger(Arc::Logger::getRootLogger(), "TestLogger", Arc::INFO);
+  output = new Arc::LogStream(stream);
+  Arc::Logger::getRootLogger().addDestination(*output);
+  logger = new Arc::Logger(Arc::Logger::getRootLogger(), "TestLogger", Arc::INFO);
 }
 
 void LoggerTest::tearDown() {
-	// delete output;
+  // delete output;
 }
 
 void LoggerTest::TestLoggerINFO() {
-	std::string res;
-	logger->msg(Arc::DEBUG, "This DEBUG message should not be seen");
-	res = stream.str();
-	CPPUNIT_ASSERT( res.empty() );
+  std::string res;
+  logger->msg(Arc::DEBUG, "This DEBUG message should not be seen");
+  res = stream.str();
+  CPPUNIT_ASSERT(res.empty());
 
-	logger->msg(Arc::INFO, "This INFO message should be seen");
-	res = stream.str();
-	res = res.substr(res.rfind(']') + 2);
-	CPPUNIT_ASSERT_EQUAL( res, std::string("This INFO message should be seen\n") );
-	stream.str("");
+  logger->msg(Arc::INFO, "This INFO message should be seen");
+  res = stream.str();
+  res = res.substr(res.rfind(']') + 2);
+  CPPUNIT_ASSERT_EQUAL(res, std::string("This INFO message should be seen\n"));
+  stream.str("");
 }
 
 
 void LoggerTest::TestLoggerDEBUG() {
-	std::string res;
-	logger->setThreshold(Arc::DEBUG);
-	logger->msg(Arc::DEBUG, "This DEBUG message should now be seen");
-	res = stream.str();
-	res = res.substr(res.rfind(']') + 2);
-	CPPUNIT_ASSERT_EQUAL( res, std::string("This DEBUG message should now be seen\n") );
-	stream.str("");
+  std::string res;
+  logger->setThreshold(Arc::DEBUG);
+  logger->msg(Arc::DEBUG, "This DEBUG message should now be seen");
+  res = stream.str();
+  res = res.substr(res.rfind(']') + 2);
+  CPPUNIT_ASSERT_EQUAL(res, std::string("This DEBUG message should now be seen\n"));
+  stream.str("");
 
-	logger->msg(Arc::INFO, "This INFO message should also be seen");
-	res = stream.str();
-	res = res.substr(res.rfind(']') + 2);
-	CPPUNIT_ASSERT_EQUAL( res, std::string("This INFO message should also be seen\n") );
-	stream.str("");
+  logger->msg(Arc::INFO, "This INFO message should also be seen");
+  res = stream.str();
+  res = res.substr(res.rfind(']') + 2);
+  CPPUNIT_ASSERT_EQUAL(res, std::string("This INFO message should also be seen\n"));
+  stream.str("");
 }
 
 
-CPPUNIT_TEST_SUITE_REGISTRATION( LoggerTest );
+CPPUNIT_TEST_SUITE_REGISTRATION(LoggerTest);
