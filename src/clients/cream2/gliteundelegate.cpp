f5e8570ae8da2e394bba7a5d2d6c2f1270192727
Ivan Marton <martoni@niif.hu>
2008-05-22 12:50:09 +0000

    gliteundelegate.cpp added

---
+++ src/clients/cream2/gliteundelegate.cpp
@@ -0,0 +1,72 @@
+// gliteundelegate.cpp
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <stdexcept>
+#include <arc/misc/ClientTool.h>
+#include <cstdlib>
+#include "cream_client.h"
+
+class GLiteSubTool: public Arc::ClientTool {
+    public:
+        std::string config_path;
+        GLiteSubTool(int argc,char* argv[]):Arc::ClientTool("gliteundelegate") {
+            ProcessOptions(argc,argv,"c:");
+        };
+    virtual void PrintHelp(void) {
+        std::cout<<"gliteundelegate delegation_id service_url"<<std::endl;
+        exit(1);
+    };
+    virtual bool ProcessOption(char option,char* option_arg) {
+        try {
+        switch(option) {
+            case 'c': config_path=option_arg; break;
+            default: {
+                std::cerr<<"Error processing option: "<<(char)option<<std::endl;
+                PrintHelp();
+                return false;
+            };
+        };} catch (std::exception& ex) {
+          std::cout << "There was an error(" << option << "): " << std::endl;
+          std::cout << ex.what() << std::endl;
+        }
+        return true;
+    };
+};
+
+int main(int argc, char* argv[]){
+    // Processing the arguments
+    GLiteSubTool tool(argc,argv);
+    if(!tool) return EXIT_FAILURE;
+    
+    try{
+        if ((argc-tool.FirstOption())!=2) {
+            tool.PrintHelp();
+            throw std::invalid_argument("Wrong number of arguments!");
+        }
+    
+        //Create the CREAMClient object
+        Arc::URL url(argv[tool.FirstOption()+1]);
+        if(!url) throw(std::invalid_argument(std::string("Can't parse specified service URL")));
+        Arc::MCCConfig cfg;
+        if(tool.config_path != "") cfg.GetOverlay(tool.config_path);
+        Arc::Cream::CREAMClient gLiteClient(url,cfg);
+        
+        // Get the delegationid from the command line arguments
+        std::string delegation_id = argv[tool.FirstOption()];
+        
+        // Create delegation
+        gLiteClient.destroyDelegation(delegation_id);
+        
+        // Notify user about the success (Just if succeeded, otherwise exception was threw)
+        std::cout << "Delegation has been destroyed." << std::endl;
+        return EXIT_SUCCESS;
+    
+    } catch (std::exception& e){
+        
+        // Notify the user about the failure
+        std::cerr << "ERROR: " << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+}
