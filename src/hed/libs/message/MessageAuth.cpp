f206337521eeee9dce1739b30f137f3d357ed45a
Weizhong Qiang <Qiang@mycompany.com>
2008-06-12 23:21:36 +0000

    Make the SecAttr Export more meaningful

--- src/hed/libs/message/MessageAuth.cpp
+++ src/hed/libs/message/MessageAuth.cpp
@@ -45,7 +45,11 @@ static void add_new_elements(XMLNode item,XMLNode element) {
   for(;(bool)element;++element) item.NewChild(element);
 }
 
-// All Subject, Resource, Action elements do to same RequestItem.
+// Subject, Resource, Action, Context  RequestItem in each component
+// goes into one seperated output <RequestItem>.
+// The Subject from all of the SecAttr goes into every output <RequestItem>,
+// because there is only one request entity (with a few SubjectAttribute) 
+//for the incoming  message chain
 bool MessageAuth::Export(SecAttr::Format format,XMLNode &val) const {
   // Currently only ARCAuth is supported
   if(format != SecAttr::ARCAuth) return false;
@@ -54,20 +58,42 @@ bool MessageAuth::Export(SecAttr::Format format,XMLNode &val) const {
   XMLNode newreq = val;
   newreq.Namespaces(ns);
   newreq.Name("ra:Request");
-  XMLNode newitem = newreq["ra:RequestItem"];
-  if(!newitem) newitem=newreq.NewChild("ra:RequestItem");
+
+  XMLNode subject(ns,"ra:RequestItem");
+
   std::map<std::string,SecAttr*>::const_iterator attr = attrs_.begin();
   for(;attr != attrs_.end();++attr) {
     XMLNode r(ns,"");
     if(!(attr->second)) return false;
     if(!(attr->second->Export(format,r))) return false;
-    XMLNode item;
-    for(item=r["RequestItem"];(bool)item;++item) {
-      add_new_elements(newitem,item["Subject"]);
-      add_new_elements(newitem,item["Resource"]);
-      add_new_elements(newitem,item["Action"]);
-      add_new_elements(newitem,item["Context"]);
+    XMLNode item, newitem;
+    item = r["RequestItem"];
+    //If there ["Resource"] or ["Action"] inside input ["RequestItem"], we generate a new 
+    //["RequstItem"] for output. This exclues the SecAttr from TLS to be a seperated 
+    //output ["RequestItem"], becausr TLS has not elements except ["Subject"], which make
+    //it difficult to define policy.
+    if(((bool)item) && ( ((bool)(item["Resource"])) || ((bool)(item["Action"])) ))
+        newitem=newreq.NewChild("ra:RequestItem");
+    for(;(bool)item;++item) {
+      add_new_elements(subject,item["Subject"]);
+      if( ((bool)(item["Resource"])) || ((bool)(item["Action"])) ) {
+        add_new_elements(newitem,item["Resource"]);
+        add_new_elements(newitem,item["Action"]);
+        add_new_elements(newitem,item["Context"]);
+      };
     };
+  };
+  //if finally, not output ["RequestItem"], we use the ["Subject"].
+  //This is the case for authorization in MCCTLS which has not elements
+  //except ["Subject"].
+  XMLNode item = newreq["ra:RequestItem"];
+  if(!item) {
+    XMLNode newitem = newreq.NewChild("ra:RequestItem");
+    add_new_elements(newitem, subject["Subject"]);
+  }
+
+  for(;(bool)item;++item) {
+    add_new_elements(item, subject["Subject"]);
   };
   return true;
 }
