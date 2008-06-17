f78d82105375287a0f628c369ff897b89a0fff00
Szalai Ferenc <szferi@niif.hu>
2008-06-17 09:34:13 +0000

    make info collector GLUE2 compatiblie

--- src/services/paul/info.cpp
+++ src/services/paul/info.cpp
@@ -2,109 +2,111 @@
 #include <config.h>
 #endif
 
+#include <arc/DateTime.h> 
+#include <arc/GUID.h>
 #include "paul.h"
+#include "sysinfo.h"
 
 namespace Paul
 {
 
 bool PaulService::information_collector(Arc::XMLNode &doc)
 {
-    // refresh dinamic system information
+    // refresh dynamic system information
     sysinfo.refresh();
-#if 0
+    // common variables
+    std::string created = TimeStamp(Arc::UTCTime);
+    std::string validity = Arc::tostring(configurator.getPeriod()*2);
+    std::string id = "urn:nordugrid:paul:" + Arc::UUID(); 
+    std::string ee_id = id + ":executionenvironment:0";
+    // Set Domain
+    doc.NewAttribute("CreationTime") = created;
+    doc.NewAttribute("Validity") = validity;
+    // Set AdminDomain
     Arc::XMLNode ad = doc.NewChild("AdminDomain");
-    ad.NewChild("ID") = "http://localhost/paul"; // XXX should be better
-    ad.NewChild("Name") = "DesktopPC";
-    ad.NewChild("Distriuted") = "no";
-    ad.NewChild("Owner") = "someone"; // get user name required
+    ad.NewAttribute("CreationTime") = created;
+    ad.NewAttribute("Validity") = validity;
+    ad.NewAttribute("BaseType") = "Domain";
+    ad.NewChild("ID") = id;
+    ad.NewChild("Distributed") = "False";
+    // Set Computing Service
+    //      Service Part
     Arc::XMLNode services = ad.NewChild("Services");
     Arc::XMLNode cs = services.NewChild("ComputingService");
-    cs.NewAttribute("type") = "Service";
-    cs.NewChild("ID") = "http://localhost/paul";
+    cs.NewAttribute("CreationTime") = created;
+    cs.NewAttribute("Validity") = validity;
+    cs.NewAttribute("BaseType") = "Service";
+    cs.NewChild("ID") = id + ":computingservice:0";
+    cs.NewChild("Name") = "Arc Paul Service";
     cs.NewChild("Type") = "org.nordugrid.paul";
     cs.NewChild("QualityLevel") = "production";
-    cs.NewChild("Complexity") = "endpoint=1,share=1,resource=1";
+    // No endpont -> pull modell
+    //      Computing Service part
     cs.NewChild("TotalJobs") = Arc::tostring(jobq.getTotalJobs());
     cs.NewChild("RunningJobs") = Arc::tostring(jobq.getRunningJobs());
     cs.NewChild("WaitingJobs") = Arc::tostring(jobq.getWaitingJobs());
-    cs.NewChild("StageingJobs") = "0"; // XXX get info from jobqueue
-    cs.NewChild("PreLRMSWaitingJobs") = "0"; // XXX get info from jobqueue
-    Arc::XMLNode endpoint = cs.NewChild("ComputingEndpoint");
-    endpoint.NewAttribute("type") = "Endpoint";
-    endpoint.NewChild("ID") = "0"; 
-    // endpoint.NewChild("URL") = "none"; // Not exsits
-    endpoint.NewChild("Specification") = "iBES";
-    endpoint.NewChild("Implementator") = "Nordugrid";
-    endpoint.NewChild("ImplementationName") = "paul";
-    endpoint.NewChild("ImplementationVersion") = "0.1";
-    endpoint.NewChild("QualityLevel") = "production";
-    // endpoint.NewChild("WSDL") = "http://"; // not required this side
-    endpoint.NewChild("HealthState") = "OK";
-    // endpoint.NewChild("ServingState") = "open"; // meaingless
-    // endpoint.NewChild("StartTime") = "now";
-    endpoint.NewChild("StageingCapabilty") = "yes";
-    Arc::XMLNode policy = endpoint.NewChild("Policy");
-    policy.NewChild("ID") = "0";
-    policy.NewChild("Scheme") = "BASIC";
-    policy.NewChild("Rule") = "?";
-    policy.NewChild("TrustedCA") = "?"; // XXX get DNs from config ca.pem or dir
-#if 0
-    Arc::XMLNode activities = endpoint.NewChild("Activities");
-    JobList::iterator it;
-    for (it = job_list.begin(); it != job_list.end(); it++) {
-        Job j = (*it);
-        Arc::XMLNode jn = activities.NewChild("ComputingActivity");
-        jn.NewAttribute("type") = "Computing";
-        jn.NewChild("ID") = j.getID();
-        jn.NewChild("Type") = "Computing";
-        jn.NewChild("Name") = j.getName(); // from jsdl
-        jn.NewChild("State") = (std::string)j.getStatus(); // from metadata
+    cs.NewChild("StageingJobs") = Arc::tostring(jobq.getStageingJobs());
+    Arc::XMLNode sh = cs.NewChild("ComputingShares").NewChild("ComputingShare");
+    sh.NewAttribute("CreationTime") = created;
+    sh.NewAttribute("Validity") = validity;
+    sh.NewAttribute("BaseType") = "Share";
+    //      Share part
+    sh.NewChild("LocalID") = "computingshare:0";
+    //      Computing Share part
+    sh.NewChild("MaxTotalJobs") = Arc::tostring(sysinfo.getLogicalCPUs());
+    sh.NewChild("MaxMemory") = Arc::tostring((int)sysinfo.getMainMemorySize()); // in MB
+    sh.NewChild("MaxDiskSpace") = Arc::tostring((int)(SysInfo::diskFree(configurator.getJobRoot())/1024)); // in GB
+    sh.NewChild("ServingState") = "production";
+    sh.NewChild("TotalJobs") = Arc::tostring(jobq.getTotalJobs());
+    sh.NewChild("RunningJobs") = Arc::tostring(jobq.getRunningJobs());
+    sh.NewChild("WaitingJobs") = Arc::tostring(jobq.getWaitingJobs());
+    sh.NewChild("StageingJobs") = Arc::tostring(jobq.getStageingJobs());
+    sh.NewChild("Associations").NewChild("ExecutionEnvironmentLocalID") = ee_id;
+    //      Manager part
+    Arc::XMLNode mg = cs.NewChild("ComputingManager");
+    mg.NewAttribute("CreationTime") = created;
+    mg.NewAttribute("Validity") = validity;
+    mg.NewAttribute("BaseType") = "Manager";
+    mg.NewChild("ID") = id + ":computingmanager:0";
+    mg.NewChild("Name") = "Paul";
+    //      Computing Manager part
+    mg.NewChild("Type") = "fork";
+    mg.NewChild("Version") = "1.0";
+    mg.NewChild("TotalPhysicalCPUs") = Arc::tostring(sysinfo.getPhysicalCPUs());
+    mg.NewChild("TotalLogicalCPUs") = Arc::tostring(sysinfo.getLogicalCPUs());
+    mg.NewChild("Homogenety") = "True";
+    mg.NewChild("WorkingAreaTotal") = Arc::tostring((int)(SysInfo::diskTotal(configurator.getJobRoot())/1024)); // in GB
+    mg.NewChild("WorkingAreaFree") = Arc::tostring((int)(SysInfo::diskFree(configurator.getJobRoot())/1024)); // in GB
+    // Application environment
+
+    Arc::XMLNode ae = mg.NewChild("ApplicationEnvironments");
+    Arc::XMLNode appenvs = configurator.getApplicationEnvironments();
+    Arc::XMLNode env;
+
+    for (int i = 0; (env = appenvs["ApplicationEnvironment"]) != false; i++) {
+        env.NewAttribute("CreationTime") = created;
+        env.NewAttribute("Validity") = validity;
+        env.NewChild("LocalID") = ("applicationenvironment:" + Arc::tostring(i));
+        env.NewChild("Associations").NewChild("ExecutionEnvironmentLocalID") = ee_id;
+        ae.NewChild(env);
     }
-#endif
-    Arc::XMLNode r = cs.NewChild("ComputingResource");
-    r.NewAttribute("type") = "Resource";
-    r.NewChild("ID") = "0";
-    r.NewChild("Name") = "DesktopPC";
-    r.NewChild("LRMSType") = "Fork/Internal";
-    r.NewChild("LRMSVersion") = "0.1";
-    r.NewChild("Total") = "1"; // number of exec env
-    r.NewChild("Used") = "0"; // comes from running job
-    r.NewChild("Homogenity") = "True";
-    r.NewChild("SessionDirTotal") = Arc::tostring(SysInfo::diskTotal(job_root));
-    r.NewChild("SessionDirFree") = Arc::tostring(SysInfo::diskFree(job_root));
-    r.NewChild("SessionDirLifetime") = "10"; // XXX should come from config in sec
-    Arc::XMLNode ee = r.NewChild("ExecutionEnvironment");
-    ee.NewChild("ID") = "0";
-    ee.NewChild("Type") = "RealNode"; // XXX should come form config wheter it is running on VM or not
-    ee.NewChild("TotalInstances") = "1";
-    ee.NewChild("UsedInstances") = "0"; // if I have multicore machine running 1 job than this is 0 or 1 or what
-    ee.NewChild("PhysicalCPUs") = Arc::tostring(sysinfo.getPhysicalCPUs());
-    ee.NewChild("LogicalCPUs") = Arc::tostring(sysinfo.getLogicalCPUs());
-    ee.NewChild("MainMemorySize") = Arc::tostring(sysinfo.getMainMemorySize());
-    ee.NewChild("VirtualMemorySize") = Arc::tostring(sysinfo.getVirtualMemorySize());
-    ee.NewChild("OSName") = sysinfo.getOSName();
-    ee.NewChild("OSRelease") = sysinfo.getOSRelease();
-    ee.NewChild("OSVersion") = sysinfo.getOSVersion();
-    ee.NewChild("PlatformType") = sysinfo.getPlatform();
-    ee.NewChild("ConnectivityIn") = "False";
-    ee.NewChild("ConnectivityOut") = "True";
-    r.NewChild("PhysicalCPUs") = Arc::tostring(sysinfo.getPhysicalCPUs());
-    r.NewChild("LogicalCPUs") = Arc::tostring(sysinfo.getLogicalCPUs());
-    Arc::XMLNode s = cs.NewChild("ComputingShare");
-    s.NewAttribute("type") = "Share";
-    s.NewChild("ID") = "0";
-    s.NewChild("Name") = "AllResources";
-    Arc::XMLNode spolicy = s.NewChild("ComputingSharePolicy");
-    spolicy.NewAttribute("type") = "SharePolicy";
-    Arc::XMLNode sstate = s.NewChild("ComputingShareState");
-    sstate.NewAttribute("type") = "ShareState";
-    sstate.NewChild("TotalJobs") = Arc::tostring(jobq.getTotalJobs());
-    sstate.NewChild("RunningJobs") = Arc::tostring(jobq.getRunningJobs());
-    sstate.NewChild("LocalRunningJobs") = Arc::tostring(jobq.getLocalRunningJobs());
-    sstate.NewChild("WaitingJobs") = Arc::tostring(jobq.getWaitingJobs());
-    sstate.NewChild("State") = "?";
-    s.NewChild(policy); // use the same policy
-#endif
+    
+    //  Resource part
+    Arc::XMLNode exec = mg.NewChild("ExecutionEnvironment");
+    exec.NewAttribute("CreationTime") = created;
+    exec.NewAttribute("Validity") = validity;
+    mg.NewAttribute("BaseType") = "Resource";
+    exec.NewChild("ID") = ee_id;
+    //  ExecutionEnvironment part
+    exec.NewChild("Platform") = sysinfo.getPlatform();
+    exec.NewChild("PhysicalCPUs") = Arc::tostring(sysinfo.getPhysicalCPUs());
+    exec.NewChild("LogicalCPUs") = Arc::tostring(sysinfo.getLogicalCPUs());
+    exec.NewChild("MainMemorySize") = Arc::tostring(sysinfo.getMainMemorySize()); // in MB
+    exec.NewChild("VirtualMemorySize") = Arc::tostring(sysinfo.getVirtualMemorySize()); // in MB
+    exec.NewChild("OSFamily") = sysinfo.getOSFamily();
+    exec.NewChild("OSName") = sysinfo.getOSName();
+    exec.NewChild("OSVersion") = sysinfo.getOSVersion();
+    
     return true;
 }
 
