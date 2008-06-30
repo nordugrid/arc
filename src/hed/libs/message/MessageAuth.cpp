bec0ae0abf1d3796ed488dd5c21bf8ef1806b5ef
Aleksandr Konstantinov <akonstantinov@yahoo.com>
2008-06-30 22:18:54 +0000

    Convering SecAttr frmaework to produce all possible Request permuations in separate RequestItem elements. This behavior shoudl become tunable in a future.

--- src/hed/libs/message/MessageAuth.cpp
+++ src/hed/libs/message/MessageAuth.cpp
@@ -41,34 +41,103 @@ SecAttr* MessageAuth::get(const std::string& key) {
   return attr->second;
 }
 
-static void add_new_elements(XMLNode item,XMLNode element) {
-  for(;(bool)element;++element) item.NewChild(element);
-}
+class _XMLPair {
+ public:
+  XMLNode element;
+  XMLNode context;
+  _XMLPair(XMLNode e,XMLNode c):element(e),context(c) { };
+};
 
-// All Subject, Resource, Action elements do to same RequestItem.
+// All permutations of Subject, Resource, Action elements are generated.
+// Each is put into separate RequestItem. Condition is put into 
+// RequestItem if it was present in one from which Subject, Resource
+// or Action are taken.
 bool MessageAuth::Export(SecAttr::Format format,XMLNode &val) const {
   // Currently only ARCAuth is supported
   if(format != SecAttr::ARCAuth) return false;
+  // Making XML document top level Request element
   NS ns;
   ns["ra"]="http://www.nordugrid.org/schemas/request-arc";
   XMLNode newreq = val;
   newreq.Namespaces(ns);
   newreq.Name("ra:Request");
-  XMLNode newitem = newreq["ra:RequestItem"];
-  if(!newitem) newitem=newreq.NewChild("ra:RequestItem");
-  std::map<std::string,SecAttr*>::const_iterator attr = attrs_.begin();
+  XMLNodeContainer xmls;
+  std::list<_XMLPair> subjects;
+  std::list<_XMLPair> resources;
+  std::list<_XMLPair> actions;
+  // Collecting elements from previously collected request
+  for(XMLNode item = newreq["RequestItem"];(bool)item;++item) {
+    for(XMLNode subject = item["Subject"];(bool)subject;++subject) {
+      subjects.push_back(_XMLPair(subject,item["Context"]));
+    };
+    for(XMLNode resource = item["Resource"];(bool)resource;++resource) {
+      resources.push_back(_XMLPair(resource,item["Context"]));
+    };
+    for(XMLNode action = item["Action"];(bool)action;++action) {
+      actions.push_back(_XMLPair(action,item["Context"]));
+    };
+  };
+  int subjects_new = subjects.size();
+  int resources_new = resources.size();
+  int actions_new = actions.size();
+  // Getting XMLs from all SecAttr
+  std::map<std::string,SecAttr*>::const_iterator attr = attrs_.begin(); 
   for(;attr != attrs_.end();++attr) {
-    XMLNode r(ns,"");
+    xmls.AddNew(XMLNode(ns,""));
+    XMLNode r = xmls[xmls.Size()-1];
     if(!(attr->second)) return false;
     if(!(attr->second->Export(format,r))) return false;
-      XMLNode item;
-      for(item=r["RequestItem"];(bool)item;++item) {
-        add_new_elements(newitem,item["Subject"]);
-        add_new_elements(newitem,item["Resource"]);
-        add_new_elements(newitem,item["Action"]);
-        add_new_elements(newitem,item["Context"]);
+    for(XMLNode item = r["RequestItem"];(bool)item;++item) {
+      for(XMLNode subject = item["Subject"];(bool)subject;++subject) {
+        subjects.push_back(_XMLPair(subject,item["Context"]));
+      };
+      for(XMLNode resource = item["Resource"];(bool)resource;++resource) {
+        resources.push_back(_XMLPair(resource,item["Context"]));
+      };
+      for(XMLNode action = item["Action"];(bool)action;++action) {
+        actions.push_back(_XMLPair(action,item["Context"]));
       };
     };
+  };
+  std::list<_XMLPair>::iterator subject = subjects.begin();
+  for(int subject_n = 0;;++subject_n) {
+    std::list<_XMLPair>::iterator action = actions.begin();
+    for(int action_n = 0;;++action_n) {
+      std::list<_XMLPair>::iterator resource = resources.begin();
+      for(int resource_n = 0;;++resource_n) {
+        if((subject_n < subjects_new) && 
+           (action_n < actions_new) &&
+           (resource_n < resources_new)) {
+          continue; // This compbination is already in request
+        };
+        XMLNode newitem = newreq.NewChild("ra:RequestItem");
+        if(subject != subjects.end()) {
+          newitem.NewChild(subject->element);
+          for(XMLNode context = subject->context;(bool)context;++context) {
+            newitem.NewChild(context);
+          };
+        };
+        if(action != actions.end()) {
+          newitem.NewChild(action->element);
+          for(XMLNode context = action->context;(bool)context;++context) {
+            newitem.NewChild(context);
+          };
+        };
+        if(resource != resources.end()) {
+          newitem.NewChild(resource->element);
+          for(XMLNode context = resource->context;(bool)context;++context) {
+            newitem.NewChild(context);
+          };
+        };
+        if(resources.size()) ++resource;
+        if(resource == resources.end()) break;
+      };
+      if(actions.size()) ++action;
+      if(action == actions.end()) break;
+    };
+    if(subjects.size()) ++subject;
+    if(subject == subjects.end()) break;
+  };
   return true;
 }
 
