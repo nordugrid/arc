3cf99452eed1a3a27e48a32e740e9fb3b643f841
Weizhong Qiang <Qiang@mycompany.com>
2008-07-03 00:50:50 +0000

    Change export processing in MessageAuth

--- src/hed/libs/message/MessageAuth.cpp
+++ src/hed/libs/message/MessageAuth.cpp
@@ -41,6 +41,7 @@ SecAttr* MessageAuth::get(const std::string& key) {
   return attr->second;
 }
 
+#if 0
 class _XMLPair {
  public:
   XMLNode element;
@@ -80,6 +81,7 @@ bool MessageAuth::Export(SecAttr::Format format,XMLNode &val) const {
   int subjects_new = subjects.size();
   int resources_new = resources.size();
   int actions_new = actions.size();
+
   // Getting XMLs from all SecAttr
   std::map<std::string,SecAttr*>::const_iterator attr = attrs_.begin(); 
   for(;attr != attrs_.end();++attr) {
@@ -87,6 +89,11 @@ bool MessageAuth::Export(SecAttr::Format format,XMLNode &val) const {
     XMLNode r = xmls[xmls.Size()-1];
     if(!(attr->second)) return false;
     if(!(attr->second->Export(format,r))) return false;
+
+    std::string str;
+    r.GetXML(str);
+    std::cout<<"SecAttr: ++++ Name: "<<attr->first<<"XML: "<<str<<std::endl;
+
     for(XMLNode item = r["RequestItem"];(bool)item;++item) {
       for(XMLNode subject = item["Subject"];(bool)subject;++subject) {
         subjects.push_back(_XMLPair(subject,item["Context"]));
@@ -99,6 +106,7 @@ bool MessageAuth::Export(SecAttr::Format format,XMLNode &val) const {
       };
     };
   };
+
   std::list<_XMLPair>::iterator subject = subjects.begin();
   for(int subject_n = 0;;++subject_n) {
     std::list<_XMLPair>::iterator action = actions.begin();
@@ -138,6 +146,99 @@ bool MessageAuth::Export(SecAttr::Format format,XMLNode &val) const {
     if(subjects.size()) ++subject;
     if(subject == subjects.end()) break;
   };
+  return true;
+}
+#endif
+
+static void add_new_elements(XMLNode item,XMLNode element) {
+  for(;(bool)element;++element) item.NewChild(element);
+}
+
+// Subject, Resource, Action, Context  RequestItem in each component
+// goes into one seperated output <RequestItem>.
+// The Subject from all of the SecAttr goes into every output <RequestItem>,
+// because there is only one request entity (with a few SubjectAttribute)
+//for the incoming  message chain
+bool MessageAuth::Export(SecAttr::Format format,XMLNode &val) const {
+  // Currently only ARCAuth is supported
+  if(format != SecAttr::ARCAuth) return false;
+  NS ns;
+  ns["ra"]="http://www.nordugrid.org/schemas/request-arc";
+  XMLNode newreq = val;
+  newreq.Namespaces(ns);
+  newreq.Name("ra:Request");
+
+  //A specific RequestItem for collecting all of the <Request> from 
+  //differenc SecAttr
+  XMLNode subjects(ns,"ra:RequestItem");
+
+  std::map<std::string,SecAttr*>::const_iterator attr = attrs_.begin();
+  for(;attr != attrs_.end();++attr) {
+    XMLNode r(ns,"");
+    if(!(attr->second)) return false;
+    if(!(attr->second->Export(format,r))) return false;
+    XMLNode item, newitem;
+    for(item = r["RequestItem"];(bool)item;++item) {
+      //If there ["Resource"] or ["Action"] inside input ["RequestItem"], we generate a new
+      //["RequstItem"] for output.
+      if(((bool)item) && ( ((bool)(item["Resource"])) || ((bool)(item["Action"])) ))
+        newitem=newreq.NewChild("ra:RequestItem");
+
+      //Collect all of the <Subject/>s. Since in HED each incomming/outgoing message
+      //is supposed to implicitly have only one meaningful entity(Subject), it doesn't hurt
+      //if we put all of the <Subject/>s together and merge(later) them into one <Subject/> (with 
+      //a number of <Attribute/>s from different original <Subject>s).
+      add_new_elements(subjects,item["Subject"]);
+
+      //<Context/> is also collected.
+      add_new_elements(subjects,item["Context"]);
+
+      //We store <Resource/> <Action/> <Context/> into the just generated new 
+      //<RequestItem/> by keeping the original organizing shape.
+      //Notice that we do not put the <Subject> into new <RequestItem> now.
+      if( ((bool)(item["Resource"])) || ((bool)(item["Action"])) ) {
+        add_new_elements(newitem,item["Resource"]);
+        add_new_elements(newitem,item["Action"]);
+        add_new_elements(newitem,item["Context"]);
+      };
+    };
+  };
+
+  //Reset the namespaces to delete the namespaces inside subnode
+  subjects.Namespaces(ns);
+
+  //Merge the <Subject>s into one <Subject>
+  XMLNode new_subject(ns, "ra:Subject");
+  XMLNode sub = subjects["ra:Subject"];
+  for(sub = subjects["Subject"];(bool)sub;++sub) {
+    add_new_elements(new_subject, sub["SubjectAttribute"]);
+  }
+
+  //Merge the <Context>s into one <Context>
+  XMLNode new_context(ns, "ra:Context");
+  for(XMLNode ctx = subjects["ra:Context"];(bool)ctx;++ctx) {
+    add_new_elements(new_context, ctx["ContextAttribute"]);
+  }
+
+  //If finally, there is not any output ["RequestItem"], we use the just generated new <Subject/>.
+  //This is the case: There is MCCTLS SecAttr which has no elements except ["Subject"], and 
+  //there is no other SecAttr exist.
+  //<Context/> is also added
+  XMLNode item = newreq["ra:RequestItem"];
+  if(!item) {
+    XMLNode newitem = newreq.NewChild("ra:RequestItem");
+    add_new_elements(newitem, new_subject);
+    add_new_elements(newitem, new_context);
+  }
+
+  //Put the new <Subject/> into each new <RequestItem/>
+  for(;(bool)item;++item) {
+    add_new_elements(item, new_subject);
+  };
+
+  //Reset the namespaces to delete the namespace inside subnode.
+  newreq.Namespaces(ns);
+
   return true;
 }
 
