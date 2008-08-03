3257bc10711170079ba9e8133c0723b08cc177bc
Aleksandr Konstantinov <akonstantinov@yahoo.com>
2008-08-03 23:05:37 +0000

    Code simplification

--- src/hed/libs/security/ArcPDP/Response.h
+++ src/hed/libs/security/ArcPDP/Response.h
@@ -2,6 +2,7 @@
 #define __ARC_SEC_RESPONSE_H__
 
 #include <list>
+#include <vector>
 #include <arc/XMLNode.h>
 #include <arc/Logger.h>
 #include "EvaluationCtx.h"
@@ -25,17 +26,16 @@ typedef struct{
 class ResponseList {
 public:
   void addItem(ResponseItem* item) {
-    int n = (resps.size());
-    resps.insert(std::pair<int, ResponseItem*>(n, item));
+    resps.push_back(item);
   };
   int size() { return resps.size();};
-  ResponseItem* getItem(int n) { return resps[n]; };
-  ResponseItem* operator[](int n) { return resps[n]; };
+  ResponseItem* getItem(int n) { if((n<0) || (n>=resps.size())) return NULL; return resps[n]; };
+  ResponseItem* operator[](int n) { getItem(n); };
   bool empty() { return resps.empty(); };
   void clear() {
-    std::map<int, ResponseItem*>::iterator it;
+    std::vector<ResponseItem*>::iterator it;
     for(it = resps.begin(); it != resps.end();it = resps.begin()){
-      ResponseItem* item = it->second;
+      ResponseItem* item = *it;
       resps.erase(it);
       if(item) {
         RequestTuple* tpl = item->reqtp;
@@ -48,7 +48,7 @@ public:
     }
   };
 private:
-  std::map<int, ResponseItem*> resps;
+  std::vector<ResponseItem*> resps;
 };
 
 ///Container for the evaluation results
