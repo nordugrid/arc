b0d96f93883b81be7a0757dd6d34c8c1095451a0
Weizhong Qiang <Qiang@mycompany.com>
2008-09-28 16:04:22 +0000

    Test code about getting authentication response from Shibboleth IdP; it will be implemented as some security handler later

--- src/services/saml/test_client_authn.cpp
+++ src/services/saml/test_client_authn.cpp
@@ -34,7 +34,12 @@
 #include <arc/XMLNode.h>
 #include <arc/DateTime.h>
 #include <arc/GUID.h>
+#include <arc/URL.h>
 #include <arc/credential/Credential.h>
+
+#include <arc/message/MCC.h>
+#include <arc/message/PayloadRaw.h>
+#include <arc/client/ClientInterface.h>
 
 #include "../../hed/libs/common/XmlSecUtils.h"
 #include "../../hed/libs/common/XMLSecNode.h"
@@ -69,23 +74,9 @@ int main(void) {
   Arc::LogStream logcerr(std::cerr);
   Arc::Logger::rootLogger.addDestination(logcerr);
 
-  // Create client chain
-  logger.msg(Arc::INFO, "Creating client side chain");
-  Arc::Config client_config("client.xml");
-  if(!client_config) {
-    logger.msg(Arc::ERROR, "Failed to load client configuration");
-    return -1;
-  };
-  Arc::Loader client_loader(&client_config);
-  logger.msg(Arc::INFO, "Client side MCCs are loaded");
-  Arc::MCC* client_entry = client_loader["soap"];
-  if(!client_entry) {
-    logger.msg(Arc::ERROR, "Client chain does not have entry point");
-    return -1;
-  };
-
   //----------------------------------
-  //     Compose AutheRequest
+  //Client-Agent and SP: Compose AuthnRequest
+  //Client-Agent access SP, SP generate AuthnRequest and respond it to Client-Agent
   //----------------------------------
   Arc::NS req_ns;
   req_ns["saml"] = SAML_NAMESPACE;
@@ -93,227 +84,156 @@ int main(void) {
 
   //Compose <samlp:AuthnRequest/>
   Arc::XMLNode authn_request(req_ns, "samlp:AuthnRequest");
-  std::string sp_name1("https://sp.com/SAML"); //TODO
+  //std::string sp_name1("https://sp.testshib.org/shibboleth-sp"); //TODO
+  std::string sp_name1("https://squark.uio.no/shibboleth-sp");
   std::string req_id = Arc::UUID();
   authn_request.NewAttribute("ID") = req_id;
   Arc::Time t1;
   std::string current_time1 = t1.str(Arc::UTCTime);
   authn_request.NewAttribute("IssueInstant") = current_time1;
   authn_request.NewAttribute("Version") = std::string("2.0");
+  //authn_request.NewAttribute("AssertionConsumerServiceURL") = std::string("https://sp.testshib.org/Shibboleth.sso/SAML2/POST");
+  authn_request.NewAttribute("AssertionConsumerServiceURL") = std::string("https://squark.uio.no/Shibboleth.sso/SAML2/POST");
+
+
+  std::string destination("https://squark.uio.no:8443/idp/profile/SAML2/Redirect/SSO");
+  authn_request.NewAttribute("Destination") = destination;
+  authn_request.NewAttribute("ProtocolBinding") = std::string("urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST");
+   
   authn_request.NewChild("saml:Issuer") = sp_name1;
   
   Arc::XMLNode nameid_policy = authn_request.NewChild("samlp:NameIDPolicy");
-  nameid_policy.NewAttribute("Format") = std::string("urn:oasis:names:tc:SAML:2.0:nameid-format:transient");
-  nameid_policy.NewAttribute("SPNameQualifier") = sp_name1;
-  //nameid_policy.NewAttribute("AllowCreate") = std::string("true"); 
+  nameid_policy.NewAttribute("AllowCreate") = std::string("1");
 
-  authn_request.NewAttribute("IsPassive") = std::string("false");
+  //nameid_policy.NewAttribute("Format") = std::string("urn:oasis:names:tc:SAML:2.0:nameid-format:transient");
+  //nameid_policy.NewAttribute("SPNameQualifier") = sp_name1;
+
+  //authn_request.NewAttribute("IsPassive") = std::string("false");
   //authn_request.NewAttribute() =  
 
-  bool must_signed = true; //TODO: get the information from metadata
+  bool must_signed = false; //TODO: get the information from metadata
   
   HttpMethod httpmethod = HTTP_METHOD_REDIRECT; //TODO
 
-  std::string privkey_file = "key.pem";
-  std::string authnRequestUrl;
+  std::string str1;
+  authn_request.GetXML(str1);
+  std::cout<<"AuthnRequest:  "<<str1<<std::endl;
+
+  std::string cert_file = "testcert.pem";
+  std::string privkey_file = "testkey-nopass.pem";
+  std::string ca_file = "cacert.pem";
+
+  std::string authnRequestQuery;
   if(httpmethod == HTTP_METHOD_REDIRECT) {
     std::string query = BuildDeflatedQuery(authn_request);
-    authnRequestUrl = SignQuery(query, Arc::RSA_SHA1, privkey_file);
+    std::cout<<"AuthnRequest after deflation: "<<query<<std::endl;
+    if(must_signed) {
+      authnRequestQuery = SignQuery(query, Arc::RSA_SHA1, privkey_file);
+      std::cout<<"After signature: "<<authnRequestQuery<<std::endl;
+    }
+    else authnRequestQuery = query;
+  }
+  
+  std::string authnRequestUrl;
+  authnRequestUrl = destination + "?SAMLRequest=" + authnRequestQuery;
+  std::cout<<"autnRequestUrl: "<<authnRequestUrl<<std::endl;
+
+  // -------------------------------------------
+  // Client-Agent: Send the AuthnRequest to IdP, get back b64 encoded saml response, and 
+  // send this saml response to SP
+  // -------------------------------------------
+  Arc::MCCConfig cfg;
+  if (!cert_file.empty())
+    cfg.AddCertificate(cert_file);
+  if (!privkey_file.empty())
+    cfg.AddPrivateKey(privkey_file);
+  if (!ca_file.empty())
+    cfg.AddCAFile(ca_file);
+
+  Arc::URL url(authnRequestUrl);
+  std::string url_str = url.str();
+  size_t pos = url_str.find(":");
+  pos = url_str.find(":", pos+1);
+  pos = url_str.find("/", pos+1);
+  std::string path = url_str.substr(pos);
+
+  std::cout<<path<<std::endl;
+
+  Arc::ClientHTTP client(cfg, url.Host(), url.Port(), url.Protocol() == "https" ? 1:0, path);
+
+  std::cout<<"URL: "<<url.str()<<std::endl<<"Host: "<<url.Host()<<std::endl<<"Port: "<<url.Port()<<std::endl<<"Protocol: "<<url.Protocol()<<std::endl;
+
+  Arc::PayloadRaw request;
+  Arc::PayloadRawInterface *response = NULL;
+  Arc::HTTPClientInfo info;
+  Arc::MCC_Status status = client.process("GET", &request, &info, &response);
+  if (!response) {
+    logger.msg(Arc::ERROR, "Request failed: No response");
+    return -1;
+  }
+  if (!status) {
+    logger.msg(Arc::ERROR, "Request failed: Error");
+    return -1;
   }
 
-  size_t f = authnRequestUrl.find("?");
-  if(f == std::string::npos) return 0;
-  std::string authnRequestQuery;
-  authnRequestQuery = authnRequestUrl.substr(f+1);
+  std::cout<<"Response content: "<<response->Content()<<std::endl;
 
+  std::string type = info.type;
+  size_t pos1 = type.find(';');
+  if (pos1 != std::string::npos)
+    type = type.substr(0, pos1);
+
+  if(type != "text/html")
+    std::cerr<<"The html from IdP is with wrong format"<<std::endl;
  
-  // -------------------------------------------------------
-  //    Compose request and send to aa service
-  // -------------------------------------------------------
-  //Compose request
-  Arc::NS ns;
-  ns["saml"] = SAML_NAMESPACE;
-  ns["samlp"] = SAMLP_NAMESPACE;
+  //--------------------------------------
+  //SP: Get the b64 encoded saml response from Client-Agent, and decode/verify/decrypt it
+  //-------------------------------------
+  Arc::XMLNode html_node(response->Content());
+  std::string saml_resp = html_node["body"]["form"]["div"]["input"].Attribute("value");
+  std::cout<<"SAML Response: "<<saml_resp<<std::endl;
 
-  std::string cert("./cert.pem");
-  std::string key("./key.pem");
-  std::string cafile("./ca.pem");
-  ArcLib::Credential cred(cert, key, "", cafile);
-  std::string local_dn = cred.GetDN();
+  Arc::XMLNode node;
 
-  //Compose <samlp:AttributeQuery/>
-  Arc::XMLNode attr_query(ns, "samlp:AttributeQuery");
-  std::string sp_name("https://sp.com/SAML"); //TODO
-  std::string query_id = Arc::UUID();
-  attr_query.NewAttribute("ID") = query_id;
-  Arc::Time t;
-  std::string current_time = t.str(Arc::UTCTime);
-  attr_query.NewAttribute("IssueInstant") = current_time;
-  attr_query.NewAttribute("Version") = std::string("2.0");
-  attr_query.NewChild("saml:Issuer") = sp_name;
-
-  //<saml:Subject/>
-  Arc::XMLNode subject = attr_query.NewChild("saml:Subject");
-  Arc::XMLNode name_id = subject.NewChild("saml:NameID");
-  name_id.NewAttribute("Format")=std::string("urn:oasis:names:tc:SAML:1.1:nameid-format:X509SubjectName");
-  name_id = local_dn;
-
-  //Add one or more <Attribute>s into AttributeQuery here, which means the Requestor would
-  //get these <Attribute>s from AA
-  //<saml:Attribute/>
-  Arc::XMLNode attribute = attr_query.NewChild("saml:Attribute");
-  attribute.NewAttribute("Name")=std::string("urn:oid:1.3.6.1.4.1.5923.1.1.1.6");
-  attribute.NewAttribute("NameFormat")=std::string("urn:oasis:names:tc:SAML:2.0:attrname-format:uri");
-  attribute.NewAttribute("FriendlyName")=std::string("eduPersonPrincipalName");
+  //Decode the saml response (b64 format)
+  Arc::BuildNodefromMsg(saml_resp, node);  
+  std::string decoded_saml_resp;
+  node.GetXML(decoded_saml_resp);
+  std::cout<<"Decoded SAML Response: "<<decoded_saml_resp<<std::endl;
 
   Arc::init_xmlsec();
-  Arc::XMLSecNode attr_query_secnd(attr_query);
-  std::string attr_query_idname("ID");
-  attr_query_secnd.AddSignatureTemplate(attr_query_idname, Arc::XMLSecNode::RSA_SHA1);
-  if(attr_query_secnd.SignNode(key,cert)) {
-    std::cout<<"Succeed to sign the signature under <samlp:AttributeQuery/>"<<std::endl;
-  }
 
-  std::string str;
-  attr_query.GetXML(str);
-  std::cout<<"++++ "<<str<<std::endl;
-
-  Arc::NS soap_ns;
-  Arc::SOAPEnvelope envelope(soap_ns);
-  envelope.NewChild(attr_query);
-  Arc::PayloadSOAP *payload = new Arc::PayloadSOAP(envelope);
-
-  std::string tmp;
-  payload->GetXML(tmp);
-  std::cout<<"SOAP request from Arc client: ++++++++++++++++"<<tmp<<std::endl;
- 
-  // Send request
-  Arc::MessageContext context;
-  Arc::Message reqmsg;
-  Arc::Message repmsg;
-  Arc::MessageAttributes attributes_in;
-  Arc::MessageAttributes attributes_out;
-  reqmsg.Payload(payload);
-  reqmsg.Attributes(&attributes_in);
-  reqmsg.Context(&context);
-  repmsg.Attributes(&attributes_out);
-  repmsg.Context(&context);
-
-  Arc::MCC_Status status = client_entry->process(reqmsg,repmsg);
-  if(!status) {
-    logger.msg(Arc::ERROR, "Request failed");
-    return -1;
-  };
-  logger.msg(Arc::INFO, "Request succeed!!!");
-  if(repmsg.Payload() == NULL) {
-    logger.msg(Arc::ERROR, "There is no response");
-    return -1;
-  };
-
-  Arc::PayloadSOAP* resp = NULL;
-  try {
-   resp = dynamic_cast<Arc::PayloadSOAP*>(repmsg.Payload());
-  } catch(std::exception&) { };
-  if(resp == NULL) {
-    logger.msg(Arc::ERROR, "Response is not SOAP");
-    delete repmsg.Payload();  Arc::final_xmlsec();
-    return -1;
-  };
-
-  // -------------------------------------------------------
-  //   Comsume the response from aa service
-  // -------------------------------------------------------
-  //
-  //Consume the response from AA
-  Arc::XMLNode attr_resp;
-  
-  //<samlp:Response/>
-  attr_resp = (*resp).Body().Child(0);
- 
-  //TODO: metadata processing.
-  //std::string aa_name = attr_resp["saml:Issuer"]; 
- 
-  //Check validity of the signature on <samlp:Response/>
-  std::string resp_idname = "ID";
-  std::string cafile1 = "./ca.pem";
-  std::string capath1 = "";
-  Arc::XMLSecNode attr_resp_secnode(attr_resp);
-  if(attr_resp_secnode.VerifyNode(resp_idname, cafile1, capath1)) {
+  //Verify the signature of saml response
+  std::string idname = "ID";
+  std::string ca_path = "";
+  Arc::XMLSecNode sec_samlresp_nd(node);
+  if(sec_samlresp_nd.VerifyNode(idname, ca_file, ca_path)) {
     logger.msg(Arc::INFO, "Succeed to verify the signature under <samlp:Response/>");
   }
   else {
     logger.msg(Arc::ERROR, "Failed to verify the signature under <samlp:Response/>");
-    delete repmsg.Payload(); Arc::final_xmlsec(); return -1;
   }
- 
- 
-  //Check whether the "InResponseTo" is the same as the local ID
-  std::string responseto_id = (std::string)(attr_resp.Attribute("InResponseTo"));
-  if(query_id != responseto_id) {
-    logger.msg(Arc::INFO, "The Response is not going to this end");
-    delete repmsg.Payload(); Arc::final_xmlsec(); return -1;
-  }
+  
+  //Decrypte the encrypted saml assertion in this saml response
+  Arc::XMLNode assertion_nd = node["saml:EncryptedAssertion"];
+  std::string saml_assertion;
+  assertion_nd.GetXML(saml_assertion);
+  std::cout<<"Encrypted saml assertion"<<saml_assertion<<std::endl;
 
-  std::string resp_time = attr_resp.Attribute("IssueInstant");
+  Arc::XMLSecNode sec_assertion_nd(assertion_nd);
+  Arc::XMLNode decrypted_assertion_nd; 
+  bool r = sec_assertion_nd.DecryptNode(privkey_file, decrypted_assertion_nd);
+  if(!r) { std::cout<<"Can not decrypted the EncryptedAssertion from saml response"<<std::endl; return 0; }
 
-  //<samlp:Status/>
-  std::string statuscode_value = attr_resp["samlp:Status"]["samlp:StatusCode"];
-  if(statuscode_value == "urn:oasis:names:tc:SAML:2.0:status:Success")
-    logger.msg(Arc::INFO, "The StatusCode is Success");
+  std::string decrypted_saml_assertion;
+  decrypted_assertion_nd.GetXML(decrypted_saml_assertion);
+  std::cout<<"Decrypted SAML Assertion: "<<decrypted_saml_assertion<<std::endl;
 
-  //<saml:Assertion/>
-  Arc::XMLNode assertion = attr_resp["saml:Assertion"];
+  //-----------------------
+  //SP: AttributeQuery
+  //-----------------------
 
-  //TODO: metadata processing.
-  //std::string aa_name = assertion["saml:Issuer"];
- 
-  //Check validity of the signature on <saml:Assertion/>
-  std::string assertion_idname = "ID";
-  std::string cafile2 = "./ca.pem";
-  std::string capath2 = "";
-  Arc::XMLSecNode assertion_secnode(assertion);
-  if(assertion_secnode.VerifyNode(assertion_idname, cafile2, capath2)) {
-    logger.msg(Arc::INFO, "Succeed to verify the signature under <saml:Assertion/>");
-  }
-  else {
-    logger.msg(Arc::ERROR, "Failed to verify the signature under <saml:Assertion/>");
-    delete repmsg.Payload(); Arc::final_xmlsec(); return -1;
-  }
-
-  //<saml:Subject/>, TODO: 
-  Arc::XMLNode subject_nd = assertion["saml:Subject"];
-
-  //<saml:Condition/>, TODO: Condition checking
-  Arc::XMLNode cond_nd = assertion["saml:Conditions"];
-
-  //<saml:AttributeStatement/>
-  Arc::XMLNode attr_statement = assertion["saml:AttributeStatement"];
-
-  //<saml:Attribute/>
-  Arc::XMLNode attr_nd;
-  std::vector<std::string> attributes_value;
-  for(int i=0;;i++) {
-    attr_nd = attr_statement["saml:Attribute"][i];
-    if(!attr_nd) break;
-
-    std::string name = attr_nd.Attribute("Name");
-    std::string nameformat = attr_nd.Attribute("NameFormat");
-    std::string friendname = attribute.Attribute("FriendlyName");
-
-    Arc::XMLNode attr_value = attr_nd["saml:AttributeValue"];
-
-    std::string str;
-    str.append("Name=").append(friendname).append(" Value=").append(attr_value);
-    attributes_value.push_back(str);
-  }
-
-  for(int i=0; i<attributes_value.size(); i++) {
-    std::cout<<"Attribute Value: "<<attributes_value[i]<<std::endl;  
-  }
-
-  delete repmsg.Payload();
-
+  delete response;
   Arc::final_xmlsec();
-
   return 0;
 }
