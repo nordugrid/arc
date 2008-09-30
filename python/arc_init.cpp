c6f35924614397f4ec5f1a24a32bc68e61192382
Aleksandr Konstantinov <akonstantinov@yahoo.com>
2008-09-30 09:42:00 +0000

    More portable solution for reloading python module

--- python/arc_init.cpp
+++ python/arc_init.cpp
@@ -9,44 +9,45 @@
 #include <glibmm/miscutils.h>
 #include <arc/ArcLocation.h>
 #include <arc/Utils.h>
+#include <arc/Logger.h>
+
+#include <Python.h>
+
 
 __attribute__((constructor)) void __arc_init(void) {
-  std::string pythonpath = Arc::GetEnv("PYTHONPATH");
+
+/*
+char* Py_GetPath ()
+Return the default module search path; this is computed from the program
+name (set by Py_SetProgramName() above) and some environment variables. The
+returned string consists of a series of directory names separated by a
+platform dependent delimiter character. The delimiter character is ":" on
+Unix, ";" on DOS/Windows, and "\n" (the ASCII newline character) on
+Macintosh. The returned string points into static storage; the caller should
+not modify its value. The value is available to Python code as the list
+sys.path, which may be modified to change the future search path for loaded
+modules.
+*/
+  std::string pythonpath = Py_GetPath();
   std::string::size_type start = 0;
-  std::string::size_type end = pythonpath.find(';');
+  std::string::size_type end = pythonpath.find_first_of(";:\n");
   if(end == std::string::npos) end=pythonpath.length();
   for(;start<pythonpath.length();) {
     std::string path = pythonpath.substr(start,end-start);
-//    std::string modulepath = Glib::Module::build_path(path,"_arc");
-    std::string modulepath = Glib::build_filename(path,"_arc.so");
+    std::string modulepath = Glib::build_filename(path,std::string("_arc.")+G_MODULE_SUFFIX);
 #ifdef HAVE_GLIBMM_BIND_LOCAL
     Glib::Module *module = new Glib::Module(modulepath,Glib::MODULE_BIND_GLOBAL);
 #else
     Glib::Module *module = new Glib::Module(modulepath);
 #endif
     if(module != NULL) {
-      if(*module) break;
+      if(*module) return;
       delete module;
     };
-    std::cerr<<"dlopen failed: "<<dlerror()<<std::endl;
     start=end+1;
-    end=pythonpath.find(';',start);
+    end=pythonpath.find_first_of(";:\n",start);
     if(end == std::string::npos) end=pythonpath.length();
   };
-/*
-  std::list<std::string> pluglist = Arc::ArcLocation::GetPlugins();
-  for(std::list<std::string>::iterator path = pluglist.begin();path!=pluglist.end();++path) {
-    std::string plugpath = Glib::Module::build_path(*path,"arcinit");
-std::cerr<<"!!!!!!!!! __arc_init: loading: "<<plugpath<<std::endl;
-#ifdef HAVE_GLIBMM_BIND_LOCAL
-    Glib::Module *module = new Glib::Module(plugpath,Glib::MODULE_BIND_GLOBAL);
-#else
-    Glib::Module *module = new Glib::Module(plugpath);
-#endif
-    if(module == NULL) {
-      std::cerr<<"dlopen failed: "<<dlerror()<<std::endl;
-    };
-  };
-*/
+  Arc::Logger::getRootLogger().msg(Arc::WARNING,"Failed to export symbols of ARC python module");
 }
 
