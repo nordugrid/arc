4da1076848c9dc1c5bad46aa5d97fbf655599963
Ivan Marton <martoni@niif.hu>
2008-11-06 08:31:54 +0000

    Doing the JobDescription tester presentation ready.

--- src/hed/libs/client/test_jobdescription.cpp
+++ src/hed/libs/client/test_jobdescription.cpp
@@ -1,50 +1,96 @@
 #include <sys/stat.h>
 #include <fstream>
 #include <arc/client/JobDescription.h>
+#include <arc/OptionParser.h>
+#include <arc/IString.h>
 
 int main(int argc, char* argv[]) {
 
-  if (argc != 2) {
-    std::cout << "Usage: test_jobdescription job_description_string_or_file" << std::endl;
+Arc::OptionParser options(istring("[job description ...]"),
+  istring("This tiny tool can be used for testing"
+          "the JobDescription's conversion abilities."),
+  istring("The job description also can be a file or a string in JDL, JSDL, or XRSL format.")
+  );
+
+std::string requested_format = "";
+  options.AddOption('f', "format",
+    istring("define the requested format (JSDL, XRSL)"),
+    istring("format"),
+    requested_format);
+
+bool show_original_description = false;
+  options.AddOption('o', "original",
+    istring("show the original job description"),
+    show_original_description);
+
+std::list<std::string> descriptions = options.Parse(argc, argv);
+  if (descriptions.empty()) {
+    std::cout << "Use --help option for detailed usage information" << std::endl;
     return 1;
   }
 
+std::cout << " [ JobDescription tester ] " << std::endl;
+
+for (std::list< std::string >::iterator it = descriptions.begin(); it != descriptions.end(); it++) {
   struct stat stFileInfo;
   int intStat;
   std::string original_description;
 
   // Attempt to get the file attributes 
-  intStat = stat(argv[1], &stFileInfo);
+  intStat = stat((*it).c_str(), &stFileInfo);
 
   if(intStat == 0) { 
     // We were able to get the file attributes 
     // so the file obviously exists. 
     std::ifstream ifs;
     std::string buffer;
-    ifs.open(argv[1], std::ios::in);
+    ifs.open((*it).c_str(), std::ios::in);
     while (std::getline(ifs, buffer)) {
       original_description += buffer + "\n";
     }
   } else {
-    original_description = argv[1];
+    original_description = (*it);
   }
-  Arc::JobDescription jd;
 
-  std::cout << " [ JobDescription tester ] " << std::endl;
-  std::cout << std::endl << " [ Parsing the orignal text ] " << std::endl << std::endl;
-  std::cout << original_description << std::endl;
-  jd.setSource( original_description );
+  if (requested_format == "JDL" || requested_format == "JSDL" || requested_format == "") {
+    Arc::JobDescription jd;
 
-  std::string test;
-  jd.getXML().GetDoc(test, true);
-  std::cout << std::endl << " [ jd.getXML() ] " << std::endl << std::endl << test << std::endl;
+    if (show_original_description) {
+      std::cout << std::endl << " [ Parsing the orignal text ] " << std::endl << std::endl;
+      std::cout << original_description << std::endl;
+    }
 
-  jd.getProduct( test, "JSDL" );
-  std::cout << std::endl << " [ JSDL ] " << std::endl << test << std::endl;
-  jd.getProduct( test, "JDL" );
-  std::cout << std::endl << " [ JDL ] " << std::endl << test << std::endl;
-  jd.getProduct( test, "XRSL" );
-  std::cout << std::endl << " [ XRSL ] " << std::endl << test << std::endl;
+    jd.setSource( original_description );
 
+    std::string test;
+    if (requested_format == "") {
+      jd.getXML().GetDoc(test, true);
+      std::cout << std::endl << " [ Inner representation ] " << std::endl << std::endl << test << std::endl;
+    }
+
+    if (requested_format == "JSDL" || requested_format == "") {
+      try {
+        jd.getProduct( test, "JSDL" );
+        Arc::XMLNode jsdl(test);
+        jsdl.GetXML(test, true);
+        std::cout << std::endl << " [ JSDL ] " << std::endl << test << std::endl;
+      } catch (Arc::JobDescriptionError jde) {
+        std::cout << std::endl << " " << jde.what() << std::endl;
+      }
+    }
+
+    if (requested_format == "JDL" || requested_format == "") {
+      try {
+        jd.getProduct( test, "JDL" );
+        std::cout << std::endl << " [ JDL ] " << std::endl << test << std::endl;
+      } catch (Arc::JobDescriptionError jde) {
+        std::cout << std::endl << " " << jde.what() << std::endl;
+      }
+    }
+    //jd.getProduct( test, "XRSL" );
+    //std::cout << std::endl << " [ XRSL ] " << std::endl << test << std::endl;
+  }
+}
   return 0;
+
 }
