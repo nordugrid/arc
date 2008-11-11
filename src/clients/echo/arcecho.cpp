29a3c752d0c0adb7a8ee8cdff7c672ccbf630bbe
Mattias Ellert <mattias.ellert@physics.uu.se>
2008-11-11 15:42:20 +0000

    Updated arcecho client

--- src/clients/echo/arcecho.cpp
+++ src/clients/echo/arcecho.cpp
@@ -1,43 +1,122 @@
-// arcecho.cpp
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
 
 #include <iostream>
-#include <fstream>
+#include <list>
 #include <string>
-#include <stdexcept>
-#include "echo_client.h"
 
-//! A prototype client for the echo service.
-/*! A prototype command line tool for the echo service. In the name,
-  "ap" means "Arc Prototype".
-  
-  Usage:
-  arcecho <message>
+#include <arc/ArcLocation.h>
+#include <arc/IString.h>
+#include <arc/Logger.h>
+#include <arc/OptionParser.h>
+#include <arc/message/MCC.h>
+#include <arc/client/ClientInterface.h>
+#include <arc/client/UserConfig.h>
 
-  Arguments:
-  <message> A message that the echo service shall return back again.
+int main(int argc, char** argv) {
 
-  Configuration:
-  Which echo service to use is specified in a configuration file. The
-  configuration file also specifies how to set up the communication
-  chain for the client. The location of the configuration file is
-  specified by the environment variable ARC_ECHO_CONFIG. If there is
-  no such environment variable, the configuration file is assumed to
-  be "echo_client.xml" in the current working directory.
-*/
-int main(int argc, char* argv[]){
+  setlocale(LC_ALL, "");
+
+  Arc::Logger logger(Arc::Logger::getRootLogger(), "arcecho");
   Arc::LogStream logcerr(std::cerr);
   Arc::Logger::getRootLogger().addDestination(logcerr);
-  Arc::Logger::rootLogger.setThreshold(Arc::VERBOSE);
-  try{
-    if (argc!=2)
-      throw std::invalid_argument("Wrong number of arguments!");
-    Arc::EchoClient ec;
-    std::string message = argv[1];
-    std::cout << ec.echo(message) << std::endl;
-    return EXIT_SUCCESS;
+  Arc::Logger::rootLogger.setThreshold(Arc::WARNING);
+
+  Arc::ArcLocation::Init(argv[0]);
+
+  Arc::OptionParser options(istring("service message"),
+                            istring("The arcecho command is a client for "
+                                    "the ARC echo service."),
+			    istring("The service argument is a URL to an ARC "
+				    "echo service.\n"
+				    "The message argument is the message the "
+				    "service should return."));
+
+  std::string conffile;
+  options.AddOption('z', "conffile",
+                    istring("configuration file (default ~/.arc/client.xml)"),
+                    istring("filename"), conffile);
+
+  std::string debug;
+  options.AddOption('d', "debug",
+                    istring("FATAL, ERROR, WARNING, INFO, DEBUG or VERBOSE"),
+                    istring("debuglevel"), debug);
+
+  bool version = false;
+  options.AddOption('v', "version", istring("print version information"),
+                    version);
+
+  std::list<std::string> args = options.Parse(argc, argv);
+
+  if (!debug.empty())
+    Arc::Logger::getRootLogger().setThreshold(Arc::string_to_level(debug));
+
+  Arc::UserConfig usercfg(conffile);
+  if (!usercfg) {
+    logger.msg(Arc::ERROR, "Failed configuration initialization");
+    return 1;
   }
-  catch (std::exception& err){
-    std::cerr << "ERROR: " << err.what() << std::endl;
-    return EXIT_FAILURE;
+
+  if (debug.empty() && usercfg.ConfTree()["Debug"]) {
+    debug = (std::string)usercfg.ConfTree()["Debug"];
+    Arc::Logger::getRootLogger().setThreshold(Arc::string_to_level(debug));
   }
+
+  if (version) {
+    std::cout << Arc::IString("%s version %s", "arcecho", VERSION)
+              << std::endl;
+    return 0;
+  }
+
+  if (args.size() != 2) {
+    logger.msg(Arc::ERROR, "Wrong number of arguments!");
+    return 1;
+  } 
+
+  std::list<std::string>::iterator it = args.begin();
+
+  Arc::URL service = *it++;
+  std::string message = *it++;
+
+  Arc::MCCConfig cfg;
+  if (usercfg.ConfTree()["ProxyPath"])
+    cfg.AddProxy((std::string)usercfg.ConfTree()["ProxyPath"]);
+  if (usercfg.ConfTree()["CertificatePath"])
+    cfg.AddCertificate((std::string)usercfg.ConfTree()["CertificatePath"]);
+  if (usercfg.ConfTree()["KeyPath"])
+    cfg.AddPrivateKey((std::string)usercfg.ConfTree()["KeyPath"]);
+  if (usercfg.ConfTree()["CACertificatesDir"])
+    cfg.AddCADir((std::string)usercfg.ConfTree()["CACertificatesDir"]);
+
+  Arc::ClientSOAP client(cfg, service.Host(), service.Port(),
+			 service.Protocol() == "https", service.Path());
+
+  Arc::NS ns("echo", "urn:echo");
+  Arc::PayloadSOAP request(ns);
+  request.NewChild("echo").NewChild("say") = message;
+
+  Arc::PayloadSOAP *response = NULL;
+
+  Arc::MCC_Status status = client.process(&request, &response);
+
+  if (!status) {
+    logger.msg(Arc::ERROR, (std::string)status);
+    if (response)
+      delete response;
+    return 1;
+  }
+
+  if (!response) {
+    logger.msg(Arc::ERROR, "No SOAP response");
+    return 1;
+  }
+
+  std::string answer = (std::string)((*response)["echoResponse"]["hear"]);
+
+  delete response;
+
+  std::cout << answer << std::endl;
+
+  return 0;
 }
