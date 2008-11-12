11bf9cd635aeaa6b034a91eaf0a9e85036a17708
Aleksandr Konstantinov <akonstantinov@yahoo.com>
2008-11-12 21:46:44 +0000

    Another wrapper for pointers - this time with multiple references.

--- src/hed/libs/common/Utils.h
+++ src/hed/libs/common/Utils.h
@@ -47,6 +47,64 @@ namespace Arc {
     operator T*(void) const { return object; };
   };
 
+  /// Wrapper for pointer with automatic destruction and mutiple references
+  /** If ordinary pointer is wrapped in instance of this class
+    it will be automatically destroyed when all instances refering to it
+    are destroyed.
+    This is useful for maintaing pointers refered from multiple structures
+    wihth automatic destruction of original object when last reference 
+    is destroyed. It is similar to Java approach with a difference that
+    desctruction time is strictly defined. 
+    Only pointers returned by new() are supported. This class is not thread-safe */
+  template <typename T> class CountedPointer {
+   private:
+    template <typename P> class Base {
+     private:
+      Base(Base<P>&) { };
+     public:
+      int cnt;
+      P* ptr;
+      Base(P* p):cnt(0),ptr(p) { add(); };
+      ~Base(void) { if(ptr) delete ptr; };
+      Base<P> add(void) { ++cnt; return this; };
+      bool rem(void) {
+        if(--cnt == 0) {
+          delete this;
+          return true;
+        };
+        return false;
+      };
+    };
+    Base<T>* object;
+   public:
+    CountedPointer(T* p):object(new Base<T>(p)) { };
+    CountedPointer(CountedPointer<T>& p):object(p.object->add()) { };
+    ~CountedPointer(void) { object->rem(); };
+    CountedPointer& operator=(T* p) {
+      if(p != object->ptr) {
+        object->rem(); object=new Base<T>(p);
+      };
+      return *this;
+    };
+    CountedPointer& operator=(CountedPointer& p) {
+      if(p.object->ptr != object->ptr) {
+        object->rem(); object=p.object->add();
+      };
+      return *this;
+    };
+    /// For refering wrapped object
+    T& operator*(void) const { return *(object->ptr); };
+    /// For refering wrapped object
+    T* operator->(void) const { return (object->ptr); };
+    /// Returns false if pointer is NULL and true otherwise.
+    operator bool(void) const { return ((object->ptr)!=NULL); };
+    /// Returns true if pointer is NULL and false otherwise.
+    bool operator!(void) const { return ((object->ptr)==NULL); };
+    /// Cast to original pointer
+    operator T*(void) const { return (object->ptr); };
+  };
+
+
 } // namespace Arc
 
 # endif // __ARC_UTILS_H__
