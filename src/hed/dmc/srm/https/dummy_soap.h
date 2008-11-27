8900c05745149443c4a7d68a66fcf35e1d41a5cf
Aleksandr Konstantinov <akonstantinov@yahoo.com>
2008-11-27 14:56:51 +0000

    Fixing dependencies of SRM DMC

---
+++ src/hed/dmc/srm/https/dummy_soap.h
@@ -0,0 +1,6 @@
+//gsoap ck service name: dummy_dummy
+//gsoap ck service location: http://www.dummy.org/dummy_dummy
+//gsoap ck service executable: dummy.cgi
+//gsoap ck service namespace: http://www.dummy.org/dummy_dummy/dummy.wsdl
+//gsoap ck schema  namespace: urn:dummy
+
