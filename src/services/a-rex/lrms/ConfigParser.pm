761189be10cb9fce2451bbe0276a093e84ff4514
Adrian Taga <v.a.taga@fys.uio.no>
2009-01-12 11:24:26 +0000

    parser with support for "control" option (merged from arc0)

--- src/services/a-rex/lrms/ConfigParser.pm
+++ src/services/a-rex/lrms/ConfigParser.pm
@@ -24,6 +24,15 @@ use warnings;
 #
 #   print $parser->list_subsections('gridftpd');      # list all subsections of 'gridftpd', but not
 #                                                     # the 'gridftpd' section itself
+# 
+# The [grid-manager] section is treated specially. A 'control' command behaves
+# as if it defined new sections. For each user <username> on the argument list
+# of a 'control' command, a pseudo-section [grid-manager/<username>] is
+# made available.  User names '.' and '*' are not handled specially. Each
+# pseudo-section has a 'controldir' option with the path set to the control dir
+# defined by the 'control' option.  No substitutions are done on the path.  The
+# 'controldir' option is ony saved in the [grid-manager] section, but not in
+# the pseudo-sections. 
 
 
 sub new($$) {
@@ -47,6 +56,13 @@ sub _parse($$) {
     my $sname;
     my $sopts;
 
+    # per-user opitions of the [grid-manager] section.
+    # Contains the options applying to the most recent user
+    # which was defined by a 'control' option.
+    my $useropts = {};
+
+    my $controldir;
+
     while (my $line =<CONFIGFILE>) {
 
         # skip comments and empy lines
@@ -56,6 +72,17 @@ sub _parse($$) {
         # new section stsrts here
         if ($line =~ /^\s*\[([\w\-\.\/]+)\]\s*$/) {
             my $newsname = $1;
+
+            # Finish processing [grid-manager] section
+            if (defined $sname and $sname eq 'grid-manager') {
+                # forget the controldir set with 'control' option
+                delete $useropts->{controldir};
+                $useropts->{controldir} = $controldir if $controldir;
+                $sopts = { %$useropts, %$sopts };
+
+                $controldir = undef;
+                $useropts = {};
+            }
 
             # store completed section
             $config{$sname} = $sopts if $sname;
@@ -75,18 +102,44 @@ sub _parse($$) {
             if ($opt eq 'name') {
                 $sname =~ s|^(.+?)(/[^/]*)?$|$1/$value|;
             }
+
+            # Special treatment for the 'control' and 'controldir' options
+            if ($sname eq 'grid-manager' and $opt eq 'control') {
+
+                my ($ctrldir, @users) = split " ", $value;
+                $useropts->{controldir} = $ctrldir;
+
+                $useropts = { %$useropts, %$sopts };
+                $sopts = {};
+
+                # each user gets a separate sub-section
+                $config{"grid-manager/$_"} = { %$useropts } for @users;
+
+            } elsif ($sname eq 'grid-manager' and $opt eq 'controldir') {
+                # Will be applied only after the whole section has been read
+                $controldir = $value;
     
-            if (not defined $sopts->{$opt}) {
-               $sopts->{$opt} = $value;
-            }
-            else {
-               $sopts->{$opt} .= "[separator]".$value;
+            } else {
+                if (not defined $sopts->{$opt}) {
+                   $sopts->{$opt} = $value;
+                }
+                else {
+                   $sopts->{$opt} .= "[separator]".$value;
+                }
             }
         } else {
             # Bad line, ignore it for now
         }
     }
     close CONFIGFILE;
+
+    # In case [grid-manager] was the last section...
+    if (defined $sname and $sname eq 'grid-manager') {
+        # forget the controldir set with 'control' option
+        delete $useropts->{controldir};
+        $useropts->{controldir} = $controldir if $controldir;
+        $sopts = { %$useropts, %$sopts };
+    }
 
     # store last section
     $config{$sname} = $sopts if $sname;
