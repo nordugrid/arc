2e9bf69eaa994a40bd39c8b52ccdcbf0c7e00403
Gabor Roczei <roczei@niif.hu>
2009-01-16 16:14:07 +0000

    CacheCheck interface, 80% ready

--- src/services/a-rex/cachecheck.cpp
+++ src/services/a-rex/cachecheck.cpp
@@ -4,7 +4,13 @@
 
 #include <arc/message/SOAPEnvelope.h>
 #include <arc/ws-addressing/WSA.h>
+#include <arc/data/FileCache.h>
+#include <sys/types.h>
+#include <pwd.h>
+
 #include "arex.h"
+#include "grid-manager/conf/conf_cache.h"
+
 
 namespace ARex {
 
@@ -18,6 +24,68 @@ Arc::MCC_Status ARexService::CacheCheck(ARexGMConfig& config,Arc::XMLNode in,Arc
    //  This information is from David Cameron
    //
    
+   {
+     std::string s;
+     in.GetXML(s);
+     logger.msg(Arc::DEBUG, "CachCheck: request = \n%s", s); 
+   };  
+
+   Arc::XMLNode resp = out.NewChild("CacheCheckResponse");
+
+   Arc::XMLNode results = resp.NewChild("CacheCheckResult");
+
+
+   bool fileexist;
+
+   Arc::FileCache * cache;
+
+   cache = new Arc::FileCache();
+
+
+/*
+
+  std::string file_owner_username = "";
+
+    struct passwd pw_;
+    struct passwd *pw;
+    char buf[BUFSIZ];
+    getpwuid_r(getuid(),&pw_,buf,BUFSIZ,&pw);
+    if(pw == NULL) {
+      olog<<"Wrong user name"<<std::endl; exit(1);
+    }   
+    if(pw->pw_name) file_owner_username=pw->pw_name;
+
+   CacheConfig * cache_config = new CacheConfig(file_owner_username);
+*/
+   for(int n = 0;;++n) {
+      Arc::XMLNode id = in["TheseFilesNeedToCheck"]["FileURL"][n];
+      if(!id) break;
+      
+      fileexist = false;
+
+	  std::string fileurl = ((std::string)in["TheseFilesNeedToCheck"]["FileURL"][n]);
+         
+      fileexist = (*cache).CheckCreated(fileurl);
+
+      Arc::XMLNode file;
+
+	  file.NewChild("FileURL") = fileurl;
+	  file.NewChild("ExistInTheCache") = (fileexist ? "true": "false");
+
+
+      Arc::XMLNode resultelement("Result");
+
+      resultelement.NewChild(file);
+
+      results.NewChild(resultelement);
+   }
+
+   {
+     std::string s;
+     out.GetXML(s);
+     logger.msg(Arc::DEBUG, "CachCheck: response = \n%s", s); 
+   };  
+
    return Arc::MCC_Status(Arc::STATUS_OK);
 }
 
