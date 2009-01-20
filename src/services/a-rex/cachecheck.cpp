2b6ab81dc52676d9a4c78420ad02482b85a338de
Gabor Roczei <roczei@niif.hu>
2009-01-20 13:38:55 +0000

    CacheCheck interface implementation, test client

--- src/services/a-rex/cachecheck.cpp
+++ src/services/a-rex/cachecheck.cpp
@@ -10,7 +10,10 @@
 
 #include "arex.h"
 #include "grid-manager/conf/conf_cache.h"
+#include "grid-manager/jobs/job.h"
+#include "grid-manager/jobs/users.h"
 
+#define CACHE_CHECK_SESSION_DIR_ID "9999999999999999999999999999999"
 
 namespace ARex {
 
@@ -30,33 +33,61 @@ Arc::MCC_Status ARexService::CacheCheck(ARexGMConfig& config,Arc::XMLNode in,Arc
      logger.msg(Arc::DEBUG, "CachCheck: request = \n%s", s); 
    };  
 
-   Arc::XMLNode resp = out.NewChild("CacheCheckResponse");
-
-   Arc::XMLNode results = resp.NewChild("CacheCheckResult");
-
-
-   bool fileexist;
-
-   Arc::FileCache * cache;
-
-   cache = new Arc::FileCache();
-
-
-/*
-
+  uid_t uid = getuid();
+  gid_t gid = getgid();
+ 
   std::string file_owner_username = "";
+
+  JobUser user(uid);
+
+  std::vector<struct Arc::CacheParameters> caches;
 
     struct passwd pw_;
     struct passwd *pw;
     char buf[BUFSIZ];
     getpwuid_r(getuid(),&pw_,buf,BUFSIZ,&pw);
     if(pw == NULL) {
-      olog<<"Wrong user name"<<std::endl; exit(1);
-    }   
+     logger.msg(Arc::ERROR, "Wrong user name"); 
+     exit(1);
+    }
     if(pw->pw_name) file_owner_username=pw->pw_name;
 
-   CacheConfig * cache_config = new CacheConfig(file_owner_username);
-*/
+    // use cache dir(s) from conf file
+    try {
+      CacheConfig * cache_config = new CacheConfig(file_owner_username);
+      std::list<std::list<std::string> > conf_caches = cache_config->getCacheDirs();
+      // add each cache to our list
+      for (std::list<std::list<std::string> >::iterator i = conf_caches.begin(); i != conf_caches.end(); i++) {
+        std::list<std::string>::iterator j = i->begin();
+        struct Arc::CacheParameters cache_params;
+        user.substitute(*j); cache_params.cache_path = (*j); j++;
+        user.substitute(*j); cache_params.cache_job_dir_path = (*j); j++;
+        if (j != i->end()) {user.substitute(*j); cache_params.cache_link_path = (*j);}
+        else cache_params.cache_link_path = "";
+        caches.push_back(cache_params);
+      }
+    }
+    catch (CacheConfigException e) {
+     logger.msg(Arc::ERROR, "Error with cache configuration: %s", e.what()); 
+     logger.msg(Arc::ERROR, "Will not use caching"); 
+    }
+
+  Arc::FileCache * cache;
+  if(!caches.empty()) {
+
+    cache = new Arc::FileCache(caches, CACHE_CHECK_SESSION_DIR_ID ,uid,gid);
+    if (!(*cache)) {
+     logger.msg(Arc::ERROR, "Error creating cache"); 
+      exit(1);
+    }
+  }
+
+  bool fileexist;
+
+  Arc::XMLNode resp = out.NewChild("CacheCheckResponse");
+
+  Arc::XMLNode results = resp.NewChild("CacheCheckResult");
+
    for(int n = 0;;++n) {
       Arc::XMLNode id = in["TheseFilesNeedToCheck"]["FileURL"][n];
       if(!id) break;
@@ -79,6 +110,17 @@ Arc::MCC_Status ARexService::CacheCheck(ARexGMConfig& config,Arc::XMLNode in,Arc
 
       results.NewChild(resultelement);
    }
+  
+
+   // TESTING: file_owner_username
+
+      fileexist = false;
+
+	  std::string fileurl = "http://knowarc1.grid.niif.hu/storage/Makefile";
+         
+      fileexist = (*cache).CheckCreated(fileurl);
+
+      logger.msg(Arc::INFO, "Cache check working or not: %s", fileexist ? "true": "false"); 
 
    {
      std::string s;
@@ -89,4 +131,4 @@ Arc::MCC_Status ARexService::CacheCheck(ARexGMConfig& config,Arc::XMLNode in,Arc
    return Arc::MCC_Status(Arc::STATUS_OK);
 }
 
-} // namespace ARex 
+} // namespace 
---
+++ src/services/a-rex/test_cache_check.cpp
@@ -0,0 +1,61 @@
+#include <map>
+#include <iostream>
+#include <signal.h>
+
+#include <arc/ArcConfig.h>
+#include <arc/Logger.h>
+#include <arc/message/SOAPEnvelope.h>
+#include <arc/message/PayloadSOAP.h>
+#include <arc/message/MCC.h>
+#include <arc/message/MCCLoader.h>
+#include <arc/URL.h>
+#include <arc/client/ClientInterface.h>
+
+int main(void) {
+
+  Arc::Logger logger(Arc::Logger::rootLogger, "Test");
+  Arc::LogStream logcerr(std::cerr);
+  Arc::Logger::rootLogger.addDestination(logcerr);
+  logger.msg(Arc::INFO, "Creating client side chain");
+
+  std::string id;
+  std::string url("https://zion.ki.iif.hu:60000/arex");
+  Arc::NS ns;
+  Arc::MCCConfig cfg;
+
+  cfg.AddProxy("/home/roczei/.globus/proxy.pem");
+
+  Arc::ClientSOAP client(cfg, url);
+
+
+    std::string faultstring;
+
+    Arc::PayloadSOAP request(ns);
+    Arc::XMLNode req = request.NewChild("CacheCheck").NewChild("TheseFilesNeedToCheck");
+
+    req.NewChild("FileURL") = "http://knowarc.grid.niif.hu/storage/Makefile";
+
+    Arc::PayloadSOAP* response;
+
+    Arc::MCC_Status status = client.process(&request, &response);
+
+    if (!status) {
+        std::cerr << "Request failed" << std::endl;
+        if(response) {
+           std::string str;
+           response->GetXML(str);
+           std::cout << str << std::endl;
+           delete response;
+        }
+     };
+
+     if (!response) {
+         std::cerr << "No response" << std::endl;
+     };
+
+   return 0;  
+};
+
+
+
+
