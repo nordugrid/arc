c42d20dd0c4da24ae2b4873ab79e8bdf14a7ac63
Weizhong Qiang <Qiang@mycompany.com>
2009-01-21 06:11:42 +0000

    manage to test against knowarc.eu voms server

--- src/clients/credentials/test2vomsserver.cpp
+++ src/clients/credentials/test2vomsserver.cpp
@@ -26,26 +26,28 @@ int main(void) {
   Arc::Logger::getRootLogger().setThreshold(Arc::VERBOSE);
 
   // The message which will be sent to voms server
-  std::string send_msg("\
-    <?xml version=\"1.0\" encoding = \"US-ASCII\"?>\
-    <voms>\
-     <command>knowarc.eu</command>\
-     <base64>1</base64>\
-     <version>4</version>\
-     <lifetime>3600</lifetime>\
-    </voms>");
+  std::string send_msg("<?xml version=\"1.0\" encoding = \"US-ASCII\"?><voms><command>G/playground.knowarc.eu</command><lifetime>43200</lifetime></voms>");
 
-  std::string voms_url("httpg://arthur.hep.lu.se:15001");
-  Arc::URL voms_service(voms_url);
+  std::cout<<"Send message to peer end through GSS communication: "<<send_msg<<" Size: "<<send_msg.length()<<std::endl;
+
+  //std::string voms_url("httpg://arthur.hep.lu.se:15001");
+  //Arc::URL voms_service(voms_url);
 
   Arc::MCCConfig cfg;
   cfg.AddProxy("/tmp/x509up_u1001");
+  //cfg.AddCertificate("/home/wzqiang/arc-0.9/src/tests/echo/usercert.pem");
+  //cfg.AddPrivateKey("/home/wzqiang/arc-0.9/src/tests/echo/userkey-nopass.pem");
   cfg.AddCADir("/home/qiangwz/.globus/certificates/");
 
-  Arc::ClientTCP client(cfg, "arthur.hep.lu.se", 15001, Arc::GSISec);
+  Arc::ClientTCP client(cfg, "arthur.hep.lu.se", 15002, Arc::GSISec);
+  //Arc::ClientTCP client(cfg, "arthur.hep.lu.se", 15001, Arc::GSISec);
+  //Arc::ClientTCP client(cfg, "squark.uio.no", 15011, Arc::GSISec);
 
   Arc::PayloadRaw request;
   request.Insert(send_msg.c_str(),0,send_msg.length());
+  Arc::PayloadRawInterface& buffer = dynamic_cast<Arc::PayloadRawInterface&>(request);
+  std::cout<<"Message in PayloadRaw:  "<<((Arc::PayloadRawInterface&)buffer).Content()<<std::endl;
+
   Arc::PayloadStreamInterface *response = NULL;
   Arc::MCC_Status status = client.process(&request, &response,true);
   if (!status) {
@@ -58,12 +60,20 @@ int main(void) {
     logger.msg(Arc::ERROR, "No stream response");
     return 1;
   }
-  int length = 1024;
+ 
+  std::string ret_str;
+  int length;
   char ret_buf[1024];
-  memset(ret_buf,0,length);
-  response->Get(ret_buf,length);
-  std::string ret_str(ret_buf);
-  logger.msg(Arc::INFO, "Returned msg from voms server: ", ret_str.c_str());
+  memset(ret_buf,0,1024);
+  int len;
+  do {
+    len = 1024;
+    response->Get(&ret_buf[0], len);
+    ret_str.append(ret_buf);
+    memset(ret_buf,0,1024);
+  }while(len == 1024);
+
+  logger.msg(Arc::INFO, "Returned msg from voms server: %s ", ret_str.c_str());
 
   if(response) delete response;
   return 0;
