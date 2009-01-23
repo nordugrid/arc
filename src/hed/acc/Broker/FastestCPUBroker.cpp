4dac8cb878890a0cefcdc9135086d993ef7cd323
Bjarte Mohn <bjarte.mohn@fysast.uu.se>
2009-01-23 11:48:48 +0000

    Implementation change, now filters on the SPECINT2006 benchmark values

--- src/hed/acc/Broker/FastestCPUBroker.cpp
+++ src/hed/acc/Broker/FastestCPUBroker.cpp
@@ -2,14 +2,34 @@
 #include <config.h>
 #endif
 
+#include <string.h>
 #include <algorithm>
-
 #include "FastestCPUBroker.h"
 
 namespace Arc {
     
   bool CheckCPUSpeeds(const ExecutionTarget& T1, const ExecutionTarget& T2){
-    return T1.CPUClockSpeed > T2.CPUClockSpeed;
+    double T1performance = 0;
+    double T2performance = 0;
+    std::list<Benchmark>::const_iterator iter;
+
+    for(iter = T1.Benchmarks.begin(); iter != T1.Benchmarks.end();iter++){
+      if(strcasestr(iter->Type.c_str(), "specint2006")){
+	T1performance = iter->Value;
+	break;
+      }
+    }
+
+    
+    for(iter = T2.Benchmarks.begin(); iter != T2.Benchmarks.end();iter++){
+      if(strcasestr(iter->Type.c_str(), "specint2006")){
+	T1performance = iter->Value;
+	break;
+      }
+    }
+
+    return T1performance > T2performance;
+
   }
 
   FastestCPUBroker::FastestCPUBroker(Config *cfg) : Broker(cfg){}
@@ -24,8 +44,20 @@ namespace Arc {
   }
 
   void FastestCPUBroker::SortTargets() {
-  	std::sort(PossibleTargets.begin(), PossibleTargets.end(), CheckCPUSpeeds);
-	TargetSortingDone = true;
+    
+    //Remove clusters with incomplete information for target sorting
+    std::vector<Arc::ExecutionTarget>::iterator iter = PossibleTargets.begin();
+    while(iter != PossibleTargets.end()){
+      if(!(iter->WaitingJobs != -1 && iter->TotalSlots != -1 && iter->FreeSlots != -1)){
+	iter = PossibleTargets.erase(iter);
+	continue;
+      }
+      iter++;
+    }
+    
+    std::sort(PossibleTargets.begin(), PossibleTargets.end(), CheckCPUSpeeds);
+    TargetSortingDone = true;
+
   }
 } // namespace Arc
 
