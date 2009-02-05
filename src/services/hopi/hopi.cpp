927992f5e2690e16dbb65a8a0d8c2f8a9827d71b
Aleksandr Konstantinov <akonstantinov@yahoo.com>
2009-02-05 22:39:53 +0000

    Added classes for handling chunked files. Not yet used.

--- src/services/hopi/hopi.cpp
+++ src/services/hopi/hopi.cpp
@@ -29,16 +29,177 @@ static Arc::Plugin *get_service(Arc::PluginArgument* arg)
     return new Hopi((Arc::Config*)(*srvarg));
 }
 
-Hopi::Hopi(Arc::Config *cfg):Service(cfg), logger(Arc::Logger::rootLogger, "Hopi")
+Arc::Logger Hopi::logger(Arc::Logger::rootLogger, "Hopi");
+
+class HopiFileChunks {
+ private:
+  static std::map<std::string,HopiFileChunks> files;
+  typedef std::list<std::pair<off_t,off_t> > chunks_t;
+  chunks_t chunks;
+  off_t size;
+  time_t last_accessed;
+  std::map<std::string,HopiFileChunks>::iterator self;
+  HopiFileChunks(void);
+ public:
+  void Add(off_t start,off_t end);
+  off_t Size(void) { return size; };
+  void Size(off_t size) { HopiFileChunks::size = size; };
+  static HopiFileChunks& Get(std::string path);
+  void Remove(void);
+  void Release(void);
+  bool Complete(void);
+};
+
+std::map<std::string,HopiFileChunks> HopiFileChunks::files;
+
+HopiFileChunks::HopiFileChunks(void):size(0),last_accessed(time(NULL)),self(files.end()) {
+}
+
+void HopiFileChunks::Remove(void) {
+  if(self == files.end()) return;
+  files.erase(self);
+  self=files.end();
+}
+
+HopiFileChunks& HopiFileChunks::Get(std::string path) {
+  std::map<std::string,HopiFileChunks>::iterator c = files.find(path);
+  if(c == files.end()) {
+    c=files.insert(std::pair<std::string,HopiFileChunks>(path,HopiFileChunks())).first;
+    c->second.self=c;
+  }
+  return (c->second);
+}
+
+void HopiFileChunks::Release(void) {
+  if(chunks.empty()) Remove();
+}
+
+void HopiFileChunks::Add(off_t start,off_t end) {
+  for(chunks_t::iterator chunk = chunks.begin();chunk!=chunks.end();++chunk) {
+    if((start >= chunk->first) && (start <= chunk->second)) {
+      // New chunk starts within existing chunk
+      if(end > chunk->second) {
+        // Extend chunk
+        chunk->second=end;
+        // Merge overlapping chunks
+        chunks_t::iterator chunk_ = chunk;
+        ++chunk_;
+        for(;chunk_!=chunks.end();) {
+          if(chunk->second < chunk_->first) break;
+          // Merge two chunks
+          if(chunk_->second > chunk->second) chunk->second=chunk_->second;
+          chunk_=chunks.erase(chunk_);
+        };
+      };
+    } else if((end >= chunk->first) && (end <= chunk->second)) {
+      // New chunk end within existing chunk
+      if(start < chunk->first) {
+        // Extend chunk
+        chunk->first=start;
+      };
+    } else if(end < chunk->first) {
+      // New chunk is between existing chunks or first chunk
+      chunks.insert(chunk,std::pair<off_t,off_t>(start,end));
+    };
+  };
+  // New chunk is last chunk or there are no chunks currently
+  chunks.insert(chunks.end(),std::pair<off_t,off_t>(start,end));
+}
+
+bool HopiFileChunks::Complete(void) {
+  if((chunks.size() == 1) &&
+     (chunks.begin()->first == 0) &&
+     (chunks.begin()->second == size)) return true;
+  return false;
+}
+
+class HopiFile {
+  int handle;
+  std::string path;
+  bool for_read;
+  bool slave;
+  HopiFileChunks& chunks;
+ public:
+  HopiFile(const std::string& path,bool for_read,bool slave);
+  ~HopiFile(void);
+  int Write(void* buf,off_t offset,int size);
+  int Read(void* buf,off_t offset,int size);
+  void Size(off_t size) { chunks.Size(size); };
+  operator bool(void) { return (handle != -1); };
+  bool operator!(void) { return (handle == -1); };
+};
+
+HopiFile::HopiFile(const std::string& path,bool for_read,bool slave):handle(-1),chunks(HopiFileChunks::Get(path)) {
+  HopiFile::for_read=for_read;
+  HopiFile::slave=slave;
+  HopiFile::path=path;
+  if(for_read) {
+    handle=open(path.c_str(),O_RDONLY);
+  } else {
+    if(slave) {
+      handle=open(path.c_str(),O_WRONLY);
+      if(handle == -1) {
+        if(errno == ENOENT) {
+          Hopi::logger.msg(Arc::ERROR, "Hopi SlaveMode is active, PUT is only allowed to existing files");
+        }
+      }
+    } else {
+      handle=open(path.c_str(),O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR);
+    }
+  }
+  if(handle == -1) {
+    Hopi::logger.msg(Arc::ERROR, Arc::StrError(errno));
+  }
+}
+  
+HopiFile::~HopiFile(void) {
+  if(handle != -1) {
+    close(handle);
+    if(!for_read) {
+      if(chunks.Complete()) {
+        if(slave) {
+          unlink(path.c_str());
+        }
+        chunks.Remove();
+        return;
+      }
+    }
+  }
+  chunks.Release();
+}
+
+int HopiFile::Write(void* buf,off_t offset,int size) {
+  if(handle == -1) return -1;
+  if(for_read) return -1;
+  int s = size;
+  if(lseek(handle,offset,SEEK_SET) != offset) return 0;
+  for(;s>0;) {
+    ssize_t l = write(handle,buf,s);
+    if(l == -1) return -1;
+    chunks.Add(offset,offset+l);
+    s-=l; buf=((char*)buf)+l; offset+=l;
+  }
+  return size;
+}
+
+int HopiFile::Read(void* buf,off_t offset,int size) {
+  if(handle == -1) return -1;
+  if(!for_read) return -1;
+  if(lseek(handle,offset,SEEK_SET) != offset) return 0;
+  return read(handle,buf,size);
+}
+
+
+Hopi::Hopi(Arc::Config *cfg):Service(cfg)
 {
     logger.msg(Arc::INFO, "Hopi Initialized"); 
     doc_root = (std::string)((*cfg)["DocumentRoot"]);
-    if (doc_root.empty() == 1) {
+    if (doc_root.empty()) {
         doc_root = "./";
     }
     logger.msg(Arc::INFO, "Hopi DocumentRoot is " + doc_root);
     slave_mode = (std::string)((*cfg)["SlaveMode"]);
-    if (slave_mode.empty() == 1) {
+    if (slave_mode.empty()) {
         slave_mode = "0";
     }  
     if (slave_mode == "1") logger.msg(Arc::INFO, "Hopi SlaveMode is on!");
--- src/services/hopi/hopi.h
+++ src/services/hopi/hopi.h
@@ -10,12 +10,12 @@ namespace Hopi {
 
 class Hopi: public Arc::Service {
     protected:
-        Arc::Logger logger;
         std::string doc_root;
         std::string slave_mode;
         Arc::PayloadRawInterface* Get(const std::string &path, const std::string &base_url);
         Arc::MCC_Status Put(const std::string &path, Arc::MessagePayload &buf);
     public:
+        static Arc::Logger logger;
         Hopi(Arc::Config *cfg);
         virtual ~Hopi(void);
         virtual Arc::MCC_Status process(Arc::Message &inmsg, Arc::Message &outmsg);
