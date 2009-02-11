34aa22aba43751ef4bb47dc0d652cecfaf545d60
Mattias Ellert <mattias.ellert@physics.uu.se>
2009-02-11 14:15:11 +0000

    use UserConfig class

--- src/clients/wsrf/arcinfo.cpp
+++ src/clients/wsrf/arcinfo.cpp
@@ -14,23 +14,14 @@
 #include <arc/infosys/InformationInterface.h>
 #include <arc/message/MCC.h>
 #include <arc/client/ClientInterface.h>
+#include <arc/client/UserConfig.h>
 #include <arc/OptionParser.h>
 
-//! A prototype client for service status queries.
-/*! A prototype command line tool for service status queries to any ARC1
-  service through LIDI. In the name, "ap" means "Arc Prototype".
-  
-  Usage:
-  arcinfo <service-URL>
-
-  Arguments:
-  <service-URL> The URL of ARC1 service.
-*/
-
-int main(int argc, char* argv[]){
+int main(int argc, char *argv[]) {
 
   setlocale(LC_ALL, "");
 
+  Arc::Logger logger(Arc::Logger::getRootLogger(), "arcinfo");
   Arc::LogStream logcerr(std::cerr);
   Arc::Logger::getRootLogger().addDestination(logcerr);
   Arc::Logger::getRootLogger().setThreshold(Arc::WARNING);
@@ -39,25 +30,10 @@ int main(int argc, char* argv[]){
 
   Arc::OptionParser options(istring("service_url"));
 
-  std::string proxy_path;
-  options.AddOption('P', "proxy", istring("path to proxy file"),
-		    istring("path"), proxy_path);
-
-  std::string cert_path;
-  options.AddOption('C', "certifcate", istring("path to certificate file"),
-		    istring("path"), cert_path);
-
-  std::string key_path;
-  options.AddOption('K', "key", istring("path to private key file"),
-		    istring("path"), key_path);
-
-  std::string ca_dir;
-  options.AddOption('A', "cadir", istring("path to CA directory"),
-		    istring("directory"), ca_dir);
-
-  std::string config_path;
-  options.AddOption('c', "config", istring("path to config file"),
-		    istring("path"), config_path);
+  std::string conffile;
+  options.AddOption('z', "conffile",
+		    istring("configuration file (default ~/.arc/client.xml)"),
+		    istring("filename"), conffile);
 
   std::string debug;
   options.AddOption('d', "debug",
@@ -78,48 +54,75 @@ int main(int argc, char* argv[]){
     return 0;
   }
 
-  try{
-    if (params.size()!=1)
+  Arc::UserConfig usercfg(conffile);
+  if (!usercfg) {
+    logger.msg(Arc::ERROR, "Failed configuration initialization");
+    return 1;
+  }
+
+  if (debug.empty() && usercfg.ConfTree()["Debug"]) {
+    debug = (std::string)usercfg.ConfTree()["Debug"];
+    Arc::Logger::getRootLogger().setThreshold(Arc::string_to_level(debug));
+  }
+
+  if (version) {
+    std::cout << Arc::IString("%s version %s", "arcstat", VERSION)
+	      << std::endl;
+    return 0;
+  }
+
+  try {
+    if (params.size() != 1)
       throw std::invalid_argument("Wrong number of arguments!");
     std::list<std::string>::iterator it = params.begin();
     Arc::URL url(*it);
-    if(!url)
+    if (!url)
       throw std::invalid_argument("Can't parse specified URL");
     Arc::MCCConfig cfg;
-    if(!proxy_path.empty()) cfg.AddProxy(proxy_path);
-    if(!key_path.empty()) cfg.AddPrivateKey(key_path);
-    if(!cert_path.empty()) cfg.AddCertificate(cert_path);
-    if(!ca_dir.empty()) cfg.AddCADir(ca_dir);
-    cfg.GetOverlay(config_path);
-    Arc::ClientSOAP client(cfg,url);
+
+    Arc::NS ns;
+    Arc::XMLNode cred(ns, "cred");
+    usercfg.ApplySecurity(cred);
+
+    if (cred["ProxyPath"])
+      cfg.AddProxy((std::string)cred["ProxyPath"]);
+    if (cred["CertificatePath"])
+      cfg.AddCertificate((std::string)cred["CertificatePath"]);
+    if (cred["KeyPath"])
+      cfg.AddPrivateKey((std::string)cred["KeyPath"]);
+    if (cred["CACertificatesDir"])
+      cfg.AddCADir((std::string)cred["CACertificatesDir"]);
+
+    Arc::ClientSOAP client(cfg, url);
     Arc::InformationRequest inforequest;
     Arc::PayloadSOAP request(*(inforequest.SOAP()));
-    Arc::PayloadSOAP* response;
-    Arc::MCC_Status status = client.process(&request,&response);
-    if(!status) {
-      if(response) delete response;
+    Arc::PayloadSOAP *response;
+    Arc::MCC_Status status = client.process(&request, &response);
+    if (!status) {
+      if (response)
+	delete response;
       throw std::runtime_error("Request failed");
-    };
-    if(!response) {
+    }
+    if (!response)
       throw std::runtime_error("There was no response");
-    };
     Arc::InformationResponse inforesponse(*response);
-    if(!inforesponse) {
+    if (!inforesponse) {
       std::string s;
       response->GetXML(s);
       delete response;
       std::cerr << "Wrong response: \n" << s << "\n" << std::endl;
       throw std::runtime_error("Response is not valid");
-    };
+    }
     std::list<Arc::XMLNode> results = inforesponse.Result();
-    for(std::list<Arc::XMLNode>::iterator r = results.begin();r!=results.end();++r) {
+    for (std::list<Arc::XMLNode>::iterator r = results.begin();
+	 r != results.end(); ++r) {
       std::string s;
-      r->GetXML(s,true);
+      r->GetXML(s, true);
       std::cout << "\n" << s << std::endl;
-    };
+    }
     return EXIT_SUCCESS;
   }
-  catch (std::exception& err){
+  catch (std::exception& err) {
     std::cerr << "ERROR: " << err.what() << std::endl;
     return EXIT_FAILURE;
   }
