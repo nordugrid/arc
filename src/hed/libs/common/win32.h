b454cb82ff2f330e5f08cd71124e65eabe98ada5
Thomas Fragat <Fragat@mycompany.com>
2009-03-10 16:04:40 +0000

    Added struct passwd which is included in pwd.h on Linux, but doesn't exist on Windows since the file systems are quite different

--- src/hed/libs/common/win32.h
+++ src/hed/libs/common/win32.h
@@ -25,5 +25,18 @@
 #define chmod(x,y) (0)
 #define symlink(x,y) (0)
 
+// pwd.h does not exist on windows
+struct passwd{
+  char *pw_name;
+  char *pw_passwd;
+  int pw_uid;
+  int pw_gid;
+  char *pw_age;
+  char *pw_comment;
+  char *pw_gecos;
+  char *pw_dir;
+  char *pw_shell;
+};
+
 std::string GetOsErrorMessage(void);
 #endif
