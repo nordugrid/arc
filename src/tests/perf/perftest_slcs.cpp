fb913f2ef0876168f30ace0ef427a56b1a6b3e65
Weizhong Qiang <Qiang@mycompany.com>
2009-04-25 15:12:49 +0000

    add some comment about perf utilities; add perf test against slcs service

--- src/tests/perf/perftest_slcs.cpp
+++ src/tests/perf/perftest_slcs.cpp
@@ -2,7 +2,7 @@
 #include <config.h>
 #endif
 
-// perftest.cpp
+// perftest_slcs.cpp
 
 #include <iostream>
 #include <fstream>
@@ -18,6 +18,10 @@
 #include <arc/message/PayloadSOAP.h>
 #include <arc/message/MCC.h>
 #include <arc/client/ClientInterface.h>
+#include <arc/client/ClientSAML2SSO.h>
+#include <arc/credential/Credential.h>
+
+#include <arc/xmlsec/XmlSecUtils.h>
 
 // Some global shared variables...
 Glib::Mutex* mutex;
@@ -30,6 +34,9 @@ Glib::TimeVal completedTime;
 Glib::TimeVal failedTime;
 Glib::TimeVal totalTime;
 std::string url_str;
+std::string idp_name;
+std::string username;
+std::string password;
 
 // Round off a double to an integer.
 int Round(double x){
@@ -47,46 +54,51 @@ void sendRequests(){
   Glib::TimeVal tAfter;
   bool connected;
 
-  //std::string url_str("https://127.0.0.1:60000/echo");
   Arc::URL url(url_str);
 
   Arc::MCCConfig mcc_cfg;
-  mcc_cfg.AddPrivateKey("../echo/userkey-nopass.pem");
-  mcc_cfg.AddCertificate("../echo/usercert.pem");
-  mcc_cfg.AddCAFile("../echo/testcacert.pem");
+  mcc_cfg.AddCAFile("../../services/slcs/cacert2.pem");
   mcc_cfg.AddCADir("../echo/certificates");
 
-  Arc::NS echo_ns; echo_ns["echo"]="urn:echo";
-  
+  Arc::NS slcs_ns;
+  slcs_ns["slcs"] = "http://www.nordugrid.org/schemas/slcs";
+ 
   while(run){
-    
     // Create a Client.
-    Arc::ClientSOAP *client = NULL;
-    client = new Arc::ClientSOAP(mcc_cfg,url);
+    Arc::ClientSOAPwithSAML2SSO *client = NULL;
+    client = new Arc::ClientSOAPwithSAML2SSO(mcc_cfg,url);
 
     connected=true;
-    //If session is not reused, then comment the following line
     //while(run and connected){
-      // Prepare the request.
-      Arc::PayloadSOAP req(echo_ns);
-      req.NewChild("echo").NewChild("say")="HELLO";
+      // Prepare the request and time it.
+      Arc::PayloadSOAP req(slcs_ns);
 
-      // Send the request and time it.
       tBefore.assign_current_time();
-      Arc::PayloadSOAP* resp = NULL;
 
-      //std::string str;
-      //req.GetXML(str);
-      //std::cout<<"request: "<<str<<std::endl;
-      Arc::MCC_Status status = client->process(&req,&resp);
+      Arc::Time t;
+      int lifetime = 12;
+      int keysize = 1024;
+      Arc::Credential request(t, Arc::Period(lifetime * 3600), keysize, "EEC");
+      std::string cert_req_str;
+      if (!request.GenerateRequest(cert_req_str))
+        throw std::runtime_error("Failed to generate certificate request");
+
+      std::string private_key;
+      request.OutputPrivatekey(private_key);
+
+      req.NewChild("GetSLCSCertificateRequest").NewChild("X509Request") = cert_req_str;
+
+      // Send the request
+      Arc::PayloadSOAP* resp = NULL;
+      Arc::MCC_Status status = client->process(&req,&resp, idp_name, username, password);
 
       tAfter.assign_current_time();
-      
+ 
       if(!status) {
         // Request failed.
         failedRequests++;
         failedTime+=tAfter-tBefore;
-	    connected=false;
+	connected=false;
       } else {
         if(resp == NULL) {
           // Response was not SOAP or no response at all.
@@ -94,9 +106,7 @@ void sendRequests(){
           failedTime+=tAfter-tBefore;
           connected=false;
         } else {
-          //std::string xml;
-          //resp->GetXML(xml);
-          if (std::string((*resp)["echoResponse"]["hear"]).size()==0){
+          if (std::string((*resp)["GetSLCSCertificateResponse"]["X509Certificate"]).size()==0){
             // The response was not what it should be.
             failedRequests++;
             failedTime+=tAfter-tBefore;
@@ -106,6 +116,9 @@ void sendRequests(){
             // Everything worked just fine!
             completedRequests++;
             completedTime+=tAfter-tBefore;
+          
+            std::string cert_str = (std::string)((*resp)["GetSLCSCertificateResponse"]["X509Certificate"]);
+            std::string ca_str = (std::string)((*resp)["GetSLCSCertificateResponse"]["CACertificate"]);
           }
         }
       }
@@ -151,14 +164,17 @@ int main(int argc, char* argv[]){
     Arc::Logger::getRootLogger().addDestination(logcerr);
   }
   // Extract command line arguments.
-  if (argc!=4){
+  if (argc!=7){
     std::cerr << "Wrong number of arguments!" << std::endl
 	      << std::endl
 	      << "Usage:" << std::endl
-	      << "perftest [-c config] [-d debug] url threads duration" << std::endl
+	      << "perftest [-c config] [-d debug] url idpname username password threads duration" << std::endl
 	      << std::endl
 	      << "Arguments:" << std::endl
-	      << "url     The url of the service." << std::endl
+	      << "url     The url of the slcs service." << std::endl
+              << "idpname   The name of the SP, e.g. https://squark.uio.no/idp/shibboleth" << std::endl
+              << "username  The username to IdP " << std::endl
+              << "password  The password to IdP   " << std::endl
 	      << "threads  The number of concurrent requests." << std::endl
 	      << "duration The duration of the test in seconds." << std::endl
 	      << "config   The file containing client chain XML configuration with " << std::endl
@@ -169,8 +185,13 @@ int main(int argc, char* argv[]){
     exit(EXIT_FAILURE);
   }
   url_str = std::string(argv[1]);
-  numberOfThreads = atoi(argv[2]);
-  duration = atoi(argv[3]);
+  idp_name = std::string(argv[2]);
+  username = std::string(argv[3]);
+  password = std::string(argv[4]);
+  numberOfThreads = atoi(argv[5]);
+  duration = atoi(argv[6]);
+
+  Arc::init_xmlsec();
 
   // Start threads.
   run=true;
@@ -178,8 +199,10 @@ int main(int argc, char* argv[]){
   //Glib::thread_init();
   mutex=new Glib::Mutex;
   threads = new Glib::Thread*[numberOfThreads];
-  for (i=0; i<numberOfThreads; i++)
+  for (i=0; i<numberOfThreads; i++) {
     threads[i]=Glib::Thread::create(sigc::ptr_fun(sendRequests),true);
+    //Glib::usleep(1000000);
+  }
 
   // Sleep while the threads are working.
   Glib::usleep(duration*1000000);
@@ -210,8 +233,8 @@ int main(int argc, char* argv[]){
 	    << failedRequests << " ("
 	    << Round(failedRequests*100.0/totalRequests)
 	    << "%)" << std::endl;
-  std::cout << "Completed requests per second: "
-            << Round(completedRequests/duration)
+  std::cout << "Completed requests per min: "
+            << Round(((double)completedRequests)/duration*60)
             << std::endl;
   std::cout << "Average response time for all requests: "
 	    << Round(1000*totalTime.as_double()/totalRequests)
@@ -225,6 +248,8 @@ int main(int argc, char* argv[]){
 	      << Round(1000*failedTime.as_double()/failedRequests)
 	      << " ms" << std::endl;
   std::cout << "========================================" << std::endl;
+
+  Arc::final_xmlsec();
 
   return 0;
 }
