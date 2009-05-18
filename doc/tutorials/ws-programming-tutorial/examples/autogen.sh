d9db8fb449ec5bf37570b4ab22c38db821d62b1d
Michael Glodek <glodek@inb.uni-luebeck.de>
2009-05-18 12:43:51 +0000

    Optimized autogen.sh and configure.ac file.

--- doc/tutorials/ws-programming-tutorial/examples/autogen.sh
+++ doc/tutorials/ws-programming-tutorial/examples/autogen.sh
@@ -1,29 +1,29 @@
+#!/bin/sh
 
-# CREATES config.h
-autoheader
-touch NEWS README AUTHORS ChangeLog
 
-# You need a stamp-h file in your project to ensure that automake regenerates config.h from config.h.in. 
-# Type 'touch stamp-h' to add this file to your project.
-touch stamp-h
+set -x
 
-# Adds aclocal.m4 to directory. Defines some m4 macros used by the auto tools.
-# These tools use the m4 programming language. aclocal adds aclocal.m4 to your project directory,
-# which contains some m4 macros which are neede 
-touch aclocal.m4
-aclocal
+cleanup() {
+    find . -type d -name autom4te.cache -print | xargs rm -rf \;
+    find . -type f \( -name missing -o -name install-sh \
+        -o -name mkinstalldirs \
+        -o -name depcomp -o -name ltmain.sh -o -name configure \
+        -o -name config.sub -o -name config.guess \
+        -o -name Makefile.in -o -name config.h.in -o -name aclocal.m4 \
+        -o -name autoscan.log -o -name configure.scan -o -name config.log \
+        -o -name config.status -o -name config.h -o -name stamp-h1 \
+        -o -name Makefile -o -name libtool \) \
+        -print | xargs rm -f
+}
 
-# ltconfig runs a series of configuration tests, then creates a system-specific libtool in the
-# current directory. 
-#ltconfig
 
-# Creates configure from configure.ac 
-autoconf
 
-# Creates Makefile.in from Makefile.am 
-automake  --add-missing
+# Refresh GNU autotools toolchain.
+echo Cleaning autotools files...
+cleanup
 
-# Creates Makefile from Makefile.in
-./configure  --prefix=/tmp/arc/tutorial/
- 
-#make
+type glibtoolize > /dev/null 2>&1 && export LIBTOOLIZE=glibtoolize
+
+echo Running autoreconf...
+autoreconf --verbose --force --install
+
