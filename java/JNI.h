39ca8411fabe3fd8bf30300950b0cd1061cfb776
Gabor Roczei <roczei@niif.hu>
2009-05-19 17:20:07 +0000

    Protect for multiple include on Windows

--- java/JNI.h
+++ java/JNI.h
@@ -2,10 +2,13 @@
 #include <config.h>
 #endif
 
+#ifndef __JNI_INCLUDE_ARC__ 
+#define __JNI_INCLUDE_ARC__ 
 #ifdef HAVE_JNI_H
 #include <jni.h>
 #else
 #ifdef HAVE_JAVAVM_JNI_H
 #include <JavaVM/jni.h>
+#endif
 #endif
 #endif
