d3d41b09b550a18dda11ad4c15d0ebc263f141e0
Jon Kerr Nilsen <jonkni@uio.no>
2009-06-01 19:34:53 +0000

    New interface, loops over increasing message size

--- src/tests/perf/perftest_msgsize.cpp
+++ src/tests/perf/perftest_msgsize.cpp
@@ -1,3 +1,5 @@
+// -*- indent-tabs-mode: nil -*-
+
 #ifdef HAVE_CONFIG_H
 #include <config.h>
 #endif
@@ -6,6 +8,7 @@
 
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <string>
 #include <stdlib.h>
 #include <glibmm/thread.h>
@@ -31,8 +34,11 @@ Glib::TimeVal failedTime;
 Glib::TimeVal totalTime;
 std::string url_str;
 bool alwaysReconnect = false;
-bool tcp_nodelay = false;
-std::string size;
+bool printTimings = false;
+bool tcpNoDelay = false;
+int start;
+int stop;
+int steplength;
 
 // Round off a double to an integer.
 int Round(double x){
@@ -52,7 +58,7 @@ void sendRequests(){
 
   //std::string url_str("https://127.0.0.1:60000/echo");
   Arc::URL url(url_str);
-
+  if(tcpNoDelay) url.AddOption("tcpnodelay","yes");
   Arc::MCCConfig mcc_cfg;
   mcc_cfg.AddPrivateKey("../echo/testuserkey-nopass.pem");
   mcc_cfg.AddCertificate("../echo/testusercert.pem");
@@ -61,34 +67,41 @@ void sendRequests(){
 
   Arc::NS echo_ns; echo_ns["echo"]="urn:echo";
   
+  std::string size;
   while(run){
-    
-    // Create a Client.
     Arc::ClientSOAP *client = NULL;
-    client = new Arc::ClientSOAP(mcc_cfg,url);
-
-    connected=true;
-    while(run and connected){
+    connected=false;
+    for(int i=start; i<stop; i+=steplength){
+      // Create a Client.
+      if(!connected){
+        client = NULL;
+        client = new Arc::ClientSOAP(mcc_cfg,url);
+        connected = true;
+        //client.NoDelay(tcpNoDelay);
+      }
+      
       // Prepare the request.
       Arc::PayloadSOAP req(echo_ns);
-      req.NewChild("size").NewChild("size")=std::string(size);
-
+      std::stringstream sstr;
+      sstr << i;
+      size = sstr.str();
+      req.NewChild("size").NewChild("size")=size;
       // Send the request and time it.
       tBefore.assign_current_time();
       Arc::PayloadSOAP* resp = NULL;
-
+       
       //std::string str;
       //req.GetXML(str);
       //std::cout<<"request: "<<str<<std::endl;
       Arc::MCC_Status status = client->process(&req,&resp);
-
+      
       tAfter.assign_current_time();
       
       if(!status) {
         // Request failed.
         failedRequests++;
         failedTime+=tAfter-tBefore;
-	    connected=false;
+        connected=false;
       } else {
         if(resp == NULL) {
           // Response was not SOAP or no response at all.
@@ -108,6 +121,7 @@ void sendRequests(){
             // Everything worked just fine!
             completedRequests++;
             completedTime+=tAfter-tBefore;
+            if (printTimings) std::cout << size << " " << tAfter.as_double()-tBefore.as_double() << std::endl;
           }
         }
       }
@@ -115,6 +129,7 @@ void sendRequests(){
       if(alwaysReconnect) connected=false;
     }
     if(client) delete client;
+  
   }
 
   // Update global variables.
@@ -138,7 +153,7 @@ int main(int argc, char* argv[]){
   Arc::LogStream logcerr(std::cerr);
 
   // Process options - quick hack, must use Glib options later
-  while(argc >= 3) {
+  while(argc >= 7) {
     if(strcmp(argv[1],"-c") == 0) {
       config_file = argv[2];
       argv[2]=argv[0]; argv+=2; argc-=2;
@@ -147,6 +162,10 @@ int main(int argc, char* argv[]){
       argv[2]=argv[0]; argv+=2; argc-=2;
     } else if(strcmp(argv[1],"-r") == 0) {
       alwaysReconnect=true; argv+=1; argc-=1;
+    } else if(strcmp(argv[1],"-v") == 0) {
+      printTimings=true; argv+=1; argc-=1;
+    } else if(strcmp(argv[1],"-t") == 0) {
+      tcpNoDelay=true; argv+=1; argc-=1;
     } else {
       break;
     };
@@ -156,30 +175,36 @@ int main(int argc, char* argv[]){
     Arc::Logger::getRootLogger().addDestination(logcerr);
   }
   // Extract command line arguments.
-  if (argc!=5){
+  if (argc!=7){
     std::cerr << "Wrong number of arguments!" << std::endl
-	      << std::endl
-	      << "Usage:" << std::endl
-	      << "perftest [-c config] [-d debug] [-r] url threads duration" << std::endl
-	      << std::endl
-	      << "Arguments:" << std::endl
-	      << "url       The url of the service." << std::endl
-	      << "threads   The number of concurrent requests." << std::endl
-	      << "duration  The duration of the test in seconds." << std::endl
-	      << "size      The size of the response from the echo servise." << std::endl
-	      << "-c config The file containing client chain XML configuration with " << std::endl
-              << "          'soap' entry point and HOSTNAME, PORTNUMBER and PATH " << std::endl
-              << "           keyword for hostname, port and HTTP path of 'echo' service." << std::endl
-	      << "-d debug   The textual representation of desired debug level. Available " << std::endl
-              << "           levels: VERBOSE, DEBUG, INFO, WARNING, ERROR, FATAL." << std::endl
-	      << "-r         If specified close connection and reconnect after " << std::endl
-              << "           every request." << std::endl;
+              << std::endl
+              << "Usage:" << std::endl
+              << "perftest [-c config] [-d debug] [-r] [-t] [-v] url threads duration start stop steplength" << std::endl
+              << std::endl
+              << "Arguments:" << std::endl
+              << "url        The url of the service." << std::endl
+              << "threads   The number of concurrent requests." << std::endl
+              << "duration  The duration of the test in seconds." << std::endl
+              << "start      The size of the first response from the echo service." << std::endl
+              << "stop       The size of the last response from the echo service." << std::endl
+              << "steplength The increase of size per call to the echo service." << std::endl
+              << "-c config  The file containing client chain XML configuration with " << std::endl
+              << "           'soap' entry point and HOSTNAME, PORTNUMBER and PATH " << std::endl
+              << "            keyword for hostname, port and HTTP path of 'echo' service." << std::endl
+              << "-d debug   The textual representation of desired debug level. Available " << std::endl
+              << "            levels: VERBOSE, DEBUG, INFO, WARNING, ERROR, FATAL." << std::endl
+              << "-r         If specified close connection and reconnect after " << std::endl
+              << "            every request." << std::endl
+              << "-t         Toggles TCP_NODELAY option " << std::endl
+              << "-v         If specified print out timings for each iteration " << std::endl;
     exit(EXIT_FAILURE);
   }
   url_str = std::string(argv[1]);
   numberOfThreads = atoi(argv[2]);
   duration = atoi(argv[3]);
-  size = argv[4];
+  start = atoi(argv[4]);
+  stop = atoi(argv[5]);
+  steplength = atoi(argv[6]);
 
   // Start threads.
   run=true;
@@ -204,35 +229,35 @@ int main(int argc, char* argv[]){
   totalTime = completedTime+failedTime;
   std::cout << "========================================" << std::endl;
   std::cout << "URL: "
-	    << url_str << std::endl;
+            << url_str << std::endl;
   std::cout << "Number of threads: "
-	    << numberOfThreads << std::endl;
+            << numberOfThreads << std::endl;
   std::cout << "Duration: "
-	    << duration << " s" << std::endl;
+            << duration << " s" << std::endl;
   std::cout << "Number of requests: "
-	    << totalRequests << std::endl;
+            << totalRequests << std::endl;
   std::cout << "Completed requests: "
-	    << completedRequests << " ("
-	    << Round(completedRequests*100.0/totalRequests)
-	    << "%)" << std::endl;
+            << completedRequests << " ("
+            << Round(completedRequests*100.0/totalRequests)
+            << "%)" << std::endl;
   std::cout << "Failed requests: "
-	    << failedRequests << " ("
-	    << Round(failedRequests*100.0/totalRequests)
-	    << "%)" << std::endl;
+            << failedRequests << " ("
+            << Round(failedRequests*100.0/totalRequests)
+            << "%)" << std::endl;
   std::cout << "Completed requests per second: "
             << Round(completedRequests/duration)
             << std::endl;
   std::cout << "Average response time for all requests: "
-	    << Round(1000*totalTime.as_double()/totalRequests)
-	    << " ms" << std::endl;
+            << Round(1000*totalTime.as_double()/totalRequests)
+            << " ms" << std::endl;
   if (completedRequests!=0)
     std::cout << "Average response time for completed requests: "
-	      << Round(1000*completedTime.as_double()/completedRequests)
-	      << " ms" << std::endl;
+              << Round(1000*completedTime.as_double()/completedRequests)
+              << " ms" << std::endl;
   if (failedRequests!=0)
     std::cout << "Average response time for failed requests: "
-	      << Round(1000*failedTime.as_double()/failedRequests)
-	      << " ms" << std::endl;
+              << Round(1000*failedTime.as_double()/failedRequests)
+              << " ms" << std::endl;
   std::cout << "========================================" << std::endl;
 
   return 0;
