6a4112bc13c05858a6419e45b7e84d12c853456d
Weizhong Qiang <Qiang@mycompany.com>
2009-08-03 14:35:15 +0000

    the authorization service and client support xacml policy, and saml2.0 profile for xacml; arc1 only

--- src/clients/charon/arcdecision.cpp
+++ src/clients/charon/arcdecision.cpp
@@ -4,18 +4,36 @@
 
 #include <stdexcept>
 
-#include <arc/XMLNode.h>
-#include <arc/ArcConfig.h>
-#include <arc/URL.h>
-#include <arc/message/MCC.h>
-#include <arc/client/ClientInterface.h>
-
 #include <iostream>
 #include <fstream>
 #include <cstdlib>
+#include <glibmm/stringutils.h>
+#include <glibmm/fileutils.h>
+#include <glibmm.h>
 
+#include <arc/XMLNode.h>
+#include <arc/ArcConfig.h>
+#include <arc/StringConv.h>
+#include <arc/URL.h>
+#include <arc/User.h>
+#include <arc/Utils.h>
+#include <arc/client/UserConfig.h>
+#include <arc/message/MCC.h>
+#include <arc/client/ClientInterface.h>
 #include <arc/ArcLocation.h>
 #include <arc/OptionParser.h>
+#include <arc/GUID.h>
+#include <arc/credential/Credential.h>
+
+//#include <arc/xmlsec/XmlSecUtils.h>
+//#include <arc/xmlsec/XMLSecNode.h>
+
+#define SAML_NAMESPACE "urn:oasis:names:tc:SAML:2.0:assertion"
+#define SAMLP_NAMESPACE "urn:oasis:names:tc:SAML:2.0:protocol"
+#define XACML_SAMLP_NAMESPACE "urn:oasis:xacml:2.0:saml:protocol:schema:os"
+
+#define XENC_NAMESPACE   "http://www.w3.org/2001/04/xmlenc#"
+#define DSIG_NAMESPACE   "http://www.w3.org/2000/09/xmldsig#"
 
 int main(int argc, char* argv[]){
 
@@ -28,6 +46,19 @@ int main(int argc, char* argv[]){
     Arc::ArcLocation::Init(argv[0]);
 
     Arc::OptionParser options(istring("service_url request_file"));
+
+    std::string service_url;
+    options.AddOption('s', "service",
+                      istring("url of the policy decision service"),
+                      istring("string"), service_url);
+
+    std::string request_path;
+    options.AddOption('r', "request",
+                      istring("path to request file"),
+                      istring("path"), request_path);
+
+    bool use_saml = false;
+    options.AddOption('p', "saml", istring("use SAML 2.0 profile of XACML v2.0 to contact the service"), use_saml);
 
     std::string config_path;
     options.AddOption('c', "config",
@@ -45,8 +76,18 @@ int main(int argc, char* argv[]){
 
     std::list<std::string> params = options.Parse(argc, argv);
 
+    Arc::UserConfig usercfg(config_path, true);
+    if (!usercfg) {
+      std::cerr<<"Failed configuration initialization"<<std::endl;
+      return EXIT_FAILURE;
+    }
+
     if (!debug.empty())
-        Arc::Logger::getRootLogger().setThreshold(Arc::string_to_level(debug));
+      Arc::Logger::getRootLogger().setThreshold(Arc::string_to_level(debug));
+    else if (debug.empty() && usercfg.ConfTree()["Debug"]) {
+      debug = (std::string)usercfg.ConfTree()["Debug"];
+      Arc::Logger::getRootLogger().setThreshold(Arc::string_to_level(debug));
+    }
 
     if (version) {
         std::cout << Arc::IString("%s version %s", "arcdecision", VERSION)
@@ -55,32 +96,85 @@ int main(int argc, char* argv[]){
     }
 
     try{
-        if (params.size()!=2) {
-            throw std::invalid_argument("Wrong number of arguments!");
+      std::string key_path = (std::string)usercfg.ConfTree()["KeyPath"];
+      std::string cert_path = (std::string)usercfg.ConfTree()["CertificatePath"];
+      std::string proxy_path = (std::string)usercfg.ConfTree()["ProxyPath"];
+      std::string ca_dir = (std::string)usercfg.ConfTree()["CACertificatesDir"];
+      std::string ca_file = (std::string)usercfg.ConfTree()["CACertificatePath"];
+      //Create a SOAP client
+      Arc::URL url(service_url);
+      if(!url) throw std::invalid_argument("Can't parse specified URL");
+      Arc::MCCConfig cfg;
+      if(!proxy_path.empty()) cfg.AddProxy(proxy_path);
+      if(!cert_path.empty()) cfg.AddCertificate(cert_path);
+      if(!key_path.empty()) cfg.AddPrivateKey(key_path);
+      if(!ca_dir.empty()) cfg.AddCADir(ca_dir);
+      if(!ca_file.empty()) cfg.AddCAFile(ca_file);
+
+      Arc::ClientSOAP client(cfg,url);
+
+      // Read the request from file to string
+      std::ifstream requestfile(request_path.c_str());
+      std::string request_str;
+      if (!requestfile)
+        throw std::invalid_argument("Could not open " + request_path);
+      getline(requestfile, request_str, '\0');
+      Arc::XMLNode request_xml(request_str); 
+
+      if(use_saml) {
+        Arc::NS ns;
+        ns["saml"] = SAML_NAMESPACE;
+        ns["samlp"] = SAMLP_NAMESPACE;
+        ns["xacml-samlp"] = XACML_SAMLP_NAMESPACE; 
+        Arc::XMLNode authz_query(ns, "xacml-samlp:XACMLAuthzDecisionQuery");
+        std::string query_id = Arc::UUID();
+        authz_query.NewAttribute("ID") = query_id;
+        Arc::Time t;
+        std::string current_time = t.str(Arc::UTCTime);
+        authz_query.NewAttribute("IssueInstant") = current_time;
+        authz_query.NewAttribute("Version") = std::string("2.0");
+
+        Arc::Credential cred(cert_path.empty() ? proxy_path : cert_path, 
+           cert_path.empty() ? proxy_path : key_path, ca_dir, ca_file);
+        std::string local_dn_str = cred.GetDN();
+        std::string local_dn = Arc::convert_to_rdn(local_dn_str);
+        std::string issuer_name = local_dn;
+        authz_query.NewChild("saml:Issuer") = issuer_name;
+
+        authz_query.NewAttribute("InputContextOnly") = std::string("false");
+        authz_query.NewAttribute("ReturnContext") = std::string("true");
+
+        authz_query.NewChild(request_xml);
+
+        Arc::NS req_ns;
+        Arc::SOAPEnvelope req_env(req_ns);
+        req_env.NewChild(authz_query);
+        Arc::PayloadSOAP req(req_env);
+
+        Arc::PayloadSOAP *resp = NULL;
+        Arc::MCC_Status status = client.process(&req,&resp);
+        if(!status) {
+          std::cerr<<"Policy Decision Service invokation failed"<<std::endl;
+          throw std::runtime_error("Policy Decision Service invokation failed");
         }
+        if(resp == NULL) {
+          std::cerr<<"There was no SOAP response"<<std::endl;
+          throw std::runtime_error("There was no SOAP response");
+        }
+        std::string str;     
+        resp->GetXML(str);
+        std::cout<<"Response: "<<str<<std::endl;
 
-        //Create a SOAP client
-        std::list<std::string>::iterator it = params.begin();
-        Arc::URL url(*it++);
-        std::string requestfilestr = *it++;
-        if(!url)
-            throw std::invalid_argument("Can't parse specified URL");
-        Arc::MCCConfig cfg;
-        if(config_path != "") cfg.GetOverlay(config_path);
+        std::string authz_res = (std::string)((*resp)["samlp:Response"]["saml:Assertion"]["xacml-saml:XACMLAuthzDecisionStatement"]["xacml-context:Response"]["xacml-context:Result"]["xacml-context:Decision"]);
 
-        Arc::ClientSOAP client(cfg,url);
+        if(resp) delete resp;
 
-        // Read the request from file to string
-        std::ifstream requestfile(requestfilestr.c_str());
-        std::string request_str;
-        if (!requestfile)
-            throw std::invalid_argument("Could not open " + requestfilestr);
-        getline(requestfile, request_str, '\0');
-        Arc::XMLNode request_xml(request_str); 
+        if(authz_res == "Permit") { std::cout<<"Authorized from remote pdp service"<<std::endl; }
+        else { std::cout<<"Unauthorized from remote pdp service"<<std::endl; }
 
+      } else {
         //Invoke the remote pdp service
         Arc::NS req_ns;
-        req_ns["ra"] = "http://www.nordugrid.org/schemas/request-arc";
         req_ns["pdp"] = "http://www.nordugrid.org/schemas/pdp";
         Arc::PayloadSOAP req(req_ns);
         Arc::XMLNode reqnode = req.NewChild("pdp:GetPolicyDecisionRequest");
@@ -111,11 +205,11 @@ int main(int argc, char* argv[]){
 
         if(authz_res == "PERMIT") { std::cout<<"Authorized from remote pdp service"<<std::endl; }
         else { std::cout<<"Unauthorized from remote pdp service"<<std::endl; }
-        
-        return EXIT_SUCCESS;
+      }      
+      return EXIT_SUCCESS;
     } catch (std::exception& e){
-        // Notify the user about the failure
-        std::cerr << "ERROR: " << e.what() << std::endl;
-        return EXIT_FAILURE;
+      // Notify the user about the failure
+      std::cerr << "ERROR: " << e.what() << std::endl;
+      return EXIT_FAILURE;
     }
 }
--- src/services/charon/charon.cpp
+++ src/services/charon/charon.cpp
@@ -17,10 +17,18 @@
 #include <arc/security/ArcPDP/EvaluatorLoader.h>
 #include <arc/security/ArcPDP/Source.h>
 #include <arc/Thread.h>
+#include <arc/StringConv.h>
+#include <arc/GUID.h>
 
 #include "charon.h"
 
 namespace ArcSec {
+
+#define SAML_NAMESPACE "urn:oasis:names:tc:SAML:2.0:assertion"
+#define SAMLP_NAMESPACE "urn:oasis:names:tc:SAML:2.0:protocol"
+#define XACML_SAMLP_NAMESPACE "urn:oasis:names:tc:xacml:2.0:saml:protocol:schema:os"
+#define XACML_SAML_NAMESPACE "urn:oasis:names:tc:xacml:2.0:saml:assertion:schema:os"
+#define XACML_CONTEXT_NAMESPACE "urn:oasis:names:tc:xacml:2.0:context:schema:os"
 
 static Arc::LogStream logcerr(std::cerr);
 
@@ -43,7 +51,7 @@ Arc::MCC_Status Charon::make_soap_fault(Arc::Message& outmsg) {
 }
 
 Arc::MCC_Status Charon::process(Arc::Message& inmsg,Arc::Message& outmsg) {
-  
+
   std::string method = inmsg.Attributes()->get("HTTP:METHOD");
 
   // Identify which of served endpoints request is for.
@@ -63,8 +71,11 @@ Arc::MCC_Status Charon::process(Arc::Message& inmsg,Arc::Message& outmsg) {
     // Analyzing request
     Arc::XMLNode request = (*inpayload)["GetPolicyDecisionRequest"];
     if(!request) {
-      logger.msg(Arc::ERROR, "soap body does not include any request node");
-      return make_soap_fault(outmsg);
+      request = (*inpayload)["XACMLAuthzDecisionQuery"];
+      if(!request) {
+        logger.msg(Arc::ERROR, "soap body does not include any request node");
+        return make_soap_fault(outmsg);
+      };
     };
     {
       std::string req_xml;
@@ -72,14 +83,25 @@ Arc::MCC_Status Charon::process(Arc::Message& inmsg,Arc::Message& outmsg) {
       logger.msg(Arc::DEBUG, "Request: %s",req_xml);
     };    
 
-    Arc::XMLNode arc_requestnd = request["Request"];
+    Arc::XMLNode arc_requestnd;
+    bool use_saml = false;
+    std::string request_name = request.Name();
+    if(request_name.find("GetPolicyDecisionRequest") != std::string::npos)
+      arc_requestnd = request["Request"];
+    else if(request_name.find("XACMLAuthzDecisionQuery") != std::string::npos) {
+      arc_requestnd = request["Request"];
+      use_saml = true;
+    }
     if(!arc_requestnd) {
       logger.msg(Arc::ERROR, "request node is empty");
       return make_soap_fault(outmsg);
     };
 
     //Call the functionality of policy engine
-    //Here the decision algorithm is the same as that in ArcPDP.cpp
+    //Here the decision algorithm is the same as that in ArcPDP.cpp,
+    //so this algorithm implicitly applies to policy with Arc format;
+    //for xacml policy (the "rlist" will only contains one item), this 
+    //decision algorithm also applies.
     Response *resp = NULL;
     resp = eval->evaluate(Source(arc_requestnd));
     ResponseList rlist = resp->getResponseItems();
@@ -97,16 +119,18 @@ Arc::MCC_Status Charon::process(Arc::Message& inmsg,Arc::Message& outmsg) {
         atleast_onedeny = true;
       if(item->res == DECISION_PERMIT)
         atleast_onepermit = true;
-
-      Subject::iterator it;
-      Subject subject = tp->sub;
-      for (it = subject.begin(); it!= subject.end(); it++){
-        AttributeValue *attrval;
-        RequestAttribute *attr;
-        attr = dynamic_cast<RequestAttribute*>(*it);
-        if(attr){
-          attrval = (*it)->getAttributeValue();
-          if(attrval) logger.msg(Arc::INFO, "%s", (attrval->encode()));
+     
+      if(tp) {
+        Subject::iterator it;
+        Subject subject = tp->sub;
+        for (it = subject.begin(); it!= subject.end(); it++){
+          AttributeValue *attrval;
+          RequestAttribute *attr;
+          attr = dynamic_cast<RequestAttribute*>(*it);
+          if(attr){
+            attrval = (*it)->getAttributeValue();
+            if(attrval) logger.msg(Arc::INFO, "%s", (attrval->encode()));
+          }
         }
       }
     }
@@ -126,23 +150,84 @@ Arc::MCC_Status Charon::process(Arc::Message& inmsg,Arc::Message& outmsg) {
     //to compose the policy decision request to pdpservice by parsing the information from the request 
     //(aiming to the client itself), and permit or deny the request by using the information responded 
     //from pdpservice; Here pdpservice only gives some coarse decision to pdpservice invoker.
-    Arc::PayloadSOAP* outpayload = new Arc::PayloadSOAP(ns_);   
-    Arc::XMLNode response = outpayload->NewChild("pdp:GetPolicyDecisionResponse");
+    if(!use_saml) {
+      Arc::PayloadSOAP* outpayload = new Arc::PayloadSOAP(ns_);
+      Arc::XMLNode response = outpayload->NewChild("pdp:GetPolicyDecisionResponse");
+      Arc::XMLNode arc_responsend = response.NewChild("response:Response");
+      Arc::XMLNode authz_res = arc_responsend.NewChild("response:AuthZResult");
+      if(result) authz_res = "PERMIT";
+      else authz_res = "DENY";
 
-    Arc::XMLNode arc_responsend = response.NewChild("response:Response");
-    Arc::XMLNode authz_res = arc_responsend.NewChild("response:AuthZResult");
-    if(result) authz_res = "PERMIT";
-    else authz_res = "DENY";
+      for(i = 0; i<size; i++){
+        ResponseItem* item = rlist[i];
+        if(item->res == DECISION_PERMIT)
+          arc_responsend.NewChild(Arc::XMLNode(item->reqxml));
+      }
+      outmsg.Payload(outpayload);
 
-    for(i = 0; i<size; i++){
-      ResponseItem* item = rlist[i];
-      if(item->res == DECISION_PERMIT)
-        arc_responsend.NewChild(Arc::XMLNode(item->reqxml));
-    } 
+    } else {
+      Arc::NS samlp_ns, saml_ns;
+      samlp_ns["samlp"] = SAMLP_NAMESPACE;
+      saml_ns["saml"] = SAML_NAMESPACE;
+      saml_ns["xsi"] = "http://www.w3.org/2001/XMLSchema-instance";
+
+      //<saml:Response/>
+      Arc::XMLNode authz_response(samlp_ns, "samlp:Response");
+      std::string local_dn = inmsg.Attributes()->get("TLS:LOCALDN");
+      std::string issuer_name = Arc::convert_to_rdn(local_dn);
+      authz_response.NewChild("samlp:Issuer") = issuer_name;
+
+      std::string response_id = Arc::UUID();
+      authz_response.NewAttribute("ID") = response_id;
+      std::string responseto_id = (std::string)(request.Attribute("ID"));
+      authz_response.NewAttribute("InResponseTo") = responseto_id;
+      Arc::Time t;
+      std::string current_time = t.str(Arc::UTCTime);
+      authz_response.NewAttribute("IssueInstant") = current_time;
+      authz_response.NewAttribute("Version") = std::string("2.0");
+
+      Arc::XMLNode status = authz_response.NewChild("samlp:Status");
+      Arc::XMLNode statuscode = status.NewChild("samlp:StatusCode");
+      std::string statuscode_value = "urn:oasis:names:tc:SAML:2.0:status:Success";
+      statuscode.NewAttribute("Value") = statuscode_value;
+
+      //<saml:Assertion/>
+      Arc::XMLNode assertion = authz_response.NewChild("saml:Assertion", saml_ns);
+      assertion.NewAttribute("Version") = std::string("2.0");
+      std::string assertion_id = Arc::UUID();
+      assertion.NewAttribute("ID") = assertion_id;
+      Arc::Time t1;
+      std::string current_time1 = t1.str(Arc::UTCTime);
+      assertion.NewAttribute("IssueInstant") = current_time1;
+      assertion.NewChild("saml:Issuer") = issuer_name;
+
+      //<saml:XACMLAuthzDecisionStatement/>
+      Arc::NS xacml_saml_ns; xacml_saml_ns["xacml-saml"] = XACML_SAML_NAMESPACE;
+      Arc::XMLNode authz_statement = assertion.NewChild("xacml-saml:XACMLAuthzDecisionStatement", xacml_saml_ns);
+      //See "xacml-context" specification
+      Arc::NS xacml_ns; xacml_ns["xacml-context"] = XACML_CONTEXT_NAMESPACE;
+      Arc::XMLNode xacml_response = authz_statement.NewChild("xacml-context:Response", xacml_ns);
+      Arc::XMLNode xacml_result = xacml_response.NewChild("xacml-context:Result");
+      xacml_result.NewAttribute("ResourceId") = "resource-id"; //TODO
+      if(result) xacml_result.NewChild("xacml-context:Decision") = "Permit";
+      else xacml_result.NewChild("xacml-context:Decision") = "Deny"; //TODO: Indeterminate, NotApplicable
+      //TODO: "xacml-context:Status"  "xacml:Obligations"
+
+
+      Arc::NS soap_ns;
+      Arc::SOAPEnvelope envelope(soap_ns);
+      envelope.NewChild(authz_response);
+      Arc::PayloadSOAP *outpayload = new Arc::PayloadSOAP(envelope);
+
+      std::string tmp;
+      outpayload->GetXML(tmp);
+      std::cout<<"Output payload: "<<tmp<<std::endl;
+
+      outmsg.Payload(outpayload);
+    }
 
     if(resp) delete resp;
     
-    outmsg.Payload(outpayload);
     return Arc::MCC_Status(Arc::STATUS_OK);
   } 
   else {
