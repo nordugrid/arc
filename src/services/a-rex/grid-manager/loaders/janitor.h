8ce6a9a62f6a769a1db80a5490049cc19d1fd2cb
Aleksandr Konstantinov <akonstantinov@yahoo.com>
2009-09-01 13:19:06 +0000

    Describing Janitor class

--- src/services/a-rex/grid-manager/loaders/janitor.h
+++ src/services/a-rex/grid-manager/loaders/janitor.h
@@ -2,6 +2,7 @@
 
 #include <arc/Thread.h>
 
+/// Class to communicate with Janitor - Dynmaic Runtime Environment handler
 class Janitor {
  private:
   std::string path_;
@@ -15,13 +16,38 @@ class Janitor {
   static void remove_thread(void* arg);
   void cancel(void);
  public:
+  /// Creates instance representing job entry in Janitor database
+  /** Takes id for job identifier and cdir for the control 
+     directory of A-Rex. constructor does not register job in 
+     the Janitor. It only associates job with this instance. */
   Janitor(const std::string& id,const std::string& cdir);
   ~Janitor(void);
+  /// Returns true if instance is valid
   operator bool(void) { return !path_.empty(); };
+  /// Returns true if instance is invalid
   bool operator!(void) { return path_.empty(); };
+  /// Registers associated job with Janitor and deploys dynamic RTEs.
+  /** This operation is asynchronous. Returned true means Janitor
+     will be contacted and deployemnt will start soon. For obtaining
+     result of operation see methods wait() and result().
+     During this operation janitor utility is called with command
+     register and optionally deploy. */
   bool deploy(void);
+  /// Removes job from those handled by Janitor and releases associated RTEs.
+  /** This operation is asynchronous. Returned true means Janitor
+     will be contacted and removal will start soon. For obtaining
+     result of operation see methods wait() and result().
+     During this operation janitor utility is called with command
+     remove. */
   bool remove(void);
+  /// Wait till operation initiated by deploy() or remove() finished.
+  /** This operation returns true if operation finished or false if
+     timeout seconds passed. It may be called repeatedly and even after
+     it previously returned true. If no operation is running it returns
+     true immeaditely. */
   bool wait(int timeout);
+  /// Returns true if operation initiated by deploy() or remove() succeeded.
+  /** It should be called after wait() returned true. */
   bool result(void);
 };
 
