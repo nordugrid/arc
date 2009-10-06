af516c41afaa35430ba4b6cedcd434503757ff65
Steffen Moeller <Moeller@mycompany.com>
2009-10-06 22:29:57 +0000

    Simplified structure of code, performed what I consider corrections, in learning phase, still, am in contact with Michael.

--- src/services/janitor/interfaces/client/janitorClient.cpp
+++ src/services/janitor/interfaces/client/janitorClient.cpp
@@ -10,143 +10,137 @@
 
 using namespace Arc;
 
-int main(int argc, char** argv) {
+/**
+ * printhelp
+ *
+ * Routine to print usage information.  It is expected that the program stops execution
+ * after these line have been shown, but this is not required.
+ *
+ * Returns value with which the function main is supposed to be left, i.e. the exit code.
+ */
+static int printhelp() {
+	printf("Usage:\n");
+	printf("\n");
+	printf("  janitor [config-file] [command] [arg0] [arg1] [arg2] ...\n");
+	printf("\n");
+	printf("config-file   - specifies the URL and the security attributes.\n");
+	printf("                \n");
+	printf("command       - REGISTER [jobid] [rte0] [rte1] ...\n");
+	printf("                DEPLOY   [jobid]\n");
+	printf("                REMOVE   [jobid]\n");
+	printf("                INFO     [jobid]\n");
+	printf("                LIST\n");
+	printf("                SETSTATE [newstate] [rte0] [rte1] ...\n");
+	printf("                SEARCH   [string] [string] ...\n");
+	printf("\n");
+	printf("Examples:\n");
+	printf("\n");
+	printf("  janitor REGISTER 1919 APPS/BIO/GRID ENV/JAVA/JRE1.5\n");
+	printf("  janitor DEPLOY 1919\n");
+	printf("  janitor INFO 1919\n");
+	printf("  janitor REMOVE 1919\n");
+	printf("  janitor SETSTATE REMOVAL_PENDING ENV/JAVA/JRE1.5\n");
+	printf("\n");
+	return -1;
+}
 
-	bool printhelp = false;
+int main(int argc, char** argv) {
 
 	std::string command;
 	std::string jobid;
 	std::string newstate;
 	std::vector <std::string> rtes;
 
-	if(argc > 1){
-
-		/** Load the client configuration file into a string */
-		std::string xmlstring;
-		std::ifstream file(argv[1]);
-		if (file.good())
-		{
-			printf("Reading XML configuration from %s\n",argv[1]);
-			file.seekg(0,std::ios::end);
-			std::ifstream::pos_type size = file.tellg();
-			file.seekg(0,std::ios::beg);
-			std::ifstream::char_type *buf = new std::ifstream::char_type[size];
-			file.read(buf,size);
-			xmlstring = buf;
-			delete []buf; 
-			file.close(); 
-		}
-		else
-		{
-			printf("File %s not found.\n\n",argv[1]);
-			printhelp = true;
-		}
-		/** */
-
-		command.assign(argv[2]);
-
-		std::string commands[] = {	std::string("REGISTER"),
-						std::string("DEPLOY"),
-						std::string("REMOVE"),
-						std::string("INFO"),
-						std::string("LIST"),
-						std::string("SETSTATE"),
-						std::string("SEARCH") };
-		unsigned n;
-		for(n = 0; n < 6 && strcmp(commands[n].c_str(), command.c_str()); n++); 
-			
-// 		if(n == anzahlStrings) /* string wurde nicht gefunden */
-// 			return -1;
-		switch(n) {
-			case 0: // REGISTER
-				if(argc > 2){
-					jobid.assign(argv[2]);
-					for(int i = 3; i < argc; i++){
-						rtes.push_back(std::string(argv[i]));
-					}
-				}else{
-					printf("Jobid is missing.\n\n");
-					printhelp = true;	
-				}
-			break;
-			case 1: // DEPLOY
-				if(argc > 2){
-					jobid.assign(argv[2]);
-				}else{
-					printf("Jobid is missing.\n\n");
-					printhelp = true;	
-				}
-			break;
-			case 2: // REMOVE
-				if(argc > 2){
-					jobid.assign(argv[2]);
-				}else{
-					printf("Jobid is missing.\n\n");
-					printhelp = true;	
-				}
-			break;
-			case 3: // INFO
-				if(argc > 2){
-					jobid.assign(argv[2]);
-				}else{
-					printf("Jobid is missing.\n\n");
-					printhelp = true;	
-				}
-			break;
-			case 4: // LIST
-			break;
-			case 5: // SETSTATE
-				if(argc > 2){
-					newstate.assign(argv[2]);
-					for(int i = 3; i < argc; i++){
-						rtes.push_back(std::string(argv[i]));
-					}
-				}else{
-					printf("New state is missing.\n\n");
-					printhelp = true;	
-				}
-			break;
-			case 6: // SEARCH
-				for(int i = 2; i < argc; i++){
-					rtes.push_back(std::string(argv[i]));
-				}
-			break;
-			default:
-				printf("Unknown janitor command.\n\n");
-				printhelp = true;
-		};
-
-	}else{
-		printhelp = true;
+	if(2 >= argc){
+		printf("Not enough arguments.\n\n",argv[1]);
+		return(printhelp());
 	}
+
+	// argc >= 3
+
+	/** Load the client configuration file into a string */
+	std::string xmlstring;
+	std::ifstream file(argv[1]);
+	if (!file.good())
+	{
+		printf("File %s not found.\n\n",argv[1]);
+		return(printhelp());
+	}
+
+	// file.good() is true
+
+	printf("Reading XML configuration from %s\n",argv[1]);
+	file.seekg(0,std::ios::end);
+	std::ifstream::pos_type size = file.tellg();
+	file.seekg(0,std::ios::beg);
+	std::ifstream::char_type *buf = new std::ifstream::char_type[size];
+	file.read(buf,size);
+	xmlstring = buf;
+	delete []buf; 
+	file.close(); 
+
+	command.assign(argv[2]);
+
+	std::string commands[] = {	std::string("REGISTER"),
+					std::string("DEPLOY"),
+					std::string("REMOVE"),
+					std::string("INFO"),
+					std::string("LIST"),
+					std::string("SETSTATE"),
+					std::string("SEARCH") };
+
+	// find  position of string that matches argument
+	unsigned n;
+	for(n = 0; n < 7 && strncmp(commands[n].c_str(), command.c_str(),8); n++); 
+	printf("Found command #%d.\n",n);
+			
+	switch(n) {
+	case 0: // REGISTER
+		if(argc > 3){
+			jobid.assign(argv[3]);
+			for(int i = 4; i < argc; i++){
+				rtes.push_back(std::string(argv[i]));
+			}
+		}else{
+			printf("Jobid is missing.\n\n");
+			return(printhelp());	
+		}
+		break;
+	case 1: // DEPLOY
+	case 2: // REMOVE
+	case 3: // INFO	- they all expect a job id
+		if(argc > 3){
+			jobid.assign(argv[3]);
+		}else{
+			printf("Jobid is missing.\n\n");
+			return(printhelp());	
+		}
+		break;
+	case 4: // LIST - takes no arguments
+		break;
+	case 5: // SETSTATE
+		if(argc > 3){
+			newstate.assign(argv[3]);
+			for(int i = 4; i < argc; i++){
+				rtes.push_back(std::string(argv[i]));
+			}
+		}else{
+			printf("New state not specified .\n\n");
+			return(printhelp());	
+		}
+		break;
+	case 6: // SEARCH
+		for(int i = 3; i < argc; i++){
+			rtes.push_back(std::string(argv[i]));
+		}
+		break;
+	default:
+		printf("Unknown janitor command: '%s'\n\n",argv[2]);
+		return(printhelp());	
+		break;
+	};
 
 	/** Arguments passed by the command line*/
-	if(printhelp){
-		printf("Usage:\n");
-		printf("\n");
-		printf("  janitor [config-file] [command] [arg0] [arg1] [arg2] ...\n");
-		printf("\n");
-		printf("config-file   - specifies the URL and the security attributes.\n");
-		printf("                \n");
-		printf("command       - REGISTER [jobid] [rte0] [rte1] ...\n");
-		printf("                DEPLOY   [jobid]\n");
-		printf("                REMOVE   [jobid]\n");
-		printf("                INFO     [jobid]\n");
-		printf("                LIST\n");
-		printf("                SETSTATE [newstate] [rte0] [rte1] ...\n");
-		printf("                SEARCH   [string] [string] ...\n");
-		printf("\n");
-		printf("Examples:\n");
-		printf("\n");
-		printf("  janitor REGISTER 1919 APPS/BIO/GRID ENV/JAVA/JRE1.5\n");
-		printf("  janitor DEPLOY 1919\n");
-		printf("  janitor INFO 1919\n");
-		printf("  janitor REMOVE 1919\n");
-		printf("  janitor SETSTATE REMOVAL_PENDING ENV/JAVA/JRE1.5\n");
-		printf("\n");
-		return -1;
-	}
-	/** */
 
 	printf("Command:  %s\n",command.c_str());
 	printf("jobid:    %s\n",jobid.c_str());
