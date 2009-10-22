2f443d928a686b766bae74ed380e3098aabc1c67
Nagy Zsombor <zsombor@niif.hu>
2009-10-22 17:18:23 +0000

    echo client test script

---
+++ src/tests/echo/echo_client.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python
+import arc
+import sys
+if len(sys.argv) < 2:
+    print "Usage: echo_client.py URL [message]"
+    print "  echo_client gets the credentials from the default user config file"
+    sys.exit(-1)
+url = arc.URL(sys.argv[1])
+try:
+    message = sys.argv[2]
+except:
+    message = 'hi!'
+cfg = arc.MCCConfig()
+uc = arc.UserConfig('')
+uc.ApplyToConfig(cfg)
+s = arc.ClientSOAP(cfg, url)
+outpayload = arc.PayloadSOAP(arc.NS('echo', 'urn:echo'))
+outpayload.NewChild('echo:echo').NewChild('echo:say').Set(message)
+resp, status = s.process(outpayload)
+print resp.GetXML()
