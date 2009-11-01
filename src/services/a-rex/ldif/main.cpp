613f368605c5a391d971ec9cc8ce3f5a0d057751
Aleksandr Konstantinov <akonstantinov@yahoo.com>
2009-11-01 22:40:57 +0000

    Rearranging code to free allocated object before leaving utility - on request of Steffen

--- src/services/a-rex/ldif/main.cpp
+++ src/services/a-rex/ldif/main.cpp
@@ -10,25 +10,31 @@
 int main(int argc,char* argv[]) {
   std::string base;
   std::istream* in = &std::cin;
+  std::istream* fin = NULL;
   Arc::NS ns;
   Arc::XMLNode xml(ns,"LDIFTree");
+  int r = -1;
   if(argc < 2) {
     std::cerr<<"Usage: ldif2xml LDIF_base [input_file [output_file]] "<<std::endl;
     return -1;
   };
   base=argv[1];
-  if(argc >= 3) in = new std::ifstream(argv[2]);
-  if(!(*in)) return -1;
-  if(!ARex::LDIFtoXML(*in,base,xml)) return -1;
-  std::string s;
-  xml.GetDoc(s);
-  if(argc < 4) {
-    std::cout<<s;
-  } else {
-    std::ofstream f(argv[3]);
-    if(!f) return -1;
-    f<<s;
+  if(argc >= 3) in = (fin = new std::ifstream(argv[2]));
+  if(*in) {
+    if(ARex::LDIFtoXML(*in,base,xml)) {
+      std::string s;
+      xml.GetDoc(s);
+      if(argc < 4) {
+        std::cout<<s; r=0;
+      } else {
+        std::ofstream f(argv[3]);
+        if(f) {
+          f<<s; r=0;
+        };
+      };
+    };
   };
-  return 0;
+  if(fin) delete fin;
+  return r;
 }
 
