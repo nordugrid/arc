68aac7fa7ae0c5d8f245f59975d9b6d701d1dc12
Aleksandr Konstantinov <akonstantinov@yahoo.com>
2009-11-13 13:28:22 +0000

    Reducing amount of files tried to be loaded as plugins - files with unproper suffix are not tried

--- src/hed/libs/loader/FinderLoader.cpp
+++ src/hed/libs/loader/FinderLoader.cpp
@@ -11,6 +11,15 @@
 
 namespace Arc {
 
+  static inline bool name_is_plugin(std::string& name) {
+    if (name.substr(0, 3) != "lib") return false;
+    std::string::size_type p = name.rfind('.');
+    if(p == std::string::npos) return false;
+    if(name.substr(p+1) != G_MODULE_SUFFIX) return false;
+    name = name.substr(3, p - 3);
+    return true;
+  }
+
   const std::list<std::string> FinderLoader::GetLibrariesList(void) {
     BaseConfig basecfg;
     NS ns;
@@ -26,11 +35,11 @@ namespace Arc {
           continue;
         try {
           Glib::Dir dir((std::string)m);
-          for (Glib::DirIterator file = dir.begin(); file != dir.end(); file++)
-            if ((*file).substr(0, 3) == "lib") {
-              std::string name = (*file).substr(3, (*file).find('.') - 3);
-              names.push_back(name);
-            }
+          for (Glib::DirIterator file = dir.begin();
+                                file != dir.end(); file++) {
+            std::string name = *file;
+            if(name_is_plugin(name)) names.push_back(name);
+          }
         } catch (Glib::FileError) {}
       }
     }
@@ -51,11 +60,12 @@ namespace Arc {
           continue;
         try {
           Glib::Dir dir((std::string)m);
-          for (Glib::DirIterator file = dir.begin(); file != dir.end(); file++)
-            if ((*file).substr(0, 3) == "lib") {
-              std::string name = (*file).substr(3, (*file).find('.') - 3);
+          for (Glib::DirIterator file = dir.begin();
+                                file != dir.end(); file++) {
+            std::string name = *file;
+            if(name_is_plugin(name)) 
               cfg.NewChild("Plugins").NewChild("Name") = name;
-            }
+          }
         } catch (Glib::FileError) {}
       }
     }
