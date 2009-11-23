b375c93a0474c939ef186c8ee354643691e9873f
Steffen Moeller <Moeller@mycompany.com>
2009-11-23 00:46:03 +0000

    Script to prepare dynamic REs feels complete.

--- src/services/janitor/util/prepareDRE.pl
+++ src/services/janitor/util/prepareDRE.pl
@@ -4,7 +4,8 @@ use strict;
 
 use Getopt::Long;
 use File::Path qw(remove_tree);
-use File::Copy::Recursive qw(dirmove);
+use File::Copy::Recursive qw(dircopy);
+use File::Basename;
 
 my $readyDir;
 my $re;
@@ -12,6 +13,10 @@ my $install;
 my $runtime;
 my $remove;
 my $help;
+my $category;
+my $homepage;
+my $description;
+my $basesystem;
 
 GetOptions(
 	"dir:s"=>\$readyDir,
@@ -19,6 +24,10 @@ GetOptions(
 	"install"=>\$install,
 	"runtime"=>\$runtime,
 	"remove"=>\$remove,
+	"category"=>\$category,
+	"homepage"=>\$homepage,
+	"description"=>\$description,
+	"basesystem"=>\$basesystem,
 	"help"=>\$help
 ) or die "Could not parse options: $@\n";
 
@@ -36,6 +45,12 @@ This script takes a directory and transforms it into a dynamic runtime
 environment (dRTE). This is particularly helpful for beginners to concentrate
 on the binaries rather than on the exact notion of the inner structure
 of the dRTEs.
+
+The script will adjust the settings of the LD_LIBRARY_PATH environment
+variable to the directory that is untared by the Janitor. More correctly
+stated: it prepares install, remove and runtime scripts that instruct
+the Janitor to parse such and prepare the real scripts that are then
+invoked by the backend scripts preparing the use of runtime environments.
 
 =head1 OPTIONS
 
@@ -76,20 +91,37 @@ Use the path relative to the --dir setting or an absolute pathname.
 path relative to the directory harboring all the files that are expected to contribute 
 to the runtime environment.
 
+=item --category I<string>
+
+the path in the runtime environment hierarchy, e.g. "APPS/BIO", defaulting to "APPS/OTHER" if unset, to appear in the catalog entryz that is pasted to stdout.
+
+=item --homepage I<string>
+
+a complete URL to indicate where to learn more about a particular program, e.g. http://autodock.scripps.edu,
+defaulting to http://www.nordugrid.org, to appear in the catalog entryz that is pasted to stdout.
+
+=item --description I<string>
+
+a verbose description of the runtime environment, to appear in the catalog enry that is pasted to stdout.
+
+=item --basesystem I<string>
+
+indication of the base system that the particular runtime environment is known to be compatible with, defaults to the RDF ID for Debian Etch, &kb;knowarc_Instance_0.
+
 =back
 
 =cut
 
-my $tmpdir=ENV("TMPDIR");
+my $tmpdir=$ENV{"TMPDIR"};
 
-if ( -z "$tmpdir" ) {
+if ( !defined($tmpdir) or -z "$tmpdir" ) {
 	$tmpdir = "/tmp";
 }
 $tmpdir .= "/prepareDRE_$$";
 
 
 # the function expects
-#	TRUE/FALSE to decide if files should be cleaned.
+	#	TRUE/FALSE to decide if files should be cleaned.
 sub quitMe($$) {
 	my $clean=shift;
 	my $message=shift;
@@ -102,36 +134,134 @@ sub quitMe($$) {
 		remove_tree("$tmpdir/data") if -d "$tmpdir/data";
 		rmdir("$tmpdir") if -d "$tmpdir";
 	}
-	print STDERR "$message\n" if defined($message) and -n "$message";
+	print STDERR "$message\n" if defined($message) and ( "" ne "$message" );
 	die "$@\n" if $@;
 	exit -1;
 }
 
-if ( -d "$tmpdir") @quitme(FALSE,"The temporary directory '$tmpdir' already exists.");
-mkdir "$tmpdir" or @quitme(TRUE,"");
-mkdir "$tmpdir/data" or @quitme(TRUE,"");
-mkdir "$tmpdir/control" or @quitme(TRUE,"");
+-d "$tmpdir" and quitMe(0,"The temporary directory '$tmpdir' already exists.");
+mkdir "$tmpdir" or quitMe(1,"");
+mkdir "$tmpdir/data" or quitMe(1,"");
+mkdir "$tmpdir/control" or quitMe(1,"");
 
-if ( -z "$readyDir" ) {
-	print STDERR "No directory specified that could be transformed into a runtime environment. Use the --dir option.\n";
-	$help=1;
+if (defined($help) and $help) {
+	use Pod::Usage;
+	pod2usage(-verbose => 2);
+}
+
+if ( !defined($re) or -z "$re" ) {
+	quitMe(1,"No filename specified under which the runtime environment should be saved.\n"
+	        . "Use the --re option.\n");
+}
+
+if ( !defined($readyDir) or -z "$readyDir" ) {
+	quitMe(1,"No directory specified that could be transformed into a runtime environment. "
+	        . "Use the --dir option.\n");
 }
 elsif ( ! -d "$readyDir" ) {
-	@quitMe(TRUE,"Directory '$readyDir' not found.");
+	quitMe(1,"Directory '$readyDir' not found.");
 }
 
-dircopy("$readyDir","$tmpdir/data" " or @quitMe(TRUE,"Could not change to directory '$readDir'.");
+dircopy("$readyDir","$tmpdir/data")
+		or quitMe(1,"Could not change to directory '$readyDir'.");
 
-open  INSTALL,">$tmpdir/control/install" or @quitMe(TRUE,"Could not create install config file");
-print INSTALL "# Nothing to do.\n";
-close(INSTALL);
-open  REMOVE,">$tmpdir/control/remove" or @quitMe(TRUE,"Could not create remove config file");
-print REMOVE "# Nothing to do.\n";
-close(REMOVE);
-open  RUNTIME,">$tmpdir/control/runtime" or @quitMe(TRUE,"Could not create runtime config file");
-print RUNTIME "# Nothing to do.\n";
-close(RUNTIME);
+if (defined($install) and "" ne "$install") {
+	File::copy("$remove","$tmpdir/control/install")
+		or quitMe(1,"Could not copy install config file");
+}
+else {
+	open  INSTALL,">$tmpdir/control/install"
+		or quitMe(1,"Could not create install config file");
+	print INSTALL "#!/bin/sh\n";
+	print INSTALL "# Nothing to do.\n";
+	close(INSTALL);
+}
 
-system("tar czvf 
+if (defined($remove) and "" ne "$remove") {
+	File::copy("$remove","$tmpdir/control/remove")
+		or quitMe(1,"Could not copy remove config file");
+}
+else {
+	open  REMOVE,">$tmpdir/control/remove"
+		or quitMe(1,"Could not create remove config file");
+	print REMOVE "#!/bin/sh\n";
+	print REMOVE "# Nothing to do.\n";
+	close(REMOVE);
+}
+
+if (defined($runtime) and "" ne "$runtime") {
+	File::copy("$remove","$tmpdir/control/runtime")
+		or quitMe(1,"Could not copy runtime config file");
+}
+else {
+	open  RUNTIME,">$tmpdir/control/runtime"
+		or quitMe(1,"Could not create runtime config file");
+	print RUNTIME "#!/bin/sh\n";
+	print RUNTIME "# Nothing to do.\n";
+	close(RUNTIME);
+}
+
+my $command="tar -C '$tmpdir' -czvf '$re' data control";
+system($command) and quitMe(1,"An error occurred while executing '$command'\n");
+
+print STDERR "Preparation of dynamic Runtime Environment was successful.\n";
+
+
+my $REid=$re;
+$REid=~ s/[. \t\n]//g; # delete some characters that seem unappropriate
+
+my $REname=basename($re);
+$REname = uc($REname);
+
+my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime time;
+$mon++;
+$year+=1900;
+
+if (!defined($category) or "" eq "$homepage") {
+	$category = "APPS/OTHER";
+}
+
+if (!defined($homepage) or "" eq "$homepage") {
+	$homepage = "http://www.nordugrid.org";
+}
+
+if (!defined($description) or "" eq "$description") {
+	$description="no description available";
+}
+
+if (!defined($basesystem) or "" eq "$basesystem") {
+	$basesystem="&kb;knowarc_Instance_0";
+}
+
+print <<EOCATENTRY;
+
+<?xml version='1.0' encoding='UTF-8'?>
+<!DOCTYPE rdf:RDF [
+         <!ENTITY rdf 'http://www.w3.org/1999/02/22-rdf-syntax-ns#'>
+         <!ENTITY a 'http://protege.stanford.edu/system#'>
+         <!ENTITY kb 'http://knowarc.eu/kb#'>
+         <!ENTITY rdfs 'http://www.w3.org/2000/01/rdf-schema#'>
+]>
+<rdf:RDF xmlns:rdf="&rdf;"
+         xmlns:a="&a;"
+         xmlns:kb="&kb;"
+         xmlns:rdfs="&rdfs;">
+
+<kb:MetaPackage rdf:about="&kb;$REid"
+         kb:homepage="$homepage"
+         kb:lastupdated="$year-$mon-$mday"
+         kb:name="$category/$REname"
+         rdfs:label="$category/$REname">
+        <kb:description>$description</kb:description>
+        <kb:instance rdf:resource="&kb;${REid}_TarPackage"/>
+</kb:MetaPackage>
+<kb:TarPackage rdf:about="&kb;${REid}_TarPackage" kb:url="$re">
+        <rdfs:label>Debian Etch (tar based): $REname</rdfs:label>
+        <kb:basesystem rdf:resource="$basesystem"/>
+</kb:TarPackage>
+</rdf:RDF>
+
+EOCATENTRY
+
 
 1;
---
+++ src/services/janitor/util/test_prepareDRE.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+
+set -e
+
+# Script to execute the prepareDRE.pl script.
+
+echo
+echo " *  Preparing simple dynamic Runtime Environment"
+
+# preparing directory to transform into a dynamic RTE
+d=$(mktemp --directory --tmpdir=. rawDirectory_XXX  )
+
+cat <<EOAPPLICATION > "$d"/welcomeApp.sh
+#!/bin/sh
+echo Welcome $* to dynamic runtime environments
+EOAPPLICATION
+
+if [ -r re-simple.tar.gz ] ; then rm re-simple.tar.gz; fi
+./prepareDRE.pl --re re-simple.tar.gz --dir "$d"
+
+# cleaning up
+if [ -n "$d" ]; then rm -r "$d"; fi
+
+if [ ! -r re-simple.tar.gz ]; then
+	echo "Could not find resulting dRE"
+	exit -1
+fi
+
+echo
+echo " *  Preparing slightly more complex Runtime Environment"
+
+# preparing directory to transform into a dynamic RTE
+d=$(mktemp --directory --tmpdir=. rawDirectory_XXX  )
+
+mkdir "$d"/bin
+mkdir "$d"/lib
+
+cat <<EOAPPLICATION > "$d"/bin/welcomeApp.sh
+#!/bin/sh
+echo Welcome $* to dynamic runtime environments
+EOAPPLICATION
+
+if [ -r re-complex.tar.gz ] ; then rm re-complex.tar.gz; fi
+./prepareDRE.pl --re re-complex.tar.gz --dir "$d"
+
+# cleaning up
+if [ -n "$d" ]; then rm -r "$d"; fi
+
+if [ ! -r re-complex.tar.gz ]; then
+	echo "Could not find resulting dRE"
+	exit -1
+fi
