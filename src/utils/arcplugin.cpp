2c822a9ddf5c1c2189a205623b335ef9b3d60290
Aleksandr Konstantinov <akonstantinov@yahoo.com>
2010-01-08 08:04:55 +0000

    Adding support for recursive directory handling in arcplugin

--- src/utils/arcplugin.cpp
+++ src/utils/arcplugin.cpp
@@ -5,12 +5,14 @@
 #include <fstream>
 
 #include <glibmm/module.h>
+#include <glibmm/fileutils.h>
+#include <glibmm/miscutils.h>
 
 #include <arc/loader/Plugin.h>
 #include <arc/StringConv.h>
 
 static std::string encode_for_var(uint32_t v) {
-  return "\"" + Arc::tostring(v) + "\"";
+    return "\"" + Arc::tostring(v) + "\"";
 }
 
 static std::string encode_for_var(const char* str) {
@@ -27,49 +29,34 @@ static std::string encode_for_var(const char* str) {
     return stro;
 }
 
-static std::string replace_file_suffix(const std::string& path) {
-  std::string newpath = path;
-  std::string::size_type name_p = newpath.rfind(G_DIR_SEPARATOR_S);
-  if(name_p == std::string::npos) {
-    name_p = 0;
-  } else {
-    ++name_p;
-  }
-  std::string::size_type suffix_p = newpath.find('.',name_p);
-  if(suffix_p != std::string::npos) {
-    newpath.resize(suffix_p);
-  }
-  newpath += ".apd";
-  return newpath;
+static std::string replace_file_suffix(const std::string& path,const std::string& newsuffix) {
+    std::string newpath = path;
+    std::string::size_type name_p = newpath.rfind(G_DIR_SEPARATOR_S);
+    if(name_p == std::string::npos) {
+        name_p = 0;
+    } else {
+        ++name_p;
+    }
+    std::string::size_type suffix_p = newpath.find('.',name_p);
+    if(suffix_p != std::string::npos) {
+        newpath.resize(suffix_p);
+    }
+    newpath += "." + newsuffix;
+    return newpath;
 }
 
-int main(int argc, char **argv)
-{
-    bool create_apd = false;
-    if (argc > 1) {
-      if (strcmp(argv[1],"-c") == 0) {
-        create_apd = true;
-        --argc; ++argv;
-      }
-    }
-   
-    if (argc < 2) {
-        std::cerr << "Invalid arguments" << std::endl;        
-        return -1;
-    }
-
-    std::string plugin_filename = argv[1];
-    std::string descriptor_filename = replace_file_suffix(plugin_filename);
-
+static bool process_module(const std::string& plugin_filename, bool create_apd) {
+    std::string descriptor_filename = replace_file_suffix(plugin_filename,"apd");
     Glib::ModuleFlags flags = Glib::ModuleFlags(0);
     flags|=Glib::MODULE_BIND_LAZY;
     Glib::Module *module = new Glib::Module(plugin_filename,flags);
     if ((!module) || (!(*module))) {
         std::cerr << "Failed to load module " << plugin_filename << ": " << Glib::Module::get_last_error() << std::endl;
-        return -1;
+        return false;
     }
 
     std::cout << "Loaded module " << plugin_filename << std::endl;
+    std::cout << std::endl;
 
     void *ptr = NULL;
     if(!module->get_symbol(PLUGINS_TABLE_SYMB,ptr)) {
@@ -82,11 +69,11 @@ int main(int argc, char **argv)
 
     std::ofstream apd;
     if(create_apd) {
-      apd.open(descriptor_filename.c_str());
-      if(!apd) {
-        std::cerr << "Failed to create descriptor file " << descriptor_filename << std::endl;
-        return -1;
-      };
+        apd.open(descriptor_filename.c_str());
+        if(!apd) {
+            std::cerr << "Failed to create descriptor file " << descriptor_filename << std::endl;
+            return false;
+        };
     };
 
     for(;desc;++desc) {
@@ -113,5 +100,41 @@ int main(int argc, char **argv)
 
     delete module;
 
+    return true;
+}
+
+int main(int argc, char **argv)
+{
+    bool create_apd = false;
+    if (argc > 1) {
+        if (strcmp(argv[1],"-c") == 0) {
+            create_apd = true;
+            --argc; ++argv;
+        };
+    };
+   
+    if (argc < 2) {
+        std::cerr << "Invalid arguments" << std::endl;        
+        return -1;
+    };
+
+    std::list<std::string> paths;
+    for(int n = 1; n < argc; ++n) paths.push_back(argv[n]);
+
+    for(std::list<std::string>::iterator path = paths.begin();
+                  path != paths.end(); ++path) {
+        try {
+          Glib::Dir dir(*path);
+          for (Glib::DirIterator file = dir.begin();
+                                file != dir.end(); file++) {
+            std::string name = *file;
+            paths.push_back(Glib::build_filename(*path, name));
+          }
+        } catch (Glib::FileError) {
+          process_module(*path, create_apd);
+        }
+    }
+
     return 0;
 }
+
