0801fc1d3d60c930ebadd536be829e0a5452bbcd
Aleksandr Konstantinov <akonstantinov@yahoo.com>
2010-02-02 10:54:40 +0000

    Do library locking through intermediate loadable module - applied to libarccrypto

---
+++ src/hed/libs/crypto/crypto.cpp
@@ -0,0 +1,9 @@
+// -*- indent-tabs-mode: nil -*-
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+// This is just an intermediate module for making libarccrypto library
+// persistent in compatible way.
+
