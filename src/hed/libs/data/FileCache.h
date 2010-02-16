52b56c2e3b6566ea9f4829b6d00b996b117bbfaa
Ali Gholami <gholami@kth.se>
2010-02-16 16:59:27 +0000

    Independent implementation of roundf function to fix the compiling problems in Solaris.

--- src/hed/libs/data/FileCache.h
+++ src/hed/libs/data/FileCache.h
@@ -6,8 +6,9 @@
 #include <sstream>
 #include <vector>
 #include <map>
-
 #include <arc/DateTime.h>
+// Independent implementaion of std roundf() 
+#define roundf(num) ((fmod(num,1) < 0.5) ? floor(num):ceil(num))
 
 #include "FileCacheHash.h"
 
