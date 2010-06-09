3732129e0ba68005d8e8ba22bdd52af67714c796
Aleksandr Konstantinov <akonstantinov@yahoo.com>
2010-06-09 12:12:34 +0000

    Fix for critical #1962 - canonical_dir() deadloops on some specific filenames

--- src/services/a-rex/grid-manager/misc/canonical_dir.cpp
+++ src/services/a-rex/grid-manager/misc/canonical_dir.cpp
@@ -1,13 +1,11 @@
-#ifdef HAVE_CONFIG_H
-#include <config.h>
-#endif
-
+#include "../std.h"
 #include <string>
 #include "canonical_dir.h"
 
 int canonical_dir(std::string &name,bool leading_slash) {
   std::string::size_type i,ii,n;
   ii=0; i=0;
+  if(name[0] != '/') name="/"+name;
   for(;i<name.length();) {
     name[ii]=name[i];
     if(name[i] == '/') {
@@ -23,21 +21,24 @@ int canonical_dir(std::string &name,bool leading_slash) {
             i=n;
             /* go 1 directory up */
             for(;;) {
-              ii--;
-              if(ii == std::string::npos) {
+              if(ii<=0) {
                  /* bad dir */
                 return 1;
               };
-              if(name[i] == '/') break;
+              ii--;
+              if(name[ii] == '/') break;
             };
+            ii--; i--;
           };
         }
         else if((n >= name.length()) || (name[n] == '/')) {
           i=n;
+          ii--; i--;
         };  
       }
       else if(name[n] == '/') {
         i=n;
+        ii--; i--;
       };
     };
     n=i; i++; ii++;
@@ -45,10 +46,12 @@ int canonical_dir(std::string &name,bool leading_slash) {
   if(leading_slash) {
     if((name[0] != '/') || (ii==0)) { name="/"+name.substr(0,ii); }
     else { name=name.substr(0,ii); };
+    //if(name.length() <= 1) return 1;
   }
   else {
     if((name[0] != '/') || (ii==0)) { name=name.substr(0,ii); }
     else { name=name.substr(1,ii-1); };
+    //if(name.length() <= 0) return 1;
   };
   return 0;
 }
