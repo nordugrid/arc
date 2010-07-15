b318ad601d1eb2d87c48afd886150ae9ad1460e5
Ali Gholami <gholami@kth.se>
2010-07-15 14:17:53 +0000

    Argus client prototype.

---
+++ src/hed/identitymap/ArgusPEP.h
@@ -0,0 +1,38 @@
+#ifndef __ARC_SEC_ARGUSPEP_H__
+#define __ARC_SEC_ARGUSPEP_H__
+
+#include <arc/ArcConfig.h>
+#include <arc/message/Message.h>
+#include <arc/message/SecHandler.h>
+#include <arc/security/PDP.h>
+#include <pep/pep.h>
+
+namespace ArcSec {
+
+class ArgusPEP : public SecHandler {
+ private:
+
+ std::string pepdlocation;
+
+
+ bool valid_; 
+  // XACML request and response
+  // xacml_request_t * request;
+  // xacml_response_t * response;
+
+ public:
+  ArgusPEP(Arc::Config *cfg);
+  virtual ~ArgusPEP(void);
+  virtual bool Handle(Arc::Message* msg) const ;
+  
+  int create_xacml_request(xacml_request_t** request, const char * subjectid, const char * resourceid, const char * actionid) const ;
+   const char * decision_tostring(xacml_decision_t decision);
+  const char * fulfillon_tostring(xacml_fulfillon_t fulfillon);
+  operator bool(void) { return valid_; };
+  bool operator!(void) { return !valid_; };
+
+};
+
+} // namespace ArcSec
+
+#endif /* __ARC_SEC_ArgusPEP_H__ */
