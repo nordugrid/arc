5e7fe664c06846261b6c2f01a5cdb3f7f81e41e5
Aleksandr Konstantinov <akonstantinov@yahoo.com>
2010-09-23 17:06:08 +0000

    On MAC java library has jnilib suffix. We still need suffix to be changed during installation phase.

--- java/arc_init.cpp
+++ java/arc_init.cpp
@@ -18,7 +18,13 @@ __attribute__((constructor)) void __arc_init(void) {
 
   Glib::Module* module = NULL;
   std::string modulepath;
+#ifdef _MACOSX
+  // According http://developer.apple.com/java/faq/development.html
+  // java libraries have jnilib suffix on MacOSX
+  modulepath = std::string("libjarc.")+".jnilib";
+#else
   modulepath = std::string("libjarc.")+G_MODULE_SUFFIX;
+#endif
 #ifdef HAVE_GLIBMM_BIND_LOCAL
   module = new Glib::Module(modulepath,Glib::ModuleFlags(0));
 #else
