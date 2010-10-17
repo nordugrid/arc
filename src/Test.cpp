eb1132d0f81078ca7063123f2c54f931ba5b655d
Martin Skou Andersen <skou@nbi.dk>
2010-10-17 10:20:45 +0000

    Enable logging in unit test when appending -v.

--- src/Test.cpp
+++ src/Test.cpp
@@ -1,13 +1,32 @@
+// -*- indent-tabs-mode: nil -*-
+
 #include <cppunit/CompilerOutputter.h>
 #include <cppunit/extensions/TestFactoryRegistry.h>
 #include <cppunit/ui/text/TestRunner.h>
 
-int main() {
+#include <arc/Logger.h>
+
+static Arc::Logger logger(Arc::Logger::getRootLogger(), "libarcdatatest");
+
+/*
+ * Use -v to enable logging when running a unit test directly.
+ * E.g.: ./hed/libs/common/test/LoggerTest -v
+ */
+int main(int argc, char **argv) {
+
+  Arc::LogStream logcerr(std::cerr);
+
+  if (argc > 1 && strcmp(argv[1], "-v") == 0) {
+    // set logging
+    Arc::Logger::getRootLogger().addDestination(logcerr);
+    Arc::Logger::getRootLogger().setThreshold(Arc::DEBUG);
+  }
+
   CppUnit::TextUi::TestRunner runner;
   runner.addTest(CppUnit::TestFactoryRegistry::getRegistry().makeTest());
 
   runner.setOutputter(CppUnit::CompilerOutputter::defaultOutputter
-		      (&runner.result(), std::cerr));
+                      (&runner.result(), std::cerr));
 
   bool wasSuccessful = runner.run();
 
