bf9679dd8f42c55dd40ce2100935f7a1414a5699
David Cameron <david.cameron@cern.ch>
2010-11-18 08:49:22 +0000

    fix WIN32 cache method (bug 1501)

--- src/hed/libs/data/FileCache.h
+++ src/hed/libs/data/FileCache.h
@@ -402,7 +402,7 @@ namespace Arc {
               int cache_min=100) {}
     FileCache(const FileCache& cache) {}
     FileCache() {}
-    bool Start(std::string url, bool& available, bool& is_locked, bool& use_remote=true) { return false; }
+    bool Start(std::string url, bool& available, bool& is_locked, bool use_remote=true) { return false; }
     bool Stop(std::string url) { return false; }
     bool StopAndDelete(std::string url) {return false; }
     std::string File(std::string url) { return url; }
