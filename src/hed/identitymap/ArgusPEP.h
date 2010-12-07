bee4d97b2fe9736e11a6d25ab09c5f46bbd9f04f
Ali Gholami <gholami@kth.se>
2010-12-07 16:44:57 +0000

    Made some variables global.

--- src/hed/identitymap/ArgusPEP.h
+++ src/hed/identitymap/ArgusPEP.h
@@ -11,38 +11,42 @@
 namespace ArcSec {
 
 class ArgusPEP : public SecHandler {
- private:
+ 
+  private:
 
- typedef enum {
-   conversion_subject,
-   conversion_direct
- } conversion_type;
+  typedef enum {
+     conversion_subject,
+     conversion_direct
+   } conversion_type;
 
- std::string pepdlocation;
- std::list<std::string> select_attrs;
- std::list<std::string> reject_attrs;
- conversion_type conversion;
-
- bool valid_; 
- static Arc::Logger logger;
+   
+  std::string pepdlocation;
+  std::list<std::string> select_attrs;
+  std::list<std::string> reject_attrs;
+  conversion_type conversion;
+  PEP * pep_handle = NULL;
+  bool res = true;
+  pep_error_t pep_rc;
+  bool valid_; 
+  static Arc::Logger logger;
   // XACML request and response
   // xacml_request_t * request;
   // xacml_response_t * response;
 
- public:
-  ArgusPEP(Arc::Config *cfg);
-  virtual ~ArgusPEP(void);
-  virtual bool Handle(Arc::Message* msg) const ;
-  operator bool(void) { return valid_; };
-  bool operator!(void) { return !valid_; };
+  public:
+    ArgusPEP(Arc::Config *cfg);
+    virtual ~ArgusPEP(void);
+    virtual bool Handle(Arc::Message* msg) const ;
+    operator bool(void) { return valid_; };
+    bool operator!(void) { return !valid_; };
   
- private:
-  int create_xacml_request(xacml_request_t** request, const char * subjectid, const char * resourceid, const char * actionid) const ;
-  int create_xacml_request(std::list<xacml_request_t*>& requests,Arc::XMLNode arcreq) const;
+  private:
+    int create_xacml_request(xacml_request_t** request, const char * subjectid, const char * resourceid, const char * actionid) const ;
+    int create_xacml_request(std::list<xacml_request_t*>& requests,Arc::XMLNode arcreq) const;
  // const char * decision_tostring(xacml_decision_t decision);
  // const char * fulfillon_tostring(xacml_fulfillon_t fulfillon);
 
-};
+  };
 
 } // namespace ArcSec
 
