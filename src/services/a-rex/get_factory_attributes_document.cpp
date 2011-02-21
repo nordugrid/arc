6bf9864c7a761148e4b2d51a22e2c506304cd125
Aleksandr Konstantinov <akonstantinov@yahoo.com>
2011-02-21 07:35:28 +0000

    Disabling reporting job EPRs in GetFactoryAttributesDocument. That is not good for BES but removes performance bottleneck.

--- src/services/a-rex/get_factory_attributes_document.cpp
+++ src/services/a-rex/get_factory_attributes_document.cpp
@@ -41,15 +41,16 @@ Arc::MCC_Status ARexService::GetFactoryAttributesDocument(ARexGMConfig& config,A
   doc.NewChild("bes-factory:IsAcceptingNewActivities")="true";
   if(!common_name_.empty()) doc.NewChild("bes-factory:CommonName")=common_name_;
   if(!long_description_.empty()) doc.NewChild("bes-factory:LongDescription")=long_description_;
-  std::list<std::string> jobs = ARexJob::Jobs(config,logger_);
-  doc.NewChild("bes-factory:TotalNumberOfActivities")=Arc::tostring(jobs.size());
-  for(std::list<std::string>::iterator j = jobs.begin();j!=jobs.end();++j) {
-    Arc::WSAEndpointReference identifier(doc.NewChild("bes-factory:ActivityReference"));
-    // Make job's ID
-    identifier.Address(config.Endpoint()); // address of service
-    identifier.ReferenceParameters().NewChild("a-rex:JobID")=(*j);
-    identifier.ReferenceParameters().NewChild("a-rex:JobSessionDir")=config.Endpoint()+"/"+(*j);
-  };
+  //std::list<std::string> jobs = ARexJob::Jobs(config,logger_);
+  //doc.NewChild("bes-factory:TotalNumberOfActivities")=Arc::tostring(jobs.size());
+  //for(std::list<std::string>::iterator j = jobs.begin();j!=jobs.end();++j) {
+  //  Arc::WSAEndpointReference identifier(doc.NewChild("bes-factory:ActivityReference"));
+  //  // Make job's ID
+  //  identifier.Address(config.Endpoint()); // address of service
+  //  identifier.ReferenceParameters().NewChild("a-rex:JobID")=(*j);
+  //  identifier.ReferenceParameters().NewChild("a-rex:JobSessionDir")=config.Endpoint()+"/"+(*j);
+  //};
+  doc.NewChild("bes-factory:TotalNumberOfActivities")=Arc::tostring(ARexJob::TotalJobs(config,logger_));
   doc.NewChild("bes-factory:TotalNumberOfContainedResources")=Arc::tostring(0);
   doc.NewChild("bes-factory:NamingProfile")="http://schemas.ggf.org/bes/2006/08/bes/naming/BasicWSAddressing";
   doc.NewChild("bes-factory:BESExtension")="http://www.nordugrid.org/schemas/a-rex";
