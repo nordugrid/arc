8bf8a72263fa6b246df0f50817bccb6f19dd9a4d
Mattias Ellert <mattias.ellert@physics.uu.se>
2011-03-02 19:21:18 +0000

    Fix broken test

--- src/hed/libs/credential/test/listfuncTest.cpp
+++ src/hed/libs/credential/test/listfuncTest.cpp
@@ -26,30 +26,30 @@ void f(void *v) {
 }
 
 void listfuncTest::listTest() {
-  
+
   char **v1,**v2,**v3,**v4;
   char *str1,*str2,*str3;
 
-  str1 = (char*) malloc(sizeof(char*)*6);
+  str1 = (char*) malloc(sizeof(char)*6);
   strncpy(str1,"Hello",6); 
-  str2 = (char*) malloc(sizeof(char*)*6);
+  str2 = (char*) malloc(sizeof(char)*6);
   strncpy(str2,"World",6); 
-  str3 = (char*) malloc(sizeof(char*)*6);
+  str3 = (char*) malloc(sizeof(char)*6);
   strncpy(str3,"World",6); 
 
-  v1 = listadd(NULL,NULL,strlen(str1));
+  v1 = listadd(NULL,NULL,sizeof(char*));
   CPPUNIT_ASSERT_EQUAL(v1,(char**)NULL);
 
-  v1 = listadd(NULL,str1,strlen(str1));
+  v1 = listadd(NULL,str1,sizeof(char*));
   CPPUNIT_ASSERT(v1);
 
-  v2 = listadd(NULL,str2,strlen(str2));
+  v2 = listadd(NULL,str2,sizeof(char*));
   CPPUNIT_ASSERT(v2);
 
   v3 = listjoin(v1,v2,2);
   CPPUNIT_ASSERT(v3);
 
-  v4 = listadd(v3,str3,strlen(str1));
+  v4 = listadd(v3,str3,sizeof(char*));
   CPPUNIT_ASSERT(v4);
 
   CPPUNIT_ASSERT(v4[0] == str1);
