06b0a84aa469f01ec127c4fb507e50b990d75cea
Aleksandr Konstantinov <akonstantinov@yahoo.com>
2011-03-08 10:40:02 +0000

    Adding unit tests for loader

---
+++ src/hed/libs/loader/test/PluginTest.cpp
@@ -0,0 +1,45 @@
+#include <cppunit/extensions/HelperMacros.h>
+
+#include <arc/loader/Loader.h>
+
+class PluginTest
+  : public CppUnit::TestFixture {
+
+  CPPUNIT_TEST_SUITE(PluginTest);
+  CPPUNIT_TEST(TestPlugin);
+  CPPUNIT_TEST_SUITE_END();
+
+public:
+  void TestPlugin();
+};
+
+class PluginTestLoader: Arc::Loader {
+public:
+  PluginTestLoader(Arc::XMLNode cfg):Arc::Loader(cfg) {
+  };
+  Arc::PluginsFactory* factory(void) {
+    return factory_;
+  };
+};
+
+void PluginTest::TestPlugin() {
+  std::string config_xml("\
+<?xml version=\"1.0\"?>\n\
+<ArcConfig xmlns=\"http://www.nordugrid.org/schemas/ArcConfig/2007\">\n\
+  <ModuleManager>\n\
+    <Path>.libs/</Path>\n\
+  </ModuleManager>\n\
+  <Plugins>\n\
+    <Name>testplugin</Name>\n\
+  </Plugins>\n\
+</ArcConfig>");
+  Arc::XMLNode cfg(config_xml);
+  PluginTestLoader loader(cfg);
+  CPPUNIT_ASSERT(loader.factory());
+  std::string plugin_name = "testplugin";
+  std::string plugin_kind = "TEST";
+  Arc::PluginArgument* plugin_arg = NULL;
+  CPPUNIT_ASSERT(loader.factory()->get_instance(plugin_kind,plugin_name,plugin_arg,false));
+}
+
+CPPUNIT_TEST_SUITE_REGISTRATION(PluginTest);
---
+++ src/hed/libs/loader/test/TestPlugin.cpp
@@ -0,0 +1,38 @@
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <iostream>
+#include "../Plugin.h"
+
+namespace Test {
+
+class TestPlugin: Arc::Plugin {
+    public:
+        TestPlugin();
+        ~TestPlugin();
+};
+
+TestPlugin::TestPlugin() {
+}
+
+TestPlugin::~TestPlugin(void) {
+}
+
+Arc::Plugin *get_instance(Arc::PluginArgument*) 
+{
+    return (Arc::Plugin *)(new TestPlugin());
+}
+
+} // namespace Test
+
+Arc::PluginDescriptor PLUGINS_TABLE_NAME[] = {
+    {
+        "testplugin",         /* name */
+        "TEST",               /* kind */
+        0,                    /* version */
+        Test::get_instance    /* get_instance function */
+    },
+    { NULL, 0, NULL }
+};
+
