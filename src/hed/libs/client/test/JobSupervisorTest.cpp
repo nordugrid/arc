86f9c232ed5c9255c361b30114b76baa0692ab85
Martin Skou Andersen <skou@nbi.dk>
2011-04-13 06:53:06 +0000

    Extend JobSupervisor unit test.

--- src/hed/libs/client/test/JobSupervisorTest.cpp
+++ src/hed/libs/client/test/JobSupervisorTest.cpp
@@ -14,6 +14,7 @@ class JobSupervisorTest
   : public CppUnit::TestFixture {
 
   CPPUNIT_TEST_SUITE(JobSupervisorTest);
+  CPPUNIT_TEST(TestConstructor);
   CPPUNIT_TEST(TestAddJob);
   CPPUNIT_TEST_SUITE_END();
 
@@ -24,6 +25,7 @@ public:
   void setUp() {}
   void tearDown() {}
 
+  void TestConstructor();
   void TestAddJob();
 
 private:
@@ -40,18 +42,50 @@ JobSupervisorTest::JobSupervisorTest() : usercfg(Arc::initializeCredentialsType(
   j.InfoEndpoint = Arc::URL("http://test.nordugrid.org");
 }
 
+void JobSupervisorTest::TestConstructor()
+{
+  std::list<Arc::Job> jobs;
+  Arc::URL id1("http://test.nordugrid.org/1234567890test1"), id2("http://test.nordugrid.org/1234567890test2");
+  j.IDFromEndpoint = id1;
+  jobs.push_back(j);
+  j.IDFromEndpoint = id2;
+  jobs.push_back(j);
+
+  js = new Arc::JobSupervisor(usercfg, jobs);
+  CPPUNIT_ASSERT(js->JobsFound());
+
+  // One and only one JobController should be loaded.
+  CPPUNIT_ASSERT_EQUAL(1, (int)js->GetJobControllers().size());
+
+  // JobController should contain 2 jobs.
+  CPPUNIT_ASSERT_EQUAL(2, (int)js->GetJobControllers().front()->GetJobs().size());
+
+  CPPUNIT_ASSERT_EQUAL(id1.str(), js->GetJobControllers().front()->GetJobs().front().IDFromEndpoint.str());
+  CPPUNIT_ASSERT_EQUAL(id2.str(), js->GetJobControllers().front()->GetJobs().back().IDFromEndpoint.str());
+
+  delete js;
+}
+
 void JobSupervisorTest::TestAddJob()
 {
   js = new Arc::JobSupervisor(usercfg, std::list<Arc::Job>());
+  CPPUNIT_ASSERT(!js->JobsFound());
+
   j.IDFromEndpoint = Arc::URL("http://test.nordugrid.org/1234567890test1");
   CPPUNIT_ASSERT(js->AddJob(j));
+  CPPUNIT_ASSERT(js->JobsFound());
 
   j.Flavour = "";
   CPPUNIT_ASSERT(!js->AddJob(j));
+  CPPUNIT_ASSERT(js->JobsFound());
 
   j.Flavour = "NON-EXISTENT";
   CPPUNIT_ASSERT(!js->AddJob(j));
+  CPPUNIT_ASSERT(js->JobsFound());
   CPPUNIT_ASSERT(!js->AddJob(j));
+  CPPUNIT_ASSERT(js->JobsFound());
+
+  delete js;
 }
 
 CPPUNIT_TEST_SUITE_REGISTRATION(JobSupervisorTest);
