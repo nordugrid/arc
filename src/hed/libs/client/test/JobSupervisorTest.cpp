2fb85ec9ec093dc5cfbfc840cd418a9a748390b3
Martin Skou Andersen <skou@nbi.dk>
2011-04-13 12:06:33 +0000

    Adding test of the JobSupervisor::Cancel method to unit test.

--- src/hed/libs/client/test/JobSupervisorTest.cpp
+++ src/hed/libs/client/test/JobSupervisorTest.cpp
@@ -16,6 +16,7 @@ class JobSupervisorTest
   CPPUNIT_TEST_SUITE(JobSupervisorTest);
   CPPUNIT_TEST(TestConstructor);
   CPPUNIT_TEST(TestAddJob);
+  CPPUNIT_TEST(TestCancel);
   CPPUNIT_TEST_SUITE_END();
 
 public:
@@ -27,12 +28,24 @@ public:
 
   void TestConstructor();
   void TestAddJob();
+  void TestCancel();
+
+  class JobStateTEST : public Arc::JobState {
+  public:
+    JobStateTEST(const std::string& state) : JobState(state, &StateMap) {}
+    static JobState::StateType StateMap(const std::string& state) {
+      return st;
+    }
+  };
 
 private:
   Arc::UserConfig usercfg;
   Arc::JobSupervisor *js;
   Arc::Job j;
+  static Arc::JobState::StateType st;
 };
+
+Arc::JobState::StateType JobSupervisorTest::st = Arc::JobState::UNDEFINED;
 
 JobSupervisorTest::JobSupervisorTest() : usercfg(Arc::initializeCredentialsType(Arc::initializeCredentialsType::SkipCredentials)) {
   Arc::SetEnv("ARC_PLUGIN_PATH", ".libs");
@@ -84,6 +97,63 @@ void JobSupervisorTest::TestAddJob()
   CPPUNIT_ASSERT(js->JobsFound());
   CPPUNIT_ASSERT(!js->AddJob(j));
   CPPUNIT_ASSERT(js->JobsFound());
+
+  delete js;
+}
+
+void JobSupervisorTest::TestCancel()
+{
+  std::list<Arc::Job> jobs;
+  Arc::URL id1("http://test.nordugrid.org/1234567890test1"),
+           id2("http://test.nordugrid.org/1234567890test2"),
+           id3("http://test.nordugrid.org/1234567890test3");
+
+  st = Arc::JobState::RUNNING;
+  j.State = JobStateTEST("R");
+  j.IDFromEndpoint = id1;
+  jobs.push_back(j);
+
+  st = Arc::JobState::FINISHED;
+  j.State = JobStateTEST("F");
+  j.IDFromEndpoint = id2;
+  jobs.push_back(j);
+
+  st = Arc::JobState::UNDEFINED;
+  j.State = JobStateTEST("U");
+  j.IDFromEndpoint = id3;
+  jobs.push_back(j);
+
+  js = new Arc::JobSupervisor(usercfg, jobs);
+
+  std::list<Arc::URL> jobsToBeCanceled;
+  jobsToBeCanceled.push_back(id1);
+  jobsToBeCanceled.push_back(id2);
+  jobsToBeCanceled.push_back(id3);
+  jobsToBeCanceled.push_back(Arc::URL("http://test.nordugrid.org/non-existent"));
+
+  JobControllerTestACCControl::cancelStatus = true;
+
+  std::list<Arc::URL> notCanceled;
+  std::list<Arc::URL> canceled = js->Cancel(jobsToBeCanceled, notCanceled);
+
+  CPPUNIT_ASSERT_EQUAL(2, (int)canceled.size());
+  CPPUNIT_ASSERT_EQUAL(id1.str(), canceled.front().str());
+  CPPUNIT_ASSERT_EQUAL(id2.str(), canceled.back().str());
+
+  CPPUNIT_ASSERT_EQUAL(1, (int)notCanceled.size());
+  CPPUNIT_ASSERT_EQUAL(id3.str(), notCanceled.back().str());
+
+
+  notCanceled.clear();
+  JobControllerTestACCControl::cancelStatus = false;
+  canceled = js->Cancel(jobsToBeCanceled, notCanceled);
+
+  CPPUNIT_ASSERT_EQUAL(1, (int)canceled.size());
+  CPPUNIT_ASSERT_EQUAL(id2.str(), canceled.back().str());
+
+  CPPUNIT_ASSERT_EQUAL(2, (int)notCanceled.size());
+  CPPUNIT_ASSERT_EQUAL(id1.str(), notCanceled.front().str());
+  CPPUNIT_ASSERT_EQUAL(id3.str(), notCanceled.back().str());
 
   delete js;
 }
