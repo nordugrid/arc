a003357a315351efab5e64166763b3a8a7631f21
Weizhong Qiang <Qiang@mycompany.com>
2011-04-21 03:46:51 +0000

    modification in voms test

--- src/hed/libs/credential/test/VOMSUtilTest.cpp
+++ src/hed/libs/credential/test/VOMSUtilTest.cpp
@@ -27,18 +27,16 @@ void VOMSUtilTest::VOMSTrustListTest() {
 
   //  CPPUNIT_ASSERT_EQUAL(!parseVOMSAC(c, emptystring, emptystring, emptylist, output, false),false);
 
-  //
   // Create the AC on the VOMS side
-  //
 
   std::string CAcert("ca_cert.pem");
-  std::string voms_key_file("host_key.pem");
-  std::string voms_cert_file("host_cert.pem");
+  std::string user_key_file("user_key.pem");
+  std::string user_cert_file("user_cert.pem");
 
-  Arc::Credential voms_cred(voms_cert_file, voms_key_file, ".", CAcert);
+  Arc::Credential issuer_cred(user_cert_file, user_key_file, "", CAcert, "userpassword");
 
-  std::string user_proxy_file("user_proxy.pem");
-  Arc::Credential proxy_cred(user_proxy_file, user_proxy_file, ".", CAcert);
+  std::string holder_proxy_file("user_proxy.pem");
+  Arc::Credential holder_cred(holder_proxy_file, "", "", CAcert);
 
   std::vector<std::string> fqan;
   fqan.push_back("/nordugrid.org");
@@ -54,7 +52,7 @@ void VOMSUtilTest::VOMSTrustListTest() {
   std::string uri = "voms.nordugrid.org:50000";
 
   std::string ac_str;
-  Arc::createVOMSAC(ac_str, voms_cred, proxy_cred, fqan, targets, attrs, voname, uri, 3600*12);
+  Arc::createVOMSAC(ac_str, issuer_cred, holder_cred, fqan, targets, attrs, voname, uri, 3600*12);
   
   //
   // Create the full AC which is an ordered list of AC 
@@ -66,32 +64,24 @@ void VOMSUtilTest::VOMSTrustListTest() {
 
   std::string voms_proxy_file("voms_proxy.pem");
 
+  // Request
   int keybits = 1024;
   int proxydepth = 10;
   Arc::Time t;
-
-  Arc::Credential proxy_req(t, Arc::Period(12*3600), keybits);
-  std::string proxy_req_file("./request_withac.pem");
+  Arc::Credential proxy_req(t, Arc::Period(12*3600), keybits, "gsi2", "limited", "", proxydepth);
+  std::string proxy_req_file("voms_req.pem");
   proxy_req.GenerateRequest(proxy_req_file.c_str());
 
-  //Signing side
-  Arc::Credential proxy;
-  proxy.InquireRequest(proxy_req_file.c_str());
-  proxy.SetProxyPolicy("gsi2", "limited", "", proxydepth);
   //Add AC extension to proxy certificat before signing it
-  proxy.AddExtension("acseq", (char**) aclist);
+  proxy_req.AddExtension("acseq", (char**) aclist);
 
-  // The User credentials should sign the voms proxy
-  std::string user_cert_file("user_cert.pem");
-  std::string user_key_file("user_key.pem");
-  Arc::Credential user_cred(user_cert_file, user_key_file, ".", CAcert,"userpass");
-
-  user_cred.SignRequest(&proxy, voms_proxy_file.c_str());
+  //Sign the voms proxy
+  issuer_cred.SignRequest(&proxy_req, voms_proxy_file.c_str());
 
   std::string private_key, signing_cert, signing_cert_chain;
   proxy_req.OutputPrivatekey(private_key);
-  user_cred.OutputCertificate(signing_cert);
-  user_cred.OutputCertificateChain(signing_cert_chain);
+  issuer_cred.OutputCertificate(signing_cert);
+  issuer_cred.OutputCertificateChain(signing_cert_chain);
 
   std::ofstream out_f(voms_proxy_file.c_str(), std::ofstream::app);
   out_f.write(private_key.c_str(), private_key.size());
@@ -101,16 +91,12 @@ void VOMSUtilTest::VOMSTrustListTest() {
 
 
   std::vector<std::string> vomscert_trust_dn;
-  vomscert_trust_dn.push_back("/O=Grid/OU=ARC/CN=localhost");
-  vomscert_trust_dn.push_back("^/O=Grid/O=NorduGrid");
+  vomscert_trust_dn.push_back("/O=Grid/OU=ARC/OU=localdomain/CN=User");
+  vomscert_trust_dn.push_back("^/O=Grid/OU=ARC/CN=CA");
   vomscert_trust_dn.push_back("NEXT CHAIN");
   vomscert_trust_dn.push_back("/O=Grid/OU=ARC/CN=CA");
 
-  //
   // Read and pars VOMS proxy
-  //
-
-//  static Arc::Logger& logger = Arc::Logger::rootLogger;
 
   Arc::Credential voms_proxy(voms_proxy_file, "", ".", CAcert);
 
@@ -123,8 +109,8 @@ void VOMSUtilTest::VOMSTrustListTest() {
     }
   }
 
-  //CPPUNIT_ASSERT(attributes.size() == 1);
-  //CPPUNIT_ASSERT(attributes[0].attributes.size() == 4);
+  CPPUNIT_ASSERT(attributes.size() == 1);
+  CPPUNIT_ASSERT(attributes[0].attributes.size() == 4);
 
 }
 
