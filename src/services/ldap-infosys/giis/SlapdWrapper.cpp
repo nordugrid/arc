95530add071bb13c222ed9fcdea2f3fcaf2086db
Mattias Ellert <mattias.ellert@physics.uu.se>
2011-08-21 16:45:24 +0000

    Better slapd wrapper that doesn't need the -d 0 trick to work

--- src/services/ldap-infosys/giis/SlapdWrapper.cpp
+++ src/services/ldap-infosys/giis/SlapdWrapper.cpp
@@ -76,18 +76,97 @@ typedef struct slap_rep SlapReply;
 
 #endif
 
-typedef int (*BI_search_op)(Operation*, SlapReply*);
-
-static BI_search_op shell_back_search;
+typedef int (*BI_bi_func)(void**);
+typedef int (*BI_op_func)(Operation*, SlapReply*);
 
 static int shell_back_search_wrapper(Operation *op, SlapReply *rs) {
-  void *save;
-  int res;
-  save = op->o_request.oq_search.rs_attrs;
+  static BI_op_func shell_back_search = NULL;
+
+  if (!shell_back_search) {
+    shell_back_search =
+      (BI_op_func) dlsym(RTLD_DEFAULT, "shell_back_search");
+  }
+
+  if (!shell_back_search) {
+    std::cerr << "Can not find shell_back_search" << std::endl;
+    exit(1);
+  }
+
+  void* save = op->o_request.oq_search.rs_attrs;
   op->o_request.oq_search.rs_attrs = NULL;
-  res = shell_back_search(op, rs);
+  int res = shell_back_search(op, rs);
   op->o_request.oq_search.rs_attrs = save;
   return res;
+}
+
+static int shell_back_initialize_wrapper(void **bi) {
+  static BI_bi_func shell_back_initialize = NULL;
+
+  if (!shell_back_initialize) {
+    shell_back_initialize =
+      (BI_bi_func) dlsym(RTLD_DEFAULT, "shell_back_initialize");
+  }
+
+  if (!shell_back_initialize) {
+    std::cerr << "Can not find shell_back_initialize" << std::endl;
+    exit(1);
+  }
+
+  int res = shell_back_initialize(bi);
+
+  static BI_op_func shell_back_search = NULL;
+
+  if (!shell_back_search) {
+    shell_back_search =
+      (BI_op_func) dlsym(RTLD_DEFAULT, "shell_back_search");
+  }
+
+  if (!shell_back_search) {
+    std::cerr << "Can not find shell_back_search" << std::endl;
+    exit(1);
+  }
+
+  for (int i = 0; i < 100; i++) {
+    if (bi[i] == (void*)shell_back_search) {
+      bi[i] = (void*)&shell_back_search_wrapper;
+      break;
+    }
+  }
+
+  return res;
+}
+
+int init_module(int, char**) {
+  backend_info_t backend_info =
+    (backend_info_t) dlsym(RTLD_DEFAULT, "backend_info");
+
+  if (!backend_info) {
+    std::cerr << "Can not find backend_info" << std::endl;
+    exit(1);
+  }
+
+  void **bi = backend_info("shell");
+
+  if (bi) {
+    BI_op_func shell_back_search =
+      (BI_op_func) dlsym(RTLD_DEFAULT, "shell_back_search");
+
+    if (!shell_back_search) {
+      std::cerr << "Can not find shell_back_search" << std::endl;
+      exit(1);
+    }
+
+    if (shell_back_search) {
+      for (int i = 0; i < 100; i++) {
+        if (bi[i] == (void*)shell_back_search) {
+          bi[i] = (void*)&shell_back_search_wrapper;
+          break;
+        }
+      }
+    }
+  }
+
+  return 0;
 }
 
 } /* extern "C" */
@@ -98,68 +177,26 @@ public:
   ~SlapdWrapper() {}
 };
 
-static void* wrapper(void*) {
-  void *handle = dlopen(NULL, RTLD_LAZY); /* handle for main program */
-  backend_info_t backend_info = (backend_info_t) dlsym(handle, "backend_info");
-  if(!backend_info) {
-    /* This is not the slapd binary - nothing to wrap */
-    dlclose(handle);
-    return NULL;
-  }
+SlapdWrapper::SlapdWrapper() {
+  BI_op_func shell_back_initialize =
+    (BI_op_func) dlsym(RTLD_DEFAULT, "shell_back_initialize");
 
-  void *shell_handle = NULL;
-  const char *arc_ldaplib_shell = getenv("ARC_LDAPLIB_SHELL");
-  if(!arc_ldaplib_shell)
-    arc_ldaplib_shell = "/usr/lib/ldap/back_shell.so";
-  shell_back_search = (BI_search_op) dlsym(handle, "shell_back_search");
-  if(!shell_back_search) {
-    /* Shell backend is not compiled statically into main program
-       Searching in modules... */
-    shell_handle = dlopen(arc_ldaplib_shell, RTLD_LAZY);
-    if(!shell_handle) {
-      std::cerr << "Error: Unable to dlopen " << arc_ldaplib_shell << std::endl;
-      exit(1);
-    }
-    shell_back_search = (BI_search_op) dlsym(shell_handle, "shell_back_search");
-  }
-
-  if(!shell_back_search) {
-    std::cerr << "Error: shell_back_search found neither in main program, nor in " << arc_ldaplib_shell << std::endl;
-    exit(1);
-  }
-
-  int i = 0;
-  void **backendInfo = NULL;
-  do {
-    sleep(1); /* let the slapd server start */
-    backendInfo = backend_info("shell");
-    i++;
-  }
-  while (!backendInfo && i < 150);
-
-  if(backendInfo)
-    for(i = 0; i < 100; i++) {
-      if(backendInfo[i] == (void*)shell_back_search) {
-	backendInfo[i] = (void*)&shell_back_search_wrapper;
-	break;
+  if (shell_back_initialize) {
+    // Look for shell_back_initialize in backend info for static backends
+    void** slap_binfo = (void**) dlsym(RTLD_DEFAULT, "slap_binfo");
+    if (slap_binfo) {
+      for (int i = 0; i < 2000; i++) {
+        if (slap_binfo[i] == (void*)shell_back_initialize) {
+          slap_binfo[i] = (void*)&shell_back_initialize_wrapper;
+          break;
+        }
       }
     }
-  else {
-    std::cerr << "Error: slapd shell backend not found - exiting" << std::endl;
-    exit(1);
   }
-
-  if(shell_handle)
-    dlclose(shell_handle);
-  dlclose(handle);
-
-  return NULL;
-}
-
-SlapdWrapper::SlapdWrapper() {
-  pthread_t pid;
-  pthread_create(&pid, NULL, &wrapper, NULL);
-  pthread_detach(pid);
+  else {
+    std::cerr << "The shell_back_initialize symbol does not exist in default scope." << std::endl;
+    std::cerr << "Try adding the slapd wrapper as a module instead." << std::endl;
+  }
 }
 
 SlapdWrapper slapdwrapper;
