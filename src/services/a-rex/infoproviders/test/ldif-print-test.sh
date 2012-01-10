a1ba2926eba346cce138198429b0f2063c3b089d
Florido Paganelli <hep-frp@lu.se>
2012-01-10 16:03:48 +0000

    - updated version of the testing script, echoes stuff while running.

--- src/services/a-rex/infoproviders/test/ldif-print-test.sh
+++ src/services/a-rex/infoproviders/test/ldif-print-test.sh
@@ -21,6 +21,7 @@ fi
 # will assume /tmp/ as the base dir for BDII stuff.
 
 # create fake bdii dirs
+echo "creating fake bdii dirs..."
 mkdir -p /tmp/run/arc/bdii
 mkdir -p /tmp/tmp/arc/bdii
 # this is not relocatable. Needs to be root
@@ -28,12 +29,14 @@ mkdir -p /var/run/arc/infosys
 
 
 # create fake bdii pidfile
+echo "Creating fake bdii pidfile"
 touch /tmp/bdii-update.pid
 
 # chmod for normal user to run CEinfo.pl and read ldif-script.sh
+echo "Chmodding dirs for normal user to access..."
 chmod -R 777 /tmp/run/arc/bdii
 chmod -R 777 /var/run/arc/infosys
 chmod -R 777 /tmp/tmp/arc/bdii
 
-# please see ldif-print-test-arc.conf and pass it to CEinfo.pl
-# ./CEinfo.pl --config test/ldif-print-test-arc.conf
+echo "please see ldif-print-test-arc.conf and pass it to CEinfo.pl"
+echo " sudo ./CEinfo.pl --config test/ldif-print-test-arc.conf  "
