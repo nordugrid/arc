795d0466a6fc114b88c824ad3aa9c3d7f804831f
Martin Skou Andersen <skou@nbi.dk>
2012-01-27 16:14:43 +0000

    Dont allow threading when handling Swig Python iterators (fixes bug 2683). Add regression test for bug 2683.

---
+++ python/test/UsingTheNextMethodOnASwigIteratorRegTest.py
@@ -0,0 +1,15 @@
+'''
+This regression test tests whether invoking the next method on a Swig
+iterator obtained from a std::list of ARC C++ objects generates a
+segmentation fault. That issue was reported in bug 2683.
+'''
+
+import arc
+
+jobs = arc.JobList()
+
+jobs.push_back(arc.Job())
+
+itJobs = jobs.__iter__()
+
+itJobs.next()
