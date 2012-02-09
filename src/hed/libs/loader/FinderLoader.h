32b41a98b1bee2944ed6fcee618d3a2c79b5a6e2
Martin Skou Andersen <skou@nbi.dk>
2012-02-09 09:53:18 +0000

    Correcting include.

--- src/hed/libs/loader/FinderLoader.h
+++ src/hed/libs/loader/FinderLoader.h
@@ -3,14 +3,14 @@
 #ifndef __ARC_FINDERLOADER_H__
 #define __ARC_FINDERLOADER_H__
 
-#include <map>
+#include <list>
 #include <string>
 
 namespace Arc {
 
   // TODO: Remove this class by partially moving its functionality
-  //  to PluginsFactory and related classes. That should remove 
-  //  dependency of plugin loading library on classes managing user 
+  //  to PluginsFactory and related classes. That should remove
+  //  dependency of plugin loading library on classes managing user
   //  configuration.
   // This class is fully static.
   class FinderLoader
