4f495e044dcbf67781184f7eacc53dc36bac6aae
Martin Skou Andersen <skou@nbi.dk>
2012-02-15 14:12:24 +0000

    Adding missing file.

---
+++ src/hed/acc/SER/DescriptorsSER.cpp
@@ -0,0 +1,18 @@
+// -*- indent-tabs-mode: nil -*-
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <arc/loader/Plugin.h>
+
+#include "ServiceEndpointRetrieverPluginEGIIS.h"
+#include "ServiceEndpointRetrieverPluginTOPBDII.h"
+#include "ServiceEndpointRetrieverPluginEMIR.h"
+
+Arc::PluginDescriptor PLUGINS_TABLE_NAME[] = {
+  { "EGIIS", "HED:ServiceEndpointRetrieverPlugin", "", 0, &Arc::ServiceEndpointRetrieverPluginEGIIS::Instance },
+  { "TOPBDII", "HED:ServiceEndpointRetrieverPlugin", "", 0, &Arc::ServiceEndpointRetrieverPluginTOPBDII::Instance },
+  { "EMIR", "HED:ServiceEndpointRetrieverPlugin", "", 0, &Arc::ServiceEndpointRetrieverPluginEMIR::Instance },
+  { NULL, NULL, NULL, 0, NULL }
+};
