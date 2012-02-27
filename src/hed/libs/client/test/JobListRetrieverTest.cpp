b1b3c1d40cdf20834297452f62cf6b0c8fdc8ebc
Martin Skou Andersen <skou@nbi.dk>
2012-02-27 14:34:45 +0000

    Add unit test for JobListRetriever.

---
+++ src/hed/libs/client/test/JobListRetrieverTest.cpp
@@ -0,0 +1,53 @@
+#include <cppunit/extensions/HelperMacros.h>
+
+#include <arc/UserConfig.h>
+#include <arc/client/Endpoint.h>
+#include <arc/client/Job.h>
+#include <arc/client/JobListRetriever.h>
+#include <arc/client/TestACCControl.h>
+
+//static Arc::Logger testLogger(Arc::Logger::getRootLogger(), "JobListRetrieverTest");
+
+class JobListRetrieverTest
+  : public CppUnit::TestFixture {
+
+  CPPUNIT_TEST_SUITE(JobListRetrieverTest);
+  CPPUNIT_TEST(PluginLoading);
+  CPPUNIT_TEST(QueryTest);
+  CPPUNIT_TEST_SUITE_END();
+
+public:
+  JobListRetrieverTest() {};
+
+  void setUp() {}
+  void tearDown() {}
+
+  void PluginLoading();
+  void QueryTest();
+};
+
+void JobListRetrieverTest::PluginLoading() {
+  Arc::EndpointRetrieverPluginLoader<Arc::ComputingInfoEndpoint, Arc::Job> l;
+  Arc::EndpointRetrieverPlugin<Arc::ComputingInfoEndpoint, Arc::Job>* p = l.load("TEST");
+  CPPUNIT_ASSERT(p != NULL);
+}
+
+
+void JobListRetrieverTest::QueryTest() {
+  Arc::EndpointQueryingStatus sInitial(Arc::EndpointQueryingStatus::SUCCESSFUL);
+
+  Arc::JobListRetrieverPluginTESTControl::delay = 1;
+  Arc::JobListRetrieverPluginTESTControl::status = sInitial;
+
+  Arc::EndpointRetrieverPluginLoader<Arc::ComputingInfoEndpoint, Arc::Job> l;
+  Arc::EndpointRetrieverPlugin<Arc::ComputingInfoEndpoint, Arc::Job>* p = l.load("TEST");
+  CPPUNIT_ASSERT(p != NULL);
+
+  Arc::UserConfig uc;
+  Arc::ComputingInfoEndpoint endpoint;
+  std::list<Arc::Job> jobs;
+  Arc::EndpointQueryingStatus sReturned = p->Query(uc, endpoint, jobs, Arc::EndpointFilter<Arc::Job>());
+  CPPUNIT_ASSERT(sReturned == Arc::EndpointQueryingStatus::SUCCESSFUL);
+}
+
+CPPUNIT_TEST_SUITE_REGISTRATION(JobListRetrieverTest);
