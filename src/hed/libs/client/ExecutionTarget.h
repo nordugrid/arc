7b9730e397ae6d52a752fad372e919c2b05a95a0
Martin Skou Andersen <skou@nbi.dk>
2012-03-15 09:44:48 +0000

    Reverting changes committed by mistake.

--- src/hed/libs/client/ExecutionTarget.h
+++ src/hed/libs/client/ExecutionTarget.h
@@ -72,8 +72,6 @@ namespace Arc {
     std::string Owner;
   };
 
-  class BenchmarkAttributes : public std::map<std::string, double> {};
-
   class ExecutionEnvironmentAttributes {
   public:
     ExecutionEnvironmentAttributes()
@@ -292,10 +290,10 @@ namespace Arc {
 
   class ComputingManagerType : public GLUE2Entity<ComputingManagerAttributes> {
   public:
-    ComputingManagerType() : Benchmarks(new BenchmarkAttributes), ApplicationEnvironments(new std::list<ApplicationEnvironment>) {}
+    ComputingManagerType() : Benchmarks(new std::map<std::string, double>), ApplicationEnvironments(new std::list<ApplicationEnvironment>) {}
     // TODO: Currently using int as key, use std::string instead for holding ID.
     std::map<int, ExecutionEnvironmentType> ExecutionEnvironment;
-    CountedPointer<BenchmarkAttributes> Benchmarks;
+    CountedPointer< std::map<std::string, double> > Benchmarks;
     /// ApplicationEnvironments
     /**
      * The ApplicationEnvironments member is a list of
@@ -350,7 +348,7 @@ namespace Arc {
       ComputingShare(new ComputingShareAttributes()),
       ComputingManager(new ComputingManagerAttributes()),
       ExecutionEnvironment(new ExecutionEnvironmentAttributes()),
-      Benchmarks(new BenchmarkAttributes()),
+      Benchmarks(new std::map<std::string, double>()),
       ApplicationEnvironments(new std::list<ApplicationEnvironment>()) {};
 
     /// Create an ExecutionTarget
@@ -371,7 +369,7 @@ namespace Arc {
                     const CountedPointer<ComputingShareAttributes>& csh,
                     const CountedPointer<ComputingManagerAttributes>& cm,
                     const CountedPointer<ExecutionEnvironmentAttributes>& ee,
-                    const CountedPointer<BenchmarkAttributes>& b,
+                    const CountedPointer< std::map<std::string, double> >& b,
                     const CountedPointer< std::list<ApplicationEnvironment> >& ae) :
       Location(l), AdminDomain(a), ComputingService(cse),
       ComputingEndpoint(ce), ComputingShare(csh), ComputingManager(cm),
@@ -471,7 +469,7 @@ namespace Arc {
     CountedPointer<ComputingShareAttributes> ComputingShare;
     CountedPointer<ComputingManagerAttributes> ComputingManager;
     CountedPointer<ExecutionEnvironmentAttributes> ExecutionEnvironment;
-    CountedPointer<BenchmarkAttributes> Benchmarks;
+    CountedPointer< std::map<std::string, double> > Benchmarks;
     CountedPointer< std::list<ApplicationEnvironment> > ApplicationEnvironments;
 
   private:
