e3c009985ebe2dec349c265287987d8e25d709f9
Nagy Zsombor <zsombor@niif.hu>
2012-03-20 16:06:45 +0000

    python example for retrieving the list of jobs from a computing element

---
+++ python/examples/joblist_retrieval.py
@@ -0,0 +1,37 @@
+#! /usr/bin/env python
+import arc
+import sys
+
+# Creating a UserConfig object with the user's proxy
+# and the path of the trusted CA certificates
+uc = arc.UserConfig()
+uc.ProxyPath("/tmp/proxy")
+uc.CACertificatesDirectory("/tmp/certificates")
+
+# Creating an endpoint for a Computing Element
+endpoint = arc.Endpoint("piff.hep.lu.se", arc.Endpoint.COMPUTINGINFO)
+
+# Creating a container which will store the retrieved jobs
+jobs = arc.JobContainer()
+
+# Create a job list retriever
+retriever = arc.JobListRetriever(uc)
+# Add our container as the consumer of this retriever, so it will get the results
+retriever.addConsumer(jobs)
+
+# import sys
+# arc.Logger.getRootLogger().addDestination(arc.LogStream(sys.stdout))
+# arc.Logger.getRootLogger().setThreshold(arc.DEBUG)
+
+# Add our endpoint to the retriever, which starts querying it
+retriever.addEndpoint(endpoint)
+
+# Wait until it finishes
+retriever.wait()
+
+# arc.Logger.getRootLogger().removeDestinations()
+
+# Get the status of the retrieval
+print retriever.getStatusOfEndpoint(endpoint).str()
+
+print "Number of jobs found:", len(jobs)
