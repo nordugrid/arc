72c6419b78f813a131ae0555849d20f6be011c74
Nagy Zsombor <zsombor@niif.hu>
2012-03-21 10:17:52 +0000

    protect the examples from segfault by calling _exit at the end

--- python/examples/joblist_retrieval.py
+++ python/examples/joblist_retrieval.py
@@ -1,37 +1,44 @@
 #! /usr/bin/env python
 import arc
 import sys
+import os
 
-# Creating a UserConfig object with the user's proxy
-# and the path of the trusted CA certificates
-uc = arc.UserConfig()
-uc.ProxyPath("/tmp/proxy")
-uc.CACertificatesDirectory("/tmp/certificates")
+def example():
+    # Creating a UserConfig object with the user's proxy
+    # and the path of the trusted CA certificates
+    uc = arc.UserConfig()
+    uc.ProxyPath("/tmp/proxy")
+    uc.CACertificatesDirectory("/tmp/certificates")
 
-# Creating an endpoint for a Computing Element
-endpoint = arc.Endpoint("piff.hep.lu.se", arc.Endpoint.COMPUTINGINFO)
+    # Creating an endpoint for a Computing Element
+    endpoint = arc.Endpoint("piff.hep.lu.se", arc.Endpoint.COMPUTINGINFO)
 
-# Creating a container which will store the retrieved jobs
-jobs = arc.JobContainer()
+    # Creating a container which will store the retrieved jobs
+    jobs = arc.JobContainer()
 
-# Create a job list retriever
-retriever = arc.JobListRetriever(uc)
-# Add our container as the consumer of this retriever, so it will get the results
-retriever.addConsumer(jobs)
+    # Create a job list retriever
+    retriever = arc.JobListRetriever(uc)
+    # Add our container as the consumer of this retriever, so it will get the results
+    retriever.addConsumer(jobs)
 
-# import sys
-# arc.Logger.getRootLogger().addDestination(arc.LogStream(sys.stdout))
-# arc.Logger.getRootLogger().setThreshold(arc.DEBUG)
+    # Add our endpoint to the retriever, which starts querying it
+    retriever.addEndpoint(endpoint)
 
-# Add our endpoint to the retriever, which starts querying it
-retriever.addEndpoint(endpoint)
+    # Wait until it finishes
+    retriever.wait()
 
-# Wait until it finishes
-retriever.wait()
+    # Get the status of the retrieval
+    print retriever.getStatusOfEndpoint(endpoint).str()
 
-# arc.Logger.getRootLogger().removeDestinations()
+    print "Number of jobs found:", len(jobs)
 
-# Get the status of the retrieval
-print retriever.getStatusOfEndpoint(endpoint).str()
+# run the example and catch all Exceptions in order to make sure we can call _exit() at the end
+try:
+    example()
+except:
+    import traceback
+    traceback.print_exc()
+    os._exit(1)
 
-print "Number of jobs found:", len(jobs)
+# _exit() is needed to avoid segmentation faults by still running background threads
+os._exit(0)
--- python/examples/service_discovery.py
+++ python/examples/service_discovery.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python
 import arc
 import sys
+import os
 
 def retrieve(uc, endpoints):
     # The ComputingServiceRetriever needs the UserConfig to know which credentials
@@ -16,50 +17,62 @@ def retrieve(uc, endpoints):
     retriever.wait()
     return retriever
 
-# Creating a UserConfig object with the user's proxy
-# and the path of the trusted CA certificates
-uc = arc.UserConfig()
-uc.ProxyPath("/tmp/proxy")
-uc.CACertificatesDirectory("/tmp/certificates")
+def example():
+    # Creating a UserConfig object with the user's proxy
+    # and the path of the trusted CA certificates
+    uc = arc.UserConfig()
+    uc.ProxyPath("/tmp/proxy")
+    uc.CACertificatesDirectory("/tmp/certificates")
 
-# Query two registries (index servers) for Computing Services
-registries = [
-    arc.Endpoint("index1.nordugrid.org", arc.Endpoint.REGISTRY),
-    arc.Endpoint("arc-emi.grid.upjs.sk/O=Grid/Mds-Vo-Name=ARC-EMI", arc.Endpoint.REGISTRY)
-]
+    # Query two registries (index servers) for Computing Services
+    registries = [
+        arc.Endpoint("index1.nordugrid.org", arc.Endpoint.REGISTRY),
+        arc.Endpoint("arc-emi.grid.upjs.sk/O=Grid/Mds-Vo-Name=ARC-EMI", arc.Endpoint.REGISTRY)
+    ]
 
-retriever = retrieve(uc, registries)
+    retriever = retrieve(uc, registries)
 
-# The retriever acts as a list containing all the discovered ComputingServices:
-print "Discovered ComputingServices:", ", ".join([service.Name for service in retriever])
+    # The retriever acts as a list containing all the discovered ComputingServices:
+    print "Discovered ComputingServices:", ", ".join([service.Name for service in retriever])
 
-# Get all the ExecutionTargets on these ComputingServices
-targets = retriever.GetExecutionTargets()
-print "Number of ExecutionTargets on these ComputingServices:", len(targets)
+    # Get all the ExecutionTargets on these ComputingServices
+    targets = retriever.GetExecutionTargets()
+    print "Number of ExecutionTargets on these ComputingServices:", len(targets)
 
-# Query the local infosys (COMPUTINGINFO) of computing elements
-computing_elements = [
-    arc.Endpoint("piff.hep.lu.se", arc.Endpoint.COMPUTINGINFO, "org.nordugrid.ldapglue2"),
-    arc.Endpoint("pgs03.grid.upjs.sk", arc.Endpoint.COMPUTINGINFO)
-]
+    # Query the local infosys (COMPUTINGINFO) of computing elements
+    computing_elements = [
+        arc.Endpoint("piff.hep.lu.se", arc.Endpoint.COMPUTINGINFO, "org.nordugrid.ldapglue2"),
+        arc.Endpoint("pgs03.grid.upjs.sk", arc.Endpoint.COMPUTINGINFO)
+    ]
 
-retriever2 = retrieve(uc, computing_elements)
+    retriever2 = retrieve(uc, computing_elements)
 
-# Get all the ExecutionTargets on these ComputingServices
-targets2 = retriever2.GetExecutionTargets()
+    # Get all the ExecutionTargets on these ComputingServices
+    targets2 = retriever2.GetExecutionTargets()
 
-print "The discovered ExecutionTargets:"
-for target in targets2:
-    target.SaveToStream(arc.CPyOstream(sys.stdout), False)
+    print "The discovered ExecutionTargets:"
+    for target in targets2:
+        target.SaveToStream(arc.CPyOstream(sys.stdout), False)
     
     
-# Query both registries and computing elements at the same time:
-endpoints = [
-    arc.Endpoint("arc-emi.grid.upjs.sk/O=Grid/Mds-Vo-Name=ARC-EMI", arc.Endpoint.REGISTRY),
-    arc.Endpoint("piff.hep.lu.se", arc.Endpoint.COMPUTINGINFO, "org.nordugrid.ldapglue2")
-]
+    # Query both registries and computing elements at the same time:
+    endpoints = [
+        arc.Endpoint("arc-emi.grid.upjs.sk/O=Grid/Mds-Vo-Name=ARC-EMI", arc.Endpoint.REGISTRY),
+        arc.Endpoint("piff.hep.lu.se", arc.Endpoint.COMPUTINGINFO, "org.nordugrid.ldapglue2")
+    ]
 
-retriever3 = retrieve(uc, endpoints)
+    retriever3 = retrieve(uc, endpoints)
 
-print "Discovered ComputingServices:", ", ".join([service.Name for service in retriever3])
+    print "Discovered ComputingServices:", ", ".join([service.Name for service in retriever3])
 
+
+# run the example and catch all Exceptions in order to make sure we can call _exit() at the end
+try:
+    example()
+except:
+    import traceback
+    traceback.print_exc()
+    os._exit(1)
+
+# _exit() is needed to avoid segmentation faults by still running background threads
+os._exit(0)
