4b2f99a7b9bc77c5494fbb0c089bb1eb65556eb3
Nagy Zsombor <zsombor@niif.hu>
2012-03-22 15:05:18 +0000

    add example to retrieve the results of a job

---
+++ python/examples/retreiving_results.py
@@ -0,0 +1,53 @@
+#! /usr/bin/env python
+import arc
+import sys
+import os
+
+# arc.Logger.getRootLogger().addDestination(arc.LogStream(sys.stderr))
+# arc.Logger.getRootLogger().setThreshold(arc.DEBUG)
+
+def example():
+    # Creating a UserConfig object with the user's proxy
+    # and the path of the trusted CA certificates
+    uc = arc.UserConfig()
+    uc.ProxyPath("/tmp/x509up_u%s" % os.getuid())
+    uc.CACertificatesDirectory("/etc/grid-security/certificates")
+    
+    # Create a new job object with a given JobID
+    job = arc.Job()
+    job.JobID = arc.URL("https://piff.hep.lu.se:443/arex/hYDLDmyxvUfn5h5iWqkutBwoABFKDmABFKDmIpHKDmYBFKDmtRy9En")
+    job.Flavour = "ARC1"
+    job.Cluster = arc.URL("https://piff.hep.lu.se:443/arex")
+    
+    print "Get job information from the computing element..."
+    # Put the job into a JobSupervisor and update its information
+    job_supervisor = arc.JobSupervisor(uc, [job])
+    job_supervisor.Update()
+    
+    print "Downloading results..."
+    # Prepare a list for storing the directories for the downloaded job results (if there would be more jobs)
+    downloadeddirectories = arc.StringList()
+    # Start retrieving results of all the selected jobs
+    #   into the "/tmp" directory (first argument)
+    #   using the jobid and not the jobname as the name of the subdirectory (second argument, usejobname = False)
+    #   do not overwrite existing directories with the same name (third argument: force = False)
+    #   collect the downloaded directories into the variable "downloadeddirectories" (forth argument)
+    success = job_supervisor.Retrieve("/tmp", False, False, downloadeddirectories)
+    if not success:
+        print "Downloading results failed."
+    for downloadeddirectory in downloadeddirectories:
+        print "Job results were downloaded to", downloadeddirectory
+        print "Contents of the directory:"
+        for filename in os.listdir(downloadeddirectory):
+            print "  ", filename
+        
+# run the example and catch all Exceptions in order to make sure we can call _exit() at the end
+try:
+    example()
+except:
+    import traceback
+    traceback.print_exc()
+    os._exit(1)
+
+# _exit() is needed to avoid segmentation faults by still running background threads
+os._exit(0)
