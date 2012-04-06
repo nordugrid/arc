6139d7a548eff11d0b71b9e83b97fa8490706b27
Martin Skou Andersen <skou@nbi.dk>
2012-04-06 08:52:26 +0000

    Moving JobListRetrieverPluginLDAPNG to ldap module and directory.

--- src/hed/acc/JLR/DescriptorsJLR.cpp
+++ src/hed/acc/JLR/DescriptorsJLR.cpp
@@ -7,12 +7,10 @@
 #include <arc/client/EndpointQueryingStatus.h>
 #include <arc/loader/Plugin.h>
 
-#include "JobListRetrieverPluginLDAPNG.h"
 #include "JobListRetrieverPluginWSRFBES.h"
 #include "JobListRetrieverPluginWSRFGLUE2.h"
 
 Arc::PluginDescriptor PLUGINS_TABLE_NAME[] = {
-  { "LDAPNG", "HED:JobListRetrieverPlugin", "", 0, &Arc::JobListRetrieverPluginLDAPNG::Instance },
   { "WSRFBES", "HED:JobListRetrieverPlugin", "", 0, &Arc::JobListRetrieverPluginWSRFBES::Instance },
   { "WSRFGLUE2", "HED:JobListRetrieverPlugin", "", 0, &Arc::JobListRetrieverPluginWSRFGLUE2::Instance },
   { NULL, NULL, NULL, 0, NULL }
--- src/hed/acc/ldap/Descriptors.cpp
+++ src/hed/acc/ldap/Descriptors.cpp
@@ -7,12 +7,14 @@
 #include <arc/client/EndpointQueryingStatus.h>
 #include <arc/loader/Plugin.h>
 
+#include "JobListRetrieverPluginLDAPNG.h"
 #include "TargetInformationRetrieverPluginLDAPGLUE1.h"
 #include "TargetInformationRetrieverPluginLDAPGLUE2.h"
 #include "TargetInformationRetrieverPluginLDAPNG.h"
 #include "ServiceEndpointRetrieverPluginEGIIS.h"
 
 Arc::PluginDescriptor PLUGINS_TABLE_NAME[] = {
+  { "LDAPNG", "HED:JobListRetrieverPlugin", "", 0, &Arc::JobListRetrieverPluginLDAPNG::Instance },
   { "LDAPGLUE1", "HED:TargetInformationRetrieverPlugin", "", 0, &Arc::TargetInformationRetrieverPluginLDAPGLUE1::Instance },
   { "LDAPGLUE2", "HED:TargetInformationRetrieverPlugin", "", 0, &Arc::TargetInformationRetrieverPluginLDAPGLUE2::Instance },
   { "LDAPNG", "HED:TargetInformationRetrieverPlugin", "", 0, &Arc::TargetInformationRetrieverPluginLDAPNG::Instance },
