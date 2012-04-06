b8c0e60f1fcb5f71ed83a7f9809d9dd48d42732f
Martin Skou Andersen <skou@nbi.dk>
2012-04-06 08:57:26 +0000

    Moving JobListRetrieverWSRFGLUE2 to ARC1 directory and module.

--- src/hed/acc/ARC1/DescriptorsARC1.cpp
+++ src/hed/acc/ARC1/DescriptorsARC1.cpp
@@ -4,6 +4,7 @@
 #include <config.h>
 #endif
 
+#include "JobListRetrieverPluginWSRFGLUE2.h"
 #include "JobControllerARC1.h"
 #include "SubmitterARC1.h"
 #include "JobControllerBES.h"
@@ -12,6 +13,7 @@
 #include "TargetInformationRetrieverPluginWSRFGLUE2.h"
 
 Arc::PluginDescriptor PLUGINS_TABLE_NAME[] = {
+  { "WSRFGLUE2", "HED:JobListRetrieverPlugin", "", 0, &Arc::JobListRetrieverPluginWSRFGLUE2::Instance },
   { "ARC1", "HED:Submitter", "A-REX (ARC REsource coupled EXecution service)", 0, &Arc::SubmitterARC1::Instance },
   { "ARC1", "HED:JobController", "A-REX (ARC REsource coupled EXecution service)", 0, &Arc::JobControllerARC1::Instance },
   { "BES",  "HED:Submitter", "OGSA-BES conforming execution service", 0, &Arc::SubmitterBES::Instance },
--- src/hed/acc/JLR/DescriptorsJLR.cpp
+++ src/hed/acc/JLR/DescriptorsJLR.cpp
@@ -8,10 +8,8 @@
 #include <arc/loader/Plugin.h>
 
 #include "JobListRetrieverPluginWSRFBES.h"
-#include "JobListRetrieverPluginWSRFGLUE2.h"
 
 Arc::PluginDescriptor PLUGINS_TABLE_NAME[] = {
   { "WSRFBES", "HED:JobListRetrieverPlugin", "", 0, &Arc::JobListRetrieverPluginWSRFBES::Instance },
-  { "WSRFGLUE2", "HED:JobListRetrieverPlugin", "", 0, &Arc::JobListRetrieverPluginWSRFGLUE2::Instance },
   { NULL, NULL, NULL, 0, NULL }
 };
