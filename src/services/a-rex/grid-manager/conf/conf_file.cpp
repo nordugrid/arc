ecbd8c9bd3e4852697216db0e3a2f0ef5d160b0a
Aleksandr Konstantinov <akonstantinov@yahoo.com>
2012-04-26 13:31:40 +0000

    Because cache does not support XML configuration it must not be called in XML mode.

--- src/services/a-rex/grid-manager/conf/conf_file.cpp
+++ src/services/a-rex/grid-manager/conf/conf_file.cpp
@@ -1134,14 +1134,15 @@ bool configure_serviced_users(Arc::XMLNode cfg,JobUsers &users/*,uid_t my_uid,co
           user->SetStrictSession(strict_session);
           user->SetFixDirectories(fixdir);
           // get cache parameters for this user
-          try {
-            CacheConfig cache_config(users.Env(),user->UnixName());
-            user->SetCacheParams(cache_config);
-          }
-          catch (CacheConfigException& e) {
-            logger.msg(Arc::ERROR, "Error with cache configuration: %s", e.what());
-            return false;
-          }
+          // Currently cache confuguration in XML mode is not supported
+          //try {
+          //  CacheConfig cache_config(users.Env(),user->UnixName());
+          //  user->SetCacheParams(cache_config);
+          //}
+          //catch (CacheConfigException& e) {
+          //  logger.msg(Arc::ERROR, "Error with cache configuration: %s", e.what());
+          //  return false;
+          //}
           /* add helper to poll for finished jobs */
           std::string cmd_ = users.Env().nordugrid_data_loc();
           cmd_+="/scan-"+default_lrms+"-job";
