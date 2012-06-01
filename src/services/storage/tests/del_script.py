b22afc9b9e34fb76f4f87fdff570258176d31fc2
Anders Waananen <waananen@nbi.ku.dk>
2012-06-01 21:58:18 +0000

    Obsolete the components:
    - ISIS
    - Chelonia
    - Hopi
    - Janitor
    - Charon

--- src/services/storage/tests/del_script.py
+++
@@ -1,4 +0,0 @@
-#! /usr/bin/env python
-import storage.client
-a = storage.client.AHashClient('http://localhost:60002/AHash')
-print a.change({'0':['0','delete','','','',{}]})
--- src/services/storage/tests/get_script.py
+++
@@ -1,4 +0,0 @@
-#! /usr/bin/env python
-import storage.client
-a = storage.client.AHashClient('http://localhost:60002/AHash')
-print a.get(['0'])
