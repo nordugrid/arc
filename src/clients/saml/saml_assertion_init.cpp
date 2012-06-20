067121fda177a49894538f754f978fea85e89eb3
Anders Waananen <waananen@nbi.ku.dk>
2012-06-20 07:52:45 +0000

    Rename to more fitting name

--- src/clients/saml/saml_assertion_init.cpp
+++ src/clients/saml/saml_assertion_init.cpp
@@ -32,7 +32,7 @@ int main(int argc, char* argv[]){
 
     setlocale(LC_ALL, "");
 
-    Arc::Logger logger(Arc::Logger::getRootLogger(), "voms_assertion_init");
+    Arc::Logger logger(Arc::Logger::getRootLogger(), "saml_assertion_init");
     Arc::LogStream logcerr(std::cerr);
     logcerr.setFormat(Arc::ShortFormat);
     Arc::Logger::getRootLogger().addDestination(logcerr);
