e844a96cc6c7c306295acfe0f586124898c0e729
Nagy Zsombor <zsombor@niif.hu>
2012-07-06 16:08:34 +0000

    Comments for the EndpointQueryingStatus. Fixing the return value of getStatus method.

--- src/hed/libs/client/EndpointQueryingStatus.h
+++ src/hed/libs/client/EndpointQueryingStatus.h
@@ -5,34 +5,61 @@
 
 namespace Arc {
 
-///
+/// Represents the status in the EntityRetriever of the query process of an Endpoint (service registry, computing element).
 /**
- *
- **/
+  An object of this class is returned by the instances of the EntityRetriever
+  (e.g. #ServiceEndpointRetriever, #TargetInformationRetriever, #JobListRetriever)
+  representing the state of the process of querying an Endpoint.
+  It contains an #EndpointQueryingStatusType enum (#getStatus), and a description string (#getDescription) 
+*/
 class EndpointQueryingStatus {
 public:
-  enum EndpointQueryingStatusType { UNKNOWN, STARTED, FAILED, NOPLUGIN, SUCCESSFUL };
+  /** The possible states: */
+  enum EndpointQueryingStatusType {
+    UNKNOWN, /**< the state is unknown */
+    STARTED, /**< the query process was started */
+    FAILED, /**< the query process failed */
+    NOPLUGIN, /**< there is no plugin for the given Endpoint InterfaceName (so the query process was not even started) */
+    SUCCESSFUL /**< the query process was successful */
+  };
 
-  //! Conversion to string.
-  /*! Conversion from EndpointQueryingStatusType to string.
-    @param s The EndpointQueryingStatusType to convert.
-  */
-  static std::string str(EndpointQueryingStatusType status);
+  /** String representation of the states in the enum #EndpointQueryingStatusType */
+      static std::string str(EndpointQueryingStatusType status);
 
+  /** A new EndpointQueryingStatus is created with #UNKNOWN status and with an empty description by default */
   EndpointQueryingStatus(EndpointQueryingStatusType status = UNKNOWN, const std::string& description = "") : status(status), description(description) {};
 
+  /** This EndpointQueryingStatus object equals to an enum #EndpointQueryingStatusType if it contains the same state */
   bool operator==(EndpointQueryingStatusType s)    { return status == s; };
+  /** This EndpointQueryingStatus object equals to another EndpointQueryingStatus object, if their state equals.
+    The description doesn't matter.
+  */
   bool operator==(const EndpointQueryingStatus& s) { return status == s.status; };
+  /** Inequality. \see operator==(EndpointQueryingStatusType) */
   bool operator!=(EndpointQueryingStatusType s)    { return status != s; };
+  /** Inequality. \see operator==(const EndpointQueryingStatus&) */
   bool operator!=(const EndpointQueryingStatus& s) { return status != s.status; };
+  /** \return true if the status is not successful */
   bool operator!() const { return status != SUCCESSFUL; };
+  /** \return true if the status is successful */
   operator bool() const  { return status == SUCCESSFUL; };
 
+  /** Setting the EndpointQueryingStatus object's state
+    \param[in] s the new enum #EndpointQueryingStatusType status 
+  */
   EndpointQueryingStatus& operator=(EndpointQueryingStatusType s) { status = s; return *this; };
+  /** Copying the EndpointQueryingStatus object into this one.
+    \param[in] s the EndpointQueryingStatus object whose status and description will be copied into this object 
+  */
   EndpointQueryingStatus& operator=(const EndpointQueryingStatus& s) { status = s.status; description = s.description; return *this; };
 
-  EndpointQueryingStatus getStatus() const { return status; };
+  /** Return the enum #EndpointQueryingStatusType contained within this EndpointQueryingStatus object */
+  EndpointQueryingStatusType getStatus() const { return status; };
+  /** Return the description string contained within this EndpointQueryingStatus object */
   const std::string& getDescription() const { return description; };
+  /** String representation of the EndpointQueryingStatus object,
+    which is currently simply the string representation of the enum #EndpointQueryingStatusType
+  */
   std::string str() const { return str(status); };
 
 private:
