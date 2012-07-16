5e609bd62bd7f00b8d18e61b436be00fddf9661a
David Cameron <david.cameron@cern.ch>
2012-07-16 12:35:19 +0000

    define socket errors to win32 equivalents

--- src/hed/libs/common/win32.h
+++ src/hed/libs/common/win32.h
@@ -43,6 +43,43 @@ inline int mkstemp(char *pattern) {
 #define getuid() (0)
 #define getgid() (0)
 
+// Socket errors are prefixed WSA in winsock2.h
+#define EWOULDBLOCK     WSAEWOULDBLOCK     /* Operation would block */
+#define EINPROGRESS     WSAEINPROGRESS     /* Operation now in progress */
+#define EALREADY        WSAEALREADY        /* Operation already in progress */
+#define ENOTSOCK        WSAENOTSOCK        /* Socket operation on non-socket */
+#define EDESTADDRREQ    WSAEDESTADDRREQ    /* Destination address required */
+#define EMSGSIZE        WSAEMSGSIZE        /* Message too long */
+#define EPROTOTYPE      WSAEPROTOTYPE      /* Protocol wrong type for socket */
+#define ENOPROTOOPT     WSAENOPROTOOPT     /* Protocol not available */
+#define EPROTONOSUPPORT WSAEPROTONOSUPPORT /* Protocol not supported */
+#define ESOCKTNOSUPPORT WSAESOCKTNOSUPPORT /* Socket type not supported */
+#define EOPNOTSUPP      WSAEOPNOTSUPP      /* Operation not supported on transport endpoint */
+#define EPFNOSUPPORT    WSAEPFNOSUPPORT    /* Protocol family not supported */
+#define EAFNOSUPPORT    WSAEAFNOSUPPORT    /* Address family not supported by protocol */
+#define EADDRINUSE      WSAEADDRINUSE      /* Address already in use */
+#define EADDRNOTAVAIL   WSAEADDRNOTAVAIL   /* Cannot assign requested address */
+#define ENETDOWN        WSAENETDOWN        /* Network is down */
+#define ENETUNREACH     WSAENETUNREACH     /* Network is unreachable */
+#define ENETRESET       WSAENETRESET       /* Network dropped connection because of reset */
+#define ECONNABORTED    WSAECONNABORTED    /* Software caused connection abort */
+#define ECONNRESET      WSAECONNRESET      /* Connection reset by peer */
+#define ENOBUFS         WSAENOBUFS         /* No buffer space available */
+#define EISCONN         WSAEISCONN         /* Transport endpoint is already connected */
+#define ENOTCONN        WSAENOTCONN        /* Transport endpoint is not connected */
+#define ESHUTDOWN       WSAESHUTDOWN       /* Cannot send after transport endpoint shutdown */
+#define ETOOMANYREFS    WSAETOOMANYREFS    /* Too many references: cannot splice */
+#define ETIMEDOUT       WSAETIMEDOUT       /* Connection timed out */
+#define ECONNREFUSED    WSAECONNREFUSED    /* Connection refused */
+#define ELOOP           WSAELOOP           /* Too many symbolic links encountered */
+#define ENAMETOOLONG    WSAENAMETOOLONG    /* File name too long */
+#define EHOSTDOWN       WSAEHOSTDOWN       /* Host is down */
+#define EHOSTUNREACH    WSAEHOSTUNREACH    /* No route to host */
+#define EUSERS          WSAEUSERS          /* Too many users */
+#define EDQUOT          WSAEDQUOT          /* Quota exceeded */
+#define ESTALE          WSAESTALE          /* Stale NFS file handle */
+#define EREMOTE         WSAEREMOTE         /* Object is remote */
+
 inline ssize_t readlink(const char *path, char *buf, size_t bufsiz) {
     return -1;
 };
