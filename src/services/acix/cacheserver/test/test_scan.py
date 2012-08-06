9876ade7fd6d59f5f47ab675a71bc650b9db93c4
David Cameron <david.cameron@cern.ch>
2012-08-06 15:08:04 +0000

    fixing unit test to run on clean system with no cache present

--- src/services/acix/cacheserver/test/test_scan.py
+++ src/services/acix/cacheserver/test/test_scan.py
@@ -1,4 +1,6 @@
 import os
+import shutil
+import tempfile
 
 from twisted.trial import unittest
 from twisted.internet import defer
@@ -9,24 +11,46 @@ from acix.cacheserver import pscan
 
 class ScanTestCase(unittest.TestCase):
 
-    CACHE_DIR1 = '/tmp/acix-test-cache/cache'
-    CACHE_DIR2 = '/tmp/acix-test-cache/cache2'
-
     def setUp(self):
-        pass
+        # fill caches
+        self.tmpdir = tempfile.mkdtemp(prefix='/tmp/acix-test-cache') 
+        os.mkdir(self.tmpdir+'/cache')
+        os.mkdir(self.tmpdir+'/cache/data')
+        os.mkdir(self.tmpdir+'/cache/data/6b')
+        f = open(self.tmpdir+'/cache/data/6b/27f066ef9e22d2e3e40c668cae72e9e163fafd.meta', 'w')
+        f.write('http://localhost/file1')
+        f.close()
+        os.mkdir(self.tmpdir+'/cache/data/a5')
+        f = open(self.tmpdir+'/cache/data/a5/7c87cedbb464eb765a9fa8b8d506686cf0d0ee.meta', 'w')
+        f.write('http://localhost/file2')
+        f.close()
+        
+        self.tmpdir2 = tempfile.mkdtemp(prefix='/tmp/acix-test-cache2') 
+        os.mkdir(self.tmpdir2+'/cache')
+        os.mkdir(self.tmpdir2+'/cache/data')
+        os.mkdir(self.tmpdir2+'/cache/data/9f')
+        f = open(self.tmpdir2+'/cache/data/9f/4f96f6aada65ef3dafce1af2e36ba8428aeb03.meta', 'w')
+        f.write('http://localhost/file3')
+        f.close()
+        os.mkdir(self.tmpdir2+'/cache/data/dc')
+        f = open(self.tmpdir2+'/cache/data/dc/294265ad76c92fe388f4f3c452734b10064ac2.meta', 'w')
+        f.write('http://localhost/file4')
+        f.close()
 
+    def tearDown(self):
+        shutil.rmtree(self.tmpdir)
+        shutil.rmtree(self.tmpdir2)
 
     @defer.inlineCallbacks
     def testScan(self):
 
-        scanner = pscan.CacheScanner(self.CACHE_DIR1)
+        scanner = pscan.CacheScanner(self.tmpdir+'/cache')
         l = []
 
         yield scanner.scan(lambda url : l.append(url))
 
         self.failUnlessIn('6b27f066ef9e22d2e3e40c668cae72e9e163fafd', l)
         self.failUnlessIn('a57c87cedbb464eb765a9fa8b8d506686cf0d0ee', l)
-        self.failUnlessIn('eb4c0bc35aa6eefca27b84c8e40fb707c9de2218', l)
 
         self.failIfIn('abc', l)
         self.failIfIn('some_other_thing', l)
@@ -35,14 +59,19 @@ class ScanTestCase(unittest.TestCase):
     @defer.inlineCallbacks
     def testScanMultipleDirs(self):
 
-        scanner = pscan.CacheScanner([self.CACHE_DIR1, self.CACHE_DIR2])
+        os.mkdir(self.tmpdir+'/cache2')
+        os.mkdir(self.tmpdir+'/cache2/data')
+        os.mkdir(self.tmpdir+'/cache2/data/9f')
+        os.mkdir(self.tmpdir+'/cache2/data/dc')
+
+        scanner = pscan.CacheScanner([self.tmpdir+'/cache', self.tmpdir2+'/cache'])
         l = []
 
         yield scanner.scan(lambda url : l.append(url))
 
         # from the first dir
         self.failUnlessIn('a57c87cedbb464eb765a9fa8b8d506686cf0d0ee', l)
-        self.failUnlessIn('eb4c0bc35aa6eefca27b84c8e40fb707c9de2218', l)
+        self.failUnlessIn('6b27f066ef9e22d2e3e40c668cae72e9e163fafd', l)
         # from the second dir
         self.failUnlessIn('9f4f96f6aada65ef3dafce1af2e36ba8428aeb03', l)
         self.failUnlessIn('dc294265ad76c92fe388f4f3c452734b10064ac2', l)
