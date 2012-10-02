41520518fe93457709c21f5a8937c45fa20dd3ca
Nagy Zsombor <zsombor@niif.hu>
2012-10-02 12:31:32 +0000

    Remove the use of arcom.logger from the python echo service.

--- src/services/echo_python/EchoService.py
+++ src/services/echo_python/EchoService.py
@@ -1,8 +1,7 @@
 import arc
 import time
 from arcom.security import parse_ssl_config, AuthRequest
-from arcom.logger import Logger
-log = Logger(arc.Logger(arc.Logger_getRootLogger(), 'EchoService.py'))
+logger = arc.Logger(arc.Logger_getRootLogger(), 'EchoService.py'))
 
 wsrf_rp_ns = "http://docs.oasis-open.org/wsrf/rp-2"
 echo_ns = "http://www.nordugrid.org/schemas/echo"
@@ -11,22 +10,22 @@ import threading
 class EchoService:
 
     def __init__(self, cfg):
-        log.msg(arc.INFO, "EchoService (python) constructor called")
+        logger.msg(arc.INFO, "EchoService (python) constructor called")
         # get the response-prefix from the config XML
         self.prefix = str(cfg.Get('prefix'))
         # get the response-suffix from the config XML
         self.suffix = str(cfg.Get('suffix'))
-        log.msg("EchoService (python) has prefix '%s' and suffix '%s'" % (self.prefix, self.suffix))
+        logger.msg(arc.DEBUG, "EchoService (python) has prefix '%s' and suffix '%s'" % (self.prefix, self.suffix))
         self.ssl_config = parse_ssl_config(cfg)
         thread_test = str(cfg.Get('ThreadTest'))
         if thread_test:
             threading.Thread(target = self.infinite, args=[thread_test]).start()
         
     def __del__(self):
-        log.msg(arc.INFO, "EchoService (python) destructor called")
+        logger.msg(arc.INFO, "EchoService (python) destructor called")
         
     def infinite(self, url):
-        log.msg(arc.INFO, "EchoService (python) thread test starting")
+        logger.msg(arc.INFO, "EchoService (python) thread test starting")
         i = 0
         while True:
             try:
@@ -37,10 +36,11 @@ class EchoService:
                 outpayload = arc.PayloadSOAP(ns)
                 outpayload.NewChild('echo:echo').NewChild('echo:say').Set('hi!')
                 resp, status = s.process(outpayload)
-                log.msg(arc.INFO, "EchoService (python) thread test, iteration", i, status)
+                logger.msg(arc.INFO, "EchoService (python) thread test, iteration %s %s" % (i, status))
                 time.sleep(3)
             except Exception, e:
-                log.msg()                
+                import traceback
+                logger.msg(arc.DEBUG, traceback.format_exc())
 
     def RegistrationCollector(self, doc):
         regentry = arc.XMLNode('<RegEntry />')
@@ -62,24 +62,24 @@ class EchoService:
         return info
 
     def process(self, inmsg, outmsg):
-        log.msg("EchoService (python) 'Process' called")
+        logger.msg(arc.DEBUG, "EchoService (python) 'Process' called")
         # time.sleep(10)
         # get the payload from the message
         inpayload = inmsg.Payload()
-        log.msg(arc.VERBOSE, 'AuthRequest(inmsg) = ', AuthRequest(inmsg))
-        log.msg(arc.VERBOSE, 'inmsg.Attributes().getAll() = ', inmsg.Attributes().getAll())
-        log.msg(arc.INFO, "EchoService (python) got:", inpayload.GetXML())
+        logger.msg(arc.VERBOSE, 'AuthRequest(inmsg) = %s' % AuthRequest(inmsg))
+        logger.msg(arc.VERBOSE, 'inmsg.Attributes().getAll() = %s ' % inmsg.Attributes().getAll())
+        logger.msg(arc.INFO, "EchoService (python) got: %s " % inpayload.GetXML())
         # the first child of the payload should be the name of the request
         request_node = inpayload.Child()
         # get the namespace
         request_namespace = request_node.Namespace()
-        log.msg("EchoService (python) request_namespace:",  request_namespace)
+        logger.msg(arc.DEBUG, "EchoService (python) request_namespace: %s" % request_namespace)
         if request_namespace != echo_ns:
             if request_namespace == wsrf_rp_ns:
                 outpayload = arc.PayloadSOAP(arc.NS({'wsrf-rp':wsrf_rp_ns}))
                 outpayload.NewChild('wsrf-rp:GetResourcePropertyDocumentResponse').NewChild(self.GetLocalInformation())
                 outmsg.Payload(outpayload)
-                log.msg("outpayload", outpayload.GetXML())
+                logger.msg(arc.DEBUG, "outpayload %s" % outpayload.GetXML())
                 return arc.MCC_Status(arc.STATUS_OK)
             raise Exception, 'wrong namespace. expected: %s' % echo_ns
         # get the name of the request without the namespace prefix
@@ -108,16 +108,16 @@ class EchoService:
                 ssl = True
             if ssl:
                 url = arc.URL('https://localhost:60000/Echo')
-                log.msg('Calling https://localhost:60000/Echo using ClientSOAP')
+                logger.msg(arc.DEBUG, 'Calling https://localhost:60000/Echo using ClientSOAP')
             else:
                 url = arc.URL('http://localhost:60000/Echo')
-                log.msg('Calling http://localhost:60000/Echo using ClientSOAP')
+                logger.msg(arc.DEBUG, 'Calling http://localhost:60000/Echo using ClientSOAP')
             # creating the ClientSOAP object
             s = arc.ClientSOAP(cfg, url)
             new_payload = arc.PayloadSOAP(ns)
             # creating the message
             new_payload.NewChild('echo:echo').NewChild('echo:say').Set(hear)
-            log.msg('new_payload', new_payload.GetXML())
+            logger.msg(arc.DEBUG, 'new_payload %s' % new_payload.GetXML())
             # sending the message
             resp, status = s.process(new_payload)
             # get the response
@@ -126,29 +126,29 @@ class EchoService:
             # if the name of the request is 'httplib'
             # we create a new echo message which we send to http://localhost:60000/echo using python's built-in http client
             import httplib
-            log.msg('Calling http://localhost:60000/Echo using httplib')
+            logger.msg(arc.DEBUG, 'Calling http://localhost:60000/Echo using httplib')
             # create the connection
             h = httplib.HTTPConnection('localhost', 60000)
             new_payload = arc.PayloadSOAP(ns)
             # create the message
             new_payload.NewChild('echo:echo').NewChild('echo:say').Set(hear)
-            log.msg('new_payload', new_payload.GetXML())
+            logger.msg(arc.DEBUG, 'new_payload %s' % new_payload.GetXML())
             # send the message
             h.request('POST', '/Echo', new_payload.GetXML())
             r = h.getresponse()
             response = r.read()
-            log.msg(response)
+            logger.msg(arc.DEBUG, response)
             resp = arc.XMLNode(response)
             # get the response
             hear = str(resp.Child().Get('echoResponse').Get('hear'))
         elif request_name == 'wait':
-            log.msg('Start waiting 10 sec...')
+            logger.msg(arc.DEBUG, 'Start waiting 10 sec...')
             time.sleep(10)
-            log.msg('Waiting ends.')
+            logger.msg(arc.DEBUG, 'Waiting ends.')
         # we create a node at '/echo:echoResponse/echo:hear' and put the string in it
         outpayload.NewChild('echo:echoResponse').NewChild('echo:hear').Set(hear)
         outmsg.Payload(outpayload)
-        log.msg("outpayload", outpayload.GetXML())
+        logger.msg(arc.DEBUG, "outpayload %s" % outpayload.GetXML())
         # return with STATUS_OK
         return arc.MCC_Status(arc.STATUS_OK)
 
