a296ce2914321c049f679e467861e1083037cc1d
David Cameron <david.cameron@cern.ch>
2012-10-15 09:57:54 +0000

    new class GMConfig combining GM configuration taken from JobUser, JobsListConfig and GMEnvironment classes. Not used yet and does not compile

---
+++ src/services/a-rex/grid-manager/conf/GMConfig.cpp
@@ -0,0 +1,353 @@
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <fcntl.h>
+#include <grp.h>
+#include <pwd.h>
+#include <sys/stat.h>
+
+#include <arc/ArcLocation.h>
+#include <arc/FileUtils.h>
+#include <arc/XMLNode.h>
+
+#include "../run/run_parallel.h"
+#include "GMConfig.h"
+
+// Defaults
+// default job ttl after finished - 1 week
+#define DEFAULT_KEEP_FINISHED (7*24*60*60)
+// default job ttr after deleted - 1 month
+#define DEFAULT_KEEP_DELETED (30*24*60*60)
+// default maximum number of jobs in download/upload
+#define DEFAULT_MAX_LOAD (10)
+// default maximal allowed amount of reruns
+#define DEFAULT_JOB_RERUNS (5)
+// default maximum down/upload retries
+#define DEFAULT_MAX_RETRIES (10)
+// default wake up period for main job loop
+#define DEFAULT_WAKE_UP (120)
+
+
+Arc::Logger GMConfig::logger(Arc::Logger::getRootLogger(), "GMConfig");
+static std::string empty_string("");
+
+GMConfig::GMConfig(const std::string& conffile):
+  valid(false) {
+  SetDefaults();
+}
+
+GMConfig::GMConfig(const Arc::XMLNode& node):
+  valid(false) {
+  SetDefaults();
+}
+
+void GMConfig::SetDefaults() {
+  conffile_is_temp = false;
+  job_log = NULL;
+  cont_plugins = NULL;
+  cred_plugin = NULL;
+  delegations = NULL;
+  share_uid = 0;
+  keep_finished = DEFAULT_KEEP_FINISHED;
+  keep_deleted = DEFAULT_KEEP_DELETED;
+  strict_session = false;
+  fixdir = fixdir_always;
+  reruns = DEFAULT_JOB_RERUNS;
+  wakeup_period = DEFAULT_WAKE_UP;
+  jobs = NULL;
+
+  use_secure_transfer = false;
+  use_passive_transfer = false;
+  use_local_transfer = false;
+  use_new_data_staging = false;
+
+  max_jobs_running = -1;
+  max_jobs_total = -1;
+  max_jobs = -1;
+  max_jobs_per_dn = -1;
+
+  min_speed = 0;
+  min_speed_time = 300;
+  min_average_speed = 0;
+  max_inactivity_time = 300;
+
+  max_jobs_staging = DEFAULT_MAX_LOAD;
+  max_jobs_staging_emergency = 1;
+  max_downloads = -1;
+  max_staging_share = 0;
+  max_retries = DEFAULT_MAX_RETRIES;
+
+  enable_arc_interface = false;
+  enable_emies_interface = false;
+}
+
+bool GMConfig::Load(const Arc::User& user) {
+  // Call CoreConfig (conf_file.h) to fill values in this object
+  bool result = false;
+  config_file_type type = config_detect(conffile);
+  if (xml_cfg || type == config_file_XML) {
+    result = CoreConfig::parseConf(&this, xml_cfg, user);
+  }
+  else if (type == config_file_INI && !conffile_data.empty()) {
+    result = CoreConfig::parseConf(&this, conffile_data, user);
+  }
+  else {
+    logger.msg(Arc::ERROR, "Could not determine configuration type or configuration is empty");
+    return false;
+  }
+  return false;
+}
+
+
+void GMConfig::SetControlDir(const std::string &dir) {
+  if (dir.empty()) control_dir = gm_user.Home() + "/.jobstatus";
+  else control_dir = dir;
+}
+
+void GMConfig::SetSessionRoot(const std::string &dir) {
+  session_roots.clear();
+  if (dir.empty() || dir == "*") session_roots.push_back(gm_user.Home() + "/.jobs");
+  else session_roots.push_back(dir);
+}
+
+void GMConfig::SetSessionRoot(const std::vector<std::string> &dirs) {
+  session_roots.clear();
+  if (dirs.empty()) {
+    std::string dir;
+    SetSessionRoot(dir);
+  } else {
+    for (std::vector<std::string>::const_iterator i = dirs.begin(); i != dirs.end(); i++) {
+      if (*i == "*") session_roots.push_back(gm_user.Home() + "/.jobs");
+      else session_roots.push_back(*i);
+    }
+  }
+}
+
+const std::string & GMConfig::SessionRoot(const std::string& job_id) const {
+  if (session_roots.empty()) return empty_string;
+  if (session_roots.size() == 1 || job_id.empty()) return session_roots[0];
+  // search for this jobid's session dir
+  struct stat st;
+  for (std::vector<std::string>::const_iterator i = session_roots.begin(); i != session_roots.end(); i++) {
+    std::string sessiondir(*i + '/' + job_id);
+    if (stat(sessiondir.c_str(), &st) == 0 && S_ISDIR(st.st_mode))
+      return *i;
+  }
+  return empty_string; // not found
+}
+
+
+
+static bool fix_directory(const std::string& path, GMConfig::fixdir_t fixmode, mode_t mode, uid_t uid, gid_t gid) {
+  if (fixmode == GMConfig::fixdir_never) {
+    struct stat st;
+    if (!Arc::FileStat(path, &st, true)) return false;
+    if (!S_ISDIR(st.st_mode)) return false;
+    return true;
+  } else if(fixmode == GMConfig::fixdir_missing) {
+    struct stat st;
+    if (Arc::FileStat(path, &st, true)) {
+      if (!S_ISDIR(st.st_mode)) return false;
+      return true;
+    }
+  }
+  // GMConfig::fixdir_always
+  if (!Arc::DirCreate(path, mode, true)) return false;
+  // Only can switch owner if running as root
+  if (::getuid() == 0) ::chown(path.c_str(), uid, gid);
+  ::chmod(path.c_str(), mode);
+  return true;
+}
+
+bool GMConfig::CreateDirectories() {
+  bool res = true;
+  if (!control_dir.empty()) {
+    mode_t mode = 0;
+    if (gm_user.get_uid() == 0) {
+      // This control dir serves multiple users and running
+      // as root (hence really can serve multiple users)
+      mode = S_IRWXU | S_IRGRP | S_IXGRP | S_IROTH | S_IXOTH;
+    } else {
+      mode = S_IRWXU;
+    }
+    if (!fix_directory(control_dir, fixdir, mode, gm_user.get_uid(), gm_user.get_gid())) res = false;
+    // Structure inside control dir is important - *always* create it
+    // Directories containing logs and job states may need access from
+    // information system, etc. So allowing them to be more open.
+    // Delegation is only accessed by service itself.
+    if (!fix_directory(control_dir+"/logs", fixdir_always, mode, gm_user.get_uid(), gm_user.get_gid())) res = false;
+    if (!fix_directory(control_dir+"/accepting", fixdir_always, mode, gm_user.get_uid(), gm_user.get_gid())) res = false;
+    if (!fix_directory(control_dir+"/restarting", fixdir_always, mode, gm_user.get_uid(), gm_user.get_gid())) res = false;
+    if (!fix_directory(control_dir+"/processing", fixdir_always, mode, gm_user.get_uid(), gm_user.get_gid())) res = false;
+    if (!fix_directory(control_dir+"/finished", fixdir_always, mode, gm_user.get_uid(), gm_user.get_gid())) res = false;
+    std::string deleg_dir = DelegationDir();
+    if (!fix_directory(deleg_dir, fixdir_always, S_IRWXU, gm_user.get_uid(), gm_user.get_gid())) res = false;
+  }
+  for (std::vector<std::string>::iterator i = session_roots.begin(); i != session_roots.end(); ++i) {
+    mode_t mode = 0;
+    if (gm_user.get_uid() == 0) {
+      if (strict_session) {
+        // For multiple users creating immediate subdirs using own account
+        // dangerous permissions, but there is no other option
+        mode = S_IRWXU | S_IRWXG | S_IRWXO | S_ISVTX;
+      } else {
+        // For multiple users not creating immediate subdirs using own account
+        mode = S_IRWXU | S_IRGRP | S_IXGRP | S_IROTH | S_IXOTH;
+      }
+    } else {
+      // For single user
+      mode = S_IRWXU;
+    }
+    if (!fix_directory(*i, fixdir, mode, gm_user.get_uid(), gm_user.get_gid())) res = false;
+  }
+  return res;
+}
+
+std::string GMConfig::DelegationDir() const {
+  std::string deleg_dir = control_dir+"/delegations";
+  uid_t u = gm_user.get_uid();
+  if (u == 0) return deleg_dir;
+  struct passwd pwbuf;
+  char buf[4096];
+  struct passwd* pw;
+  if (::getpwuid_r(u, &pwbuf, buf, sizeof(buf), &pw) == 0) {
+    if (pw && pw->pw_name) {
+      deleg_dir+=".";
+      deleg_dir+=pw->pw_name;
+    }
+  }
+  return deleg_dir;
+}
+
+bool GMConfig::Substitute(std::string& param, const Arc::User& user) const {
+  std::string::size_type curpos = 0;
+  for (;;) {
+    if (curpos >= param.length()) break;
+    std::string::size_type pos = param.find('%', curpos);
+    if (pos == std::string::npos) break;
+    pos++; if (pos >= param.length()) break;
+    if (param[pos] == '%') { curpos=pos+1; continue; };
+    std::string to_put;
+    switch (param[pos]) {
+      case 'R': to_put = SessionRoot(""); break; // First session dir will be used if there are multiple
+      case 'C': to_put = ControlDir(); break;
+      case 'U': to_put = UnixName(); break;
+      case 'H': to_put = user.Home(); break;
+      case 'Q': to_put = DefaultQueue(); break;
+      case 'L': to_put = DefaultLRMS(); break;
+      case 'u': to_put = Arc::tostring(user.get_uid()); break;
+      case 'g': to_put = Arc::tostring(user.get_gid()); break;
+      case 'W': to_put = Arc::ArcLocation::Get(); break;
+      case 'F': to_put = conffile; break;
+      case 'G':
+        logger.msg(Arc::ERROR, "Globus location variable substitution is not supported anymore. Please specify path directly.");
+        break;
+      default: to_put = param.substr(pos-1, 2); break;
+    }
+    curpos = pos+1+(to_put.length() - 2);
+    param.replace(pos-1, 2, to_put);
+  }
+  return true;
+}
+
+void GMConfig::SetShareID(uid_t suid) {
+  share_uid = suid;
+  share_gids.clear();
+  if (suid <= 0) return;
+  struct passwd pwd_buf;
+  struct passwd* pwd = NULL;
+#ifdef _SC_GETPW_R_SIZE_MAX
+  int buflen = sysconf(_SC_GETPW_R_SIZE_MAX);
+  if (buflen <= 0) buflen = 16384;
+#else
+  int buflen = 16384;
+#endif
+  char* buf = (char*)malloc(buflen);
+  if (!buf) return;
+  if (getpwuid_r(suid, &pwd_buf, buf, buflen, &pwd) == 0) {
+    if (pwd) {
+#ifdef HAVE_GETGROUPLIST
+#ifdef _MACOSX
+      int groups[100];
+#else
+      gid_t groups[100];
+#endif
+      int ngroups = 100;
+      if (getgrouplist(pwd->pw_name, pwd->pw_gid, groups, &ngroups) >= 0) {
+        for (int n = 0; n<ngroups; ++n) {
+          share_gids.push_back((gid_t)(groups[n]));
+        }
+      }
+#endif
+      share_gids.push_back(pwd->pw_gid);
+    }
+  }
+  free(buf);
+}
+
+bool GMConfig::MatchShareGid(gid_t sgid) const {
+  for (std::list<gid_t>::const_iterator i = share_gids.begin(); i != share_gids.end(); ++i) {
+    if (sgid == *i) return true;
+  }
+  return false;
+}
+
+bool GMConfig::RunHelpers() {
+  bool started = true;
+  for (std::list<ExternalHelper>::iterator i = helpers.begin(); i != helpers.end(); ++i) {
+    started &= i->run(*this);
+  }
+  return started;
+}
+
+void GMConfig::PrepareToDestroy() {
+  // TODO: send signals to helpers and stop started threads
+}
+
+GMConfig::ExternalHelper::ExternalHelper(const std::string &cmd) {
+  command = cmd;
+  proc = NULL;
+}
+
+GMConfig::ExternalHelper::~ExternalHelper() {
+  if(proc != NULL) {
+    delete proc;
+    proc=NULL;
+  }
+}
+
+bool GMConfig::ExternalHelper::run(const GMConfig& config) {
+  if (proc != NULL) {
+    if (proc->Running()) {
+      return true; // it is already/still running
+    }
+    delete proc;
+    proc = NULL;
+  }
+  // start/restart
+  if (command.length() == 0) return true;  // has anything to run ?
+  char* args[100]; // up to 98 arguments should be enough
+  std::string args_s = command;
+  std::string arg_s;
+  int n;
+  for (n=0; n<99; n++) {
+    arg_s = config_next_arg(args_s);
+    if (arg_s.length() == 0) break;
+    args[n] = strdup(arg_s.c_str());
+  }
+  args[n] = NULL;
+  logger.msg(Arc::VERBOSE, "Starting helper process: %s", command);
+  std::string helper_id = "helper.";
+  bool started = RunParallel::run(config, helper_id.c_str(), args, &proc);
+  for (n=0; n<99; n++) {
+    if (args[n] == NULL) break;
+    free(args[n]);
+  }
+  if (started) return true;
+  if (proc && (*proc)) return true;
+  if (proc) { delete proc; proc = NULL; };
+  logger.msg(Arc::ERROR, "Helper process start failed: %s", command);
+  // start failed, doing nothing - maybe in the future
+  return false;
+}
---
+++ src/services/a-rex/grid-manager/conf/GMConfig.h
@@ -0,0 +1,333 @@
+#ifndef GMCONFIG_H_
+#define GMCONFIG_H_
+
+#include <string>
+
+#include <arc/Run.h>
+#include <arc/User.h>
+#include <arc/XMLNode.h>
+
+#include "conf_cache.h"
+
+// Forward declarations for classes for which this is just a container
+class JobLog;
+class ContinuationPlugins;
+class RunPlugin;
+class JobsList;
+namespace ARex {
+  class DelegationStores;
+}
+
+/// Configuration information related to the grid manager part of A-REX.
+/**
+ * This class contains all configuration variables related to grid-manager. It
+ * also acts as a container for objects which are used in different parts of
+ * A-REX. Therefore since this class contains pointers to complex objects, it
+ * cannot be copied and hence the copy constructor and assignment operator are
+ * private. Those pointers should be managed outside this class. GMConfig
+ * should be instantiated once when the grid-manager is initialised and only
+ * destroyed with the GM has finished. Ideally this would be a singleton but
+ * that would prevent running multiple A-REXes in the same container.
+ */
+class GMConfig {
+
+public:
+
+  /// Different options for fixing directories
+  enum fixdir_t {
+   fixdir_always,
+   fixdir_missing,
+   fixdir_never
+  };
+
+  /// Checks for configuration file but does not parse it.
+  /**
+   * This object will be valid if the configuration file supplied or guessed
+   * exists. Guessing uses the default location /etc/arc.conf, $ARC_CONFIG or
+   * $ARC_LOCATION/etc/arc.conf. The configuration data will be read in as a
+   * string, but not parsed. Load() should be used to parse the configuration
+   * and fill member variables.
+   * @param conffile Path to configuration file, will be guessed if empty
+   */
+  GMConfig(const std::string& conffile="");
+  /// Use the given XMLNode as a source of information instead of a file.
+  GMConfig(const Arc::XMLNode& node);
+
+  /// Load configuration from file or XML node into members of this object.
+  /**
+   * An optional user can be given which allows substitutions of certain
+   * parameter values based on the user. For example session directories may be
+   * allocated based on a dynamically mapped user. This object is not valid if
+   * fatal errors are found during parsing.
+   * @param user Optional user which allows substitutions
+   */
+  bool Load(const Arc::User& username=Arc::User());
+  /// Print a summary of configuration to stderr
+  void Print();
+
+  /// Returns true if configuration is found and/or valid
+  operator bool() const { return valid; }
+  /// Returns true if configuration is not found and/or valid
+  bool operator!() const { return !valid; }
+
+  /// Get path to configuration file
+  const std::string& ConfigFile() const { return conffile; }
+  /// Set path to configuration file
+  void ConfigFile(const std::string& file) { conffile = file; }
+  /// Returns true if configuration file is temporary
+  bool ConfigIsTemp() const { return conffile_is_temp; }
+  /// Sets whether configuration file is temporary
+  void ConfigIsTemp(bool temp) { conffile_is_temp = temp; }
+
+  /// Create control structure and session directories with correct permissions
+  bool CreateDirectories();
+
+  /// Start/restart all helper processes
+  bool RunHelpers();
+
+  /// Substitute characters in param specified by % with real values. An
+  /// optional User can be specified for the user-related substitutions.
+  bool Substitute(std::string& param, const Arc::User& user=Arc::User()) const;
+
+  /// Send signals to helpers to shut them down cleanly (not implemented yet)
+  void PrepareToDestroy();
+
+  /// Set control directory
+  void SetControlDir(const std::string &dir);
+  /// Set session root dir
+  void SetSessionRoot(const std::string &dir);
+  /// Set multiple session root dirs
+  void SetSessionRoot(const std::vector<std::string> &dirs);
+  /// Set cache configuration
+  void SetCacheParams(CacheConfig& params);
+  /// Set uid used by other process sharing information with A-REX
+  void SetShareID(uid_t suid);
+
+  /// Certificates directory location
+  const std::string& CertDir() const { return cert_dir; }
+  /// VOMS lsc files root directory location
+  const std::string& VomsDir() const { return voms_dir; }
+  /// Location of RTE setup scripts
+  const std::string& RTEDir() const { return rte_dir; }
+  /// Directory storing delegations
+  std::string DelegationDir() const;
+
+  /// email address of person responsible for this ARC installation
+  const std::string& SupportMailAddress() const { return support_email_address; }
+
+  /// JobLog object
+  JobLog* GetJobLog() const { return job_log; }
+  /// Plugins run at state transitions
+  ContinuationPlugins* ContPlugins() const { return cont_plugins; }
+  /// Plugin used to acquire local credentials
+  RunPlugin* CredPlugin() const { return cred_plugin; }
+  /// DelegationsStores object
+  ARex::DelegationStores* Delegations() const { return delegations; }
+
+  /// Control directory
+  const std::string & ControlDir() const { return control_dir; }
+  /// Session root directory corresponding to given job ID. If the session
+  /// dir corresponding to job_id is not found an empty string is returned.
+  const std::string & SessionRoot(const std::string& job_id) const;
+  /// Session directories
+  const std::vector<std::string> & SessionRoots() const { return session_roots; }
+  /// Base scratch directory for job execution on node
+  const std::string& ScratchDir() const { return scratch_dir; }
+  /// Whether access to session dir must be performed under mapped uid
+  bool StrictSession() const { return strict_session; }
+
+  /// Cache configuration
+  CacheConfig CacheParams() const { return cache_params; }
+
+  /// URL of cluster's headnode
+  const std::string & HeadNode() const { return headnode; }
+
+  /// Default LRMS
+  const std::string & DefaultLRMS() const { return default_lrms; }
+  /// Default queue
+  const std::string & DefaultQueue() const { return default_queue; }
+
+  /// Username of user running A-REX
+  const std::string & UnixName() const { return gm_user.Name(); }
+
+  /// Length of time to keep session dir after job finishes
+  time_t KeepFinished() const { return keep_finished; }
+  /// Length of time to keep control information after job finishes
+  time_t KeepDeleted() const { return keep_deleted; }
+
+  /// Maximum number of job re-runs allowed
+  int Reruns() const { return reruns; }
+
+  /// Strategy for fixing directories
+  fixdir_t FixDirectories() const { return fixdir; }
+
+  /// Maxmimum time for A-REX to wait between job processing loops
+  unsigned int WakeupPeriod() const { return wakeup_period; }
+
+  /// Max jobs being processed (from PREPARING to FINISHING)
+  int MaxJobs() const { return max_jobs; };
+  /// Max jobs in the LRMS
+  int MaxRunning() const { return max_jobs_running; }
+  /// Max jobs being processed per-DN
+  int MaxPerDN() const { return max_jobs_per_dn; }
+  /// Max total jobs in the system
+  int MaxTotal() const { return max_jobs_total; }
+
+  /// Max jobs in data staging states (PREPARING and FINISHING)
+  int MaxJobsStaging() const { return max_jobs_staging; }
+  /// Max jobs started if all staging slots are taken by one state
+  int MaxStagingEmergency() const { return max_jobs_staging_emergency; }
+  /// Max files downloaded in parallel per job
+  int MaxDownloads() const { return max_downloads; }
+
+  /// Whether to use new data staging (DTR)
+  bool NewDataStaging() const { return use_new_data_staging; }
+
+  /// Returns true if the shared uid matches the given uid
+  bool MatchShareUid(uid_t suid) const { return ((share_uid==0) || (share_uid==suid)); };
+  /// Returns true if any of the shared gids matches the given gid
+  bool MatchShareGid(gid_t sgid) const;
+
+private:
+
+  /// Class to run external processes (helper)
+  class ExternalHelper {
+   private:
+    /// Command being run
+    std::string command;
+    /// Object representing running process
+    Arc::Run *proc;
+   public:
+    ExternalHelper(const std::string &cmd);
+    ~ExternalHelper();
+    /// Start process if it is not running yet
+    bool run(const GMConfig& config);
+  };
+
+  /// True if configuration was loaded successfully
+  bool valid;
+  /// Configuration file
+  std::string conffile;
+  /// Whether configuration file is temporary
+  bool conffile_is_temp;
+  /// The configuration file as a string, read in during the constructor
+  std::string conffile_data;
+  /// Configuration passed as an XMLNode
+  Arc::XMLNode xml_cfg;
+  /// For logging job information to external logging service
+  JobLog* job_log;
+  /// Plugins run at certain state changes
+  ContinuationPlugins* cont_plugins;
+  /// Plugin for acquiring local credentials
+  RunPlugin* cred_plugin;
+  /// Delegated credentials stored by A-REX
+  // TODO: this should go away after proper locking in DelegationStore is implemented
+  ARex::DelegationStores* delegations;
+
+  /// Certificates directory
+  std::string cert_dir;
+  /// VOMS LSC files directory
+  std::string voms_dir;
+  /// RTE directory
+  std::string rte_dir;
+  /// email address for support
+  std::string support_email_address;
+
+  /// Scratch directory
+  std::string scratch_dir;
+  /// Directory where files explaining jobs are stored
+  std::string control_dir;
+  /// Directories where directories used to run jobs are created
+  std::vector<std::string> session_roots;
+  /// Cache information
+  CacheConfig cache_params;
+  /// URL of the cluster's headnode
+  std::string headnode;
+  /// Default LRMS and queue to use
+  std::string default_lrms;
+  std::string default_queue;
+  /// User running A-REX
+  Arc::User gm_user;
+  /// uid and gid(s) running other ARC processes that share files with A-REX
+  uid_t share_uid;
+  std::list<gid_t> share_gids;
+  /// How long jobs are kept after job finished
+  time_t keep_finished;
+  time_t keep_deleted;
+  /// Whether session must always be accessed under mapped user's uid
+  bool strict_session;
+  /// Strategy for fixing directories
+  fixdir_t fixdir;
+  /// Maximal value of times job is allowed to be rerun
+  int reruns;
+  /// Maximum time for A-REX to wait between each loop processing jobs
+  unsigned int wakeup_period;
+  /// List of associated external processes
+  std::list<ExternalHelper> helpers;
+  /// List of jobs (filled by external functions)
+  JobsList* jobs;
+
+  /// Whether to use secure (encrypted) data transfer
+  bool use_secure_transfer;
+  /// Whether to use passive transfer
+  bool use_passive_transfer;
+  /// Whether to do staging on the worker node instead of frontend
+  bool use_local_transfer;
+  /// Whether to use the new data staging framework (DTR)
+  bool use_new_data_staging;
+  /// Pattern defining the preferred choice of replica
+  std::string preferred_pattern;
+
+  /// Maximum number of jobs running (between PREPARING and FINISHING)
+  int max_jobs_running;
+  /// Maximum total jobs in the system, including FINISHED and DELETED
+  int max_jobs_total;
+  /// Maximum jobs in the LRMS
+  int max_jobs;
+  /// Maximum jobs running per DN
+  int max_jobs_per_dn;
+
+  /// Minimum speed for transfer over min_speed_time seconds
+  unsigned long long int min_speed;
+  /// Time over which to calculate min_speed
+  time_t min_speed_time;
+  /// Minimum average speed for entire transfer
+  unsigned long long int min_average_speed;
+  /// Maximum time with no transfer activity
+  time_t max_inactivity_time;
+
+  // The next block of parameters is replaced in DTR by StagingConfig
+  /// Maximum number of jobs in data staging states (PREPARING or FINISHING)
+  int max_jobs_staging;
+  /// Maximum number of jobs in a staging state if the other staging state is
+  /// using all the available slots
+  int max_jobs_staging_emergency;
+  /// Maximum files downloaded in parallel for each job
+  int max_downloads;
+  /// Maximum jobs staging in each share
+  unsigned int max_staging_share;
+  /// Type of data staging share
+  std::string share_type;
+  /// List of shares with defined limits
+  std::map<std::string, int> limited_share;
+  /// Maximum retries for data transfer
+  int max_retries;
+
+  /// Whether WS-interface is enabled
+  bool enable_arc_interface;
+  /// Whether EMI-ES interface is enabled
+  bool enable_emies_interface;
+
+  /// Logger object
+  static Arc::Logger logger;
+
+  /// Set defaults for all configuration parameters. Called by constructors.
+  void SetDefaults();
+
+  /// Assignment operator and copy constructor are private to prevent copying.
+  GMConfig& operator=(const GMConfig& conf);
+  GMConfig(const GMConfig& conf);
+};
+
+
+#endif /* GMCONFIG_H_ */
