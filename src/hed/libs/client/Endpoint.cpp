bc53390696cc62e501125bc240a377eaa0dfc422
Nagy Zsombor <zsombor@niif.hu>
2012-10-16 12:00:27 +0000

    don't strip interface and capability names in the log messages

--- src/hed/libs/client/Endpoint.cpp
+++ src/hed/libs/client/Endpoint.cpp
@@ -61,23 +61,14 @@ namespace Arc {
   
   std::string Endpoint::str() const {
     std::string mainCapability = "<no Capability>";
-    size_t lastDotPos;
     if (!Capability.empty()) {
       mainCapability = Capability.front();
-      lastDotPos = mainCapability.rfind(".");
-      if (lastDotPos != mainCapability.npos) {
-        mainCapability = mainCapability.substr(lastDotPos+1);
-      }      
     }
-    std::string strippedInterfaceName = "<empty InterfaceName>";
+    std::string interfaceNameToPrint = "<empty InterfaceName>";
     if (!InterfaceName.empty()) {
-      strippedInterfaceName = InterfaceName;
-      lastDotPos = strippedInterfaceName.rfind(".");
-      if (lastDotPos != strippedInterfaceName.npos) {
-        strippedInterfaceName = strippedInterfaceName.substr(lastDotPos+1);
-      }
+      interfaceNameToPrint = InterfaceName;
     }
-    return URLString + " (" + mainCapability + ", " + strippedInterfaceName + ")";
+    return URLString + " (" + mainCapability + ", " + interfaceNameToPrint + ")";
   }
   
   bool Endpoint::operator<(const Endpoint& other) const {
