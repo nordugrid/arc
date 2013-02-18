134aeddf32e0717d0bf06d6582e0f30008e6a028
David Cameron <david.cameron@cern.ch>
2013-02-18 16:59:10 +0000

    Put version macros in doxygen and add a brief front page for the SDK

--- src/hed/libs/common/ArcVersion.h
+++ src/hed/libs/common/ArcVersion.h
@@ -3,17 +3,38 @@
 #ifndef __ARC_ARCVERSION_H__
 #define __ARC_ARCVERSION_H__
 
+/// \file ArcVersion.h
+/** ARC API version */
 #define ARC_VERSION "3.0.0"
+/** ARC API version number */
 #define ARC_VERSION_NUM 0x030000
+/** ARC API major version number */
 #define ARC_VERSION_MAJOR 3
+/** ARC API minor version number */
 #define ARC_VERSION_MINOR 0
+/** ARC API patch number */
 #define ARC_VERSION_PATCH 0
 
 /// Arc namespace contains all core ARC classes.
 namespace Arc {
 
+  // Front page for ARC SDK documentation
+  /**
+   * \mainpage
+   * The ARC Software Development Kit (ARC) is a set of tools that allow
+   * manipulation of jobs and data in a Grid environment. The SDK is divided
+   * into a set of <a href="modules.html">modules</a> which take care of
+   * different aspects of Grid interaction.
+   * \version The version of the SDK that this documentation refers to can be
+   * found from #ARC_VERSION.
+   */
+
   /// Determines ARC HED libraries version at runtime
-  /** \headerfile ArcVersion.h arc/ArcVersion.h */
+  /**
+   * ARC also provides pre-processor macros to determine the API version at
+   * compile time in \ref ArcVersion.h.
+   * \headerfile ArcVersion.h arc/ArcVersion.h
+   */
   class ArcVersion {
   public:
     /// Major version number
