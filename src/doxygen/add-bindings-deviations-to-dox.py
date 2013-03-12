eb03ca71d4a8f3adeeedd5720c1ea60cab5754c6
Martin Skou Andersen <skou@nbi.dk>
2013-03-12 14:02:41 +0000

    Improving bindings deviation script: Take method parameters into account; Add support for #ifndef; Add support for DataStaging, ArcCredential and AuthN namespaces.

--- src/doxygen/add-bindings-deviations-to-dox.py
+++ src/doxygen/add-bindings-deviations-to-dox.py
@@ -20,14 +20,16 @@ filename = sys.argv[1]
 # Location of generated doxygen HTML documentation
 sdkDocumentationLocation = sys.argv[2]
 
+# Use list to deal with scoping of #if and #ifdef statements.
 inIfdef = []
 
+# Use dictionary below to group %rename and %ignore statements per HTML file.
 expressionsFound = {}
 
 f = open(filename, "r")
 for line in f:
     line = line.strip()
-    regMatch = re.match('\A#if(def)?\s+(\w+)', line)
+    regMatch = re.match('\A#if(n?def)?\s+(\w+)', line)
     if regMatch:
         inIfdef.append(regMatch.group(2))
         #print " #ifdef %s" % inIfdef
@@ -41,7 +43,7 @@ for line in f:
     if regMatch:
         ignoredName = regMatch.group(1)
         #print "Expression ignored: %s" % ignoredName
-        regMatch = re.match('\A(Arc)::([^:<]+)(<([^:>]+(::[^:>]+)+)>)?::(.*)', ignoredName)
+        regMatch = re.match('\A(Arc|ArcCredential|AuthN|DataStaging)::([^:<]+)(<([^:>]+(::[^:>]+)+)>)?::(.*)', ignoredName)
         if regMatch:
             namespaceName, className, _, templateParameters, _, methodName = regMatch.groups()
             if templateParameters:
@@ -63,7 +65,7 @@ for line in f:
     if regMatch:
         #print "Expression '%s' renamed to '%s'" % (regMatch.group(2), regMatch.group(1))
         toName, renameFullName = regMatch.groups()
-        regMatch = re.match('\A(Arc)::([^:<]+)(<([^:>]+(::[^:>]+)+)>)?::(.*)', renameFullName)
+        regMatch = re.match('\A(Arc|ArcCredential|AuthN|DataStaging)::([^:<]+)(<([^:>]+(::[^:>]+)+)>)?::(.*)', renameFullName)
         if regMatch:
             namespaceName, className, _, templateParameters, _, methodName = regMatch.groups()
             if templateParameters:
@@ -71,12 +73,12 @@ for line in f:
                 print "Error: Unable to handle template signatures %s" % renameFullName
                 continue
             #print " Ignoring method '%s' in class '%s' in Arc namespace." % (methodName, className)
-            sdkFNOfIgnoredInstance = sdkDocumentationLocation + '/class' + namespaceName + '_1_1' + className + '.html'
-            if not expressionsFound.has_key(sdkFNOfIgnoredInstance):
-                expressionsFound[sdkFNOfIgnoredInstance] = []
+            sdkFNOfRenamedInstance = sdkDocumentationLocation + '/class' + namespaceName + '_1_1' + className + '.html'
+            if not expressionsFound.has_key(sdkFNOfRenamedInstance):
+                expressionsFound[sdkFNOfRenamedInstance] = []
             renameScope = ["Python"] if "SWIGPYTHON" in inIfdef else ["Python", "Java"]
             renameScope = ["Java"]   if "SWIGJAVA"   in inIfdef else renameScope
-            expressionsFound[sdkFNOfIgnoredInstance].append({"text" : "Renamed to <tt>" + toName + "</tt>", "scope" : renameScope, "name" : methodName})
+            expressionsFound[sdkFNOfRenamedInstance].append({"text" : "Renamed to <tt>" + toName + "</tt>", "scope" : renameScope, "name" : methodName})
             continue
         print "Error: Couldn't parse rename signature %s" % renameFullName
         continue
@@ -104,24 +106,39 @@ for filename, v in expressionsFound.iteritems():
         #print doxMethodName
 
         for entry in v:
-            regMatch = re.match("(operator\(\)|[^(]+)(\(([^(]+)\))?\s*(const)?", entry["name"])
+            regMatch = re.match("(operator\(\)|[^(]+)"  "(\(([^(]*)\))?"  "\s*(const)?", entry["name"])
             if regMatch:
               methodName, _, methodParameters, isConst = regMatch.groups()
               #print "Method name: '%s'; Parameters: '%s'; isConst: %s" % (methodName, methodParameters, str(bool(isConst)))
               #print "'%s\Z', %s" % (methodName.strip(), doxMethodName)
-              if re.search(methodName.strip() + '\Z', doxMethodName):
+              doxMethodName = doxMethodName.replace("&gt;", ">")
+              if doxMethodName.endswith(methodName.strip()):
                   #print "Method '%s' found in file '%s' as '%s'" % (methodName, filename, doxMethodName)
-                  v.remove(entry)
-                  
+
                   isInsideMemdocDiv = False
+                  methodParameters = methodParameters.split(",") if methodParameters else []
                   while True:
                       i += 1
-                      if isInsideMemdocDiv and re.match('</div>', doxHTMLFileLines[i]):
+                      regMatch = re.match('\s+<td class="paramtype">(.+)</td>', doxHTMLFileLines[i])
+                      if regMatch:
+                          doxParam = regMatch.group(1).replace("&#160;", "").replace(" &amp;", "\s*&").strip()
+                          doxParam = re.sub('</?a[^>]*>', '', doxParam) # Remove anchor tags  
+                          if len(methodParameters) == 0:
+                              if doxParam != "void": # Doesn't match that in HTML document
+                                doxHTMLFile.write(doxHTMLFileLines[i])
+                                break
+                          elif re.match(doxParam, methodParameters[0]):
+                              methodParameters.pop(0)
+                      elif isInsideMemdocDiv and re.match('</div>', doxHTMLFileLines[i]):
+                          if len(methodParameters) > 0: # Doesn't match that in HTML document
+                              doxHTMLFile.write(doxHTMLFileLines[i])
+                              break
                           for scope in entry["scope"]:
                             doxHTMLFile.write('<dl class="section attention"><dt>' + scope + ' interface deviation</dt><dd>' + entry["text"] + ' in ' + scope + ' interface</dd></dl>')
+                          v.remove(entry)
                           doxHTMLFile.write(doxHTMLFileLines[i])
                           break
-                      if re.search('<div class="memdoc">', doxHTMLFileLines[i]):
+                      elif re.search('<div class="memdoc">', doxHTMLFileLines[i]):
                           isInsideMemdocDiv = True
                       doxHTMLFile.write(doxHTMLFileLines[i])
                   
@@ -133,9 +150,10 @@ for filename, v in expressionsFound.iteritems():
     doxHTMLFile.close()
     
     if v:
-        print "Error: The following methods was not found in the file '%s':" % filename
+        print "Error: The following methods was not found in the HTML file '%s':" % filename
         for entry in v:
             print "       %s" % entry["name"]
+        print "??? => Is there a API description in the corresponding header file for these?"
     
     
 
