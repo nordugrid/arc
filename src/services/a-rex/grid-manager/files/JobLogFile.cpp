1f46fbfa83da7cc039bc4efa9bcba0af506e0603
David Cameron <david.cameron@cern.ch>
2013-03-22 13:17:11 +0000

    rewrite code for writing accounting record to use common arc file-handling functions

--- src/services/a-rex/grid-manager/files/JobLogFile.cpp
+++ src/services/a-rex/grid-manager/files/JobLogFile.cpp
@@ -2,13 +2,9 @@
 #include <config.h>
 #endif
 
-#include <string>
-#include <list>
-#include <fstream>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
-#include <fcntl.h>
 #include <pwd.h>
 
 #include <arc/StringConv.h>
@@ -16,18 +12,15 @@
 #include <arc/FileUtils.h>
 #include <arc/compute/JobDescription.h>
 #include "../jobs/GMJob.h"
-#include "ControlFileHandling.h"
 #include "../conf/GMConfig.h"
-#include "../conf/ConfigUtils.h"
+#include "ControlFileHandling.h"
 
 #include "JobLogFile.h"
 
 namespace ARex {
 
-const char * const sfx_local       = ".local";
 const char * const sfx_desc        = ".description";
 const char * const sfx_diag        = ".diag";
-const char * const sfx_proxy       = ".proxy";
 
 static void extract_integer(std::string& s,std::string::size_type n = 0) {
   for(;n<s.length();n++) {
@@ -65,225 +58,201 @@ static bool string_to_number(std::string& s,unsigned int& n) {
 // Create multiple files for sending to logger
 // TODO - make it SOAP XML so that they could be sent directly
 bool job_log_make_file(const GMJob &job,const GMConfig& config,const std::string &url,std::list<std::string> &report_config) {
-  std::string fname_dst = config.ControlDir()+"/logs/"+job.get_id()+".XXXXXX";
-  std::string fname_src;
-  std::string status;
-  int l;
-  time_t t;
-  char buf[256];
-  if(!Arc::TmpFileCreate(fname_dst, "")) return false;
-  fix_file_owner(fname_dst,job);
-  std::ofstream o_dst(fname_dst.c_str());
+
+  // contents of the job record
+  std::string job_data;
+
   // URL to send info to
-  if(url.length()) {
-    o_dst<<"loggerurl="<<url<<std::endl; if(o_dst.fail()) goto error;
-  };
+  if (!url.empty()) job_data += "loggerurl=" + url + '\n';
+
   // Configuration options for usage reporter tool
   for (std::list<std::string>::iterator sp = report_config.begin();
-       sp != report_config.end();
-       ++sp)
-    {
-      o_dst<<*sp<<std::endl;
-    }
+       sp != report_config.end(); ++sp) {
+    job_data += *sp + '\n';
+  }
+
   // Copy job description
-  {
-  fname_src = config.ControlDir() + "/job." + job.get_id() + sfx_desc;
-  int h_src=open(fname_src.c_str(),O_RDONLY);
-  if(h_src==-1) goto error;
-  o_dst<<"description=";
-  for(;;) {
-    l=read(h_src,buf,sizeof(buf));
-    if(l==0) break;
-    if(l==-1) goto error;
-    for(char* p=buf;p;) { p=(char*)memchr(buf,'\r',l); if(p) (*p)=' '; };
-    for(char* p=buf;p;) { p=(char*)memchr(buf,'\n',l); if(p) (*p)=' '; };
-    o_dst.write(buf,l);
-    if(o_dst.fail()) goto error;
-  };
-  o_dst<<std::endl;
+  std::string fname_src(config.ControlDir() + "/job." + job.get_id() + sfx_desc);
+  std::string desc;
+  if (!Arc::FileRead(fname_src, desc)) return false;
+  // Remove line breaks
+  std::replace( desc.begin(), desc.end(), '\r', ' ');
+  std::replace( desc.begin(), desc.end(), '\n', ' ');
+  job_data += "description=" + desc + '\n';
+
+  // Find local owner of job from owner of desc file
   struct stat st;
-  if(fstat(h_src,&st) == 0) {
+  if (Arc::FileStat(fname_src, &st, false)) {
     struct passwd pw_;
     struct passwd *pw;
     char buf[BUFSIZ];
     getpwuid_r(st.st_uid,&pw_,buf,BUFSIZ,&pw);
-    if(pw != NULL) {
-      if(pw->pw_name) o_dst<<"localuser="<<pw->pw_name<<std::endl;
-    };
-  };
-  close(h_src);
-  };
+    if (pw != NULL && pw->pw_name) {
+      job_data += "localuser=" + std::string(pw->pw_name) + '\n';
+    }
+  }
+
   // Start time and identifier
-  t = job_mark_time(fname_src);
-  o_dst<<"submissiontime="<<Arc::Time(t).str(Arc::MDSTime)<<std::endl;
-  o_dst<<"ngjobid="<<job.get_id()<<std::endl;
-  if(o_dst.fail()) goto error;
+  time_t t = job_mark_time(fname_src);
+  job_data += "submissiontime=" + Arc::Time(t).str(Arc::MDSTime) + '\n';
+  job_data += "ngjobid=" + job.get_id() + '\n';
+
   // Analyze job.ID.local and store relevant information
-  {
-  fname_src = config.ControlDir() + "/job." + job.get_id() + sfx_local;
   JobLocalDescription local;
-  if(!local.read(fname_src)) goto error;
-  if(!local.DN.empty()) { o_dst<<"usersn="<<local.DN<<std::endl; }
-  if(!local.headnode.empty()) { o_dst<<"headnode="<<local.headnode<<std::endl; }
-  if(!local.lrms.empty()) { o_dst<<"lrms="<<local.lrms<<std::endl; }
-  if(!local.queue.empty()) { o_dst<<"queue="<<local.queue<<std::endl; }
-  if(!local.localid.empty()) { o_dst<<"localid="<<local.localid<<std::endl; }
-  if(!local.jobname.empty()) { o_dst<<"jobname="<<local.jobname<<std::endl; }
-  if(!local.globalid.empty()) { o_dst<<"globalid="<<local.globalid<<std::endl; }
-  for(std::list<std::string>::const_iterator projectname = local.projectnames.begin();
+  if (!job_local_read_file(job.get_id(), config, local)) return false;
+  if (!local.DN.empty()) job_data += "usersn=" + local.DN + '\n';
+  if (!local.headnode.empty()) job_data += "headnode=" + local.headnode + '\n';
+  if (!local.lrms.empty()) job_data += "lrms=" + local.lrms + '\n';
+  if (!local.queue.empty()) job_data += "queue=" + local.queue + '\n';
+  if (!local.localid.empty()) job_data += "localid=" + local.localid + '\n';
+  if (!local.jobname.empty()) job_data += "jobname=" + local.jobname + '\n';
+  if (!local.globalid.empty()) job_data += "globalid=" + local.globalid + '\n';
+  for (std::list<std::string>::const_iterator projectname = local.projectnames.begin();
                       projectname != local.projectnames.end(); ++projectname) {
-    o_dst<<"projectname="<<*projectname<<std::endl;
-  };
-  if(!local.clientname.empty()) { o_dst<<"clienthost="<<local.clientname<<std::endl; }
-  };
+    job_data += "projectname=" + *projectname + '\n';
+  }
+  if (!local.clientname.empty()) job_data += "clienthost=" + local.clientname + '\n';
 
   // Copy public part of user certificate chain incl. proxy
-  {
+  fname_src = job_proxy_filename(job.get_id(), config);
+  std::list<std::string> proxy_data;
+  if (Arc::FileRead(fname_src, proxy_data)) {
     std::string user_cert;
-    fname_src = config.ControlDir() + "/job." + job.get_id() + sfx_proxy;
-    std::ifstream proxy_src(fname_src.c_str());
     bool in_private=false;
-    for(;;) {
-      std::string line;
-      std::getline(proxy_src,line);
-      if(proxy_src.eof()) break;
-      if(proxy_src.fail()) {
-        user_cert.clear();
-        break;
-      }
-      if(in_private) { // Skip private key
-        if (line.find("-----END") != std::string::npos &&
-            line.find("PRIVATE KEY-----") != std::string::npos) { // can be RSA, DSA etc.
+    for (std::list<std::string>::iterator line = proxy_data.begin();
+         line != proxy_data.end(); ++line) {
+      if (in_private) { // Skip private key
+        if (line->find("-----END") != std::string::npos &&
+            line->find("PRIVATE KEY-----") != std::string::npos) { // can be RSA, DSA etc.
           in_private=false;
         }
       }
       else {
-        if (line.find("-----BEGIN") != std::string::npos &&
-            line.find("PRIVATE KEY-----") != std::string::npos) { // can be RSA, DSA etc.
+        if (line->find("-----BEGIN") != std::string::npos &&
+            line->find("PRIVATE KEY-----") != std::string::npos) { // can be RSA, DSA etc.
           in_private=true;
         }
         else {
-          user_cert+=line;
-          if (!proxy_src.eof()) user_cert+='\\';
+          user_cert += *line + '\\';
         }
       }
     }
-    if(user_cert.length()) {
-      o_dst<<"usercert="<<user_cert<<std::endl; if(o_dst.fail()) goto error;
-    }
+    if (!user_cert.empty()) job_data += "usercert=" + user_cert + '\n';
   }
 
   // Extract requested resources
-  {
-    fname_src = config.ControlDir() + "/job." + job.get_id() + sfx_desc;
-    std::string job_desc_str;
-    if (!job_description_read_file(fname_src, job_desc_str)) goto error;
-    Arc::JobDescription arc_job_desc;
-    {
-      std::list<Arc::JobDescription> arc_job_desc_list;
-      if (!Arc::JobDescription::Parse(job_desc_str, arc_job_desc_list, "", "GRIDMANAGER") || arc_job_desc_list.size() != 1) goto error;
-      arc_job_desc = arc_job_desc_list.front();
-    }
-//    if(!get_arc_job_description(fname_src, arc_job_desc)) goto error;
-    if(arc_job_desc.Resources.IndividualPhysicalMemory.max>=0) o_dst<<"requestedmemory="<<arc_job_desc.Resources.IndividualPhysicalMemory.max<<std::endl;
-    if(arc_job_desc.Resources.TotalCPUTime.range.max>=0) o_dst<<"requestedcputime="<<arc_job_desc.Resources.TotalCPUTime.range.max<<std::endl;
-    if(arc_job_desc.Resources.TotalWallTime.range.max>=0) o_dst<<"requestedwalltime="<<arc_job_desc.Resources.TotalWallTime.range.max<<std::endl;
-    if(arc_job_desc.Resources.DiskSpaceRequirement.DiskSpace.max>=0) o_dst<<"requesteddisk="<<(arc_job_desc.Resources.DiskSpaceRequirement.DiskSpace.max*1024*1024)<<std::endl;
-    if(arc_job_desc.Resources.RunTimeEnvironment.getSoftwareList().size()>0) {
-      std::string rteStr;
-      for (std::list<Arc::Software>::const_iterator itSW = arc_job_desc.Resources.RunTimeEnvironment.getSoftwareList().begin();
-           itSW != arc_job_desc.Resources.RunTimeEnvironment.getSoftwareList().end(); itSW++) {
-        if (!itSW->empty() && !itSW->getVersion().empty()) {
-          if (!rteStr.empty()) rteStr += " ";
-          rteStr += *itSW;
-        }
+  Arc::JobDescription arc_job_desc;
+  std::list<Arc::JobDescription> arc_job_desc_list;
+  if (!Arc::JobDescription::Parse(desc, arc_job_desc_list, "", "GRIDMANAGER")
+      || arc_job_desc_list.size() != 1) return false;
+  arc_job_desc = arc_job_desc_list.front();
+
+  if (arc_job_desc.Resources.IndividualPhysicalMemory.max>=0) {
+    job_data += "requestedmemory=" + Arc::tostring(arc_job_desc.Resources.IndividualPhysicalMemory.max) + '\n';
+  }
+  if (arc_job_desc.Resources.TotalCPUTime.range.max>=0) {
+    job_data += "requestedcputime=" + Arc::tostring(arc_job_desc.Resources.TotalCPUTime.range.max) + '\n';
+  }
+  if (arc_job_desc.Resources.TotalWallTime.range.max>=0) {
+    job_data += "requestedwalltime=" + Arc::tostring(arc_job_desc.Resources.TotalWallTime.range.max) + '\n';
+  }
+  if (arc_job_desc.Resources.DiskSpaceRequirement.DiskSpace.max>=0) {
+    job_data += "requesteddisk=" + Arc::tostring((arc_job_desc.Resources.DiskSpaceRequirement.DiskSpace.max*1024*1024)) + '\n';
+  }
+  if (arc_job_desc.Resources.RunTimeEnvironment.getSoftwareList().size()>0) {
+    std::string rteStr;
+    for (std::list<Arc::Software>::const_iterator itSW = arc_job_desc.Resources.RunTimeEnvironment.getSoftwareList().begin();
+         itSW != arc_job_desc.Resources.RunTimeEnvironment.getSoftwareList().end(); itSW++) {
+      if (!itSW->empty() && !itSW->getVersion().empty()) {
+        if (!rteStr.empty()) rteStr += " ";
+        rteStr += *itSW;
       }
-      if (!rteStr.empty()) o_dst<<"runtimeenvironment="<<rteStr<<std::endl;
     }
-  };
+    if (!rteStr.empty()) job_data += "runtimeenvironment=" + rteStr + '\n';
+  }
+
   // Analyze diagnostics and store relevant information
-  {
   fname_src = config.ControlDir() + "/job." + job.get_id() + sfx_diag;
-  std::ifstream i_src(fname_src.c_str());
-  if(!i_src.fail()) {
+  std::list<std::string> diag_data;
+  if (Arc::FileRead(fname_src, diag_data)) {
     std::string nodenames;
     int nodecount = 0;
     float cputime = 0;
-    for(;;) {
-      // if(i_src.fail()) goto error; - it is better to provide something than nothing
-      if(o_dst.fail()) goto error;
-      // if(i_src.eof()) break; - config_read_line handles that
-      std::string value;
-      std::string key = config_read_line(i_src,value,'=');
-      if(key.empty()) break; // only case config_read_line returns "" is then can't read anymore
-      if(key=="nodename") {
-        if(nodecount) nodenames+=":"; nodenames+=value;
+    for (std::list<std::string>::iterator line = diag_data.begin();
+         line != diag_data.end(); ++line) {
+      std::string::size_type p = line->find('=');
+      if (p == std::string::npos) continue;
+      std::string key(Arc::lower(line->substr(0, p)));
+      std::string value(line->substr(p+1));
+      if (key.empty()) continue;
+      if (key == "nodename") {
+        if (nodecount) nodenames+=":"; nodenames+=value;
         nodecount++;
-      } else if(strcasecmp(key.c_str(),"walltime") == 0) {
+      } else if(key == "walltime") {
         float f;
-        if(string_to_number(value,f))
-          o_dst<<"usedwalltime="<<(unsigned int)f<<std::endl;
-      } else if(strcasecmp(key.c_str(),"kerneltime") == 0) {
+        if (string_to_number(value,f)) {
+          job_data += "usedwalltime=" + Arc::tostring((unsigned int)f) + '\n';
+        }
+      } else if(key == "kerneltime") {
         float f;
         if(string_to_number(value,f)) {
-          o_dst<<"usedkernelcputime="<<(unsigned int)f<<std::endl;
-          cputime+=f;
+          job_data += "usedkernelcputime=" + Arc::tostring((unsigned int)f) + '\n';
+          cputime += f;
         }
-      } else if(strcasecmp(key.c_str(),"usertime") == 0) {
+      } else if(key == "usertime") {
         float f;
         if(string_to_number(value,f)) {
-          o_dst<<"usedusercputime="<<(unsigned int)f<<std::endl;
-          cputime+=f;
+          job_data += "usedusercputime=" + Arc::tostring((unsigned int)f) + '\n';
+          cputime += f;
         }
-      } else if(strcasecmp(key.c_str(),"averagetotalmemory") == 0) {
+      } else if(key == "averagetotalmemory") {
         float f;
-        if(string_to_number(value,f))
-          o_dst<<"usedmemory="<<(unsigned int)f<<std::endl;
-      } else if(strcasecmp(key.c_str(),"averageresidentmemory") == 0) {
+        if(string_to_number(value,f)) {
+          job_data += "usedmemory=" + Arc::tostring((unsigned int)f) + '\n';
+        }
+      } else if(key == "averageresidentmemory") {
         float f;
-        if(string_to_number(value,f))
-          o_dst<<"usedaverageresident="<<(unsigned int)f<<std::endl;
-      } else if(strcasecmp(key.c_str(),"maxresidentmemory") == 0) {
+        if(string_to_number(value,f)) {
+          job_data += "usedaverageresident=" + Arc::tostring((unsigned int)f) + '\n';
+        }
+      } else if(key == "maxresidentmemory") {
         float f;
-        if(string_to_number(value,f))
-          o_dst<<"usedmaxresident="<<(unsigned int)f<<std::endl;
-      } else if(strcasecmp(key.c_str(),"exitcode") == 0) {
+        if(string_to_number(value,f)) {
+          job_data += "usedmaxresident=" + Arc::tostring((unsigned int)f) + '\n';
+        }
+      } else if(key == "exitcode") {
         int n;
-        if(Arc::stringto(value,n)) o_dst<<"exitcode="<<n<<std::endl;
-      };
-    };
+        if(Arc::stringto(value,n)) job_data += "exitcode=" + Arc::tostring(n) + '\n';
+      }
+    }
     if(nodecount) {
-      o_dst<<"nodename="<<nodenames<<std::endl;
-      o_dst<<"nodecount="<<nodecount<<std::endl;
-    };
-    o_dst<<"usedcputime="<<(unsigned int)cputime<<std::endl;
-  };
-  };
+      job_data += "nodename=" + nodenames + '\n';
+      job_data += "nodecount=" + Arc::tostring(nodecount) + '\n';
+    }
+    job_data += "usedcputime=" + Arc::tostring((unsigned int)cputime) + '\n';
+  }
+
   // Endtime and failure reason
-  if(job.get_state() == JOB_STATE_FINISHED) {
+  std::string status;
+  if (job.get_state() == JOB_STATE_FINISHED) {
     status="completed";
     t = job_state_time(job.get_id(),config);
-    if(t == 0) t=::time(NULL);
-    o_dst<<"endtime="<<Arc::Time(t).str(Arc::MDSTime)<<std::endl;
-    if(job_failed_mark_check(job.get_id(),config)) {
+    if (t == 0) t=::time(NULL);
+    job_data += "endtime=" + Arc::Time(t).str(Arc::MDSTime) + '\n';
+    if (job_failed_mark_check(job.get_id(),config)) {
       std::string failure = job_failed_mark_read(job.get_id(),config);
-      o_dst<<"failurestring="<<failure<<std::endl;
+      job_data += "failurestring=" + failure + '\n';
       status="failed";
-    };
-  };
-  if(status.length()) o_dst<<"status="<<status<<std::endl;
-  // Identity of cluster
-//@   try {
-//@     Certificate cert(HOSTCERT);
-//@     o_dst<<"cluster="<<cert.GetSN()<<std::endl;
-//@   } catch (std::exception e) { };
-  if(o_dst.fail()) goto error;
-  o_dst.close();
+    }
+  }
+  if(!status.empty()) job_data += "status=" + status + '\n';
+
+  // Store in file
+  std::string out_file(config.ControlDir()+"/logs/"+job.get_id()+".XXXXXX");
+  if (!Arc::TmpFileCreate(out_file, job_data)) return false;
+  fix_file_owner(out_file, job);
+
   return true;
-error:
-  o_dst.close();
-  unlink(fname_dst.c_str());
-  return false;
 }
 
 } // namespace ARex
--- src/services/a-rex/grid-manager/files/JobLogFile.h
+++ src/services/a-rex/grid-manager/files/JobLogFile.h
@@ -9,6 +9,7 @@ namespace ARex {
 class GMJob;
 class GMConfig;
 
+/// Extract job information from control files and write job summary file used by reporting tools
 bool job_log_make_file(const GMJob &job,const GMConfig& config,const std::string &url,std::list<std::string> &report_config);
 
 } // namespace ARex
