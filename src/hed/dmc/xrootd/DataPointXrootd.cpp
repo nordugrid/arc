e67c557731c4a7b04be42c8286a996b0451ab340
David Cameron <david.cameron@cern.ch>
2014-05-09 15:03:44 +0000

    implement writing/deleting operations in xrootd DMC

--- src/hed/dmc/xrootd/DataPointXrootd.cpp
+++ src/hed/dmc/xrootd/DataPointXrootd.cpp
@@ -24,6 +24,10 @@ namespace ArcDMCXrootd {
       writing(false){
     // set xrootd log level
     set_log_level();
+    // xrootd requires 2 slashes at the start of the URL path
+    if (url.Path().find("//") != 0) {
+      this->url.ChangePath(std::string("/"+url.Path()));
+    }
   }
 
   DataPointXrootd::~DataPointXrootd() {
@@ -169,14 +173,149 @@ namespace ArcDMCXrootd {
     return DataStatus::Success;
   }
 
+  void DataPointXrootd::write_file_start(void *object) {
+    ((DataPointXrootd*)object)->write_file();
+  }
 
-  DataStatus DataPointXrootd::StartWriting(DataBuffer& buffer,
-                                      DataCallback *space_cb) {
-    logger.msg(ERROR, "Writing to xrootd is not (yet) supported");
-    return DataStatus(DataStatus::WriteError, EOPNOTSUPP);
+  void DataPointXrootd::write_file() {
+    int handle;
+    unsigned int length;
+    unsigned long long int position;
+    unsigned long long int offset = 0;
+    ssize_t bytes_written = 0;
+    unsigned int chunk_offset;
+
+    for (;;) {
+      // Ask the DataBuffer for a buffer with data to write,
+      // and the length and position where to write to
+      if (!buffer->for_write(handle, length, position, true)) {
+        // no more data from the buffer, did the other side finished?
+        if (!buffer->eof_read()) {
+          // the other side hasn't finished yet, must be an error
+          buffer->error_write(true);
+        }
+        break;
+      }
+
+      // if the buffer gives different position than we are currently in the
+      // destination, then we have to seek there
+      if (position != offset) {
+        logger.msg(DEBUG, "DataPointXrootd::write_file got position %d and offset %d, has to seek", position, offset);
+        XrdPosixXrootd::Lseek(fd, position, SEEK_SET);
+        offset = position;
+      }
+
+      // we want to write the chunk we got from the buffer,
+      // but we may not be able to write it in one shot
+      chunk_offset = 0;
+      while (chunk_offset < length) {
+        bytes_written = XrdPosixXrootd::Write(fd, (*(buffer))[handle] + chunk_offset, length - chunk_offset);
+        if (bytes_written < 0) break; // there was an error
+        // calculate how far we got into to the chunk
+        chunk_offset += bytes_written;
+        // if the new chunk_offset is still less then the length of the chunk,
+        // we have to continue writing
+      }
+
+      // we finished with writing (even if there was an error)
+      buffer->is_written(handle);
+      offset += length;
+
+      // if there was an error during writing
+      if (bytes_written < 0) {
+        logger.msg(VERBOSE, "xrootd write failed: %s", StrError(errno));
+        buffer->error_write(true);
+        break;
+      }
+    }
+    buffer->eof_write(true);
+    // Close the file
+    if (fd != -1) {
+      if (XrdPosixXrootd::Close(fd) < 0) {
+        logger.msg(WARNING, "xrootd close failed: %s", StrError(errno));
+      }
+      fd = -1;
+    }
+    transfer_cond.signal();
+  }
+
+
+  DataStatus DataPointXrootd::StartWriting(DataBuffer& buf,
+                                           DataCallback *space_cb) {
+
+    if (reading) return DataStatus::IsReadingError;
+    if (writing) return DataStatus::IsWritingError;
+    writing = true;
+
+    {
+      CertEnvLocker env(usercfg);
+      // Open the file
+      fd = XrdPosixXrootd::Open(url.str().c_str(), O_WRONLY | O_CREAT, 0600);
+    }
+    if (fd < 0) {
+      // If no entry try to create parent directories
+      if (errno == ENOENT) {
+        logger.msg(VERBOSE, "Failed to open %s, trying to create parent directories", url.str());
+        std::string original_path(url.Path());
+        url.ChangePath(Glib::path_get_dirname(url.Path()));
+        DataStatus r = CreateDirectory(true);
+        url.ChangePath(original_path);
+        if (!r) return r;
+
+        { // Try to open again
+          CertEnvLocker env(usercfg);
+          fd = XrdPosixXrootd::Open(url.str().c_str(), O_WRONLY | O_CREAT, 0600);
+        }
+      }
+      if (fd < 0) {
+        logger.msg(VERBOSE, "xrootd open failed: %s", StrError(errno));
+        writing = false;
+        return DataStatus(DataStatus::WriteStartError, errno);
+      }
+    }
+
+    // Remember the DataBuffer we got, the separate writing thread will use it
+    buffer = &buf;
+    transfer_cond.reset();
+    // StopWriting will wait for this condition,
+    // which will be signalled by the separate writing thread
+    // Create the separate writing thread
+    if (!CreateThreadFunction(&DataPointXrootd::write_file_start, this)) {
+      if (fd != -1 && XrdPosixXrootd::Close(fd) < 0) {
+        logger.msg(WARNING, "close failed: %s", StrError(errno));
+      }
+      writing = false;
+      return DataStatus(DataStatus::WriteStartError, "Failed to create writing thread");
+    }
+    return DataStatus::Success;
   }
 
   DataStatus DataPointXrootd::StopWriting() {
+    if (!writing) return DataStatus(DataStatus::WriteStopError, EARCLOGIC, "Not writing");
+    writing = false;
+    if (!buffer) return DataStatus(DataStatus::WriteStopError, EARCLOGIC, "Not writing");
+
+    // If the writing is not finished, trigger writing error
+    if (!buffer->eof_write()) buffer->error_write(true);
+
+    // Wait until the writing thread finishes
+    logger.msg(DEBUG, "StopWriting starts waiting for transfer_condition.");
+    transfer_cond.wait();
+    logger.msg(DEBUG, "StopWriting finished waiting for transfer_condition.");
+
+    // Close the file if not done already
+    if (fd != -1) {
+      if (XrdPosixXrootd::Close(fd) < 0) {
+        logger.msg(WARNING, "xrootd close failed: %s", StrError(errno));
+      }
+      fd = -1;
+    }
+    // If there was an error (maybe we triggered it)
+    if (buffer->error_write()) {
+      buffer = NULL;
+      return DataStatus::WriteError;
+    }
+    buffer = NULL;
     return DataStatus::Success;
   }
 
@@ -263,18 +402,88 @@ namespace ArcDMCXrootd {
   }
 
   DataStatus DataPointXrootd::Remove() {
-    logger.msg(ERROR, "Cannot (yet) remove files through xrootd");
-    return DataStatus(DataStatus::DeleteError, EOPNOTSUPP);
+    if (reading) return DataStatus(DataStatus::IsReadingError, EARCLOGIC);
+    if (writing) return DataStatus(DataStatus::IsReadingError, EARCLOGIC);
+
+    struct stat st;
+    CertEnvLocker env(usercfg);
+    if (XrdPosixXrootd::Stat(url.str().c_str(), &st) != 0) {
+      if (errno == ENOENT) return DataStatus::Success;
+      logger.msg(VERBOSE, "File is not accessible %s: %s", url.str(), StrError(errno));
+      return DataStatus(DataStatus::DeleteError, errno, "Failed to stat file "+url.str());
+    }
+    // path is a directory
+    if (S_ISDIR(st.st_mode)) {
+      if (XrdPosixXrootd::Rmdir(url.str().c_str()) != 0) {
+        logger.msg(VERBOSE, "Can't delete directory %s: %s", url.str(), StrError(errno));
+        return DataStatus(DataStatus::DeleteError, errno, "Failed to delete directory "+url.str());
+      }
+      return DataStatus::Success;
+    }
+    // path is a file
+    if (XrdPosixXrootd::Unlink(url.str().c_str()) != 0) {
+      logger.msg(VERBOSE, "Can't delete file %s: %s", url.str(), StrError(errno));
+      return DataStatus(DataStatus::DeleteError, errno, "Failed to delete file "+url.str());
+    }
+    return DataStatus::Success;
   }
 
   DataStatus DataPointXrootd::CreateDirectory(bool with_parents) {
-    logger.msg(ERROR, "Cannot (yet) create directories through xrootd");
-    return DataStatus(DataStatus::CreateDirectoryError, EOPNOTSUPP);
+
+    std::string::size_type slashpos = url.Path().find("/", 1); // don't create root dir
+    int r;
+    URL dir(url);
+
+    if (!with_parents) {
+      dir.ChangePath(url.Path().substr(0, url.Path().rfind("/")));
+      if (dir.Path().empty() || dir == url.Path()) return DataStatus::Success;
+      logger.msg(VERBOSE, "Creating directory %s", dir.str());
+
+      CertEnvLocker env(usercfg);
+      r = XrdPosixXrootd::Mkdir(dir.str().c_str(), 0775);
+      if (r == 0 || errno == EEXIST) return DataStatus::Success;
+
+      logger.msg(VERBOSE, "Error creating required dirs: %s", StrError(errno));
+      return DataStatus(DataStatus::CreateDirectoryError, errno, StrError(errno));
+    }
+    while (slashpos != std::string::npos) {
+      dir.ChangePath(url.Path().substr(0, slashpos));
+      // stat dir to see if it exists
+      struct stat st;
+      CertEnvLocker env(usercfg);
+      r = XrdPosixXrootd::Stat(dir.str().c_str(), &st);
+      if (r == 0) {
+        slashpos = url.Path().find("/", slashpos + 1);
+        continue;
+      }
+
+      logger.msg(VERBOSE, "Creating directory %s", dir.str());
+      r = XrdPosixXrootd::Mkdir(dir.str().c_str(), 0775);
+      if (r != 0) {
+        if (errno != EEXIST) {
+          logger.msg(VERBOSE, "Error creating required dirs: %s", StrError(errno));
+          return DataStatus(DataStatus::CreateDirectoryError, errno, StrError(errno));
+        }
+      }
+      slashpos = url.Path().find("/", slashpos + 1);
+    }
+    return DataStatus::Success;
   }
 
   DataStatus DataPointXrootd::Rename(const URL& newurl) {
-    logger.msg(ERROR, "Cannot (yet) rename files through xrootd");
-    return DataStatus(DataStatus::RenameError, EOPNOTSUPP);
+    logger.msg(VERBOSE, "Renaming %s to %s", url.str(), newurl.str());
+
+    URL tmpurl(newurl);
+    // xrootd requires 2 slashes at the start of the URL path
+    if (tmpurl.Path().find("//") != 0) {
+      tmpurl.ChangePath(std::string("/"+tmpurl.Path()));
+    }
+
+    if (XrdPosixXrootd::Rename(url.str().c_str(), tmpurl.str().c_str()) != 0) {
+      logger.msg(VERBOSE, "Can't rename file %s: %s", url.str(), StrError(errno));
+      return DataStatus(DataStatus::RenameError, errno, "Failed to rename file "+url.str());
+    }
+    return DataStatus::Success;
   }
 
   bool DataPointXrootd::RequiresCredentialsInFile() const {
