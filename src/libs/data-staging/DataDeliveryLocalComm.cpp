54a1bd9e1d6d44da0887149a0265abb6f8f04bf6
David Cameron <david.cameron@cern.ch>
2014-05-19 13:18:14 +0000

    use transfer url when deciding if file-based credentials are required (bug 3378)

--- src/libs/data-staging/DataDeliveryLocalComm.cpp
+++ src/libs/data-staging/DataDeliveryLocalComm.cpp
@@ -87,9 +87,12 @@ namespace DataStaging {
       args.push_back(surl);
       args.push_back("--durl");
       args.push_back(durl);
-      if (!dtr->get_source()->RequiresCredentialsInFile() &&
-          !dtr->get_destination()->RequiresCredentialsInFile() &&
-          !dtr->get_usercfg().CredentialString().empty()) {
+      // Check if credentials are needed for source/dest
+      Arc::DataHandle surl_h(surl, dtr->get_usercfg());
+      Arc::DataHandle durl_h(durl, dtr->get_usercfg());
+      if (!dtr->get_usercfg().CredentialString().empty() &&
+          !surl_h->RequiresCredentialsInFile() &&
+          !durl_h->RequiresCredentialsInFile()) {
         // If file-based credentials are not required then send through stdin
         stdin_ = dtr->get_usercfg().CredentialString();
       } else {
