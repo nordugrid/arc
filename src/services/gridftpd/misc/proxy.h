8c7bb12394effac4d8503bbbc2ebbf3598d059bb
Aleksandr Konstantinov <akonstantinov@yahoo.com>
2016-02-03 11:36:47 +0000

    Removing unused code

--- src/services/gridftpd/misc/proxy.h
+++ src/services/gridftpd/misc/proxy.h
@@ -5,14 +5,8 @@
 
 namespace gridftpd {
 
-  int prepare_proxy(void);
-  int remove_proxy(void);
-  int renew_proxy(const char* old_proxy,const char* new_proxy);
-  bool myproxy_renew(const char* old_proxy_file,const char* new_proxy_file,const char* myproxy_server);
   char* write_proxy(gss_cred_id_t cred);
   char* write_cert_chain(const gss_ctx_id_t gss_context);
-  void free_proxy(gss_cred_id_t cred);
-  gss_cred_id_t read_proxy(const char* filename);
 
 } // namespace gridftpd
 
