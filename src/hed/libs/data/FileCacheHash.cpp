2106adc07609bc280b76cf80756adac0aa5098ac
Aleksandr Konstantinov <akonstantinov@yahoo.com>
2016-09-21 17:12:31 +0000

    SHA computation to OpenSSL 1.1.0

--- src/hed/libs/data/FileCacheHash.cpp
+++ src/hed/libs/data/FileCacheHash.cpp
@@ -18,26 +18,30 @@ int FileCacheHash::MAX_SHA1_LENGTH = 40;
 
 std::string FileCacheHash::getHash(std::string url) {
 
+  std::string res("");
   /*
    * example borrowed from http://www.openssl.org/docs/crypto/EVP_DigestInit.html
    */
-  EVP_MD_CTX mdctx;
-  const EVP_MD *md = EVP_sha1(); // change to EVP_md5() for md5 hashes
-  char *mess1 = (char*)url.c_str();
-  unsigned char md_value[EVP_MAX_MD_SIZE];
-  unsigned int md_len, i;
+  EVP_MD_CTX* mdctx = EVP_MD_CTX_new();
+  if(mdctx) {
+    const EVP_MD *md = EVP_sha1(); // change to EVP_md5() for md5 hashes
+    if(md) {
+      char *mess1 = (char*)url.c_str();
+      unsigned char md_value[EVP_MAX_MD_SIZE];
+      unsigned int md_len, i;
 
-  EVP_MD_CTX_init(&mdctx);
-  EVP_DigestInit_ex(&mdctx, md, NULL);
-  EVP_DigestUpdate(&mdctx, mess1, strlen(mess1));
-  EVP_DigestFinal_ex(&mdctx, md_value, &md_len);
-  EVP_MD_CTX_cleanup(&mdctx);
+      EVP_MD_CTX_init(mdctx);
+      EVP_DigestInit_ex(mdctx, md, NULL);
+      EVP_DigestUpdate(mdctx, mess1, strlen(mess1));
+      EVP_DigestFinal_ex(mdctx, md_value, &md_len);
 
-  char result[3];
-  std::string res("");
-  for (i = 0; i < md_len; i++) {
-    snprintf(result, 3, "%02x", md_value[i]);
-    res.append(result);
+      char result[3];
+      for (i = 0; i < md_len; i++) {
+        snprintf(result, 3, "%02x", md_value[i]);
+        res.append(result);
+      }
+    }
+    EVP_MD_CTX_free(mdctx);
   }
   return res;
 }
