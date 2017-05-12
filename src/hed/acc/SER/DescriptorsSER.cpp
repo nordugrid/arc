0a5f1aa886ca9679f7f175f429d85b86664f8651
Aleksandr Konstantinov <akonstantinov@yahoo.com>
2017-05-12 21:15:51 +0000

    Merged with trunk up to revison 32442

--- src/hed/acc/SER/DescriptorsSER.cpp
+++ src/hed/acc/SER/DescriptorsSER.cpp
@@ -8,8 +8,14 @@
 #include <arc/loader/Plugin.h>
 
 #include "ServiceEndpointRetrieverPluginEMIR.h"
+#ifdef HAVE_LDNS
+#include "ServiceEndpointRetrieverPluginARCHERY.h"
+#endif
 
 extern Arc::PluginDescriptor const ARC_PLUGINS_TABLE_NAME[] = {
   { "EMIR", "HED:ServiceEndpointRetrieverPlugin", "EMIR registry", 0, &Arc::ServiceEndpointRetrieverPluginEMIR::Instance },
+#ifdef HAVE_LDNS
+  { "ARCHERY", "HED:ServiceEndpointRetrieverPlugin", "ARC Hierarchical Endpoints DNS-based Registry", 0, &Arc::ServiceEndpointRetrieverPluginARCHERY::Instance },
+#endif
   { NULL, NULL, NULL, 0, NULL }
 };
