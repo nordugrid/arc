c3ff935e460e384897278a45f27442d72ac8426d
Mattias Ellert <mattias.ellert@physics.uu.se>
2018-01-26 17:10:40 +0000

    Parser w/o swig module

--- python/__init__.py
+++ python/__init__.py
@@ -1,13 +1,18 @@
-# Import/initialise all the swig modules (low level wrappers)
-import _arc
+try:
 
-# Import the high level wrappers (proxy classes) into this namespace
-from arc.common import *
-from arc.loader import *
-from arc.message import *
-from arc.compute import *
-from arc.communication import *
-from arc.credential import *
-from arc.data import *
-from arc.delegation import *
-from arc.security import *
+    # Import/initialise all the swig modules (low level wrappers)
+    import _arc
+
+    # Import the high level wrappers (proxy classes) into this namespace
+    from arc.common import *
+    from arc.loader import *
+    from arc.message import *
+    from arc.compute import *
+    from arc.communication import *
+    from arc.credential import *
+    from arc.data import *
+    from arc.delegation import *
+    from arc.security import *
+
+except ImportError:
+    pass
