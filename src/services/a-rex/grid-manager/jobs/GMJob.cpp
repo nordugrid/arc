e83aa560b3cd916726c7699cd57bd4b403a16339
Maiken Pedersen <maiken.pedersen@usit.uio.no>
2018-02-20 15:55:54 +0000

    Arex crashed due to trying to erase past last item.

--- src/services/a-rex/grid-manager/jobs/GMJob.cpp
+++ src/services/a-rex/grid-manager/jobs/GMJob.cpp
@@ -248,7 +248,7 @@ bool GMJobQueue::PushSorted(GMJobRef& ref, comparator_t compare) {
           };
           if(rpos != opos) {  // no reason to move to itself
             queue_.insert(rpos.base(),*opos);
-            queue_.erase(opos.base());
+            queue_.erase(++opos.base());
           };
           break;
         };
