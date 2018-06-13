b152e3eb0cfbf31a18aca825974e96e3af10b4e5
Andrii Salnikov <manf@grid.org.ua>
2018-06-13 18:07:21 +0200

    Querying EGI VO database and VOMS-Admin servers for certificate chains

--- src/utils/python/arc/control/__init__.py
+++ src/utils/python/arc/control/__init__.py
@@ -1,4 +1,5 @@
 from RunTimeEnvironment import RTEControl
 from Jobs import JobsControl
+from ThirdPartyDeployment import ThirdPartyControl
 
-CTL_COMPONENTS = [RTEControl, JobsControl]
+CTL_COMPONENTS = [RTEControl, JobsControl, ThirdPartyControl]
