dcb5f684ac0867cf6b60ba3eb0356d072c4aaaab
Andrii Salnikov <manf@grid.org.ua>
2018-06-25 17:50:49 +0200

    Defaults parsing implementation (defaults file itself is not complete) Working with defaults in .reference file

--- src/utils/python/arc/utils/config.py
+++ src/utils/python/arc/utils/config.py
@@ -1,46 +1,243 @@
 import logging
 import json
 import re
+import sys
+import os
+import subprocess
 
 # init module logger
 logger = logging.getLogger('ARC.ConfigParserPy')
-logger.setLevel(logging.WARNING)
+# logger.setLevel(logging.WARNING)
+logger.setLevel(logging.DEBUG)
 log_handler_stderr = logging.StreamHandler()
 log_handler_stderr.setFormatter(
     logging.Formatter('[%(asctime)s] [%(name)s] [%(levelname)s] [%(process)d] [%(message)s]'))
 logger.addHandler(log_handler_stderr)
 
-# module-wise data structures to store parsed config
+# set relevant paths
+try:
+    from arc import paths
+    ARC_CONF = paths.ARC_CONF
+    ARC_DATA_DIR = paths.ARC_DATA_DIR
+    ARC_RUN_DIR = paths.ARC_RUN_DIR
+except ImportError:
+    paths = None
+    logger.error('There are no installation specific paths defined. '
+                 'It seams you are running config parser without autoconf - hardcoded defaults will be used.')
+    ARC_CONF = '/etc/arc.conf'
+    ARC_DATA_DIR = '/usr/share/arc'
+    ARC_RUN_DIR = '/var/run/arc'
+
+# module-wise data structures to store parsed configs
 __parsed_config = {}
 __parsed_blocks = []
+__default_config = {}
+__default_blocks = []
+
+# processing constants and regexes
+__def_path_arcconf = ARC_CONF
+__def_path_defaults = ARC_DATA_DIR + '/arc.defaults.conf'
+__def_path_runconf = ARC_RUN_DIR + '/arc.running.conf'
+
+__no_default = 'not set'
+_var_re = re.compile(r'\$VAR\{(?:\[(?P<block>[^\[\]]+)\])?(?P<option>[^{}[\]]+)\}')
+_exec_re = re.compile(r'\$EXEC\{(?P<command>[^{}]+)\}')
+_eval_re = re.compile(r'\$EVAL\{(?P<evalstr>[^{}]+)\}')
+__arcconf_re = {
+    'block': re.compile(r'^\s*\[(?P<block>[^:\[\]]+(?P<block_name>:[^\[\]]+)?)\]\s*$'),
+    'skip': re.compile(r'^(?:#.*|\s*)$'),
+    # includes support for the following cases:
+    #   'option=value'
+    #   'option = value'
+    #   'option=' or 'option'
+    'option': re.compile(r'^\s*(?P<option>[^=\[\]\n\s]+)\s*(?:=|(?=\s*$))\s*(?P<value>.*)\s*$')
+}
 
 
-# define parsing function
-def parse_arc_conf(conf_f='/etc/arc.conf'):
-    __regexes = {
-        'block': re.compile(r'^\s*\[(?P<block>[^:\[\]]+(?P<block_name>:[^\[\]]+)?)\]\s*$'),
-        'skip': re.compile(r'^(?:#.*|\s*)$'),
-        # includes support for the following cases:
-        #   'option=value'
-        #   'option = value' (just in case we will decide to allow spaces)
-        #   'option=' or 'option'
-        'option': re.compile(r'^\s*(?P<option>[^=\[\]\n\s]+)\s*(?:=|(?=\s*$))\s*(?P<value>.*)\s*$')
-    }
+# arc.conf parsing function
+def parse_arc_conf(conf_f=__def_path_arcconf, defaults_f=__def_path_defaults):
+    """General entry point for parsing arc.conf (with defaults substitutions if defined)"""
+    # parse /etc/arc.conf first
+    _parse_config(conf_f, __parsed_config, __parsed_blocks)
+    if defaults_f is not None:
+        if os.path.exists(defaults_f):
+            # parse defaults
+            _parse_config(defaults_f, __default_config, __default_blocks)
+            # merge defaults to parsed arc.conf
+            _merge_defults()
+        else:
+            logger.error('There is no defaults file at %s. Default values will not be substituted.', defaults_f)
+    # evaluate special values
+    _evaluate_values()
+
+
+def dump_arc_conf(conf_f=__def_path_runconf):
+    """Dump stored config dict to file"""
+    with open(conf_f, 'wb') as arcconf:
+        for b in __parsed_blocks:
+            arcconf.write('[{}]\n'.format(b))
+            for opt, val in __parsed_config[b].iteritems():
+                if opt == '__block_name':
+                    continue
+                arcconf.write('{} = {}\n'.format(opt, val))
+            arcconf.write('\n')
+
+
+def save_run_config(runconf_f=__def_path_runconf):
+    """Ensure directory exists and dump stored config"""
+    runconf_dir = '/'.join(runconf_f.split('/')[:-1])
+    if not os.path.exists(runconf_dir):
+        logger.debug('Making %s directory', runconf_dir)
+        os.makedirs(runconf_dir, mode=0755)
+    dump_arc_conf(runconf_f)
+
+
+def load_run_config(runconf_f=__def_path_runconf):
+    """Load runing configuration (without parsing defaults)"""
+    if not os.path.exists(runconf_f):
+        logger.error('There is no running configuration in %s. Dump it first.', runconf_f)
+        sys.exit(1)
+    parse_arc_conf(runconf_f)
+
+
+def defaults_defpath():
+    """Return default path for defaults file"""
+    return __def_path_defaults
+
+
+def runconf_defpath():
+    """Return default path for running configuration"""
+    return __def_path_runconf
+
+
+def arcconf_defpath():
+    """Return default path for arc.conf"""
+    return __def_path_arcconf
+
+
+def _conf_substitute_exec(optstr):
+    """Search for $EXEC{} values in parsed config structure and returns substituted command output"""
+    subst = False
+    execr = _exec_re.finditer(optstr)
+    for ev in execr:
+        try:
+            exec_sp = subprocess.Popen(ev.groupdict()['command'].split(' '), stdout=subprocess.PIPE)
+            value = exec_sp.stdout.readline().strip()
+        except OSError:
+            logger.error('Failed to find %s command to substitute in %s. Config is not usable, terminating.',
+                         ev.groupdict()['command'], ev.group(0))
+            sys.exit(1)
+        logger.debug('Substituting command output %s value: %s', ev.group(0), value)
+        optstr = optstr.replace(ev.group(0), value)
+        subst = True
+    return subst, optstr
+
+
+def _conf_substitute_var(optstr, current_block):
+    """Search for $VAR{} values in parsed config structure and returns substituted variable value"""
+    subst = False
+    valr = _var_re.finditer(optstr)
+    for v in valr:
+        refvar = v.groupdict()['option']
+        block = v.groupdict()['block']
+        if block is None:
+            block = current_block
+        # references is already in arc.conf dict
+        value = None
+        if block in __parsed_blocks:
+            if refvar in __parsed_config[block]:
+                value = __parsed_config[block][refvar]
+        # if not - something goes wrong
+        if value is None:
+            # check default - references to not-defined block in arc.conf with warning
+            if block in __default_blocks:
+                if refvar in __default_config[block]:
+                    value = __default_config[block][refvar]
+                    logger.warning('Reference variable for %s is in the block not defined in arc.conf', v.group(0))
+        if value is None:
+            logger.error('Failed to find reference variable value for %s', v.group(0))
+        else:
+            logger.debug('Substituting variable %s value: %s', v.group(0), value)
+            optstr = optstr.replace(v.group(0), value)
+            subst = True
+    return subst, optstr
+
+
+def _conf_substitute_eval(optstr):
+    """Search for $EVAL{} values in parsed config structure and returns substituted evaluation value"""
+    subst = False
+    evalr = _eval_re.finditer(optstr)
+    for ev in evalr:
+        try:
+            value = str(eval(ev.groupdict()['evalstr']))
+            logger.debug('Substituting evaluation %s value: %s', ev.group(0), value)
+            optstr = optstr.replace(ev.group(0), value)
+            subst = True
+        except SyntaxError:
+            logger.error('Wrong syntax to evaluate %s', ev.group(0))
+        except NameError as err:
+            logger.error('Wrong identifiers to evaluate %s: %s', ev.group(0), str(err))
+    return subst, optstr
+
+
+def _merge_defults():
+    """Add not specified config options from defaults file to parsed config dict (defined blocks only)"""
+    for block, optdict in __parsed_config.iteritems():
+        if ':' in block:
+            block = block.split(':')[0].strip()
+        for opt, val in __default_config[block].iteritems():
+            if opt not in optdict:
+                if val != __no_default:
+                    optdict.update({opt: val})
+
+
+def _evaluate_values():
+    """Process substitutions of $EXEC{} $VAR{} and $EVAL{} for values in parsed dictionary"""
+    #
+    # NOTICE: Substitution runs ONLY ONCE in the DEFINED ORDER!
+    #         More complex substitutions (e.g. EXEC depends on EVAL value) is not supported
+    #         and not needed to achieve current arc.conf needs.
+    #
+    # Evaluate substitutions: EXEC
+    # e.g. $EXEC{hostname -f}
+    for block, optdict in __parsed_config.iteritems():
+        for opt, val in __parsed_config[block].iteritems():
+            subst, subval = _conf_substitute_exec(val)
+            if subst:
+                __parsed_config[block][opt] = subval
+    # Evaluate substitutions: VAR
+    # e.g. $VAR{[common]globus_tcp_port_range}
+    for block, optdict in __parsed_config.iteritems():
+        for opt, val in __parsed_config[block].iteritems():
+            subst, subval = _conf_substitute_var(val, block)
+            if subst:
+                __parsed_config[block][opt] = subval
+    # Evaluate substitutions: EVAL
+    # e.g. $EVAL{$VAR{provider_timeout} + $VAR{infoproviders_timelimit} + ${wakeupperiod}}
+    for block, optdict in __parsed_config.iteritems():
+        for opt, val in __parsed_config[block].iteritems():
+            subst, subval = _conf_substitute_eval(val)
+            if subst:
+                __parsed_config[block][opt] = subval
+
+
+def _parse_config(conf_f, parsed_confdict_ref, parsed_blockslist_ref):
+    """Parse arc.conf-formatted configuration file to specified data structures"""
     with open(conf_f, 'rb') as arcconf:
         block_id = None
         for ln, confline in enumerate(arcconf):
-            if re.match(__regexes['skip'], confline):
+            if re.match(__arcconf_re['skip'], confline):
                 continue
-            block_match = re.match(__regexes['block'], confline)
+            block_match = re.match(__arcconf_re['block'], confline)
             if block_match:
                 block_dict = block_match.groupdict()
                 block_id = block_dict['block']
-                __parsed_config[block_id] = {}
-                __parsed_blocks.append(block_id)
+                parsed_confdict_ref[block_id] = {}
+                parsed_blockslist_ref.append(block_id)
                 if block_dict['block_name'] is not None:
-                    __parsed_config[block_id]['__block_name'] = block_dict['block_name'][1:]
+                    parsed_confdict_ref[block_id]['__block_name'] = block_dict['block_name'][1:].strip()
                 continue
-            option_match = re.match(__regexes['option'], confline)
+            option_match = re.match(__arcconf_re['option'], confline)
             if option_match:
                 if block_id is None:
                     logger.error('Option definition comes before block definition and will be ignored - line #%s: %s',
@@ -50,17 +247,18 @@ def parse_arc_conf(conf_f='/etc/arc.conf'):
                 value = option_match.groupdict()['value']
                 if value.startswith('"') and value.endswith('"'):
                     value = value[1:-1]
-                if option in __parsed_config[block_id]:
-                    if isinstance(__parsed_config[block_id][option], list):
-                        value = __parsed_config[block_id][option] + [value]
+                if option in parsed_confdict_ref[block_id]:
+                    if isinstance(parsed_confdict_ref[block_id][option], list):
+                        value = parsed_confdict_ref[block_id][option] + [value]
                     else:
-                        value = [__parsed_config[block_id][option], value]
-                __parsed_config[block_id][option] = value
+                        value = [parsed_confdict_ref[block_id][option], value]
+                parsed_confdict_ref[block_id][option] = value
                 continue
             logger.warning("Failed to parse line #%s: %s", ln + 1, confline.strip('\n'))
 
 
 def _config_subset(blocks=None):
+    """Returns configuration dictionary for requested blocks"""
     if blocks is None:
         blocks = []
     blocks_dict = {}
@@ -71,6 +269,7 @@ def _config_subset(blocks=None):
 
 
 def _blocks_list(blocks=None):
+    """Returns list from string or list argument"""
     if blocks is None:
         blocks = []
     # ability to pass single block name as a string
@@ -80,6 +279,12 @@ def _blocks_list(blocks=None):
 
 
 def export_json(blocks=None, subsections=False):
+    """
+    Returns JSON representation of parsed config structure
+    :param blocks: List of blocks to export
+    :param subsections: Export all subblocks
+    :return: JSON-dumped string
+    """
     if blocks is not None:
         if subsections:
             blocks = get_subblocks(blocks)
@@ -88,6 +293,12 @@ def export_json(blocks=None, subsections=False):
 
 
 def export_bash(blocks=None, subsections=False):
+    """
+    Returns CONFIG_key="value" strings ready to eval in shell
+    :param blocks: List of blocks to export
+    :param subsections: Export all subblocks
+    :return: Eval-ready string
+    """
     # it does not make sense to export entire configuration this way
     # if blocks chain is not specified - default is to parse just [common]
     if blocks is None:
@@ -115,6 +326,14 @@ def export_bash(blocks=None, subsections=False):
 
 
 def get_value(option, blocks=None, force_list=False, bool_yesno=False):
+    """
+    Return the config option value
+    :param option: option name
+    :param blocks: list of blocks (in order to check for option)
+    :param force_list: always return list
+    :param bool_yesno: return boolean True/False instead of string yes/no
+    :return: option value
+    """
     for b in _blocks_list(blocks):
         if b in __parsed_config:
             if option in __parsed_config[b]:
@@ -131,6 +350,13 @@ def get_value(option, blocks=None, force_list=False, bool_yesno=False):
 
 
 def get_subblocks(blocks=None, is_reversed=False, is_sorted=False):
+    """
+    Fetch all subblocks for defined parent blocks
+    :param blocks: List of blocks
+    :param is_reversed: Subblocks in the list is before parent block
+    :param is_sorted: Subblocks within the block are sorted alphabetically
+    :return: List of blocks with subblocks
+    """
     if blocks is None:
         blocks = []
     subblocks = []
@@ -149,7 +375,23 @@ def get_subblocks(blocks=None, is_reversed=False, is_sorted=False):
     return subblocks
 
 
+def get_config_dict():
+    """Returns the entire dictionary that holds parsed configuration"""
+    return __parsed_config
+
+
+def get_config_blocks():
+    """Returns list of configuration blocks (order is preserved)"""
+    return __parsed_blocks
+
+
 def check_blocks(blocks=None, and_logic=True):
+    """
+    Check specified blocks exists in configuration
+    :param blocks: List of blocks to check
+    :param and_logic: All blocks should be there to return True
+    :return: Check status
+    """
     result = and_logic
     for b in _blocks_list(blocks):
         if and_logic:
---
+++ src/utils/python/arc/utils/reference.py
@@ -0,0 +1,92 @@
+import logging
+import re
+import sys
+from config import _parse_config
+
+# init module logger
+logger = logging.getLogger('ARC.ConfigReference')
+logger.setLevel(logging.WARNING)
+log_handler_stderr = logging.StreamHandler()
+log_handler_stderr.setFormatter(
+    logging.Formatter('[%(asctime)s] [%(name)s] [%(levelname)s] [%(process)d] [%(message)s]'))
+logger.addHandler(log_handler_stderr)
+
+_reference_config = {}
+_reference_default_lines = {}
+__default_config = {}
+__default_blocks = []
+
+
+def parse_defaults(defaults_f):
+    _parse_config(defaults_f, __default_config, __default_blocks)
+
+
+def process_reference(reference_f, print_defaults=False, print_reference=False):
+    __regexes = {
+        'block': re.compile(r'^#\s*\[(?P<block>[^:\[\]]+(?P<block_name>:[^\[\]]+)?)\]\s*$'),
+        'default': re.compile(r'^##\s*default:(?P<default>.*)\s*$'),
+        'skip': re.compile(r'^(?:## .*|\s*)$'),
+        'option': re.compile(r'^#(?P<option>[^=\[\]\n\s]+)=\s*(?P<value>.*)\s*$')
+    }
+    with open(reference_f, 'rb') as referecnce:
+        block_id = None
+        default_value = None
+        default_line = None
+        for ln, confline in enumerate(referecnce):
+            # block name
+            block_match = re.match(__regexes['block'], confline)
+            if block_match:
+                block_dict = block_match.groupdict()
+                block_id = block_dict['block']
+                if ':' in block_id:
+                    block_id = block_id.split(':')[0].strip()
+                _reference_config[block_id] = {}
+                logger.info('Found [%s] configuration block definition', block_id)
+                if print_defaults:
+                    sys.stdout.write('\n[{}]\n'.format(block_id))
+                if print_reference:
+                    sys.stdout.write(confline)
+                continue
+            # default value in .reference
+            default_match = re.match(__regexes['default'], confline)
+            if default_match:
+                if block_id is None:
+                    logger.error('Default value definition comes before block definition - line #%s: %s',
+                                 ln + 1, confline.strip('\n'))
+                    if print_reference:
+                        sys.stdout.write(confline)
+                    continue
+                default_value = default_match.groupdict()['default'].strip()
+                default_line = ln
+                if print_reference:
+                    if default_line not in _reference_default_lines:
+                        logger.error('Failed to find parameter to print default value for at line #%s: %s',
+                                     ln + 1, confline.strip('\n'))
+                        sys.stdout.write('## default: not defined\n')
+                    else:
+                        sys.stdout.write('## default: {}\n'.format(
+                            __default_config[block_id][_reference_default_lines[ln]]))
+                        continue
+            # option in .reference
+            option_match = re.match(__regexes['option'], confline)
+            if option_match:
+                if block_id is None:
+                    logger.error('Option definition comes before block definition - line #%s: %s',
+                                 ln + 1, confline.strip('\n'))
+                    if print_reference:
+                        sys.stdout.write(confline)
+                    continue
+                option = option_match.groupdict()['option']
+                if option not in _reference_config[block_id]:
+                    if default_value is None:
+                        logger.error('There is no default value for option definition - line #%s: %s',
+                                     ln + 1, confline.strip('\n'))
+                        default_value = 'no default value'
+                    _reference_config[block_id].update({option: default_value})
+                    _reference_default_lines.update({default_line: option})
+                    if print_defaults:
+                        sys.stdout.write('{}={}\n'.format(option, default_value))
+                    default_value = None
+                    default_line = None
+            if print_reference:
+                sys.stdout.write(confline)
