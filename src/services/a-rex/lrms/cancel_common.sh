bda4c48fc7e8c2432a091bb15676d68e4c87fe64
Andrii Salnikov <manf@grid.org.ua>
2018-06-26 16:31:19 +0200

    initial configuration parsing sequence cleanup (PBS)

--- src/services/a-rex/lrms/cancel_common.sh
+++ src/services/a-rex/lrms/cancel_common.sh
@@ -3,23 +3,3 @@
 # must be called with the grami file as argument
 # remember to set $joboption_lrms
 
-if [ -z "$joboption_lrms" ]; then echo 'joboption_lrms must be set' 1>&2; exit 1; fi
-if [ -z "$pkgdatadir" ]; then echo 'pkgdatadir must be set' 1>&2; exit 1; fi
-
-. ${pkgdatadir}/configure-${joboption_lrms}-env.sh || exit $?
-
-arg_file=$1
-##############################################################
-# Source the argument file.
-##############################################################
-if [ -z "$arg_file" ] ; then
-   echo "Arguments file should be specified" 1>&2
-   exit 1
-fi
-if [ ! -f $arg_file ] ; then
-   echo "Missing arguments file expected at '$arg_file'." 1>&2
-   exit 1
-fi
-
-. $arg_file
-:
