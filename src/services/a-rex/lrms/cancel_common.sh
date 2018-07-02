aa746629bfd045c7b8ffd69cf0c9c8d5c3699d21
Andrii Salnikov <manf@grid.org.ua>
2018-07-02 12:26:43 +0200

    scan and cancell common flow reworked

--- src/services/a-rex/lrms/cancel_common.sh
+++ src/services/a-rex/lrms/cancel_common.sh
@@ -3,3 +3,13 @@
 # must be called with the grami file as argument
 # remember to set $joboption_lrms
 
+common_init () {
+   # parse grami file
+   no_grami_extra_processing=1
+   parse_grami_file $1
+   #  parse configuration
+   read_arc_conf
+   # read pbs-specific environment
+   .  ${pkgdatadir}/configure-${joboption_lrms}-env.sh || exit $?
+}
+
