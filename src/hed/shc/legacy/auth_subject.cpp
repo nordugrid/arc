ef5eca519ba89490d78b6b977774ed27ee6df326
Aleksandr Konstantinov <akonstantinov@yahoo.com>
2018-10-03 23:24:37 +0300

    Add yes/no argument to all rule and remove support for quotes in authgroups.

--- src/hed/shc/legacy/auth_subject.cpp
+++ src/hed/shc/legacy/auth_subject.cpp
@@ -11,39 +11,9 @@
 namespace ArcSHCLegacy {
 
 AuthResult AuthUser::match_subject(const char* line) {
-  // A bit of hacking here to properly split DNs with spaces
-  std::string line_(line);
-  std::string subj;
-  std::string::size_type pos = line_.find_first_not_of(" \t");
-  if(pos == std::string::npos) return AAA_NO_MATCH;
-  bool enc = (line_[pos] == '"');
-  pos = Arc::get_token(subj,line_,pos," \t", "\"", "\"");
-  while(true) {
-    if(subj.empty() && (pos == std::string::npos)) break;
-    if((!enc) && (!subj.empty()) && (pos != std::string::npos)) {
-      std::string subj_;
-      std::string::size_type pos_ = line_.find_first_not_of(" \t",pos);
-      if(pos_ != std::string::npos) {
-        bool enc_ = (line_[pos_] == '"');
-        if(!enc_) {
-          pos_ = Arc::get_token(subj_,line_,pos_," \t", "\"", "\"");
-          if(subj_[0] != '/') {
-            // Merge tokens
-            subj=subj+line_.substr(pos,pos_-pos);
-            pos=pos_;
-            continue;
-          };
-        };
-      };
-    };
-    if(subject_ == subj) {
-      return AAA_POSITIVE_MATCH;
-    };
-    pos = line_.find_first_not_of(" \t",pos);
-    if(pos == std::string::npos) break;
-    enc = (line_[pos] == '"');
-    pos = Arc::get_token(subj,line_,pos," \t", "\"", "\"");
-  };
+  std::string subj = Arc::trim(line);
+  if(subj.empty()) return AAA_NO_MATCH; // can't match empty subject - it is dangerous
+  if(subject_ == subj) return AAA_POSITIVE_MATCH;
   return AAA_NO_MATCH;
 }
 
