38de7c152c1d1d677a4ef2559be4f8d08b3d5e5a
Andrii Salnikov <manf@grid.org.ua>
2019-02-18 09:46:15 +0100

    removing EMIR, ARCHERY is the only SER now

--- src/hed/acc/ARCHERY/DescriptorsARCHERY.cpp
+++ src/hed/acc/ARCHERY/DescriptorsARCHERY.cpp
@@ -7,13 +7,11 @@
 #include <arc/compute/EndpointQueryingStatus.h>
 #include <arc/loader/Plugin.h>
 
-#include "ServiceEndpointRetrieverPluginEMIR.h"
 #ifdef HAVE_LDNS
 #include "ServiceEndpointRetrieverPluginARCHERY.h"
 #endif
 
 extern Arc::PluginDescriptor const ARC_PLUGINS_TABLE_NAME[] = {
-  { "EMIR", "HED:ServiceEndpointRetrieverPlugin", "EMIR registry", 0, &Arc::ServiceEndpointRetrieverPluginEMIR::Instance },
 #ifdef HAVE_LDNS
   { "ARCHERY", "HED:ServiceEndpointRetrieverPlugin", "ARC Hierarchical Endpoints DNS-based Registry", 0, &Arc::ServiceEndpointRetrieverPluginARCHERY::Instance },
 #endif
