60c71a89e06da3575bde63a5c342cf501f0ed82b
Aleksandr Konstantinov <akonstantinov@yahoo.com>
2019-10-28 23:00:02 +0200

    Moving GFAL DMC into external process.

---
+++ src/hed/dmc/gfal/DataPointGFALDelegate.h
@@ -0,0 +1,31 @@
+// -*- indent-tabs-mode: nil -*-
+
+#ifndef __ARC_DATAPOINTGFALDELEGATE_H__
+#define __ARC_DATAPOINTGFALDELEGATE_H__
+
+#include <list>
+#include <string>
+
+#include <arc/Thread.h>
+#include <arc/URL.h>
+#include <arc/Run.h>
+#include <arc/Utils.h>
+#include <arc/data/DataPointDelegate.h>
+
+namespace ArcDMCGFAL {
+
+  using namespace Arc;
+
+  class DataPointGFALDelegate
+    : public DataPointDelegate {
+  public:
+    DataPointGFALDelegate(const URL& url, const UserConfig& usercfg, PluginArgument* parg);
+    virtual ~DataPointGFALDelegate();
+    static Plugin* Instance(PluginArgument *arg);
+    virtual bool RequiresCredentials() const;
+  };
+
+} // namespace ArcDMCGFAL
+
+#endif // __ARC_DATAPOINTGFALDELEGATE_H__
+
