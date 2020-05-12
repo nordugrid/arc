b5e64292f44889f57c57d2251b7127d5f75509ae
Andrii Salnikov <manf@grid.org.ua>
2020-05-12 23:18:41 +0300

    proper suppor for multiple community rtes software dir

--- src/services/a-rex/lrms/community_rtes.sh
+++ src/services/a-rex/lrms/community_rtes.sh
@@ -11,18 +11,24 @@ community_software_prepare () {
     echo "ERROR: SOFTWARE_DIR is not defined for ${rte_name} community RTE. Failed to prepare software files." 1>&2
     exit 1
   fi
+  # software location in the sessiondir
+  RUNTIME_SOFTWARE_DIR="${joboption_directory}/${COMMUNITY_RTES_SW_SUBDIR}/${rte_name}"
+  mkdir -p "${RUNTIME_SOFTWARE_DIR%/*}"
   # copy software if not shared, link if shared
   if [ "$SOFTWARE_SHARED" != "True" ]; then
-    echo "Copying community software for RTE ${rte_name} into job directory." 1>&2 
-    cp -rv "${SOFTWARE_DIR}" ${joboption_directory}/${COMMUNITY_RTES_SW_SUBDIR}
+    echo "Copying community software for RTE ${rte_name} into job directory." 1>&2
+    cp -rv "${SOFTWARE_DIR}" "${RUNTIME_SOFTWARE_DIR}"
   else
-    echo "Linking community software for RTE ${rte_name} into job directory." 1>&2 
-    ln -sf "${SOFTWARE_DIR}" ${joboption_directory}/${COMMUNITY_RTES_SW_SUBDIR}
+    echo "Linking community software for RTE ${rte_name} into job directory." 1>&2
+    ln -sf "${SOFTWARE_DIR}" "${RUNTIME_SOFTWARE_DIR}"
   fi
+  # define ${RUNTIME_JOB_SWDIR} for RTE stage 0
+  RUNTIME_JOB_SWDIR="\${RUNTIME_JOB_DIR}/${COMMUNITY_RTES_SW_SUBDIR}/${rte_name}"
 }
 
 community_software_environment () {
   # skip if this is not a community-defined RTE
   [ -e "${rte_params_path}.community" ] || return
-  echo "RUNTIME_JOB_SWDIR=\"\${RUNTIME_JOB_DIR}/${COMMUNITY_RTES_SW_SUBDIR}\""
+  # define RUNTIME_JOB_SWDIR for RTE stage 1/2 in the jobscript
+  echo "RUNTIME_JOB_SWDIR=\"\${RUNTIME_JOB_DIR}/${COMMUNITY_RTES_SW_SUBDIR}/${rte_name}\""
 }
