ebd626eb367af2933284388a98637c13c93e47a8
Aleksandr Konstantinov <akonstantinov@yahoo.com>
2020-06-10 22:59:27 +0300

    Show all jobs not processed by infosys as being in initial state.

--- src/services/a-rex/tools.h
+++ src/services/a-rex/tools.h
@@ -3,13 +3,11 @@
 
 namespace ARex {
 
-void convertActivityStatus(const std::string& gm_state,std::string& bes_state,std::string& arex_state,bool failed = false,bool pending = false);
-
-Arc::XMLNode addActivityStatus(Arc::XMLNode pnode,const std::string& gm_state,Arc::XMLNode glue_xml = Arc::XMLNode(),bool failed = false,bool pending = false);
-
 void convertActivityStatusES(const std::string& gm_state,std::string& primary_state,std::list<std::string>& state_attributes,bool failed,bool pending,const std::string& failedstate,const std::string& failedcause);
 
-Arc::XMLNode addActivityStatusES(Arc::XMLNode pnode,const std::string& gm_state,Arc::XMLNode glue_xml = Arc::XMLNode(),bool failed = false,bool pending = false,const std::string& failedstate = "",const std::string& failedcause = "");
+Arc::XMLNode addActivityStatusES(Arc::XMLNode pnode,const std::string& gm_state,bool failed,bool pending,const std::string& failedstate = "",const std::string& failedcause = "");
+
+Arc::XMLNode addActivityStatusES(Arc::XMLNode pnode,Arc::XMLNode glue_xml);
 
 class JobIDGenerator {
  public:
