5a81eb68a45cdfc5216e13eafc94c740e787b555
Aleksandr Konstantinov <akonstantinov@yahoo.com>
2021-09-22 00:29:38 +0300

    Fix extracting job state for REST interface.

--- src/services/a-rex/rest/rest.cpp
+++ src/services/a-rex/rest/rest.cpp
@@ -1176,7 +1176,7 @@ static bool processJobStatus(Arc::Message& inmsg,ARexConfigContext& config, Arc:
         for(Arc::XMLNode snode = glue_xml["State"]; (bool)snode ; ++snode) {
           std::string state_str = snode;
           if(state_str.compare(0, 8, "arcrest:") == 0) {
-            rest_state = state_str.substr(7);
+            rest_state = state_str.substr(8);
           }
         }
       }
