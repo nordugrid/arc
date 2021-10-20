3d7628b8b8b335f2bbd2dbca5471c05d96908daa
Aleksandr Konstantinov <akonstantinov@yahoo.com>
2021-10-20 22:30:12 +0300

    Removing unused code which runs external credentials plugins.

--- src/services/a-rex/grid-manager/run/RunParallel.h
+++ src/services/a-rex/grid-manager/run/RunParallel.h
@@ -4,23 +4,17 @@
 #include <arc/Run.h>
 
 #include "../jobs/JobsList.h"
-#include "RunPlugin.h"
 
 namespace ARex {
 
 /// Run child process in parallel with stderr redirected to job.jobid.errors
 class RunParallel {
  private:
-  RunParallel(const char* procid, const char* errlog,
-              RunPlugin* cred, RunPlugin::substitute_t subst, void* subst_arg)
-    :procid_(procid?procid:""), errlog_(errlog?errlog:""),
-     cred_(cred), subst_(subst), subst_arg_(subst_arg) { };
+  RunParallel(const char* procid, const char* errlog)
+    :procid_(procid?procid:""), errlog_(errlog?errlog:"") { };
   ~RunParallel(void) { };
   std::string procid_;
   std::string errlog_;
-  RunPlugin* cred_;
-  RunPlugin::substitute_t subst_;
-  void* subst_arg_;
   static void initializer(void* arg);
   operator bool(void) { return true; };
   bool operator!(void) { return false; };
@@ -28,8 +22,6 @@ class RunParallel {
                   const char* procid, const char* errlog, std::string* errstr,
                   const std::string& args, Arc::Run**,
                   const char* job_proxy, bool su = true,
-                  RunPlugin* cred = NULL,
-                  RunPlugin::substitute_t subst = NULL, void* subst_arg = NULL,
                   void (*kicker_func)(void*) = NULL, void* kicker_arg = NULL);
  public:
   static bool run(const GMConfig& config, const GMJob& job, JobsList& list, std::string* errstr,
