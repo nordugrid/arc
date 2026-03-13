984a66b77da4f78d6c045de4caf476ef6be6dd75
Roger Oscarsson <roger@hpc2n.umu.se>
2026-03-13 16:40:38 +0100

    Avoid writing default values for to RTE params-file
    Writing down the default is a problem because it locks down a default
    even if no one has even chosen them. This becomes a problem if the
    default changes. The RTE will then stay with the old default (which may
    or may not work). And some extreme cases when the RTE meta-data is not
    in sync with the actual code it can become much worse (see nordugrid bug
    and suddenly an unchanged parameter is changed!
    
    This commit does the work but it could maybe done better. Its adds an
    hash key 'set' to keep track of whether a value for a parameter has
    been set or not. The name is acceptable(?) but should it really just
    be an integer with values 0,1,2,3?  Would not an enum be better? Or do
    we even need it? Just use 'value' equal to None mean that no value is
    set. And use the fact that 'description' is not set to know that the
    parameter is undocumented.
    
    It will now also show keys and values that are not documented with
    RTE meta-data. It will complain about them but they will be shown.
    This is particularly good if/when parameters are removed/renamed.
    
    One can now see these key/values and decide if they are actually
    needed (probably not) or should be removed (much more probable).
    And one can now easily remove them with arcctl rte params-unset.
    arcctl will complain twice about it put will happily do it!
    
    What *can't* be done is setting an undocumented parameter! This
    is either a feature or not. There are cases where this being able
    to set an undocumented parameter could be useful, but that is only
    on RTEs that have undocumented parameters. Which they really should
    not have ... ?
    
    When consequence of this merge request is that when one unsets a
    parameter with arcctl param-unset it will now delete the parameter from
    the params-file. Which sort of better matches the verb in the command.
    It still will lead to the RTE using the default value, since the code
    still will use the default value if it is not set in the params-file.
    
    But there is no longer any easy way to *set* the value to the default
    value. With this merge request one way to do it requires two commands,
    arcctl rte params-get -l too see the default value and then copy and
    paste that value to an arcctl rte params-set command. Maybe there should
    be a way to do this directly?
    
    What would also be nice if get-params could show if the value is set or
    just the default value. But just adding more to the long output would
    just make the output even more computer unfriendly (and frankly even
    human unfriendly).  The best idea I have is making the long output use
    two separate lines. Something like:
    
    COPY_CACERT_DIR=No
    USE_DELEGATION_DB=No	# Default value
    X509_CERT_DIR=/etc/grid-security/certificates
    
    Not sure that this is the best thing. Another completely differemt
    alternative is adding json output to ... ALL arcctl sub-commands!
    One needs to add it to all to avoid the annoying thing were some
    sub-commands have json-output and some don't.

--- src/utils/python/arc/control/RunTimeEnvironment.py
+++ src/utils/python/arc/control/RunTimeEnvironment.py
@@ -322,14 +322,24 @@ class RTEControl(ComponentControl):
                         'allowed_values': param_re.group(2).split(','),
                         'default_value': param_re.group(3),
                         'value': param_re.group(3),
-                        'description': param_re.group(4)
+                        'description': param_re.group(4),
+                        'set': 0,
                     }
-                    params_defined = self.__params_read(rte)
-                    if pname in params_defined:
-                        params[pname]['value'] = params_defined[pname]
                 max_lines -= 1
                 if not max_lines:
                     break
+        params_defined = self.__params_read(rte)
+        for pname, value in params_defined.items():
+            if pname in params:
+                params[pname]['set'] = 1
+            else:
+                self.logger.warning('Undocumented parameter %s with value %s for RunTimeEnvironment %s', pname, value, rte)
+                params[pname] = {
+                    'name': pname,
+                    # XXX - should we use a different value here?
+                    'set': 3,
+                }
+            params[pname]['value'] = value
         return params
 
     def __params_read(self, rte, suffix=''):
@@ -361,7 +371,8 @@ class RTEControl(ComponentControl):
         try:
             with open(rte_params_file, 'w') as rte_parm_f:
                 for p in params.values():
-                    rte_parm_f.write('{name}="{value}"\n'.format(**p))
+                    if p['set']:
+                        rte_parm_f.write('{name}="{value}"\n'.format(**p))
         except EnvironmentError as err:
             self.logger.error('Failed to write RTE parameters file %s. Error: %s', rte_params_file, str(err))
             sys.exit(1)
@@ -374,10 +385,13 @@ class RTEControl(ComponentControl):
                 # set strings for undefined values output
                 if pdescr['value'] == '':
                     pdescr['value'] = 'undefined'
-                if pdescr['default_value'] == '':
-                    pdescr['default_value'] = 'undefined'
-                print('{name:>16} = {value:10} {description} (default is {default_value}) '
-                      '(allowed values are: {allowed_string})'.format(**pdescr))
+                if pdescr['set'] < 3:
+                    if pdescr['default_value'] == '':
+                        pdescr['default_value'] = 'undefined'
+                    print('{name:>16} = {value:10} {description} (default is {default_value}) '
+                          '(allowed values are: {allowed_string})'.format(**pdescr))
+                else:
+                    print('{name:>16} = {value:10} [UNDOCUMENTED PARAMETER]'.format(**pdescr))
             else:
                 print('{name}={value}'.format(**pdescr))
         # community software deployment (read-only) params
@@ -391,29 +405,39 @@ class RTEControl(ComponentControl):
                 print(fstring.format(k, cparams[k]))
 
     def params_unset(self, rte, parameter):
-        self.params_set(rte, parameter, None, use_default=True)
+        self.params_set(rte, parameter, None)
 
     def params_set(self, rte, parameter, value, use_default=False):
         params = self.__params_parse(rte)
         if parameter not in params:
+            # allowed to set undocumented parameters? force option?
             self.logger.error('There is no such parameter %s for RunTimeEnvironment %s', parameter, rte)
             sys.exit(1)
-        # use default value if requested
-        if use_default:
-            value = params[parameter]['default_value']
-        # check type and allowed values
-        if params[parameter]['allowed_string'] == 'string':
-            pass
-        elif params[parameter]['allowed_string'] == 'int':
-            if not re.match(r'[-0-9]+', value):
-                self.logger.error('Parameter %s for RunTimeEnvironment %s should be integer', parameter, rte)
+        if params[parameter]['set'] > 2:
+            if value is None:
+                self.logger.warning('Clearing undocumented parameter %s for RunTimeEnvironment %s', parameter, rte)
+            else:
+                # allowed to set undocumented parameters? force option?
+                self.logger.error('There is no such parameter %s for RunTimeEnvironment %s', parameter, rte)
                 sys.exit(1)
-        elif value not in params[parameter]['allowed_values']:
-            self.logger.error('Parameter %s for RunTimeEnvironment %s should be one of %s',
-                              parameter, rte, params[parameter]['allowed_string'])
-            sys.exit(1)
+        else:
+            # use default value if requested
+            if use_default:
+                value = params[parameter]['default_value']
+            # check type and allowed values
+            if params[parameter]['allowed_string'] == 'string':
+                pass
+            elif params[parameter]['allowed_string'] == 'int':
+                if not re.match(r'[-0-9]+', value):
+                    self.logger.error('Parameter %s for RunTimeEnvironment %s should be integer', parameter, rte)
+                    sys.exit(1)
+            elif value not in params[parameter]['allowed_values']:
+                self.logger.error('Parameter %s for RunTimeEnvironment %s should be one of %s',
+                                  parameter, rte, params[parameter]['allowed_string'])
+                sys.exit(1)
         # assign new value
         params[parameter]['value'] = value
+        params[parameter]['set'] = 2 if value is not None else 0
         self.__params_write(rte, params)
 
     def cat_rte(self, rte):
